fba2035360260cee85b18c821a0afddd558e1c0b
Xavier Bol (xbo) <xbo@odoo.com>
2023-06-26 17:41:33 +0200

    [REM] helpdesk_timesheet: remove helpdesk.ticket.create.timesheet wizard
    Before this commit, when the user stopped the timer in the ticket form
    view, a wizard was displayed to generate the timesheet according to the
    time elapsed with the timer. Since the timeshet is now generated when the
    timer is started, this wizard will just update the existing timesheet.
    This update could be done in the sub tree containing the timesheets
    related to the ticket.
    
    That's why, this commit removes this wizard.
    
    task-2276015

--- helpdesk_timesheet/models/helpdesk_ticket.py
+++ helpdesk_timesheet/models/helpdesk_ticket.py
@@ -104,26 +104,8 @@ class HelpdeskTicket(models.Model):
     def action_timer_stop(self):
         # timer was either running or paused
         if self.display_timesheet_timer and self.user_timer_id:
-            minutes_spent = self.user_timer_id._get_minutes_spent()
-            minutes_spent = self.env['account.analytic.line'].get_rounded_time(minutes_spent)
-            return self._action_open_new_timesheet(minutes_spent / 60)
+            return super().action_timer_stop()
         return False
-
-    def _action_open_new_timesheet(self, time_spent):
-        return {
-            "name": _("Confirm Time Spent"),
-            "type": 'ir.actions.act_window',
-            "res_model": 'helpdesk.ticket.create.timesheet',
-            "views": [[False, "form"]],
-            "target": 'new',
-            "context": {
-                **self.env.context,
-                'active_id': self.id,
-                'active_model': self._name,
-                'default_time_spent': time_spent,
-                'dialog_size': 'medium',
-            },
-        }
 
     def _create_record_to_start_timer(self):
         """ Create a timesheet to launch a timer """
