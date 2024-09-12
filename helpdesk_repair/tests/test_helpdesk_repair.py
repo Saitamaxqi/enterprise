471a9aac417e02e5b3d1d450375b6ded3a9a54f4
lase@odoo.com <lase@odoo.com>
2024-09-12 12:18:47 +0000

    [FIX] repair: not assign repair move to return picking
    Steps to reproduce:
    
    - Create a Helpdesk team with the options "Return" and "Repairs"
    - Create a product, sell 1 unit and deliver it.
    - Create a ticket for the Helpdesk Team, select the customer who bought
      the product > create and validate a return of the product to your
      warehouse through the ticket.
    - Create a repair order and mark it as done.
    
    > If you go back to the product return, the stock move of the original
      return appears twice.
    
    Cause of the Issue:
    
    Clicking on the `End Repair` button of the repair will call the
    `action_repair_end` with a `default_picking_id` equal to the
    `picking_id` of the `repair.order` in the context. However, during this
    call a stock move groing from repair location to the repair location
    will be created and mark as done for the final product to be assocaited
    with the repair order:
    https://github.com/odoo/odoo/blob/e3a5d82f8ea6a92ced59c287c50f7cd33277a4bd/addons/repair/models/repair.py#L379-L382
    https://github.com/odoo/odoo/blob/e3a5d82f8ea6a92ced59c287c50f7cd33277a4bd/addons/repair/models/repair.py#L417-L442
    https://github.com/odoo/odoo/blob/e3a5d82f8ea6a92ced59c287c50f7cd33277a4bd/addons/repair/models/repair.py#L444-L449
    Since no `picking_id` is specified in its `move_vals`, the default
    `picking_id` (that is the return) will be added to the vals of the
    create here:
    https://github.com/odoo/odoo/blob/e3a5d82f8ea6a92ced59c287c50f7cd33277a4bd/odoo/models.py#L4564
    So that the newly created move will be linked to the return move even
    thought this makes no sense.
    
    opw-4159779
    
    closes odoo/enterprise#71019
    
    X-original-commit: 1db8a8dade04951cde2692de9643b60c55aed24f
    Related: odoo/odoo#182042
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

--- helpdesk_repair/tests/test_helpdesk_repair.py
+++ helpdesk_repair/tests/test_helpdesk_repair.py
@@ -56,3 +56,59 @@ class TestRepair(HelpdeskCommon):
 
         self.assertTrue(repair_order.display_name in last_message and 'Repair' in last_message,
             'Repair validation should be logged on the ticket')
+
+    def test_helpdesk_return_and_repair(self):
+        self.test_team.use_product_returns = True
+        self.test_team.use_product_repairs = True
+
+        product = self.env['product.product'].create({
+            'name': 'product 1',
+            'is_storable': True,
+            'invoice_policy': 'order',
+        })
+        so = self.env['sale.order'].create({
+            'partner_id': self.partner.id,
+        })
+        self.env['sale.order.line'].create({
+            'product_id': product.id,
+            'price_unit': 10,
+            'product_uom_qty': 1,
+            'order_id': so.id,
+        })
+        so.action_confirm()
+        delivery = so.picking_ids
+        delivery.move_ids.quantity = 1
+        delivery.move_ids.picked = True
+        delivery.button_validate()
+
+        ticket = self.env['helpdesk.ticket'].create({
+            'name': 'test',
+            'partner_id': self.partner.id,
+            'team_id': self.test_team.id,
+            'sale_order_id': so.id,
+        })
+        stock_picking_form = Form(self.env['stock.return.picking'].with_context({
+            'active_model': 'helpdesk.ticket',
+            'default_ticket_id': ticket.id
+        }))
+
+        stock_picking_form.picking_id = delivery
+        wizzard = stock_picking_form.save()
+        wizzard.product_return_moves.write({'quantity': 1.0})
+        res = wizzard.action_create_returns()
+        return_picking = self.env['stock.picking'].browse(res['res_id'])
+        return_picking.move_ids.quantity = 1
+        return_picking.move_ids.picked = True
+        return_picking.button_validate()
+
+        ro_form = Form(self.env['repair.order'].with_context(
+            active_model='helpdesk.ticket', default_ticket_id=ticket.id, default_picking_id=delivery.id
+        ))
+        ro_form.product_id = product
+        ro_form.partner_id = self.partner
+        repair_order = ro_form.save()
+        repair_order.action_validate()
+        repair_order.action_repair_start()
+        repair_order.action_repair_end()
+
+        self.assertRecordValues(delivery.move_ids, [{'product_id': product.id, 'quantity': 1.0}])
--- stock_barcode/static/tests/tours/tour_test_barcode_flows_picking.js
+++ stock_barcode/static/tests/tours/tour_test_barcode_flows_picking.js
@@ -2030,6 +2030,10 @@ registry.category("web_tour.tours").add('test_delivery_different_products_with_s
     },
 
     {
+        trigger: '.o_barcode_line:has(.o_product_label:contains(productlot1)) .qty-done:contains(2)',
+    },
+
+    {
         trigger: '.o_barcode_client_action',
         run: 'scan productlot2',
     },
@@ -2042,6 +2046,10 @@ registry.category("web_tour.tours").add('test_delivery_different_products_with_s
     {
         trigger: '.o_barcode_client_action',
         run: 'scan lot1',
+    },
+
+    {
+        trigger: '.o_barcode_line:has(.o_product_label:contains(productlot2)) .qty-done:contains(2)',
     },
     // Open the form view to trigger a save
     {
