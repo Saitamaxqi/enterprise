29acaa3d0785016d0fd01a0da6759df0463d0462
Arthur Pierrot (arpi) <arpi@odoo.com>
2024-09-19 16:19:46 +0200

    [REF] l10n_us_hr_payroll: merge l10n_us_hr_payroll_state_calculation
    The module l10n_us_hr_payroll_state_calculation was added in 17.0. In master, it
     needs to be merged to l10n_us_hr_payroll.
    
     Task: 4191924
    
    closes odoo/enterprise#70310
    
    Related: odoo/upgrade#6535
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- l10n_us_hr_payroll/models/hr_employee.py
+++ l10n_us_hr_payroll/models/hr_employee.py
@@ -97,6 +97,18 @@ class HrEmployee(models.Model):
         groups="hr.group_hr_user",
         help="Employee received third-party sick pay benefits from a third party during the tax year. If checked off it will appear in box 13 of the W2 Report.")
 
+    l10n_us_state_withholding_allowance = fields.Float(
+        string="State Withholding Allowance",
+        groups="hr.group_hr_user",
+        help="Add the allowance the employee wants to deduct from their State's withholding (usually an annual amount) "
+             "according to their State Withholding Certificate. If the employee didn't fill out a State Withholding "
+             "Certificate, leave it blank")
+    l10n_us_state_extra_withholding = fields.Float(
+        string="State Extra Withholding",
+        groups="hr.group_hr_user",
+        help="Extra amount the employee requests to be withheld per pay period according to their State Withholding "
+             "Certificate. If the employee didn't fill out a State Withholding Certificate, leave it blank.")
+
     @api.constrains('l10n_us_state_filing_status', 'address_id')
     def _check_us_state_filling_status(self):
         for employee in self:
