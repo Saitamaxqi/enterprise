76de4af1a73dc67c98f5c83ee88bfae512ca4d3a
abd-msyukyu-odoo <abd@odoo.com>
2024-09-24 13:40:52 +0000

    [REF] knowledge: convert the file behavior to an embedded component
    The file embedded component was moved to html_editor. This commits extends the
    feature to add the Macro capabilities of the file block, allowing to attach it
    to a previously visited record, or to send it by email through that previously
    visited record.
    
    task-3672747
    
    X-original-commit: 09950acf1bd605faa633d45feddc98e41200c3a6
    Part-of: odoo/enterprise#71082
    Related: odoo/odoo#182145
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>
    Co-authored-by: Adrien Schoffeniels <adsc@odoo.com>
    Co-authored-by: Julien Banken <jbn@odoo.com>
    Co-authored-by: Thomas Josse <thjo@odoo.com>

--- knowledge/static/src/editor/embedded_components/embedding_sets.js
+++ knowledge/static/src/editor/embedded_components/embedding_sets.js
@@ -1,13 +1,17 @@
 import { macrosClipboardEmbedding } from "@knowledge/editor/embedded_components/backend/clipboard/macros_embedded_clipboard";
+import { macrosFileEmbedding } from "@knowledge/editor/embedded_components/backend/file/macros_file";
+import { readonlyMacrosFileEmbedding } from "@knowledge/editor/embedded_components/backend/file/readonly_macros_file";
 import { articleIndexEmbedding } from "@knowledge/editor/embedded_components/backend/article_index/article_index";
 import { readonlyArticleIndexEmbedding } from "@knowledge/editor/embedded_components/core/article_index/readonly_article_index";
 
 export const KNOWLEDGE_EMBEDDINGS = [
     articleIndexEmbedding,
     macrosClipboardEmbedding,
+    macrosFileEmbedding,
 ];
 
 export const KNOWLEDGE_READONLY_EMBEDDINGS = [
     macrosClipboardEmbedding,
     readonlyArticleIndexEmbedding,
+    readonlyMacrosFileEmbedding,
 ];
