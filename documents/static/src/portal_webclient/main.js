78420e97df7394639df055a0b2ef81c0b87bde2a
Lopes Marc (loma) <loma@odoo.com>
2024-09-26 12:25:10 +0000

    [IMP] documents: fix portal list view crash
    When portal users try to switch to the list view, they get a client
    error.
    
    The error is solved by keeping the studio service accessible to the
    ListRenderer.
    
    task-3373836
    
    closes odoo/enterprise#70880
    
    X-original-commit: 52a0e64a0a21c15150da8ef062f9aee926ebb066
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

--- documents/static/src/portal_webclient/main.js
+++ documents/static/src/portal_webclient/main.js
@@ -1,7 +1,5 @@
 /** @odoo-module **/
 import { startWebClient } from "@web/start";
 import { PortalWebclientWebClient } from "./portal_webclient";
-import { removeRestricted } from "./remove_restricted";
 
-removeRestricted();
 startWebClient(PortalWebclientWebClient);
