705c68c51db39b9cbb1636ac5c7bbede191b565b
jugj-odoo <jugj@odoo.com>
2024-09-26 08:07:09 +0000

    [FIX] l10n_ch_hr_payroll_elm: Add module updating
    closes odoo/enterprise#71124
    
    X-original-commit: a4c9d89517e12e81744d5845f67de78d63a02e6f
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- l10n_ch_hr_payroll/models/hr_payslip.py
+++ l10n_ch_hr_payroll/models/hr_payslip.py
@@ -228,6 +228,7 @@ class HrPayslip(models.Model):
                 'data/hr_payroll_structure_type_data.xml',
                 'data/hr_payroll_structure_data.xml',
                 'data/hr_rule_parameters_data.xml',
+                'data/hr_payslip_input_type_data.xml',
                 'data/hr_salary_rule_data.xml',
             ])]
 
