3fc2f26f344c7554e585f5b95b96b656cc0c034d
Lucas Lefèvre (lul) <lul@odoo.com>
2024-10-08 16:40:09 +0200

    [REF] spreadsheet_edition: remove duplicated menu item
    This menu item is also defined in o-spreadsheet but overwritten here with
    the same thing (except the name, sequence and separator, which have been adapted
    in o-spreadsheet)
    
    Task: 4242125
    Part-of: odoo/enterprise#72689
    Related: odoo/odoo#185050
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- spreadsheet_edition/static/src/bundle/pivot/index.js
+++ spreadsheet_edition/static/src/bundle/pivot/index.js
@@ -1,6 +1,5 @@
 /** @odoo-module */
 
-import { _t } from "@web/core/l10n/translation";
 import * as spreadsheet from "@odoo/o-spreadsheet";
 import { initCallbackRegistry } from "@spreadsheet/o_spreadsheet/init_callbacks";
 
@@ -10,7 +9,7 @@ import { PivotDetailsSidePanel } from "./side_panels/pivot_details_side_panel";
 import "./autofill";
 import { insertPivot } from "./pivot_init_callback";
 
-const { featurePluginRegistry, cellMenuRegistry, pivotSidePanelRegistry } = spreadsheet.registries;
+const { featurePluginRegistry, pivotSidePanelRegistry } = spreadsheet.registries;
 
 featurePluginRegistry.add("odooPivotAutofillPlugin", PivotAutofillPlugin);
 
@@ -19,20 +18,3 @@ pivotSidePanelRegistry.add("ODOO", {
 });
 
 initCallbackRegistry.add("insertPivot", insertPivot);
-
-cellMenuRegistry.add("pivot_properties", {
-    name: _t("See pivot properties"),
-    sequence: 170,
-    execute(env) {
-        const position = env.model.getters.getActivePosition();
-        const pivotId = env.model.getters.getPivotIdFromPosition(position);
-        env.openSidePanel("PivotSidePanel", { pivotId });
-    },
-    isVisible: (env) => {
-        const position = env.model.getters.getActivePosition();
-        return env.model.getters.isExistingPivot(
-            env.model.getters.getPivotIdFromPosition(position)
-        );
-    },
-    icon: "o-spreadsheet-Icon.PIVOT",
-});
