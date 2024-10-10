7658f386e70a4164a1d52f374d7cda92feacb412
Thibault Delavallée <tde@odoo.com>
2024-10-10 14:53:03 +0200

    [IMP] helpdesk, _*: define default recipients for ticket templates
    Improve default recipients so that emails are correctly formatted.
    Helpdesk tickets always prioritize emails when they are different
    from partner emails.
    
    Task-2667099: [mail] Improve default template values
    
    Part-of: odoo/enterprise#66502
    Related: odoo/odoo#172714
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- helpdesk/models/helpdesk_ticket.py
+++ helpdesk/models/helpdesk_ticket.py
@@ -794,6 +794,21 @@ class HelpdeskTicket(models.Model):
         self.ensure_one()
         return self.name
 
+    def _message_get_default_recipients(self):
+        # override to grant access to partner email value, sometimes MC environment
+        # create ACL issues; also partner_email takes precedence on partner_id.email
+        res = {}
+        for ticket in self.sudo():
+            ticket_email_n = tools.email_normalize(ticket.partner_email, strict=False)
+            if ticket_email_n and ticket_email_n != ticket.partner_id.email_normalized:
+                email_to = ','.join(tools.email_normalize_all(ticket.partner_email))
+                if not email_to:  # keep value to ease debug / trace update
+                    email_to = ticket.partner_email
+                res[ticket.id] = {'partner_ids': [], 'email_to': email_to, 'email_cc': False}
+            else:
+                res[ticket.id] = {'partner_ids': ticket.partner_id.ids, 'email_to': False, 'email_cc': False}
+        return res
+
     def _message_post_after_hook(self, message, msg_vals):
         if not self.partner_email:
             return super()._message_post_after_hook(message, msg_vals)
