7d7aab4c507d195ca8be3ef6f4b9ae54f5bc0b79
Pierre Rousseau <pro@odoo.com>
2024-10-14 08:02:18 +0000

    [FIX] spreadsheet_edition: remove useless data given to spreadsheet
    Before this commit, the data given to the spreadsheet init callback in
    the case of inserting a pivot view was not used. This could lead to
    an error if the data was too big to be stored in the browser's session
    storage.
    
    This commit removes the useless data given to the spreadsheet init.
    
    Note that it's not necessary to write a new test for this, as the
    existing tests already cover this case.
    
    closes odoo/enterprise#71956
    
    Task: 4255049
    X-original-commit: 402228f94323a4cc1b74a5593e21e457d88b51d1
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- spreadsheet_edition/static/src/assets/pivot_view/pivot_view.js
+++ spreadsheet_edition/static/src/assets/pivot_view/pivot_view.js
@@ -62,7 +62,6 @@ patch(PivotRenderer.prototype, {
         const actionOptions = {
             preProcessingAsyncAction: "insertPivot",
             preProcessingAsyncActionData: {
-                data: this.model.data,
                 metaData: this.model.metaData,
                 searchParams: {
                     ...this.model.searchParams,
