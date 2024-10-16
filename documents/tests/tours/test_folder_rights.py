8e2f61332fc264bf69ec6661accf7c35857ffe55
Vivek Pathak <vivp@odoo.com>
2024-10-16 06:37:40 +0000

    [FIX] documents: fix test for document folder rights
    This commit fixes the test.
    When there is extra company then test is failing because it replaces
    the companies in user, instead of adding the new company.
    
    closes odoo/enterprise#72153
    
    X-original-commit: 707b8ed95a3287642e9319f9024bac830132ac4d
    Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>
    Signed-off-by: Vivek Pathak (vivp) <vivp@odoo.com>

--- documents/tests/tours/test_folder_rights.py
+++ documents/tests/tours/test_folder_rights.py
@@ -2,13 +2,14 @@
 
 from odoo.tests import tagged
 from odoo.tests.common import HttpCase
+from odoo import Command
 
 
 @tagged("post_install", "-at_install")
 class TestDocumentFolderRights(HttpCase):
     def test_document_folder_rights_for_multi_company_tour(self):
         company_a = self.env['res.company'].create({'name': 'Company_A'})
-        self.env.ref('base.user_admin').company_ids = [self.env.company.id, company_a.id]
+        self.env.ref('base.user_admin').company_ids = [Command.link(company_a.id)]
         self.env['documents.document'].create({
             "access_internal": "view",
             "company_id": self.env.company.id,
