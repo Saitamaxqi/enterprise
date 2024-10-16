71841cb8eeb58137207e27ad7c17e67e3fe432ff
Luis González <lgonzalez@vauxoo.com>
2024-10-16 01:16:44 +0000

    [FIX] approvals: use correct selector in JS tour for statusbar button
    To retrieve the radio button corresponding to the currently-selected
    status in the statusbar, selector is currently relying on the attribute
    `aria-label`, but such attribute is incorrect and will be removed.
    Better relying on the attribute `aria-checked`, which indicates if the
    radio button is actually checked.
    
    closes odoo/enterprise#76410
    
    X-original-commit: 737b3695363a17789ec6e28606b6aef62cb2ac84
    Related: odoo/odoo#192253
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- approvals/static/tests/tours/approvals.js
+++ approvals/static/tests/tours/approvals.js
@@ -84,7 +84,7 @@ registry.category("web_tour.tours").add("approvals_tour", {
             run: "click",
         },
         {
-            trigger: 'button[aria-label="Current state is Refused"][data-value="refused"]',
+            trigger: 'button[aria-checked="true"][data-value="refused"]',
             content: "wait the request status compute",
         },
         {
