c71b6e6d6999ac3300c24ac13943a88440318899
Julien Van Roy <juvr@odoo.com>
2024-10-22 08:50:59 +0000

    [FIX] l10n_pe_edi_pos: fix AccessError when posting invoices
    Context: an AccessError is raised while running test
    `TestAccountMove.test_add_followers_on_post` and having
    `l10n_pe_edi_pos` installed:
    
    ```
    odoo.exceptions.AccessError: You are not allowed to access 'Point of
    Sale Orders' (pos.order) records.
    
    This operation is allowed for the following groups:
    	- Inventory/User
    	- Point of Sale/User
    
    Contact your administrator to request access if necessary.
    ```
    
    Reason: commit 3045807f9a318fed810095fc5746c0e7bbdb0063 extended the
    `_post` method on `account.move`, and is reading the field
    `pos_order_ids`. But one may post an invoice without having sufficient
    security groups to access `pos.order` records (Inventory/User, Point of
    Sale/User).
    
    runbot-102743
    
    closes odoo/enterprise#72992
    
    X-original-commit: 27d824ae71fbb198657f3e81afce7ce1befa3771
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- l10n_pe_edi_pos/models/account_move.py
+++ l10n_pe_edi_pos/models/account_move.py
@@ -8,6 +8,6 @@ class AccountMove(models.Model):
 
     def _post(self, soft=True):
         posted = super()._post(soft=soft)
-        self.filtered(lambda am: am.pos_order_ids).edi_document_ids.filtered(
+        self.filtered(lambda am: am.sudo().pos_order_ids).edi_document_ids.filtered(
                 lambda d: d.state == 'to_send')._process_documents_web_services(job_count=1)
         return posted
