dff91dcedb69cc4483fda2df63a7a5c63034ddda
Logan Staelens <lost@odoo.com>
2024-10-24 07:29:14 +0000

    [IMP] account_reports: Revert back to date instead of invoice dates for the general ledger
    In a previous pull request: https://github.com/odoo/enterprise/pull/46498, the columns date was switched to invoice date.
    
    We want to revert back to the state before this commit.
    So using the accounting_date instead of the invoice_date on the general ledger.
    
    task-4270453
    
    closes odoo/enterprise#74338
    
    X-original-commit: 2c84bafd0782d83820c75c01f399b1575389cc86
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Logan Staelens (lost) <lost@odoo.com>

--- account_reports/models/account_general_ledger.py
+++ account_reports/models/account_general_ledger.py
@@ -439,7 +439,7 @@ class AccountGeneralLedgerReportHandler(models.AbstractModel):
                     account_move_line.partner_id,
                     account_move_line.currency_id,
                     account_move_line.amount_currency,
-                    COALESCE(account_move_line.invoice_date, account_move_line.date) AS invoice_date,
+                    account_move_line.date                  AS date,
                     %(debit_select)s                        AS debit,
                     %(credit_select)s                       AS credit,
                     %(balance_select)s                      AS balance,
