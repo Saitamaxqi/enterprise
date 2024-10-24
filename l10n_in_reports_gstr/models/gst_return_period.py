f8af7a4b04773d929c7dd6f5f107e30be230cad2
Nishant Jain (NIAI) <niai@odoo.com>
2024-10-24 06:08:35 +0000

    [FIX] l10n_in_reports_gstr: fix traceback when not selecting month in gst return
    Before this commit:
    - Creating a GST return period with a quarterly periodicity without selecting a
    quarter will raise a traceback.
    - Creating a GST return period with a monthly periodicity without selecting a
    month will raise a traceback.
    
    Reason:
    - While calculating the start_date and end_date of the quarterly return period:
    `period_start = fields.Date.context_today(self).replace(day=1, month=int(record.month), year=int(record.year))`
    If there is no month, then int(record.month) will return 0 and The month should
    be between 1 and 12, so it will raise an error.
    - While calculating the start_date and end_date of the monthly return period:
    `period_start = fields.Date.context_today(self).replace(day=1, month=int(record.quarter), year=int(record.year))`
    If there is no quarter, then int(record.quarter) will return 0 and The month
    should be between 1 and 12, so it will raise an error.
    
    After this commit:
    - The start_date and end_date will not be calculated if a
    month or quarter is not defined for the monthly or quarterly return periods.
    
    Task-4243468
    
    closes odoo/enterprise#72968
    
    X-original-commit: eae17f75f55e1efeca5d633447ea6a119bdd89cd
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Nishant Jain(niai) <niai@odoo.com>

--- l10n_in_reports_gstr/models/gst_return_period.py
+++ l10n_in_reports_gstr/models/gst_return_period.py
@@ -213,9 +213,9 @@ class L10n_InGstReturnPeriod(models.Model):
 
     def _compute_name(self):
         for period in self:
-            if period.periodicity == "monthly":
+            if period.periodicity == "monthly" and period.start_date:
                 period.name = format_date(self.env, period.start_date, date_format="MMM-yyyy")
-            elif period.periodicity == "trimester":
+            elif period.periodicity == "trimester" and period.start_date and period.end_date:
                 quarter_names = get_quarter_names("abbreviated", locale=get_lang(self.env).code)
                 period.name = quarter_names[date_utils.get_quarter_number(period.end_date)]
                 period.name += format_date(self.env, period.start_date, date_format="-yyyy")
@@ -233,12 +233,14 @@ class L10n_InGstReturnPeriod(models.Model):
     @api.depends('month', 'quarter', 'year')
     def _compute_period_dates(self):
         for record in self:
-            if record.periodicity == "monthly":
+            if record.periodicity == "monthly" and record.month:
                 period_start = fields.Date.context_today(self).replace(day=1, month=int(record.month), year=int(record.year))
                 time_period = date_utils.get_month(period_start)
-            else:
+            elif record.periodicity == "trimester" and record.quarter:
                 period_start = fields.Date.context_today(self).replace(day=1, month=int(record.quarter), year=int(record.year))
                 time_period = date_utils.get_quarter(period_start)
+            else:
+                time_period = (False, False)
             record.start_date, record.end_date = time_period
 
     @api.depends("start_date")
