5778dcedfead6cb9510d51af0781446ade9d9ed2
Thomas Josse (THJO) <thjo@odoo.com>
2024-11-05 13:47:13 +0100

    [IMP] website_knowledge: add toggle component to public
    This commit adds the new toggle embedded component to the embedding sets for
    the public version of knowledge.
    
    This way the component is correctly displayed on the frontend.
    
    task-2796594
    
    closes odoo/enterprise#73329
    
    Related: odoo/odoo#185639
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>

--- website_knowledge/static/src/frontend/editor/embedded_components/embedding_sets.js
+++ website_knowledge/static/src/frontend/editor/embedded_components/embedding_sets.js
@@ -1,6 +1,7 @@
 import { readonlyExcalidrawEmbedding } from "@html_editor/others/embedded_components/core/excalidraw/readonly_excalidraw";
 import { readonlyFileEmbedding } from "@html_editor/others/embedded_components/core/file/readonly_file";
 import { readonlyTableOfContentEmbedding } from "@html_editor/others/embedded_components/core/table_of_content/table_of_content";
+import { toggleBlockEmbedding } from "@html_editor/others/embedded_components/core/toggle_block/toggle_block";
 import { videoEmbedding } from "@html_editor/others/embedded_components/core/video/video";
 import { readonlyArticleIndexEmbedding } from "@knowledge/editor/embedded_components/core/article_index/readonly_article_index";
 import { clipboardEmbedding } from "@knowledge/editor/embedded_components/core/clipboard/embedded_clipboard";
@@ -14,6 +15,7 @@ export const KNOWLEDGE_PUBLIC_EMBEDDINGS = [
     readonlyExcalidrawEmbedding,
     readonlyFileEmbedding,
     readonlyTableOfContentEmbedding,
+    toggleBlockEmbedding,
     videoEmbedding,
     viewPlaceholderEmbedding,
 ];
