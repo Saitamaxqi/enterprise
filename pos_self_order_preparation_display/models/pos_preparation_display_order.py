ea1fea2dcd916827648eaef5278f1113d5b7b8b1
David Monnom <moda@odoo.com>
2024-11-05 21:15:01 +0100

    [IMP] pos_self_order_preparation_display: Add pos presets
    Ensure the user has access to easy pre configured switch for
    Eat in/ Takeout / Delivery
    
    Taskid: 4310935
    Part-of: odoo/enterprise#73348
    Related: odoo/odoo#186359
    Related: odoo/upgrade#6743
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- pos_self_order_preparation_display/models/pos_preparation_display_order.py
+++ pos_self_order_preparation_display/models/pos_preparation_display_order.py
@@ -5,7 +5,6 @@ class Pos_Preparation_DisplayOrder(models.Model):
     _inherit = 'pos_preparation_display.order'
 
     pos_table_stand_number = fields.Char(string="Table Stand Number")
-    pos_takeaway = fields.Boolean(string="Take Away", default=False)
 
     def _export_for_ui(self, preparation_display):
         order_for_ui = super()._export_for_ui(preparation_display)
@@ -19,8 +18,6 @@ class Pos_Preparation_DisplayOrder(models.Model):
     def _get_preparation_order_values(self, order):
         order_to_create = super()._get_preparation_order_values(order)
 
-        if order.get('pos_takeaway'):
-            order_to_create['pos_takeaway'] = order['pos_takeaway']
         if order.get('pos_table_stand_number'):
             order_to_create['pos_table_stand_number'] = order['pos_table_stand_number']
 
