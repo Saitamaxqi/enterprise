76bf208a7e9747fe1c69deec6ff9657101e24401
Thibault Delavallée <tde@odoo.com>
2024-11-05 11:28:40 +0000

    [MOV] test_whatsapp: regroup tests
    Ease undertanding of test coverage by avoiding useless file split.
    
    X-original-commit: 72a51a759bdd0146eeb9d918c42ff8dd68529cb9
    Related: odoo/odoo#186335
    Related: odoo/odoo#186458
    Part-of: odoo/enterprise#73387
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- test_whatsapp/tests/test_discuss_channel.py
+++ test_whatsapp/tests/test_discuss_channel.py
@@ -2,6 +2,7 @@
 
 from markupsafe import escape
 
+from odoo import fields
 from odoo.addons.test_whatsapp.tests.common import WhatsAppFullCase
 from odoo.addons.whatsapp.tests.common import MockIncomingWhatsApp
 from odoo.tests import tagged, users
@@ -123,3 +124,71 @@ class DiscussChannel(WhatsAppFullCase, MockIncomingWhatsApp):
         second_valid_date = discuss_channel.whatsapp_channel_valid_until
 
         self.assertEqual(first_valid_date, second_valid_date)
+
+    def test_message_reaction(self):
+        """Check a reaction is correctly added on a whatsapp message."""
+        with self.mockWhatsappGateway():
+            self._receive_whatsapp_message(self.whatsapp_account, "test", "32499123456")
+        discuss_channel = self.assertWhatsAppDiscussChannel("32499123456", wa_msg_count=1, msg_count=1)
+        message = discuss_channel.message_ids[0]
+        with self.mockWhatsappGateway():
+            self._receive_whatsapp_message(
+                self.whatsapp_account, "", "32499123456",
+                additional_message_values={
+                    "reaction": {
+                        "message_id": message.wa_message_ids[0].msg_uid,
+                        "emoji": "😊",
+                    },
+                    "type": "reaction",
+                },
+            )
+        self._reset_bus()
+        with self.assertBus(
+            [
+                (self.cr.dbname, "discuss.channel", discuss_channel.id),
+                (self.cr.dbname, "discuss.channel", discuss_channel.id),
+            ],
+            [{
+                "type": "mail.record/insert",
+                "payload": {
+                    "mail.message": [{
+                        "id": message.id,
+                        "reactions": [["DELETE", {"message": message.id, "content": "😊"}]],
+                    }],
+                },
+            }, {
+                "type": "mail.record/insert",
+                "payload": {
+                    "MessageReactions": [{
+                        "content": "👍",
+                        "count": 1,
+                        "message": message.id,
+                        "personas": [{"id": message.author_id.id, "type": "partner"}],
+                        # new reaction, and there is no way that we can get the id of the reaction, so that the sequence is directly +1
+                        "sequence": message.reaction_ids.ids[0] + 1,
+                    }],
+                    "mail.message": [{
+                        "id": message.id,
+                        "reactions": [["ADD", [{"message": message.id, "content": "👍"}]]],
+                    }],
+                    "res.partner": [{
+                        "id": message.author_id.id,
+                        "name": "+32499123456",
+                        "write_date": fields.Datetime.to_string(
+                            message.author_id.write_date
+                        ),
+                    }],
+                },
+            }],
+        ):
+            with self.mockWhatsappGateway():
+                self._receive_whatsapp_message(
+                    self.whatsapp_account, "", "32499123456",
+                    additional_message_values={
+                        "reaction": {
+                            "message_id": message.wa_message_ids[0].msg_uid,
+                            "emoji": "👍",
+                        },
+                        "type": "reaction",
+                    },
+                )
