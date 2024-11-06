69c928cd7916f0472db547ea2d7c5e96343fb2f5
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-11-06 13:05:03 +0000

    [FIX] account_sepa_direct_debit module description
    The module description of account_sepa_direct_debit in the manifest
    states that posting an invoice will automatically generate a payment.
    Actually that feature was dropped in Odoo 13 with commit
    e3d390c4455c620793c481874b4503fa91bb6125 but the `__manifest__.py` was
    not updated at that time. This makes the situation uncomfortable where a
    feature not present in the code is still advertised in the module
    description.
    
    This commit updates the description of the module in the manifest.
    
    closes odoo/enterprise#74653
    
    X-original-commit: c62fa8e4a4a8253f86629e62234a21a7082650b6
    Related: odoo/odoo#188740
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- account_sepa_direct_debit/__manifest__.py
+++ account_sepa_direct_debit/__manifest__.py
@@ -9,30 +9,23 @@ This module enables the generation of SEPA Direct Debit (SDD)-compliant XML file
 with pain.008.001.02 specification) to send to your bank in order to
 collect a set of payments.
 
-To be elligible for this payment method, a customer must have first
-returned a mandate to the company, giving his consent to use direct debit.
-This consent must have been encoded as a 'customer mandate' into Odoo.
+To be eligible for this payment method, a customer must have first
+returned a mandate to the company, giving their consent to use direct debit.
+This consent must have been encoded as a 'customer mandate' in Odoo.
 
 You also need to meet the following requirements in order to properly
-generate a SDD file:
+generate an SDD file:
 - Your company account must be set to a valid IBAN number
-- Your company must have been given a creditor identifier (this can be done in the 'settings' menu of the accounting module)
+- Your company must have been given a creditor identifier (this can be done in Settings - General Settings - Accounting section)
 - Your company must have defined a journal to receive SDD payments (again, in 'settings' of accounting module)
-- Every customer for which you generate a payment must have a valid IBAN account number.
+- Every customer for whom you generate a payment must have a valid IBAN account number.
 
 Odoo will let you know if any of these requirements are not satisfied.
 
-Any invoice that gets validated for a customer with a mandate will be
-automatically set in 'paid' state, and a payment will be generated. An
-option in the dashboard will then allow you to view all the payments generated
-via SDD and to generate a XML collection file for them, grouping them as
-you see fit.
-
-A dedicated 'SEPA Direct Debit' payment method is also available for
-open invoices, when selecting a bank account in the 'register payment' wizard.
-Use it to generate a SDD payment for the invoices if you added a mandate
-for its owner after its validation. You will see an error message if you
-try to use this method on an invoice for whose payment no mandate can be used.
+To register a payment for open invoices, you can use the dedicated
+'SEPA Direct Debit' option by selecting a bank account in the 'Register
+Payment' wizard. An error message will appear if no valid mandate is
+available for generating a payment on the selected invoice.
     """,
 
     'category': 'Accounting/Accounting',
