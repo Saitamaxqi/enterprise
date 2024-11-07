eb469210f12fa74cd307e7e462ec7da776fe5c55
colu <colu@odoo.com>
2024-11-07 10:22:10 -0500

    [FIX] stock_barcode_barcodelookup: Misspelled error message
    Barcode --> Operations --> Receipts --> NEW
    Scan a barcode that doesn't exist in the DB ie: "5449000000996"
    
    The current error message's spelling and grammar are off.
    
    opw-4307102
    
    closes odoo/enterprise#73499
    
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- stock_barcode_barcodelookup/static/src/models/barcode_model.js
+++ stock_barcode_barcodelookup/static/src/models/barcode_model.js
@@ -28,11 +28,11 @@ patch(BarcodeModel.prototype, {
             if (!barcodeData.error) {
                 if (this.groups.group_tracking_lot) {
                     barcodeData.error = _t(
-                        `This product doesn't exists either scan a package
-                                available at the picking location or create new product`
+                        "This product doesn't exist. Either scan a package " +
+                        "available at the picking location or create a new product."
                     );
                 } else {
-                    barcodeData.error = _t("This product doesn't exists");
+                    barcodeData.error = _t("This product doesn't exist.");
                 }
             }
             return this.notification(barcodeData.error, {
