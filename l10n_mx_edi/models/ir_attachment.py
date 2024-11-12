6f21aedf1a107acb4c89f7a8264171597068e102
igbe <igbe@odoo.com>
2024-11-12 17:22:40 +0100

    [IMP] l10n_mx_edi: delete imported invoice
    When you import an invoice with CFDI, the CFDI fields are populated with the invoice state set to draft.
    The current behaviour is to prevent invoice deletion when we have the CFDI.
    But when it's an imported invoice, we should allow the user to delete the invoice if needed.
    
    This commit add a new condition on delete invoice, so now an CFDI sent invoice with state to draft can be deleted.
    
    task-4306416
    
    closes odoo/enterprise#73680
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- l10n_mx_edi/models/ir_attachment.py
+++ l10n_mx_edi/models/ir_attachment.py
@@ -16,7 +16,12 @@ class IrAttachment(models.Model):
     @api.ondelete(at_uninstall=False)
     def _unlink_except_cfdi_document(self):
         has_cfdi_document = self.env['l10n_mx_edi.document'].sudo().search_count(
-            [('attachment_id', 'in', self.ids), ('move_id.move_type', 'not in', ['in_invoice', 'in_refund'])],
+            [
+                ('attachment_id', 'in', self.ids),
+                ('move_id.state', '!=', 'draft'),
+                ('move_id.l10n_mx_edi_cfdi_uuid', '!=', False),
+                ('move_id.move_type', 'not in', ['in_invoice', 'in_refund']),
+            ],
             limit=1,
         )
         if has_cfdi_document:
