351adee221ab211b30c6e3a58922abe0f735d5e4
Tanguy Quéguineur <taqu@odoo.com>
2024-11-13 13:05:15 +0000

    [FIX] hr_work_entry_contract_enterprise: add year scale
    Steps
    - install hr_work_entry_contract_enterprise
    - Payroll > Work Entries > Select "This year" filter
    - ** UncaughtPromiseError > OwlError **
    
    Cause
    With the addition of variable time ranges in Gantt, a new
    `year` scale is selectable but is missing from the view.
    
    Change
    Add year scale. When clicking on a cell or clicking on the
    "New" button, the start and end date of the work entries
    to create will be the beginning and end of the month.
    
    opw-4321645
    
    closes odoo/enterprise#74606
    
    X-original-commit: a28746ba3d4c95327df4bc990eaafb530bf42b39
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- hr_work_entry_contract_enterprise/static/src/work_entries_gantt_controller.js
+++ hr_work_entry_contract_enterprise/static/src/work_entries_gantt_controller.js
@@ -24,11 +24,16 @@ export class WorkEntriesGanttController extends GanttController {
 
     openDialog(props, options = {}) {
         if (!props.resId) {
-            const date = new Date(props['context']['default_date_stop']);
-            date.setHours(9,0,0);
-            props['context']['default_date_start'] = date.toISOString().replace(/T|Z/g, ' ').trim().substring(0, 19);
-            date.setHours(17,0,0)
-            props['context']['default_date_stop'] = date.toISOString().replace(/T|Z/g, ' ').trim().substring(0, 19);
+            // Add a day to the start date unless it is already the same as the stop date
+            const startDate = new Date(props['context']['default_date_start']);
+            const stopDate = new Date(props['context']['default_date_stop']);
+            if (startDate.getDate() !== stopDate.getDate() || startDate.getMonth() !== stopDate.getMonth()) {
+                startDate.setDate(startDate.getDate() + 1);
+            }
+            startDate.setHours(9,0,0);
+            props['context']['default_date_start'] = startDate.toISOString().replace(/T|Z/g, ' ').trim().substring(0, 19);
+            stopDate.setHours(17,0,0);
+            props['context']['default_date_stop'] = stopDate.toISOString().replace(/T|Z/g, ' ').trim().substring(0, 19);
         }
 
         super.openDialog(...arguments);
@@ -38,12 +43,13 @@ export class WorkEntriesGanttController extends GanttController {
         const { scale, globalStart, globalStop } = this.model.metaData;
         const today = DateTime.local().startOf("day");
         if (scale.unit !== "day" && globalStart <= today.endOf("day") && today <= globalStop.minus({ millisecond: 1 })) {
-            let start = today;
+            let start;
             let stop;
             if (["week", "month"].includes(scale.unit)) {
                 start = today.set({ hours: 9, minutes: 0, seconds: 0 });
                 stop = today.set({ hours: 17, minutes: 0, seconds: 0 });
             } else {
+                start = today.startOf(scale.interval);
                 stop = today.endOf(scale.interval);
             }
             const context = this.model.getDialogContext({ start, stop, withDefault: true });
