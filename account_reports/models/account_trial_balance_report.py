da3c6e8e8dd4228e2090c8b9c0b83d0c71f01b30
Xavier ALT <xal@odoo.com>
2024-11-13 10:08:35 +0000

    [FIX] account_reports: TB empty end balance values w/ blank if zero opt
    To reproduce:
    - Edit 'Trial Balance' report and enable the "Blank if Zero" option
      for both debit and credit lines
    
    - Open a 'Trial Balance', observe there is no 'End Balance' values
      for line where there are only debit or only credit amounts.
    
    This commit also update the `is_zero` option for end balance columns
    value to ensure there are correctly shown when "Blank if Zero" option
    is enabled.
    
    closes odoo/enterprise#74038
    
    X-original-commit: 556ba56b40b1bdb705657c3e3225d8f037771f67
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Xavier Alt (xal) <xal@odoo.com>

--- account_reports/models/account_trial_balance_report.py
+++ account_reports/models/account_trial_balance_report.py
@@ -51,18 +51,20 @@ class AccountTrialBalanceReportHandler(models.AbstractModel):
 
             # End balance: sum all the previous columns for both debit and credit
             if end_balance_debit_index is not None:
-                line['columns'][end_balance_debit_index]['no_format'] = sum(
+                end_balance_debit_sum = sum(
                     currency.round(column['no_format'])
                     for index, column in enumerate(line['columns'])
                     if column.get('expression_label') == 'debit' and index != end_balance_debit_index and column['no_format'] is not None
                 )
+                _update_column(line, end_balance_debit_index, end_balance_debit_sum)
 
             if end_balance_credit_index is not None:
-                line['columns'][end_balance_credit_index]['no_format'] = sum(
+                end_balance_credit_sum = sum(
                     currency.round(column['no_format'])
                     for index, column in enumerate(line['columns'])
                     if column.get('expression_label') == 'credit' and index != end_balance_credit_index and column['no_format'] is not None
                 )
+                _update_column(line, end_balance_credit_index, end_balance_credit_sum)
 
             _update_balance_columns(line, end_balance_debit_index, end_balance_credit_index)
 
