b8f2184a83859ec08dd52e8893921ff68b0c0d75
Adnan Saiyed <adsa@odoo.com>
2024-11-14 11:54:40 +0000

    [FIX] web_studio: remove video command in report
    Before this commit:
    
    In studio, in some report, trying to add a video using
    video commnad throws traceback. Adding a video doesn't
    make sense in a report so the video command shouldn't
    be available in report at the first place.
    
    After this commit:
    
    This commit aims to remove video command from studio
    report.
    
    task-4285231
    
    closes odoo/enterprise#76260
    
    X-original-commit: 9dded2ca4d6a24c26b6fa7b0a9b71b3d150512a7
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- web_studio/static/src/client_action/report_editor/report_editor_wysiwyg/report_editor_wysiwyg.js
+++ web_studio/static/src/client_action/report_editor/report_editor_wysiwyg/report_editor_wysiwyg.js
@@ -312,6 +312,7 @@ export class ReportEditorWysiwyg extends Component {
                     user_commands: this.getUserCommands(),
                     powerbox_items: this.getPowerboxCommands(),
                 },
+                disableVideo: true,
             },
             this.env.services
         );
