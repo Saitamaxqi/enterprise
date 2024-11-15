d4cadb3a09ba5ae96a29363a6be9dd99dfeaf97e
Lucas Lefèvre (lul) <lul@odoo.com>
2024-11-15 08:57:58 +0000

    [FIX] spreadsheet_edition: reduce thumbnail size
    This commit drastically reduces the spreadsheet thumbnails size.
    
    Go to CRM lead and insert the pivot in a spreadsheet:
    
    The size of the thumbnail:
    
    before:		   107Kb
    after quality=0.5: 7.8Kb
    after quality=0:   2.9Kb
    
    Image quality is indeed reduced, but the thumbnails are always
    displayed so small that it doesn't really make any difference
    to the naked eye.
    
    Note: I'm not backporting this fix to 16.0 because the code changed
    and webp was not supported at the time (even though we could use
    jpeg)
    
    closes odoo/enterprise#74339
    
    Task: 4337496
    X-original-commit: 80b264173579b536d0ae67bf59fb4a18410a52ea
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- spreadsheet_edition/static/src/bundle/hooks.js
+++ spreadsheet_edition/static/src/bundle/hooks.js
@@ -50,7 +50,7 @@ export function useSpreadsheetThumbnail() {
         const dimensions = SPREADSHEET_DIMENSIONS;
         const canvas = document.querySelector(".o-grid canvas:not(.o-figure-canvas)");
         const canvasResizer = document.createElement("canvas");
-        const size = 750;
+        const size = 500;
         canvasResizer.width = size;
         canvasResizer.height = size;
         const canvasCtx = canvasResizer.getContext("2d");
@@ -73,6 +73,8 @@ export function useSpreadsheetThumbnail() {
                 size
             );
         }
-        return canvasResizer.toDataURL().replace("data:image/png;base64,", "");
+        const type = "image/webp";
+        const quality = 0;
+        return canvasResizer.toDataURL(type, quality).replace(`data:${type};base64,`, "");
     };
 }
