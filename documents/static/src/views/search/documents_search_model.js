6a8bf8cda822fc9f760caf47379dc1ecb8ca0b04
Florian Charlier <flch@odoo.com>
2024-11-18 16:06:37 +0000

    [FIX] documents: discard default filters coming from url
    Reproduce:
    1. Configure a default filter on Documents so that not all documents are visible
    2. Select a document that does not match this default filter
    3. Copy the url of this document (copy url when it is selected)
    4. Paste in navigation bar / refresh page
    5. You do not end up on the document because the default filter still applied
    
    This commit prevents loading default filters when we come from such URLs,
    not otherwise (refresh the page after deselecting the document in the folder).
    
    Task-4342960
    
    closes odoo/enterprise#74089
    
    X-original-commit: 3418e7318ed993abc2a601ae601e7214e28293a3
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>
    Signed-off-by: Florian Charlier (flch) <flch@odoo.com>

--- documents/static/src/views/search/documents_search_model.js
+++ documents/static/src/views/search/documents_search_model.js
@@ -26,7 +26,14 @@ export class DocumentsSearchModel extends SearchModel {
     }
 
     async load(config) {
-        await super.load(...arguments);
+        if (this.documentService.initData.documentId && this.documentService.initData.openPreview) {
+            // Make sure target document is found (if accessible).
+            config.irFilters.forEach((fil) => {
+                fil.is_default = false;
+            });
+        }
+
+        await super.load(config);
 
         let folderId = router.current.folder_id || this.getSelectedFolderId();
 
