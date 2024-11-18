5af2edc3eccb3924eeb7570bb381f10079034d2e
Demesmaeker <edm@odoo.com>
2024-11-18 13:51:49 +0000

    [FIX] sale_amazon_avatax: only show the tax excluded amount
    When synchronizing orders from Amazon with Avatax enabled, sales order
    lines currently show no taxes and incorrectly set the tax included
    price as being the subtotal.
    
    This occurs because the fiscal position used does not include any tax,
    which is the expected behavior for Avatax. The recomputation then uses
    the full amount of the line (which is tax included), and in the absence
    of tax, consider this is also the subtotal.
    
    We now intentionally do not consider the tax amount from Amazon on the
    order lines, because the distribution of the Amazon's given tax amount
    is unknown. The multiple real taxes across that line is not an
    information that Amazon gives us, and needs to be seen from Avatax.
    
    This setup ensures that the subtotal on sales order lines remain
    accurate.
    
    opw-4214775
    
    closes odoo/enterprise#75027
    
    X-original-commit: c2b7e145d2db13647283e0e4a8bd55c3465b1f02
    Signed-off-by: Morgane Demesmaeker <edm@odoo.com>

--- sale_amazon_avatax/models/amazon_account.py
+++ sale_amazon_avatax/models/amazon_account.py
@@ -12,3 +12,15 @@ class AmazonAccount(models.Model):
         if order.fiscal_position_id.is_avatax:
             order.fiscal_position_id = False
         return order
+
+    def _recompute_subtotal(self, subtotal, tax_amount, taxes, currency, _fiscal_pos=None):
+        """ Override to not consider any tax and only use the tax_excluded amount from Amazon.
+
+        This is done because Amazon does its own tax report to Avatax, but doesn't send the complete
+        information to fill each individual separate tax. As when using Avatax, the tax report must
+        be done within Avatax and not Odoo, we only include the tax excluded amount in the sale
+        order line.
+        """
+        if not _fiscal_pos or not _fiscal_pos.is_avatax:
+            return super()._recompute_subtotal(subtotal, tax_amount, taxes, currency, _fiscal_pos)
+        return subtotal
