Saitamaxqi/enterprise

commit e98f1bc5433fb7f5f8831770744860e59286e9a7

[CLN] various: rename product_uom field to product_uom_id
This commit adept changes done in community PR to rename
`product_uom` field to `product_uom_id` also rename
related fields in following models:
- sale.subscription.report
- sale.rental.schedule
- sale.rental.report

task-4206350

closes odoo/enterprise#73293

Related: odoo/odoo#186250
Related: odoo/upgrade#6732
Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

sale_subscription/models/sale_order_line.py
@@ -275,7 +275,7 @@ class SaleOrderLine(models.Model):
             )
             for invoice_line in related_invoice_lines:
                 line_sign = amount_sign.get(invoice_line.move_id.move_type, 1)
-                qty_invoiced += line_sign * invoice_line.product_uom_id._compute_quantity(invoice_line.quantity, line.product_uom)
+                qty_invoiced += line_sign * invoice_line.product_uom_id._compute_quantity(invoice_line.quantity, line.product_uom_id)
             result[line.id] = qty_invoiced
         return result
 
@@ -298,7 +298,7 @@ class SaleOrderLine(models.Model):
             else:
                 line.recurring_monthly = line.price_subtotal * INTERVAL_FACTOR[line.order_id.plan_id.billing_period_unit] / line.order_id.plan_id.billing_period_value
 
-    @api.depends('order_id.subscription_id', 'product_id', 'product_uom', 'price_unit', 'order_id', 'order_id.plan_id')
+    @api.depends('order_id.subscription_id', 'product_id', 'product_uom_id', 'price_unit', 'order_id', 'order_id.plan_id')
     def _compute_parent_line_id(self):
         """
         Compute the link between a SOL and the line in the parent order. The matching is done based on several
@@ -313,9 +313,9 @@ class SaleOrderLine(models.Model):
             # We use a rounding to avoid -326.40000000000003 != -326.4 for new records.
             matching_line_ids = parent_line_ids.filtered(
                 lambda l:
-                (l.order_id, l.product_id, l.product_uom, l.order_id.currency_id, l.order_id.plan_id,
+                (l.order_id, l.product_id, l.product_uom_id, l.order_id.currency_id, l.order_id.plan_id,
                  l.order_id.currency_id.round(l.price_unit) if l.order_id.currency_id else round(l.price_unit, 2)) ==
-                (line.order_id.subscription_id, line.product_id, line.product_uom, line.order_id.currency_id, line.order_id.plan_id,
+                (line.order_id.subscription_id, line.product_id, line.product_uom_id, line.order_id.currency_id, line.order_id.plan_id,
                  line.order_id.currency_id.round(line.price_unit) if line.order_id.currency_id else round(line.price_unit, 2)
                 ) and l.id in parent_line_ids.ids
             )
@@ -489,7 +489,6 @@ class SaleOrderLine(models.Model):
                 'parent_line_id': line.id,
                 'name': line.name + "(*)" if line in description_needed else line.name,
                 'product_id': product.id,
-                'product_uom': line.product_uom.id,
                 'product_uom_qty': 0 if subscription_state == '7_upsell' else line.product_uom_qty,
                 'price_unit': line.price_unit,
             }))
@@ -533,7 +532,6 @@ class SaleOrderLine(models.Model):
                     'product_id': line.product_id.id,
                     'name': line.name,
                     'product_uom_qty': line.product_uom_qty,
-                    'product_uom': line.product_uom.id,
                     'price_unit': line.price_unit,
                     'discount': 0,
                     'order_id': subscription.id

sale_mrp_renting/models/sale_order_line.py
@@ -17,7 +17,7 @@ class SaleOrderLine(models.Model):
                 'outgoing_moves': lambda m: m.location_dest_id != m.company_id.rental_loc_id and m.to_refund
             }
             order_qty = previous_product_uom_qty.get(self.id, 0) if previous_product_uom_qty else self.product_uom_qty
-            order_qty = self.product_uom._compute_quantity(order_qty, bom.product_uom_id)
+            order_qty = self.product_uom_id._compute_quantity(order_qty, bom.product_uom_id)
             qty_to_compute = outgoing_moves._compute_kit_quantities(self.product_id, order_qty, bom, filters)
-            qty = bom.product_uom_id._compute_quantity(qty_to_compute, self.product_uom)
+            qty = bom.product_uom_id._compute_quantity(qty_to_compute, self.product_uom_id)
         return qty

sale_planning/models/sale_order_line.py
@@ -12,7 +12,7 @@ class SaleOrderLine(models.Model):
     planning_hours_planned = fields.Float(compute='_compute_planning_hours_planned', store=True, compute_sudo=True, export_string_translation=False)
     planning_hours_to_plan = fields.Float(compute='_compute_planning_hours_to_plan', store=True, compute_sudo=True, export_string_translation=False)
 
-    @api.depends('product_uom', 'product_uom_qty', 'state')
+    @api.depends('product_uom_id', 'product_uom_qty', 'state')
     def _compute_planning_hours_to_plan(self):
         sol_planning = self.filtered_domain([('state', 'not in', ['draft', 'sent'])])
         if sol_planning:
@@ -20,11 +20,11 @@ class SaleOrderLine(models.Model):
             uom_hour = self.env.ref('uom.product_uom_hour')
             uom_unit = self.env.ref('uom.product_uom_unit')
             for sol in sol_planning:
-                if sol.product_uom == uom_hour or sol.product_uom == uom_unit:
+                if sol.product_uom_id == uom_hour or sol.product_uom_id == uom_unit:
                     sol.planning_hours_to_plan = sol.product_uom_qty
                 else:
                     sol.planning_hours_to_plan = float_round(
-                        sol.product_uom._compute_quantity(sol.product_uom_qty, uom_hour, raise_if_failure=False),
+                        sol.product_uom_id._compute_quantity(sol.product_uom_qty, uom_hour, raise_if_failure=False),
                         precision_digits=2
                     )
         for line in self - sol_planning:

sale_renting/models/sale_order_line.py
@@ -193,7 +193,7 @@ class SaleOrderLine(models.Model):
                 self.product_id.with_context(**self._get_product_price_context()),
                 self.product_uom_qty or 1.0,
                 currency=self.currency_id,
-                uom=self.product_uom,
+                uom=self.product_uom_id,
                 date=self.order_id.date_order or fields.Date.today(),
                 start_date=self.start_date,
                 end_date=self.return_date,

sale_stock_renting/models/sale_order_line.py
@@ -417,7 +417,7 @@ class SaleOrderLine(models.Model):
                 for move in outgoing_moves:
                     if move.state != 'done':
                         continue
-                    qty += move.product_uom._compute_quantity(move.quantity, line.product_uom, rounding_method='HALF-UP')
+                    qty += move.product_uom._compute_quantity(move.quantity, line.product_uom_id, rounding_method='HALF-UP')
                 line.qty_delivered = qty
 
     @api.depends('pickedup_lot_ids', 'returned_lot_ids', 'reserved_lot_ids')

commit c1d5013e64bf5083a05f7e2e8058cc51cc02fc95

[FIX] sale_subscription: fix default uom issue
This commit fix default uom issue when user update uom on sol and renew their subscription it should take uom from SOL not default uom on product.

Issue introduced by e98f1bc5433fb7f5f8831770744860e59286e9a7

closes odoo/enterprise#74444

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

sale_subscription/models/sale_order_line.py
@@ -489,6 +489,7 @@ class SaleOrderLine(models.Model):
                 'parent_line_id': line.id,
                 'name': line.name + "(*)" if line in description_needed else line.name,
                 'product_id': product.id,
+                'product_uom_id': line.product_uom_id.id,
                 'product_uom_qty': 0 if subscription_state == '7_upsell' else line.product_uom_qty,
                 'price_unit': line.price_unit,
             }))
@@ -532,6 +533,7 @@ class SaleOrderLine(models.Model):
                     'product_id': line.product_id.id,
                     'name': line.name,
                     'product_uom_qty': line.product_uom_qty,
+                    'product_uom_id': line.product_uom_id.id,
                     'price_unit': line.price_unit,
                     'discount': 0,
                     'order_id': subscription.id