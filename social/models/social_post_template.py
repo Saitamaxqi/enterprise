9f5e6f295f77969029458a7e5935268390084b05
amdi-odoo <amdi@odoo.com>
2024-11-26 15:51:38 +0100

    [IMP] social: post only images
    Purpose
    ======
    When adding images to a post, only accept the most common images mimetypes.
    There is no point uploading pdf files, excel files or even svg images which
    are not correctly handled by the different media APIs.
    
    Accepted mimetypes:
    image/jpg, image/jpeg, image/jpe, image/png, image/gif, image/webp
    
    Technical:
    Prevent messing up the images order while computing the image_urls by changing
    the use of a set with an OrderedSet.
    
    Task-4243326
    
    closes odoo/enterprise#71753
    
    Related: odoo/odoo#183325
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

--- social/models/social_post_template.py
+++ social/models/social_post_template.py
@@ -6,7 +6,7 @@ import json
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 from odoo.osv import expression
-from odoo.tools import format_datetime
+from odoo.tools import format_datetime, OrderedSet
 
 
 class SocialPostTemplate(models.Model):
@@ -111,7 +111,7 @@ class SocialPostTemplate(models.Model):
         """See field 'help' for more information."""
         image_fields = ['image_ids'] + list(self._images_fields().values())
         for post in self:
-            all_image_ids = {image_id for field in image_fields for image_id in post[field].ids}
+            all_image_ids = OrderedSet([image_id for field in image_fields for image_id in post[field].ids])
             post.image_urls = json.dumps([f'/web/image/{image_id}' for image_id in all_image_ids if image_id])
 
     @api.depends('account_ids')
