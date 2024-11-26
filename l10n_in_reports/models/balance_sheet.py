6757531b1a692bc6e567b2e38caf76f8c9dde24f
jbw-odoo <jbw@odoo.com>
2024-11-26 10:16:35 +0000

    [FIX] l10n_in_reports: missing super call
    Since https://github.com/odoo/enterprise/commit/e598fcb48b5e4f0126406a4008f175a88528ba85 the balance sheet report has a custom handler. Make indian balance sheet handler execute super method in it's _custom_options_initializer() do execute parent logic.
    
    closes odoo/enterprise#74837
    
    X-original-commit: 8ac0ed32d5b1d65cb9a877c94ff0c10a88a062d1
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- l10n_in_reports/models/balance_sheet.py
+++ l10n_in_reports/models/balance_sheet.py
@@ -7,6 +7,7 @@ class L10n_In_ReportsBalanceSheetReportHandler(models.AbstractModel):
     _description = 'Indian Custom Handler for Generic Balance Sheet'
 
     def _custom_options_initializer(self, report, options, previous_options):
+        super()._custom_options_initializer(report, options, previous_options)
         if (not previous_options or 'horizontal_split' not in previous_options or previous_options.get('companies') != options['companies']) \
             and self.env.company.chart_template == 'in':
             options['horizontal_split'] = True
