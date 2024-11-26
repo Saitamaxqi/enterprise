8eb337611faf3e14d0b7ff96ac9e1279c86a41b8
Alvaro Fuentes <afu@odoo.com>
2024-11-26 15:51:40 +0000

    [FIX] web_grid: unlink window actions mapping
    When a grid view is removed we should cascade the removal to the window
    actions mapping. Grid views are considered experimental. They do not
    have a `_get_default_grid_view` method, and crafting an acceptable
    general one is close to impossible due to grid views' requirements in
    terms of the model fields.
    
    Allowing the grid mapping for window actions have been proven to cause
    issues during upgrades if clients uninstall one of the few modules
    providing grid views (like analytic_enterprise).
    
    closes odoo/enterprise#75595
    
    X-original-commit: c7e0b9ac0adbce749a66bbad69144ea992b135fb
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>
    Co-authored-by: Christophe Simonis <chs@odoo.com>

--- web_grid/models/ir_ui_view.py
+++ web_grid/models/ir_ui_view.py
@@ -11,3 +11,17 @@ class IrUiView(models.Model):
 
     def _get_view_info(self):
         return {'grid': {'icon': 'fa fa-th'}} | super()._get_view_info()
+
+    def unlink(self):
+        if not any(v.type == "grid" for v in self):
+            return super().unlink()
+        self.env["ir.actions.act_window.view"].search(
+            [("view_mode", "=", "grid"), ("view_id", "in", self.ids)]
+        ).unlink()
+        res = super().unlink()
+        grid_models = list(set(self.search([("type", "=", "grid")]).mapped("model")))
+        for action in self.env["ir.actions.act_window"].search(
+            [("view_mode", "like", "grid"), ("res_model", "not in", grid_models)]
+        ):
+            action.view_mode = ",".join(mode for mode in action.view_mode.split(",") if mode != "grid")
+        return res
