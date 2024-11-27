786a08095ec019fe618842aa71be799276051002
Adrien Schoffeniels <adsc@odoo.com>
2024-11-27 09:51:05 +0100

    [IMP] knowledge: add permission panel tests
    Purpose:
    --------
    Add unit tests for the permission panel.
    
    Task-4273102
    
    Part-of: odoo/enterprise#76261
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>

--- knowledge/static/tests/knowledge_test_helpers.js
+++ knowledge/static/tests/knowledge_test_helpers.js
@@ -1,10 +1,36 @@
 import { mailModels } from "@mail/../tests/mail_test_helpers";
-import { defineModels } from "@web/../tests/web_test_helpers";
+import { defineModels, onRpc } from "@web/../tests/web_test_helpers";
 import { KnowledgeArticle } from "./mock_server/mock_models/knowledge_article";
 import { KnowledgeArticleThread } from "./mock_server/mock_models/knowledge_article_thread";
+import { beforeEach, expect } from "@odoo/hoot";
 
 export function defineKnowledgeModels() {
     return defineModels(knowledgeModels);
+}
+
+export function mockKnowledgePermissionPanelRpc() {
+    return beforeEach(() => {
+        onRpc("knowledge.article", "remove_member", ({ args }) => {
+            const [article_id, member_id] = args;
+            expect.step(`remove member ${member_id} on article ${article_id}`);
+        });
+        onRpc("knowledge.article", "set_internal_permission", ({ args }) => {
+            const [article_id, permission] = args;
+            expect.step(`change permission to ${permission} on article ${article_id}`);
+        });
+        onRpc("knowledge.article", "set_is_article_visible_by_everyone", ({ args }) => {
+            const [article_id, is_visible_by_everyone] = args;
+            expect.step(`change visibility to ${is_visible_by_everyone ? "everyone" : "members"} on article ${article_id}`);
+        });
+        onRpc("knowledge.article", "set_member_permission", ({ args }) => {
+            const [article_id, member_id, permission] = args;
+            expect.step(`change permission of member ${member_id} to ${permission} on article ${article_id}`);
+        });
+        onRpc("knowledge.article", "restore_article_access", ({ args }) => {
+            const [article_id] = args;
+            expect.step(`restoring access on article ${article_id}`);
+        });
+    });
 }
 
 export const knowledgeModels = { ...mailModels, KnowledgeArticle, KnowledgeArticleThread };
