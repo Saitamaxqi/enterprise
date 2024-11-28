8bb5278ad121bf86fcda97492f3b6299169a2636
guva-odoo <guva@odoo.com>
2024-11-28 10:07:27 +0000

    [FIX] account_3way_match: journal dashboard query
    Update the open sale/purchase journals query to include credit
    notes/refunds,  and override the 'Bills to validate' filter to be
    consistent with the dashboard query which is overridden for
    `account_3way_match`
    
    closes odoo/enterprise#76221
    
    X-original-commit: 1fc76eed603b474f4c4979d128000015dc193d4b
    Related: odoo/odoo#191840
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Guillaume Vanleynseele (guva) <guva@odoo.com>

--- account_3way_match/tests/test_account_journal_dashboard.py
+++ account_3way_match/tests/test_account_journal_dashboard.py
@@ -154,8 +154,8 @@ class AccountJournalDashboard3WayWatchTest(TestAccountJournalDashboardCommon):
         self.assertEqual(dashboard_data['number_draft'], 0)
         self.assertIn('0.00', dashboard_data['sum_draft'])
 
-        self.assertEqual(dashboard_data['number_waiting'], 1)
-        self.assertIn('81.72', dashboard_data['sum_waiting'])
+        self.assertEqual(dashboard_data['number_waiting'], 2)
+        self.assertIn('68.42', dashboard_data['sum_waiting'])
 
         # Check partial on refund
         payment = self.env['account.payment'].create({
@@ -174,12 +174,12 @@ class AccountJournalDashboard3WayWatchTest(TestAccountJournalDashboardCommon):
         self.assertEqual(dashboard_data['number_draft'], 0)
         self.assertIn('0.00', dashboard_data['sum_draft'])
 
-        self.assertEqual(dashboard_data['number_waiting'], 1)
-        self.assertIn('81.72', dashboard_data['sum_waiting'])
+        self.assertEqual(dashboard_data['number_waiting'], 2)
+        self.assertIn('68.42', dashboard_data['sum_waiting'])
 
         dashboard_data = journal._get_journal_dashboard_data_batched()[journal.id]
-        self.assertEqual(dashboard_data['number_late'], 1)
-        self.assertIn('81.72', dashboard_data['sum_late'])
+        self.assertEqual(dashboard_data['number_late'], 2)
+        self.assertIn('68.42', dashboard_data['sum_late'])
 
     def test_sale_purchase_journal_for_multi_currency_sale(self):
         currency = self.other_currency
