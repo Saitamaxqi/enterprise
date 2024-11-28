6a7e79a4ec950cface94179d088d52120f6d0598
hupo-odoo <hupo@odoo.com>
2024-11-28 14:33:10 +0000

    [FIX] account_report: options on composite report export
    Steps to reproduce
     - Activate developer mode
     - Go to Accounting -> Configuration -> Accounting Reports
     - Create a composite report with 2 existing reports that
     have the "filter_unfold_all" activated, i.e. "Balance Sheet" and "Profit and Loss"
     - Create a menu item in the action of the report
     - Open the report
     - Press the filter "Unfold All"
     - Press the "PDF" button
    
     -> Results: the pdf has no line unfolded while they should (the web UI works as expected)
    
     Reason of the issue:
     In the dispatch_report_action() function, the get_options() was wrong and unnecessary. This function
     is first called on the first section, then called on the composite report with its own options.
     This led to use the options of the composite report which does not have the filter_unfold_all field
     set to True.
    
     This also handles the case where a composite report has reports A and B, A having the
     filter_unfold_all while B doesn't have it. The expected result when printing it is
     that only report A has unfolded lines.
    
     task-4317649
    
    closes odoo/enterprise#76501
    
    X-original-commit: 8213813e68f02d88934285a44a3413505f6ca187
    Related: odoo/odoo#192420
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -2435,10 +2435,9 @@ class AccountReport(models.Model):
 
         if on_sections_source:
             report_to_call = self.env['account.report'].browse(options['sections_source_id'])
-            options = report_to_call.get_options(previous_options={**options, 'no_report_reroute': True})
             return report_to_call.dispatch_report_action(options, action, action_param=action_param, on_sections_source=False)
 
-        if options['report_id'] != self.id:
+        if self.id not in (options['report_id'], options.get('sections_source_id')):
             raise UserError(_("Trying to dispatch an action on a report not compatible with the provided options."))
 
         check_method_name(action)
