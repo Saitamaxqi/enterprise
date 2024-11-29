0a26cb63c4544c7b9be15b6a10341b52f5c275ec
Pedram (pebr) <pebr@odoo.com>
2024-11-29 15:35:14 +0000

    [FIX] l10n_de_pos_res_cert: get order when sync
    Before this commit, when opening a table with an order, the draft order
    would not be shown. This was because, when there was no order to sync,
    the sync function was not called to retrieve the orders from the
    server. This is needed in the restaurant module.
    
    opw-4370055
    
    closes odoo/enterprise#75287
    
    X-original-commit: 002c0703137da6d458689372eaf946e0b18a0723
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- l10n_de_pos_res_cert/static/tests/tours/fiskaly_tour.js
+++ l10n_de_pos_res_cert/static/tests/tours/fiskaly_tour.js
@@ -6,6 +6,7 @@ import * as ProductScreenResto from "@pos_restaurant/../tests/tours/utils/produc
 const ProductScreen = { ...ProductScreenPos, ...ProductScreenResto };
 import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
 import * as FloorScreen from "@pos_restaurant/../tests/tours/utils/floor_screen_util";
+import * as Order from "@point_of_sale/../tests/generic_helpers/order_widget_util";
 import { registry } from "@web/core/registry";
 
 registry.category("web_tour.tours").add("FiskalyTour", {
@@ -20,6 +21,11 @@ registry.category("web_tour.tours").add("FiskalyTour", {
             ProductScreen.clickOrderButton(),
             FloorScreen.clickTable("105"),
             ProductScreen.orderlinesHaveNoChange(),
+            Chrome.clickPlanButton(),
+            FloorScreen.clickTable("5"),
+            Order.hasLine({
+                productName: "Coca-Cola",
+            }),
             ProductScreen.addOrderline("Coca-Cola", "1", "5"),
             ProductScreen.clickOrderButton(),
             FloorScreen.clickTable("105"),
