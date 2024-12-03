6f0fd28aa3af20c6ad2f6c577fdfc9f8fbf2ac6b
Louis Travaux <lotr@odoo.com>
2024-12-03 15:14:03 +0000

    [IMP] iot: remove Reboot IoT Box button
    LNE Certification (scales certification) requires not to be able to reboot the IoT Box with a simple button.
    We removed it from the IoT Box form view.
    
    Community PR: [https://github.com/odoo/odoo/pull/189467](https://github.com/odoo/odoo/pull/189467)
    
    closes odoo/enterprise#75218
    
    Task: 4345731
    X-original-commit: a2eaa14a4f669e40d602f3a90fffdd7bdc5cfdfa
    Related: odoo/odoo#189767
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>

--- iot/static/src/view_widgets/iot_restart_odoo.js
+++ iot/static/src/view_widgets/iot_restart_odoo.js
@@ -8,7 +8,7 @@ import { Component } from "@odoo/owl";
 
 let restarting = false;
 
-export class IoTRestartOdooOrReboot extends Component {
+export class IoTRestartOdoo extends Component {
     static template = `iot.IoTRestartOdooOrReboot`;
     static props = {
         ...standardWidgetProps,
@@ -29,11 +29,8 @@ export class IoTRestartOdooOrReboot extends Component {
 
     async onClick() {
         this.dialog.add(ConfirmationDialog, {
-            body:
-                this.props.action == "restart_odoo"
-                    ? _t("Are you sure you want to restart Odoo on the IoT box?")
-                    : _t("Are you sure you want to reboot the IoT box?"),
-            confirm: () => this.restartOdooOrReboot(),
+            body: _t("Are you sure you want to restart Odoo on the IoT box?"),
+            confirm: () => this.restartOdoo(),
             cancel: () => {},
         });
     }
@@ -60,8 +57,7 @@ export class IoTRestartOdooOrReboot extends Component {
                 _t("Restarting"),
                 "warning"
             );
-            const endpoint = this.props.action === "restart_odoo" ? "restart_odoo_service" : "restart_iotbox";
-            const response = await this.http.get(`${this.ip_url}/hw_posbox_homepage/${endpoint}`);
+            const response = await this.http.get(`${this.ip_url}/hw_posbox_homepage/restart_odoo_service`);
             return response.status;
         } catch (error) {
             this.doWarnFail(this.ip_url);
@@ -77,7 +73,7 @@ export class IoTRestartOdooOrReboot extends Component {
             let server_response;
             try {
                 server_response = await this.http.get(this.ip_url + "/hw_proxy/hello", "text");
-                if (server_response == "ping" && restarting) {
+                if (server_response === "ping" && restarting) {
                     this.showMsgAndClearInterval(
                         responseInterval,
                         _t("Restart finished"),
@@ -112,14 +108,14 @@ export class IoTRestartOdooOrReboot extends Component {
         }, 600000);
     }
 
-    async restartOdooOrReboot() {
+    async restartOdoo() {
         /// Call restart method on server, then ping it until restarting is finished
         /// Only restart if there are no other restart processes active
         /// If an error is encountered, display it and stop
         let restartResponse;
         if (!restarting) {
             restartResponse = await this.callRestartMethodOnServer();
-            if (restartResponse == "success") {
+            if (restartResponse === "success") {
                 this.pingServerUntilItFinishedRestarting();
             } else {
                 // If the python code triggerred an exception, display its message and stop
@@ -145,8 +141,8 @@ export class IoTRestartOdooOrReboot extends Component {
     }
 }
 
-export const ioTRestartOdooOrReboot = {
-    component: IoTRestartOdooOrReboot,
+export const ioTRestartOdoo = {
+    component: IoTRestartOdoo,
     extractProps: ({ attrs }) => {
         return {
             btn_name: attrs.btn_name,
@@ -154,4 +150,4 @@ export const ioTRestartOdooOrReboot = {
         };
     },
 };
-registry.category("view_widgets").add("iot_restart_odoo_or_reboot", ioTRestartOdooOrReboot);
+registry.category("view_widgets").add("iot_restart_odoo_or_reboot", ioTRestartOdoo);
