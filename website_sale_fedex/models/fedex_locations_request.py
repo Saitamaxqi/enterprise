53b9199c6cdbe2a056cd4865f1b2d0e7b60b84a9
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-12-11 14:31:27 +0000

    [REV] website_sale_fedex: fix translation in request
    DUring the request to fexed, FEDEXLocationsRequest is not a model, so it
    does not have an environment.
    
    Partial revert of 25d394443a6a99db1e850d0f7dd0e1243133ae25
    
    closes odoo/enterprise#75506
    
    Signed-off-by: Lina Eweis (liew) <liew@odoo.com>

--- website_sale_fedex/models/fedex_locations_request.py
+++ website_sale_fedex/models/fedex_locations_request.py
@@ -8,6 +8,7 @@ from odoo.tools.zeep.exceptions import Fault
 
 from odoo.exceptions import UserError
 from odoo.addons.delivery_fedex.models.fedex_request import remove_accents, FedexRequest, LogPlugin, STATECODE_REQUIRED_COUNTRIES
+from odoo.tools import _
 from odoo.tools.misc import file_path
 
 
@@ -94,11 +95,11 @@ class FEDEXLocationsRequest(FedexRequest):
                 )
 
         except Fault as fault:
-            raise UserError(self.env._('There was an error retrieving Fedex localisations:\n%s', fault))
+            raise UserError(_('There was an error retrieving Fedex localisations:\n%s', fault))
         except OSError:
-            raise UserError(self.env._('Fedex Server Not Found'))
+            raise UserError(_('Fedex Server Not Found'))
         except ValueError:
-            raise UserError(self.env._('No Fedex pick-up points available for that shipping address'))
+            raise UserError(_('No Fedex pick-up points available for that shipping address'))
         return self._sanitize_response(response)
 
     def _sanitize_response(self, response):
