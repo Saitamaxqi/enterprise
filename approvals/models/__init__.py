80b26df3756b20c0c2676500cab07a07f799c11f
Didier (did) <did@odoo.com>
2024-12-12 09:10:29 +0100

    [IMP] approvals: create a file for approval.approver model
    closes odoo/enterprise#79065
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- approvals/models/__init__.py
+++ approvals/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import approval_approver
 from . import approval_category
 from . import approval_category_approver
 from . import approval_product_line
