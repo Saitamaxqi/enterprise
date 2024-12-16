3932af36e65771af9ba91214aea4867fd51d1916
nape-odoo <nape@odoo.com>
2024-12-16 13:05:50 +0000

    [FIX] l10n_mx_reports: Tax return report date filter OWL error
    The error in question was caused by the MX localization MexicanAccountReportCustomHandler DIOT report model neither including tax_periodicity in it's own _custom_options_initializer() which overrides the version inherited from account_generic_tax_report.py, therefore account_reports/static/src/components/account_report/filters/filters.js -> hideTaxPeriodFilter() showed an error when trying to get it.
    
    closes odoo/enterprise#76048
    
    Task: 4402723
    X-original-commit: e3f831a0c82a2a261b83c8a8be3458c277101439
    Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>

--- l10n_mx_reports/models/account_diot.py
+++ l10n_mx_reports/models/account_diot.py
@@ -46,6 +46,7 @@ class L10n_MxReportHandler(models.AbstractModel):
     _description = 'Mexican Account Report Custom Handler'
 
     def _custom_options_initializer(self, report, options, previous_options):
+        super()._custom_options_initializer(report, options, previous_options)
         options['columns'] = [column for column in options['columns']]
         options.setdefault('buttons', []).extend((
             {'name': _('DIOT (txt)'), 'sequence': 40, 'action': 'export_file', 'action_param': 'action_get_diot_txt', 'file_export_type': _('DIOT')},
