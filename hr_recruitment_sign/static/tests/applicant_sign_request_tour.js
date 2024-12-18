b4857f6ec52a8632c20122108b603ba741d3c7d7
ravb-odoo <ravb@odoo.com>
2024-12-18 12:29:03 +0530

    [IMP] hr_recruitment_sign: improve test tour for recruitment
    This PR improves the test tour for the recruitment module. As per the task
    requirements, the default group by in all application menus has been removed,
    which caused the test tour to fail. To address this, the step that opens the
    grouped list has been removed from the tour to ensure it passes successfully.
    
    task-4222504
    
    closes odoo/enterprise#75798
    
    Related: odoo/odoo#182877
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_recruitment_sign/static/tests/applicant_sign_request_tour.js
+++ hr_recruitment_sign/static/tests/applicant_sign_request_tour.js
@@ -19,11 +19,6 @@ registry.category("web_tour.tours").add('applicant_sign_request_tour', {
             run: 'click',
         },
         {
-            content: "Open group",
-            trigger: 'tr.o_group_has_content:contains("None")',
-            run: 'click',
-        },
-        {
             content: "Open Saitama's application",
             trigger: '.o_data_cell[data-tooltip="Saitama"]',
             run: 'click',
