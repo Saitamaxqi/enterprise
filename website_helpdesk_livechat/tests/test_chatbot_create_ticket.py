5cd2390954605eff539d15d185533f2509753d75
Sébastien Theys <seb@odoo.com>
2024-12-19 12:36:56 +0000

    [IMP] website_helpdesk_livechat: clean up chatbot test
    Enterprise counter-part.
    
    task-4354325
    
    closes odoo/enterprise#76416
    
    X-original-commit: 57c46691b4c93f4b064cbb7fc78135f75c8f7c51
    Related: odoo/odoo#192263
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- website_helpdesk_livechat/tests/test_chatbot_create_ticket.py
+++ website_helpdesk_livechat/tests/test_chatbot_create_ticket.py
@@ -1,17 +1,15 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.website_helpdesk_livechat.tests.helpdesk_livechat_chatbot_common import HelpdeskChatbotCase
-from odoo.tests import tagged, users
+from odoo.tests import tagged
 
 
 @tagged("post_install", "-at_install")
 class TestChatbotCreateTicket(HelpdeskChatbotCase):
 
-    @users('user_public')
     def test_chatbot_helpdesk_ticket_public_user(self):
         """ Create a ticket from a public user and check that information are correctly propagated. """
-        self._chatbot_create_helpdesk_ticket(self.user_public)
+        self._chatbot_create_helpdesk_ticket()
 
         created_ticket = self.env['helpdesk.ticket'].sudo().search([
             ('name', '=', "Testing Bot's Ticket")
@@ -25,42 +23,39 @@ class TestChatbotCreateTicket(HelpdeskChatbotCase):
 
         self.assertFalse(bool(created_ticket.team_id))
 
-    @users('user_portal')
     def test_chatbot_helpdesk_ticket_portal_user(self):
         """ Create a ticket from a portal user and check that information are correctly propagated. """
+        self.authenticate(self.user_portal.login, self.user_portal.login)
         self.step_helpdesk_create_ticket.write({'helpdesk_team_id': self.helpdesk_team.id})
-        self._chatbot_create_helpdesk_ticket(self.user_portal)
+        self._chatbot_create_helpdesk_ticket()
 
         created_ticket = self.env['helpdesk.ticket'].sudo().search([
             ('name', '=', "Testing Bot's Ticket")
         ], limit=1)
         # should use email defined on base partner since it's not empty
-        self.assertNotEqual(created_ticket.partner_email, 'helpme@example.com', "")
+        self.assertNotEqual(created_ticket.partner_email, "helpme@example.com")
         # phone however WAS empty -> check that it has been updated
         self.assertEqual(created_ticket.partner_phone, '+32499112233')
 
         self.assertEqual(created_ticket.team_id, self.helpdesk_team)
 
-    def _chatbot_create_helpdesk_ticket(self, user):
+    def _chatbot_create_helpdesk_ticket(self):
         data = self.make_jsonrpc_request("/im_livechat/get_session", {
             'anonymous_name': 'Test Visitor',
             'channel_id': self.livechat_channel.id,
             'chatbot_script_id': self.chatbot_script.id,
-            'user_id': user.id,
         })
         discuss_channel = self.env['discuss.channel'].sudo().browse(data["discuss.channel"][0]['id'])
 
         self._post_answer_and_trigger_next_step(
-            discuss_channel,
-            self.step_selection_ticket.name,
-            chatbot_script_answer=self.step_selection_ticket
+            discuss_channel, chatbot_script_answer=self.step_selection_ticket
         )
 
         self.assertEqual(discuss_channel.chatbot_current_step_id, self.step_helpdesk_issue)
         self._post_answer_and_trigger_next_step(discuss_channel, 'There is a problem with my printer.')
 
         self.assertEqual(discuss_channel.chatbot_current_step_id, self.step_helpdesk_email)
-        self._post_answer_and_trigger_next_step(discuss_channel, 'helpme@example.com')
+        self._post_answer_and_trigger_next_step(discuss_channel, email="helpme@example.com")
 
         self.assertEqual(discuss_channel.chatbot_current_step_id, self.step_helpdesk_phone)
         self._post_answer_and_trigger_next_step(discuss_channel, '+32499112233')
