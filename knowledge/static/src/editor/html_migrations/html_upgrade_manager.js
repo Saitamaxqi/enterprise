c0daec6d25b19d1bc1e0c29049790f35e0ff5603
Walid (wasa) <wasa@odoo.com>
2024-12-20 09:51:09 +0000

    [FIX] knowledge: convert self-closing tags to open and closed
    **Problem**:
    Similar to the issue addressed in [this commit](https://github.com/odoo/odoo/pull/191311/commits/9b159179a8d0ad85300bf31d3e1d67d7f1324b14), the `knowledge` module patches `HtmlField` and parses its content. Without this fix, self-closing tags are improperly handled when the `knowledge` module is installed.
    
    **Solution**:
    Apply the same fix to convert self-closing tags into open and closed tags to ensure proper handling in the `knowledge` module.
    
    opw-4394833
    
    closes odoo/enterprise#76304
    
    X-original-commit: 9b6bcc8acc89d33165f81350e186a65001250e15
    Related: odoo/odoo#192044
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- knowledge/static/src/editor/html_migrations/html_upgrade_manager.js
+++ knowledge/static/src/editor/html_migrations/html_upgrade_manager.js
@@ -5,6 +5,7 @@ import {
 } from "@knowledge/editor/html_migrations/manifest";
 import { registry } from "@web/core/registry";
 import { markup } from "@odoo/owl";
+import { fixInvalidHTML } from "@html_editor/utils/sanitize";
 
 export class HtmlUpgradeManager {
     constructor(env = {}) {
@@ -33,7 +34,7 @@ export class HtmlUpgradeManager {
         }
         this.originalValue = value;
         this.upgradedValue = value;
-        this.body = this.parser.parseFromString(value.toString(), "text/html").body;
+        this.body = this.parser.parseFromString(fixInvalidHTML(value.toString()), "text/html").body;
         const versionNode = this.body.querySelector(VERSION_SELECTOR);
         const version = versionNode?.dataset.oeVersion || "0.0";
         const VERSIONS = htmlEditorVersions();
