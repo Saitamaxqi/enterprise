c8674db426a6b8650c456dac36f2bfe66ee8cd09
Hesham Saleh <hsal@odoo.com>
2024-12-26 16:43:38 +0100

    [IMP] l10n_fr_fec_import: Added the option to import big FEC files
    This commit changes the import mechanism of FEC files to support importing big files.
    The FEC files are first processed and chunked at the client side.
    They are then sent one chunk at a time to the server side.
    The chunking ensures that no memory or time limit at the server side are ever exceeded.
    
    Because some of the processing is done in JS side and the rest in Py, the tests cover both sides.
    A tour was also added to test the integration between the two sides.
    The FEC test files in `fec_test_files` folder were named with the following convension:
    fec_[new-line character]_[fields delimiter]_[encoding]_[extra details about the file if any]
    The files with .txt or .csv extension are regular FEC files.
    The files with .json extension are their equivalent parsed files by the JS code in FecParser.
    The JSON files are used for output validation in JS tests, and they are used as input files for Py tests.
    
    task-3597066
    
    closes odoo/enterprise#76216
    
    Related: odoo/upgrade#7235
    Signed-off-by: William André (wan) <wan@odoo.com>

--- l10n_fr_fec_import/__manifest__.py
+++ l10n_fr_fec_import/__manifest__.py
@@ -38,7 +38,17 @@ https://github.com/DGFiP/Test-Compta-Demat
     'license': 'OEEL-1',
     'assets': {
         'web.assets_backend': [
+            'l10n_fr_fec_import/static/src/components/**/*',
+            'l10n_fr_fec_import/static/src/hooks/**/*',
+            'l10n_fr_fec_import/static/src/views/**/*',
             'l10n_fr_fec_import/static/src/xml/**/*',
+        ],
+        'web.assets_tests': [
+            'l10n_fr_fec_import/static/tests/tours/**/*',
+        ],
+        'web.assets_unit_tests': [
+            'l10n_fr_fec_import/static/tests/**/*',
+            ('remove', 'l10n_fr_fec_import/static/tests/tours/**/*'),
         ],
     },
 }
