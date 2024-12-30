1f4af1ab7549a87156b0975ca73bedde2e21505f
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2024-12-30 16:31:02 +0100

    [IMP] sale_timesheet_enterprise: allow timesheet managers to configure billing targets
    Before this commit:
    Currently, billing targets can only be viewed and edited on the employee form view by HR officers.
    This doesn't make sense as all users can see this information in the timesheets app.
    
    After this commit:
    Timesheet managers are able to edit this information even without having HR Officers via a new
    menu in Timesheet app.
    
    It was done via this menu instead of the employee public form because all fields are readonly and
    we cannot set an editable field.
    
    Billing rate target field was also moved to the 'Work Information' notebook under a dedicated
    'timesheets' section.
    
    task-4173890
    
    closes odoo/enterprise#70588
    
    Related: odoo/odoo#181230
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- sale_timesheet_enterprise/models/__init__.py
+++ sale_timesheet_enterprise/models/__init__.py
@@ -2,6 +2,7 @@
 
 from . import account_move_line
 from . import account_analytic_line
+from . import hr_employee_base
 from . import hr_employee
 from . import hr_timesheet_tip
 from . import project_project
@@ -9,3 +10,4 @@ from . import project_task
 from . import res_company
 from . import res_config_settings
 from . import sale_order_line
+from . import ir_ui_menu
