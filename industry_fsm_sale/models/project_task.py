a483c54149cf6c7ee452bf2ea4dc096192aa8a09
Julien Schwanen <scju@odoo.com>
2025-01-06 14:23:04 +0000

    [FIX] industry_fsm_sale: disable invoice copy in task copy
    Steps to reproduce:
    -----
    - Create a FSM task
    - Add products
    - Mark as done
    - Create invoice
    - Duplicate the task
    
    Issue:
    -----
    The newly created task is linked to the invoice of the duplicated task.
    This comportment is not intended.
    
    Fix:
    ----
    Creation of the copy method so the sale_order_id and the sale_line_id
    get set to false when duplicating a record.
    
    opw-4393596
    
    closes odoo/enterprise#78133
    
    X-original-commit: 05dd717b40285d0e5fdb5b8f9ec0806e3e9c55b7
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Julien Schwanen (scju) <scju@odoo.com>

--- industry_fsm_sale/models/project_task.py
+++ industry_fsm_sale/models/project_task.py
@@ -647,6 +647,16 @@ class ProjectTask(models.Model):
                 'timesheet_ids': [fields.Command.update(timesheet.id, {'so_line': sale_order_line.id}) for timesheet in not_billed_timesheets if not timesheet.is_so_line_edited]
             })
 
+    def copy_data(self, default=None):
+        vals_list = super().copy_data(default)
+        for task, vals in zip(self, vals_list):
+            if task.is_fsm:
+                vals.update({
+                    'sale_order_id': False,
+                    'sale_line_id': False,
+                })
+        return vals_list
+
     def _prepare_materials_delivery(self):
         # While industry_fsm_stock is not installed then we automatically deliver materials
         read_group_timesheets = self.env['account.analytic.line'].sudo().search_read([('task_id', 'in', self.ids), ('project_id', '!=', False), ('so_line', '!=', False)], ['so_line'])
--- industry_fsm_sale/tests/test_industry_fsm_sale_flow.py
+++ industry_fsm_sale/tests/test_industry_fsm_sale_flow.py
@@ -306,3 +306,53 @@ class TestFsmFlowSale(TestFsmFlowSaleCommon):
         self.assertEqual(so.invoice_status, 'invoiced')
         sol.price_unit = 0
         self.assertEqual(so.invoice_status, 'invoiced')
+
+    def test_copy_task_sale_fields(self):
+        normal_project = self.env['project.project'].create({
+            'name': 'Normal Project',
+            'allow_billable': True,
+            'allow_timesheets': True,
+            'company_id': self.env.company.id,
+        })
+        normal_task = self.env['project.task'].create({
+            'name': 'Original Task for Copy Test',
+            'project_id': normal_project.id,
+            'partner_id': self.partner_1.id,
+        })
+        
+        fsm_product = self.env['product.product'].create({
+            'name': 'Fsm Product',
+            'list_price': 100,
+        })
+        fsm_task = self.env['project.task'].create({
+            'name': 'Original Task for Copy Test',
+            'project_id': self.fsm_project.id,
+            'partner_id': self.partner_1.id,
+        })
+        
+        fsm_product.with_context({'fsm_task_id': fsm_task.id}).set_fsm_quantity(10.0)
+        fsm_product.with_context({'fsm_task_id': normal_task.id}).set_fsm_quantity(10.0)
+        
+        self.assertTrue(fsm_task.sale_order_id)
+        self.assertEqual(len(fsm_task.sale_order_id.order_line), 1)
+    
+        self.assertTrue(normal_task.sale_order_id)
+        self.assertEqual(len(normal_task.sale_order_id.order_line), 1)
+        
+        fsm_task.action_fsm_validate()
+        fsm_task.sale_order_id._create_invoices()
+        
+        normal_task.action_fsm_validate()
+        normal_task.sale_order_id._create_invoices()
+        
+        self.assertTrue(fsm_task.sale_order_id.invoice_ids)
+        self.assertTrue(normal_task.sale_order_id.invoice_ids)
+        
+        fsm_task.update(fsm_task.copy_data()[0])
+        normal_task.update(normal_task.copy_data()[0])
+
+        self.assertFalse(fsm_task.sale_order_id)
+        self.assertFalse(fsm_task.sale_order_id.invoice_ids)
+        
+        self.assertTrue(normal_task.sale_order_id)
+        self.assertTrue(normal_task.sale_order_id.invoice_ids)
