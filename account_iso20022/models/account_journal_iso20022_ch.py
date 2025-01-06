60267723acdcb97515f3869b564d518dfff2dd66
william-andre <wan@odoo.com>
2025-01-06 14:12:31 +0000

    [FIX] account_iso20022: provide the BIC when possible even in CH
    The fix previously done[^1] was always removing the BIC number because it
    is "optional". While it should be optional, some banks require it for
    payments done in EUR.
    The reason the previous fix was done was to avoid having the `Othr` tag,
    but that tag won't be used outside of methods returning `False`
    explicitly for `_skip_CdtrAgt`.
    
    opw-4423834
    
    [^1]: 1a221f2f3160a48dc7ba08ea59c6781071b1adf3
    
    closes odoo/enterprise#76822
    
    X-original-commit: 2f906896981bec71a05fb76af08c8ae4fd5a0cd1
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- account_iso20022/models/account_journal_iso20022_ch.py
+++ account_iso20022/models/account_journal_iso20022_ch.py
@@ -10,12 +10,6 @@ class AccountJournal(models.Model):
             return 'http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd'
         return super().get_document_namespace(payment_method_code)
 
-    def _skip_CdtrAgt(self, partner_bank, payment_method_code):
-        # Creditor Agent can be omitted with IBAN and QR-IBAN accounts
-        if payment_method_code == 'iso20022_ch' and self._is_qr_iban({'partner_bank_id': partner_bank.id, 'journal_id': self.id}):
-            return True
-        return super()._skip_CdtrAgt(partner_bank, payment_method_code)
-
     def _get_Dbtr(self, payment_method_code):
         Dbtr = super()._get_Dbtr(payment_method_code)
         if payment_method_code == 'iso20022_ch':
