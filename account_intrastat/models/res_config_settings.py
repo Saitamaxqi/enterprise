98aa8caa2e4dfb87bae32cabdae4eb35bf74707d
Seif (segn) <segn@odoo.com>
2025-01-06 09:48:43 +0000

    [FIX] account_intrastat: Apply transaction code domain in default settings
    Starting from saas-17.4, the default transaction code in settings shows the commidity code instead of transaction codes.
    This commit applies the domain for showing transaction codes instead.
    
    opw-4419718
    
    closes odoo/enterprise#76766
    
    X-original-commit: dd7e501932cfd6fe72112704029965450f605a9d
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Seif Gneedy (segn) <segn@odoo.com>

--- account_intrastat/models/res_config_settings.py
+++ account_intrastat/models/res_config_settings.py
@@ -7,6 +7,12 @@ from odoo import fields, models, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
+    _transaction_code_domain = '''
+        [('type', '=', 'transaction'),
+        '|', ('expiry_date', '>', context_today().strftime('%Y-%m-%d')), ('expiry_date', '=', None),
+        '|', ('start_date', '<', context_today().strftime('%Y-%m-%d')), ('start_date', '=', None)]
+    '''
+
     company_country_id = fields.Many2one(
         'res.country', string="Company country", readonly=True,
         related='company_id.account_fiscal_country_id'
@@ -15,12 +21,14 @@ class ResConfigSettings(models.TransientModel):
         'account.intrastat.code',
         string='Default invoice transaction code',
         related='company_id.intrastat_default_invoice_transaction_code_id',
+        domain=_transaction_code_domain,
         readonly=False,
     )
     intrastat_default_refund_transaction_code_id = fields.Many2one(
         'account.intrastat.code',
         string='Default refund transaction code',
         related='company_id.intrastat_default_refund_transaction_code_id',
+        domain=_transaction_code_domain,
         readonly=False,
     )
     intrastat_region_id = fields.Many2one(
