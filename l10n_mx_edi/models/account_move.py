78878f79be895cc0575dfbd63c9c0975ccd876bb
Mathieu Coutant <mcou@odoo.com>
2025-01-06 09:58:30 +0000

    [FIX] l10n_mx_edi: fix the "Request Cancel" button on payments
    Steps to reproduce:
    - Install "l10n_mx" and switch to a Mxican company
    - Create an invoice with a Mexican partner and confirm
    - Create a Payment
    - In the invoice form view got to the "CFDI" page
    - Click "Update CFDI"
    - The payment appears, click on "Show"
    - The button "Request Cancel" does nothing
    
    Cause:
    the method `button_request_cancel` on move is called from the payment model but does not return anything:
    ```
    def button_request_cancel(self):
        self.move_id.button_request_cancel()
    ```
    But the `button_request_cancel` from `l10n_mx_edi` is returning a wizard that is never catched.
    
    Solution:
    When clicking the "Request cancel" button, the method `action_request_cancel` is called instead of `action_cancel` which will dispatch the request depending on the type of move being cancelled.
    
    opw-4332483
    
    closes odoo/enterprise#76805
    
    X-original-commit: b429e4da99edc73b5cf451c05dba120dde5b3895
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Mathieu Coutant (mcou) <mcou@odoo.com>

--- l10n_mx_edi/models/account_move.py
+++ l10n_mx_edi/models/account_move.py
@@ -862,11 +862,8 @@ class AccountMove(models.Model):
                 x.attachment_uuid == self.l10n_mx_edi_cfdi_uuid
                 and x.state in ('invoice_sent', 'payment_sent')
             ))[0]
-            if doc.state == 'invoice_sent':
-                return doc.action_request_cancel()
-            else:
-                return doc.action_request_cancel_payment()
-
+            return doc.action_cancel()
+        
         return super().button_request_cancel()
 
     def _reverse_moves(self, default_values_list=None, cancel=False):
