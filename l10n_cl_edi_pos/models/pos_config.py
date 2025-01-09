367ed231f9ada73793c6cdfe8b8ea0891f5c2a0b
David Monnom <moda@odoo.com>
2025-01-09 14:15:59 +0100

    [IMP] l10n_cl_edi_pos, l10n_ec_edi_pos: partner list infinite scroll
    Load new partner when scroll reach the bottom of the list
    
    closes odoo/enterprise#76823
    
    Taskid: 4028885
    Related: odoo/odoo#192602
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- l10n_cl_edi_pos/models/pos_config.py
+++ l10n_cl_edi_pos/models/pos_config.py
@@ -18,8 +18,8 @@ class PosConfig(models.Model):
                 raise UserError(_("You have to set a country in your company setting."))
         return super().open_ui()
 
-    def get_limited_partners_loading(self):
-        partner_ids = super().get_limited_partners_loading()
+    def get_limited_partners_loading(self, offset=0):
+        partner_ids = super().get_limited_partners_loading(offset)
         if (self.env.ref('l10n_cl.par_cfa').id,) not in partner_ids:
             partner_ids.append((self.env.ref('l10n_cl.par_cfa').id,))
         return partner_ids
