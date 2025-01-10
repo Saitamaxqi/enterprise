adcc2d05b7097faaf0a034b794fb3f0c03891afe
asno-odoo <asno@odoo.com>
2025-01-10 12:55:34 +0000

    [FIX] sale_subscription_stock: check dates null
    The fields start_date and next_invoice_date of sale.order can be
    null. We only want to compare to dates. So its better to check that
    the dates are not null before comparing them.
    
    closes odoo/enterprise#77309
    
    X-original-commit: 2e82dfe164c2687fe560530b29b8a830292ed580
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>
    Signed-off-by: Aslam Noman (asno) <asno@odoo.com>

--- sale_subscription_stock/models/sale_order.py
+++ sale_subscription_stock/models/sale_order.py
@@ -18,7 +18,7 @@ class SaleOrder(models.Model):
     def _compute_recurring_stock_products(self):
         self.display_recurring_stock_delivery_warning = False
         for order in self:
-            if order.state == 'sale' and order.is_subscription and order.next_invoice_date <= order.start_date:
+            if order.state == 'sale' and order.is_subscription and order.next_invoice_date and order.start_date and order.next_invoice_date <= order.start_date:
                 has_stock_sub_lines = bool(len(order.order_line._get_stock_subscription_lines()))
                 order.display_recurring_stock_delivery_warning = has_stock_sub_lines
 
