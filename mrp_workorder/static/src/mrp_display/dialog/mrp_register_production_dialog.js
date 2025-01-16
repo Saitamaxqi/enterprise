e2dd0f280c292b62a9548b72fd2c070c0afd8f07
Lancelot Semal <lase@odoo.com>
2025-01-16 12:59:06 +0000

    [FIX] quality_mrp_workorder: allow finished lot production via quality checks
    Steps to reproduce:
    
    - Create a storable product FP tracked by SN with a BOM:
        - 1 Operation: - instruction: Register Production type (per product)
        - 1 x Comp (storable product in stock)
    - Create and confirm an MO for one unit of FP.
    - Go to the shopfoor app > open the instruction of your operation
    - Type a non-existing serial number in the search bar > Create "new_sn"
    > This opens a form view of the stock.lot model
    - Save and close the new stock.lot
    > Invalid operation: You are not allowed to create or edit a lot or
    > serial number for the components with the operation type
    > "Manufacturing". To change this, go on the operation type and tick the
    > box "Create New Lots/Serial Numbers for Components"
    
    Expected behavior:
    
    The invalid operation should not be raised as the "required" option
    concerns only componenets and not final products.
    
    Cause of the issue:
    
    Creating a stock lot will first launch a call of the `_check_create`:
    https://github.com/odoo/odoo/blob/c25d71f14fa168807f07cd88bed805d15db8add5/addons/stock/models/stock_lot.py#L150-L153
    However, since this check does not verifies that the product for which
    we create the lot is indeed related to a component of the MO it will
    raise the invalid operation even for final products:
    https://github.com/odoo/odoo/blob/c25d71f14fa168807f07cd88bed805d15db8add5/addons/mrp/models/stock_lot.py#L11-L17
    
    Issue 2:
    
    Currently, the `quickCreate` ("Create *sn_name*") option of the lot
    field of te "register production" dialog calls a `name_create` that will
    set an error message in the logs as the product_id required field can
    not be provided neither in the vals or the context of the `name_create`.
    You then fall back on the same form dialog than the "Create and edit".
    The quickCreate option should therefore just be disabled.
    
    opw-4452747
    
    closes odoo/enterprise#80973
    
    X-original-commit: 48b55d846a1093c66d650e3b47c0a2a16304169d
    Related: odoo/odoo#200771
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

--- mrp_workorder/static/src/mrp_display/dialog/mrp_register_production_dialog.js
+++ mrp_workorder/static/src/mrp_display/dialog/mrp_register_production_dialog.js
@@ -52,6 +52,7 @@ export class MrpRegisterProductionDialog extends MrpQualityCheckConfirmationDial
         return {
             name: "lot_producing_id",
             record: this.props.record,
+            canQuickCreate: false,
             context: {
                 default_product_id: this.recordData.product_id[0],
                 default_company_id: this.recordData.company_id[0],
