291238ab44a0ff748fb074fb85c0f7380bdaa1e8
Anh Thao Pham (pta) <pta@odoo.com>
2025-01-21 08:28:35 +0000

    [FIX] account_asset: allow to create asset models
    Steps to reproduce:
    - Install Accounting
    - Go to "Accounting / Configuration / Management / Asset Models"
    - Create an asset model
    
    Issue:
    Upon save, the following UserError is raised:
    "You cannot add or remove bills when the asset is already running
    or closed."
    
    Cause:
    A check has been added to prevent adding bills to a running asset.
    The check is excluding draft assets but not asset models that are
    assets in "model" state.
    
    Issue:
    Exclude asset models from the check.
    
    opw-4479698
    
    closes odoo/enterprise#77662
    
    X-original-commit: fe5b4fca2f2d778838a67565b20d2d513161ca8b
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- account_asset/models/account_asset.py
+++ account_asset/models/account_asset.py
@@ -462,7 +462,7 @@ class AccountAsset(models.Model):
         for asset in self:
             if asset.original_move_line_ids and asset.related_purchase_value == 0:
                 raise UserError(_("You cannot create an asset from lines containing credit and debit on the account or with a null amount"))
-            if asset.state != 'draft':
+            if asset.state not in ('model', 'draft'):
                 raise UserError(_("You cannot add or remove bills when the asset is already running or closed."))
 
     # -------------------------------------------------------------------------
