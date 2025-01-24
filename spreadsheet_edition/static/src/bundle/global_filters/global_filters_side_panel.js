d1cdd6eb8406676309eeb60cdadbe7a2e58af5d0
dhrp-odoo <dhrp@odoo.com>
2025-01-24 17:27:13 +0530

    [FIX] drag_and_drop_hook: Rename 'containerEl' to 'scrollableContainerEl'
    Renamed `containerEl` to `scrollableContainerEl` for better clarity.
    
    Task: 3817565
    Part-of: odoo/enterprise#78984
    Related: odoo/odoo#197153
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- spreadsheet_edition/static/src/bundle/global_filters/global_filters_side_panel.js
+++ spreadsheet_edition/static/src/bundle/global_filters/global_filters_side_panel.js
@@ -91,7 +91,7 @@ export class GlobalFiltersSidePanel extends Component {
             draggedItemId: filter.id,
             initialMousePosition: event.clientY,
             items: filtersItems,
-            containerEl: this.filtersListRef.el,
+            scrollableContainerEl: this.filtersListRef.el,
             onDragEnd: (filterId, finalIndex) => this.onDragEnd(filterId, finalIndex),
         });
     }
