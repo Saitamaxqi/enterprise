e1cf459531f62dba82061d76737aea3617bb3c86
igbe <igbe@odoo.com>
2025-01-27 13:31:19 +0000

    [IMP] l10n_fr_intrastat: new xml export for services
    This commit add a new xml export (DES) for intrastat report services. Regarding the legal specifications in the linked task.
    
    task-4402569
    
    closes odoo/enterprise#81131
    
    X-original-commit: 5c36363cbff439f02316637cf12c72b2e9c924a6
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Igor Bertrand (igbe) <igbe@odoo.com>

--- l10n_fr_intrastat/tests/test_fr_intrastat_report.py
+++ l10n_fr_intrastat/tests/test_fr_intrastat_report.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import Command
 from odoo.tests import tagged
 from odoo.addons.account_reports.tests.common import TestAccountReportsCommon
 from odoo.addons.account_reports.models.account_report import AccountReportFileDownloadException
@@ -21,8 +22,10 @@ class TestFRIntrastatReport(TestAccountReportsCommon):
             'l10n_fr_intrastat_envelope_id': 'D090',
             'intrastat_region_id': cls.env.ref('l10n_fr_intrastat.intrastat_region_01').id,
         })
-        cls.report = cls.env.ref('account_intrastat.intrastat_report')
-        cls.report_handler = cls.env['account.intrastat.goods.report.handler']
+        cls.report_goods = cls.env.ref('account_intrastat.intrastat_report')
+        cls.report_services = cls.env.ref('account_intrastat.intrastat_report_services')
+        cls.report_handler_goods = cls.env['account.intrastat.goods.report.handler']
+        cls.report_handler_services = cls.env['account.intrastat.services.report.handler']
         cls.partner_a = cls.env['res.partner'].create({
             'name': "Miskatonic University",
             'country_id': italy.id,
@@ -40,6 +43,13 @@ class TestFRIntrastatReport(TestAccountReportsCommon):
             'name': 'Interesting Antarctic Rock and Soil Specimens',
             'intrastat_code_id': cls.env.ref('account_intrastat.commodity_code_2023_25309050').id,
             'weight': 19,
+            'intrastat_origin_country_id': italy.id,
+        })
+        cls.product_service = cls.env['product.product'].create({
+            'name': 'Personal service',
+            'intrastat_code_id': cls.env.ref('account_intrastat.commodity_code_2023_25309050').id,
+            'standard_price': 200.0,
+            'type': 'service',
             'intrastat_origin_country_id': italy.id,
         })
         cls.inwards_vendor_bill = cls.env['account.move'].create({
@@ -92,50 +102,79 @@ class TestFRIntrastatReport(TestAccountReportsCommon):
             })],
         })
 
+        cls.invoice_with_service = cls.env['account.move'].create({
+            'move_type': 'out_invoice',
+            'partner_id': cls.partner_a.id,
+            'invoice_date': '2023-10-25',
+            'date': '2023-10-25',
+            'intrastat_country_id': italy.id,
+            'intrastat_transport_mode_id': cls.env.ref('account_intrastat.account_intrastat_transport_1').id,
+            'company_id': cls.company_data['company'].id,
+            'invoice_line_ids': [
+                Command.create({
+                    'product_uom_id': cls.env.ref('uom.product_uom_unit').id,
+                    'product_id': cls.product_service.id,
+                    'intrastat_transaction_id': cls.env.ref('account_intrastat.account_intrastat_transaction_11').id,
+                    'quantity': 2,
+                    'price_unit': 500,
+                }),
+                Command.create({
+                    'product_uom_id': cls.env.ref('uom.product_uom_unit').id,
+                    'product_id': cls.product_service.id,
+                    'intrastat_transaction_id': cls.env.ref('account_intrastat.account_intrastat_transaction_11').id,
+                    'quantity': 4,
+                    'price_unit': 300,
+                })],
+        })
+
         cls.inwards_vendor_bill.action_post()
         cls.outwards_customer_invoice.action_post()
         cls.outwards_customer_invoice2.action_post()
+        cls.invoice_with_service.action_post()
 
-    def _generate_options(self, wizard_options):
+    def _generate_options(self, report, wizard_options=None):
         # EXTENDS account_intrastat
-        options = super()._generate_options(self.report, date_from='2023-10-01', date_to='2023-10-31')
-        wizard = self.env['l10n_fr_intrastat.export.wizard'].create(wizard_options)
-        options['l10n_fr_intrastat_wizard_id'] = wizard.id
-        arrivals, dispatches = options['intrastat_type']
-        arrivals['selected'], dispatches['selected'] = arrivals, dispatches
+        options = super()._generate_options(report, date_from='2023-10-01', date_to='2023-10-31')
+        if wizard_options:
+            wizard = self.env['l10n_fr_intrastat.export.wizard'].create(wizard_options)
+            options['l10n_fr_intrastat_wizard_id'] = wizard.id
+            arrivals, dispatches = options['intrastat_type']
+            arrivals['selected'], dispatches['selected'] = arrivals, dispatches
         return options
 
-    def _l10n_fr_intrastat_generate_report(self, options):
+    def _l10n_fr_intrastat_generate_report_xml(self, options, report_handler):
         with freeze_time('2023-11-01 10:00:00'):
-            return self.report_handler.l10n_fr_intrastat_export_to_xml(options)
+            if report_handler == self.report_handler_services:
+                return report_handler.l10n_fr_intrastat_services_export_to_xml(options)
+            return report_handler.l10n_fr_intrastat_export_to_xml(options)
 
     def test_fr_intrastat_export_both_export_types_and_flows(self):
         """ Test generating an XML export when the export type contains both documents (statistical survey and
         vat summary statement) and both export flow for the EMEBI export (arrivals and dispatches)"""
-        options = self._generate_options({
+        options = self._generate_options(self.report_goods, {
             'export_type': 'statistical_survey_and_vat_summary_statement',
             'emebi_flow': 'arrivals_and_dispatches',
         })
-        report = self._l10n_fr_intrastat_generate_report(options)
+        report = self._l10n_fr_intrastat_generate_report_xml(options, self.report_handler_goods)
         self._report_compare_with_test_file(report, 'both_types_flows.xml')
 
     def test_fr_intrastat_export_statistical_survey_with_arrival_emebi(self):
         """ Test generating an XML export when the export type contains only the statistical survey and the export
         flow for the EMEBI export is only arrivals"""
-        options = self._generate_options({
+        options = self._generate_options(self.report_goods, {
             'export_type': 'statistical_survey',
             'emebi_flow': 'arrivals',
         })
-        report = self._l10n_fr_intrastat_generate_report(options)
+        report = self._l10n_fr_intrastat_generate_report_xml(options, self.report_handler_goods)
         self._report_compare_with_test_file(report, 'survey_arrival_emebi.xml')
 
     def test_fr_intrastat_export_vat_summary_stmt(self):
         """ Test generating an XML export when the export type contains only the vat summary statement"""
-        options = self._generate_options({
+        options = self._generate_options(self.report_goods, {
             'export_type': 'vat_summary_statement',
             'emebi_flow': 'arrivals_and_dispatches',  # Does not matter in case of vat_summary_statement
         })
-        report = self._l10n_fr_intrastat_generate_report(options)
+        report = self._l10n_fr_intrastat_generate_report_xml(options, self.report_handler_goods)
         self._report_compare_with_test_file(report, 'vat_summary_stmt.xml')
 
     def test_fr_intrastat_export_errors(self):
@@ -150,13 +189,13 @@ class TestFRIntrastatReport(TestAccountReportsCommon):
             'company_registry': False,
             'intrastat_region_id': False,
         })
-        options = self._generate_options({
+        options = self._generate_options(self.report_goods, {
             'export_type': 'statistical_survey_and_vat_summary_statement',
             'emebi_flow': 'arrivals_and_dispatches',
         })
 
         try:
-            self._l10n_fr_intrastat_generate_report(options)
+            self._l10n_fr_intrastat_generate_report_xml(options, self.report_handler_goods)
         except AccountReportFileDownloadException as arfde:
             self.assertEqual(set(arfde.errors.keys()), {
                 'company_vat_or_siret_missing',
@@ -169,3 +208,9 @@ class TestFRIntrastatReport(TestAccountReportsCommon):
             for error in arfde.errors.values():
                 self.assertEqual({'action', 'action_text', 'message'}, set(error.keys()))
                 self.assertTrue('name' in error['action'])
+
+    def test_fr_intrastat_export_report_for_services(self):
+        """ Test generating an XML export for services intrastat """
+        options = self._generate_options(self.report_services)
+        report_xml = self._l10n_fr_intrastat_generate_report_xml(options, self.report_handler_services)
+        self._report_compare_with_test_file(report_xml, 'services_export.xml')
