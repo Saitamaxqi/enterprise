9508beb8a787c38c95649d65eee7cb5bee41dd38
Romaric MOYEUVRE <romo@odoo.com>
2025-01-28 14:21:40 +0100

    [REF] website_appointment_sale: convert public widget to interaction
    closes odoo/enterprise#77995
    
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- website_appointment_sale/static/src/interactions/appointment_sale_confirmation.js
+++ website_appointment_sale/static/src/interactions/appointment_sale_confirmation.js
@@ -1,27 +1,30 @@
-import publicWidget from "@web/legacy/js/public/public_widget";
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+
 import { user } from "@web/core/user";
 
-publicWidget.registry.appointmentSaleConfirmation = publicWidget.Widget.extend({
-    selector: '.o_wappointment_sale_confirmation_card',
+export class AppointmentSaleConfirmation extends Interaction {
+    static selector = ".o_wappointment_sale_confirmation_card";
 
     /**
      * Store in local storage the appointment booked for the appointment type.
      * This value is used later to display information on the upcoming appointment
      * if an appointment is already taken. If the user is logged don't store anything
      * as everything is computed by the /appointment/get_upcoming_appointments route.
-     * @override
      */
-    start: function() {
-        return this._super(...arguments).then(() => { 
-            if (user.userId) {
-                return;
-            }
-            const eventAccessToken = this.el.dataset.eventAccessToken;
-            const allAppointmentsToken = JSON.parse(localStorage.getItem('appointment.upcoming_events_access_token')) || [];
-            if (eventAccessToken && !allAppointmentsToken.includes(eventAccessToken)) {
-                allAppointmentsToken.push(eventAccessToken);
-                localStorage.setItem('appointment.upcoming_events_access_token', JSON.stringify(allAppointmentsToken));
-            }
-        });
-    },
-});
+    setup() {
+        if (user.userId) {
+            return;
+        }
+        const eventAccessToken = this.el.dataset.eventAccessToken;
+        const allAppointmentsToken = JSON.parse(localStorage.getItem("appointment.upcoming_events_access_token")) || [];
+        if (eventAccessToken && !allAppointmentsToken.includes(eventAccessToken)) {
+            allAppointmentsToken.push(eventAccessToken);
+            localStorage.setItem("appointment.upcoming_events_access_token", JSON.stringify(allAppointmentsToken));
+        }
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("website_appointment_sale.appointment_sale_confirmation", AppointmentSaleConfirmation);
