8e7d2b9dc32d5e4efc65cf73ddefa7318aaa3a45
Rahul Prajapati <rapr@odoo.com>
2025-01-29 14:26:39 +0100

    [IMP] room: convert QUnit tests to hoot
    This commit aims to convert QUnit tests to hoot.
    
    Part of Task-3818666
    
    closes odoo/enterprise#75354
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- room/__manifest__.py
+++ room/__manifest__.py
@@ -29,11 +29,10 @@
         'web.assets_backend_lazy': [
             'room/static/src/room_booking_gantt_view/**/*',
         ],
-        'web.qunit_suite_tests': [
-            'room/static/tests/*.js',
-            'room/static/src/room_booking/**/*.js',
-            'room/static/src/room_booking/**/*.xml',
-            'room/static/tests/room_booking_view_patch.xml',
+        "web.assets_unit_tests": [
+            "room/static/src/room_booking/**/*.js",
+            "room/static/src/room_booking/**/*.xml",
+            "room/static/tests/**/*",
         ],
         'room.assets_room_booking': [
             # 1 Define room variables (takes priority)
