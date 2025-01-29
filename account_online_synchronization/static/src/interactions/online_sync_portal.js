158681cb361a5908c8b7f50d98914351abbffb39
Romaric MOYEUVRE <romo@odoo.com>
2025-01-29 11:44:57 +0100

    [REF] account_online_synchronization: convert public widget to interaction
    closes odoo/enterprise#78091
    
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- account_online_synchronization/static/src/interactions/online_sync_portal.js
+++ account_online_synchronization/static/src/interactions/online_sync_portal.js
@@ -1,55 +1,57 @@
-    import publicWidget from "@web/legacy/js/public/public_widget";
-    import { loadJS } from "@web/core/assets";
-    /* global OdooFin */
+/* global OdooFin */
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
 
-    publicWidget.registry.OnlineSyncPortal = publicWidget.Widget.extend({
-        selector: '.oe_online_sync',
-        events: Object.assign({}, {
-            'click #renew_consent_button': '_onRenewConsent',
-        }),
+import { loadJS } from "@web/core/assets";
+import { post } from "@web/core/network/http_service";
 
-        OdooFinConnector: function (parent, action) {
-            // Ensure that the proxyMode is valid
-            const modeRegexp = /^[a-z0-9-_]+$/i;
-            if (!modeRegexp.test(action.params.proxyMode)) {
-                return;
-            }
-            const url = 'https://' + action.params.proxyMode + '.odoofin.com/proxy/v1/odoofin_link';
-
-            loadJS(url)
-                .then(() => {
-                    // Create and open the iframe
-                    const params = {
-                        data: action.params,
-                        proxyMode: action.params.proxyMode,
-                        onEvent: function (event, data) {
-                            switch (event) {
-                                case 'success':
-                                    const processUrl = window.location.pathname + '/complete' + window.location.search;
-                                    $('.js_reconnect').toggleClass('d-none');
-                                    $.post(processUrl, {csrf_token: odoo.csrf_token});
-                                default:
-                                    return;
-                            }
-                        },
-                    };
-                    OdooFin.create(params);
-                    OdooFin.open();
-                });
-            return;
+export class OnlineSyncPortal extends Interaction {
+    static selector = ".oe_online_sync #renew_consent_button";
+    dynamicContent = {
+        _root: {
+            "t-on-click.prevent.withTarget": this.onRenewConsentClick,
         },
-
-        /**
-         * @private
-         * @param {Event} ev
-         */
-        _onRenewConsent: async function (ev) {
-            ev.preventDefault();
-            const action = JSON.parse($(ev.currentTarget).attr('iframe-params'));
-            return this.OdooFinConnector(this, action);
-        },
-    });
-
-    export default {
-        OnlineSyncPortal: publicWidget.registry.OnlineSyncPortal,
     };
+
+    async OdooFinConnector(action) {
+        // Ensure that the proxyMode is valid
+        const modeRegexp = /^[a-z0-9-_]+$/i;
+        if (!modeRegexp.test(action.params.proxyMode)) {
+            return;
+        }
+
+        await loadJS(`https://${action.params.proxyMode}.odoofin.com/proxy/v1/odoofin_link`);
+
+        // Create and open the iframe
+        const params = {
+            data: action.params,
+            proxyMode: action.params.proxyMode,
+            onEvent: function (event, data) {
+                if (event === "success") {
+                    const processUrl = `${window.location.pathname}/complete${window.location.search}`;
+                    const reconnectEls = document.querySelectorAll(".js_reconnect");
+                    for (const reconnectEl of reconnectEls) {
+                        reconnectEl.classList.toggle("d-none");
+                    }
+                    post(processUrl, { csrf_token: odoo.csrf_token });
+                }
+            },
+        };
+
+        OdooFin.create(params);
+        OdooFin.open();
+    }
+
+    /**
+     * @param {PointerEvent} ev
+     * @param {HTMLElement} currentTargetEl
+     */
+    onRenewConsentClick(ev, currentTargetEl) {
+        const action = JSON.parse(currentTargetEl.getAttribute("iframe-params"));
+        this.OdooFinConnector(action);
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("account_online_synchronization.online_sync_portal", OnlineSyncPortal);
