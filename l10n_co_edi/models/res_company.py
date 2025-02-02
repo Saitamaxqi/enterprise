699b3b028cda885c7b96973fc4cf111b0c29d5f9
kcv-odoo <kcv@odoo.com>
2025-02-02 15:11:28 +0530

    [FIX] l10n_co_edi: fix string of username and password fields
    This commit give proper localisation string to electronic invoice
    username and password field to avoid conflicting string with
    other username and password related field added on company model
    in python and set specific string in xml for UX.
    
    task-3628329
    
    Part-of: odoo/enterprise#75472
    Related: odoo/odoo#190312
    Related: odoo/upgrade#7000
    Signed-off-by: Kartik Chavda (kcv) <kcv@odoo.com>

--- l10n_co_edi/models/res_company.py
+++ l10n_co_edi/models/res_company.py
@@ -11,8 +11,8 @@ TEMPLATE_CODE = [
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    l10n_co_edi_username = fields.Char(string='Username', groups='base.group_system')
-    l10n_co_edi_password = fields.Char(string='Password', groups='base.group_system')
+    l10n_co_edi_username = fields.Char(string="Carvajal Username", groups='base.group_system')
+    l10n_co_edi_password = fields.Char(string="Carvajal Password", groups='base.group_system')
     l10n_co_edi_company = fields.Char(string='Company Registry')
     l10n_co_edi_account = fields.Char(string='Account ID', groups='base.group_system')
     l10n_co_edi_test_mode = fields.Boolean(string='Test mode', default=True)
