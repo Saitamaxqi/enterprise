9cd37d131c2c72a2562b9564704364d147ac1db8
Rémy Voet (ryv) <ryv@odoo.com>
2025-02-04 08:29:42 +0100

    [FIX] core: create None value in cache
    Some query count can increase since with the change done in _create, we mark less fields as dirty. It can generate extra query since the _write_multi is batched by fields set changed (counterintuitive).
    
    closes odoo/enterprise#78483
    
    Related: odoo/odoo#195454
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- hr_payroll/tests/test_performance.py
+++ hr_payroll/tests/test_performance.py
@@ -37,7 +37,7 @@ class TestPayrollPerformance(TestPayslipBase):
     @warmup
     def test_performance_work_entry_generation(self):
         """ Work entry generation """
-        with self.assertQueryCount(__system__=15, admin=17):
+        with self.assertQueryCount(__system__=16, admin=18):
             self.employees.generate_work_entries(date(2018, 1, 1), date(2018, 1, 2))
         self.reset_work_entries()
 
