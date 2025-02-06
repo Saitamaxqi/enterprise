6d18782c93e06c463a9b33e627971bea13ae5dda
std-odoo <std@odoo.com>
2025-02-06 08:32:43 +0100

    [IMP] documents(_hr): simplify the `document_ids` field on `res.users`
    Purpose
    =======
    `res.users` inherits from `res.partner`, and it's therefor not needed
    to define the fields 2 times.
    
    The field `document_ids` can be a simple o2m (with a related on
    `res.users`), no need a computed field for that.
    
    Task-4550993
    
    closes odoo/enterprise#78629
    
    Signed-off-by: Florian Charlier (flch) <flch@odoo.com>

--- documents/models/res_partner.py
+++ documents/models/res_partner.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, _
-from odoo.osv import expression
+from odoo import models, fields
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+    document_ids = fields.One2many('documents.document', 'partner_id', string='Documents')
     document_count = fields.Integer('Document Count', compute='_compute_document_count')
 
     def _compute_document_count(self):
-        read_group_var = self.env['documents.document']._read_group(
-            expression.AND([
-                [('partner_id', 'in', self.ids)],
-                [('type', '!=', 'folder')],
-            ]),
+        document_count_dict = dict(self.env['documents.document']._read_group(
+            [('partner_id', 'in', self.ids)],
             groupby=['partner_id'],
-            aggregates=['__count'])
+            aggregates=['__count']))
 
-        document_count_dict = {partner.id: count for partner, count in read_group_var}
         for record in self:
-            record.document_count = document_count_dict.get(record.id, 0)
+            record.document_count = document_count_dict.get(record, 0)
 
     def action_see_documents(self):
         self.ensure_one()
