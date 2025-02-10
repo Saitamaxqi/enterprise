996f1f5f085423d07440d61ac80be350e463e945
Mélanie <mepe@odoo.com>
2025-02-10 11:49:10 +0100

    [FIX] l10n_be_hr_payroll: Fix 274 declaration code
    STEP TO REPRODUCE:
    1- Install The belgium payroll
    2- Go to Payroll
    3- Go to reporting > Declaration 274.XX
    4- Click on generate xml file
    
    closes odoo/enterprise#78976
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- l10n_be_hr_payroll/models/l10n_be_274_XX.py
+++ l10n_be_hr_payroll/models/l10n_be_274_XX.py
@@ -266,16 +266,16 @@ class L10n_Be274_Xx(models.Model):
             raise UserError(_('There is no valid payslip to declare.'))
 
         year_period_code = {
-            2022: '6',
-            2023: '7',
-            2024: '8',
-            2021: '9',
+            2: '6',
+            3: '7',
+            0: '8',
+            1: '9',
         }
         reference_number = payslips[0].company_id.l10n_be_company_number
         # payment reference - 12 Characters
         first_10_characters = "%s%s%s" % (
             reference_number[1:8], # 1 - 7
-            year_period_code[payslips[0].date_from.year],  # 8
+            year_period_code[payslips[0].date_from.year % 4],  # 8
             str(payslips[0].date_from.month).zfill(2), # 9-10
         )
         payment_reference = "%s%s" % (
