6f5a6c022bcadbc7a94544c108abcb85f7a9330c
Julien Carion (juca) <juca@odoo.com>
2025-02-13 14:20:16 +0100

    [FIX] pos_restaurant_appointment: dark mode assets in gantt
    This commit fixes an asset issue with web_gantt where the dark mode
    specific assets from web_gantt were included in the main (light mode)
    assets from pos_restaurant_appointment.
    
    Part of task-4571228
    
    closes odoo/enterprise#79251
    
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- pos_restaurant_appointment/__manifest__.py
+++ pos_restaurant_appointment/__manifest__.py
@@ -26,6 +26,10 @@
             'appointment/static/src/views/gantt/**/*',
             'calendar/static/src/views/widgets/**/*',
             'calendar/static/src/views/calendar_form/**/*',
+            ('remove', 'web_gantt/static/src/**/*.dark.scss'),
+        ],
+        'point_of_sale.assets_prod_dark': [
+            'web_gantt/static/src/**/*.dark.scss',
         ],
     'web.assets_tests': [
             'pos_restaurant_appointment/static/tests/tours/**/*',
