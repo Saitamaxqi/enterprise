ddb35da4c7e82e00a58bb18692a2adef2e402fbd
aath-odoo <aath@odoo.com>
2025-02-13 05:14:34 +0000

    [FIX] knowledge: incorrect sidebar resizing computation in rtl mode
    Previously, in RTL mode, sidebar resizing calculations were incorrect, causing
    the sidebar to not follow the mouse properly resulted in incorrect resizing.
    This fix ensures accurate resizing behavior.
    
    Task-4571290
    
    closes odoo/enterprise#79762
    
    X-original-commit: e0396172bfca577ec5673596a95fa88d100aee67
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>

--- knowledge/static/src/components/sidebar/sidebar.js
+++ knowledge/static/src/components/sidebar/sidebar.js
@@ -8,6 +8,7 @@ import {
     KnowledgeSidebarSharedSection,
     KnowledgeSidebarWorkspaceSection
 } from "./sidebar_section";
+import { localization } from "@web/core/l10n/localization";
 import { user } from "@web/core/user";
 import { throttleForAnimation } from "@web/core/utils/timing";
 import { useNestedSortable } from "@web/core/utils/nested_sortable";
@@ -866,9 +867,10 @@ export class KnowledgeSidebar extends Component {
      * Resize the sidebar horizontally.
      */
     resize() {
+        const isRtl = localization.direction === "rtl";
         const onPointerMove = throttleForAnimation(event => {
             event.preventDefault();
-            this.state.sidebarSize = event.pageX;
+            this.state.sidebarSize = isRtl ? document.documentElement.clientWidth - event.pageX : event.pageX;
         });
         const onPointerUp = () => {
             document.removeEventListener('pointermove', onPointerMove);
