634795bb44576c3c88f93a34997f0806ba4dcd03
Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
2025-02-14 15:18:48 +0000

    [FIX] sale_subscription: inheritance of the payment exception was not possible
    As there might be use cases which do follow a different logic, we need to be able
    to enter the resetting payment exception via inheritance easily.
    
    closes odoo/enterprise#80189
    
    X-original-commit: 05ee788e221e8e138316f83f2a556e9a729447e5
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- sale_subscription/models/sale_order.py
+++ sale_subscription/models/sale_order.py
@@ -1468,6 +1468,12 @@ class SaleOrder(models.Model):
     def _subscription_launch_cron_parallel(self, batch_size):
         self.env.ref('sale_subscription.account_analytic_cron_for_invoice')._trigger()
 
+    def _get_subscription_payment_exception_condition(self):
+        """ Return a boolean if we agree that the payment_exception should be reset.
+        This is needed for being able to inherit easily for other use cases than implemented by now.
+        """
+        return False
+
     def _create_recurring_invoice(self, batch_size=30):
         today = fields.Date.today()
         # TODO remove all config['test_enable'] when current_thread().testing will be removed
@@ -1566,7 +1572,7 @@ class SaleOrder(models.Model):
                 # Handle automatic payment or invoice posting
                 existing_invoices = subscription.with_context(recurring_automatic=True)._handle_automatic_invoices(invoice, auto_commit) or self.env['account.move']
                 account_moves |= existing_invoices
-                if all(inv.state != 'draft' for inv in existing_invoices):
+                if subscription._get_subscription_payment_exception_condition() or all(inv.state != 'draft' for inv in existing_invoices):
                     # when the invoice is not confirmed, we keep it and keep the payment_exception flag
                     # Failed payment that delete the invoice will also be handled here and the flag will be removed
                     subscription.with_context(mail_notrack=True).payment_exception = False
