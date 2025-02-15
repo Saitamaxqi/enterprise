Saitamaxqi/enterprise

commit 0d904521645b7001233545bab1569f1b1869ef63

[IMP] documents, *: add Kanban selection
This commit reimplements the way Documents handle record selection. Since it
is now supported to select Kanban cards globally, we can now move some implemented
logic where it makes sense (KanbanRecord, KanbanController).

We can now also select cards by clicking on them with the 'Alt' key pressed, since
this is the main key being chosen for the feature to work on most kanbans ('Ctrl' key
cannot be used, since it opens records in new tabs). For Documents, we have decided
to continue supporting 'Ctrl' for the moment.

Code has been cleaned and simplified. Visual consistency with all kanbans has been
given, by slighlty adapting the look of the cards.

In other modules, tests have been adapted to kanban changes (e.g. 'has_group' is called).

task-4318322

Part-of: odoo/enterprise#77329
Related: odoo/odoo#194090
Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

documents/static/src/core/document_service.js
@@ -52,6 +52,7 @@ export class DocumentService {
         if (this._initData.folderId) {
             browser.localStorage.setItem("searchpanel_documents_document", this._initData.folderId);
         }
+        this.getSelectionActions = null;
     }
 
     /**

documents/static/src/scss/documents_kanban_view.scss
@@ -24,7 +24,6 @@
         width: 100%;
 
         .o_kanban_record {
-            display: flex;
             flex-direction: column;
             width: 260px;
             margin: .5rem;
@@ -67,37 +66,39 @@
                 z-index: 5;
             }
 
-            .o_kanban_stack {
-                width: calc(100% - 6px);
-                border: $border-width solid $border-color;
-                height: 100%;
-                background-color: $o-view-background-color;
-                &:nth-of-type(1){
-                    z-index: $o-dms-kanban-stack-zindex;
-                }
-                &:nth-of-type(2) {
-                    position: absolute;
-                    z-index: $o-dms-kanban-stack-zindex - 1;
-                    transform: translate(3px, 3px);
-                }
-                &:nth-of-type(3) {
-                    position: absolute;
-                    z-index: $o-dms-kanban-stack-zindex - 2;
-                    transform: translate(6px, 6px);
-                }
-            }
-
             .o_documents_in_folder {
                 font-size: 13px;
             }
         }
 
-        .o_kanban_record .o_kanban_attachment {
-            &.o_record_selected, &.o_record_selected ~ div  {
-                --border-color: #{$o-component-active-border};
+        .o_kanban_record {
+            &:has(.o_kanban_stack) {
+                z-index: $o-dms-kanban-stack-zindex;
 
-                opacity: 1;
-                background-color: $o-component-active-bg;
+                &:before, &:after {
+                    content: '';
+                    z-index: $o-dms-kanban-stack-zindex - 1;
+                    background: $o-view-background-color;
+                    outline: 1px solid var(--border-color);
+                    width: 100%;
+                    height: 100%;
+                    position: absolute;
+                    transform: translate(6px, 6px);
+                }
+
+                &:after {
+                    transform: translate(3px, 3px);
+                }
+            }
+
+            > div:nth-of-type(1) {
+                background: $o-view-background-color;;
+            }
+
+            &.o_record_selected {
+                > div:nth-of-type(1), &:before, &:after {
+                    background: var(--KanbanColumn__highlight-selected);
+                }
             }
 
             &.oe_file_request {
@@ -186,14 +187,6 @@
 
             .o_field_kanban_activity {
                 margin-top: 2px;
-            }
-        }
-
-        .o_kanban_record:not(.o_kanban_ghost){
-            &.o_record_selected, &.o_record_selected ~ .o_kanban_stack {
-                opacity: 1;
-                border-color: $o-component-active-border;
-                background-color: $o-component-active-bg;
             }
         }
 

commit 947c52799f8aad5ea257e61ac6abed3f3888309f

[IMP] documents: make various improvements
This commits brings the following changes to Documents. Mostly UI
improvements:

Keep image preview aspect ratio
==============================

Currently, images are distorted because the aspect ratio is not respected.
Images must retain their original proportions, with the addition of
horizontal or vertical spacing if necessary, so that each so that each
preview frame has the same dimensions.

Also, we want the background of the image wrapper being the same as the
body.

Add tooltip to toggle chatter btn
=================================

Add the following tooltip to the toggle chatter button: "Info & tags".

Display existing email alias in help content
===========================================

When an email alias has been configured for a given folder, and a search
on this folder returns 0 record, the helper content displayed should
include the email alias.

In case of a search on 'Home' or 'Company': no message about alias, like
for 'My Drive' or 'Shared with me'.

Also, in 'documents_details_panel':
Change labels Email Alias for Email, and Alias Tags for Tags.

Enhance Home kanban view style
==============================

Enhance the style of the file / folder group titles by turning their
h3 element into a h4*, and removing the border-bottom class.

* actually we keep the elements as they are, to avoid conflicting with
potential customizations, and reduce the police size.

Avoid scrollbar on topbar Chatter
================================

Limit the display, on large devices, of the horizontal scrollbar in the
Chatter topbar by adding 20px to the Chatter container width, and
showing the scrollbar only when the cursor is over that area.

Scroll to selected record when info panel on
===========================================

When toggling the information panel on, the space available to display
documents/folders is significantly reduced. Sometimes, the selected
document moves further down the screen and disappears from the view.

This commit makes it so the view instantly scroll to the selected
document when the info panel is toggled on.

Improve email aliases display
============================

We remove the 'Email Aliases' menu item from the 'Configuration' menu
and add an optional column in the list view to display the email aliases
(widget=email).

Also we hide the Alias Name and Domain columns. We keep the fields in
the view as they are needed for the details panel.

Hide member invite instead of disabling it
=========================================

We currently disable the member invite component in the permission panel
for users with insufficient rights (not editor) or for inactive
documents/folders.

But the restriction is not really obvious (visually) for users. We
decided to hide the invitation to members to limit their confusion.

Add database creator to highest security group
==============================================

Generally the user who creates the database receives the highest permissions
for each application, except for specific cases.

This commits follows the general logic and links 'base.user_admin' to
the 'System Administrator' group.

Fix spacing in control panel
============================

This commit aims to limit the vertical spacing between the control
panel and the menu section.

The minimum height applied to breadcrumbs to prevent vertical shift
of the view when the breadcrumbs changes dimension has been modified.
This height is defined in 'rem' rather than 'vh' to avoid affecting the
height of the control panel, particularly on high-resolution screens.

Finally, this FW port fixes a bug introduced in 0d904521 using an obsolete

documents/i18n/documents.pot
@@ -3900,3 +3900,9 @@ msgstr ""
 #: code:addons/documents/static/src/views/hooks.js:0
 msgid "Put in %s"
 msgstr ""
+
+#. module: documents
+#. odoo-javascript
+#: code:addons/documents/static/src/components/documents_details_panel/documents_details_panel.xml:0
+msgid "Email"
+msgstr ""

documents/security/security.xml
@@ -23,7 +23,7 @@
             <field name="name">System Administrator</field>
             <field name="category_id" ref="base.module_category_productivity_documents"/>
             <field name="implied_ids" eval="[(4, ref('group_documents_manager'))]"/>
-            <field name="users" eval="[(4, ref('base.user_root'))]"/>
+            <field name="users" eval="[(4, ref('base.user_root')), (4, ref('base.user_admin'))]"/>
         </record>
 
         <data noupdate="1">

documents/static/src/components/documents_details_panel/documents_details_panel.xml
@@ -103,7 +103,7 @@
             <!-- Email Alias Section -->
             <div t-if="record.data.type === 'folder' and !record.data.shortcut_document_id"
                 class="text-dark d-flex mb-3 w-100">
-                <span class="fw-bold me-2 text-nowrap">Email Alias</span>
+                <span class="fw-bold me-2 text-nowrap">Email</span>
                 <div t-if="!userPermissionViewOnly and !!documentService.userIsDocumentManager" class="d-flex">
                     <div class="o_field_widget o_field_char w-50">
                         <CharField
@@ -166,7 +166,7 @@
                 t-if="record.data.type === 'folder' and !record.data.shortcut_document_id and record.data.alias_name and record.data.alias_domain_id"
                 class="o_field_widget o_field_many2many_tags d-flex mb-3 w-100"
             >
-                <span class="fw-bold me-2">Alias Tags</span>
+                <span class="fw-bold me-2">Tags</span>
                 <DocumentsDetailsMany2ManyTagsField
                     name="'alias_tag_ids'"
                     readonly="userPermissionViewOnly"

documents/static/src/components/documents_permission_panel/documents_permission_panel.xml
@@ -28,13 +28,13 @@
                 class="alert alert-warning py-1 mb-1"
                 role="alert"
                 t-out="warningMessage"/>
-            <section t-att-class="{ 'mb-3' : state.mainPage }" class="o_documents_permission_panel_member_invite">
+            <section t-if="state.access.user_permission ==='edit' and state.access.active" t-att-class="{ 'mb-3' : state.mainPage }" class="o_documents_permission_panel_member_invite">
                 <DocumentsMemberInvite
                     access="state.access"
                     accessPartners="state.access.access_ids"
                     autoSave.bind="save"
                     close.bind="close"
-                    disabled="state.loading || state.access.user_permission !=='edit' || !state.access.active"
+                    disabled="state.loading"
                     invitePage="!state.mainPage"
                     pendingSave="pendingSave"
                     roles="state.selections.doc_access_roles"

documents/static/src/core/document_service.js
@@ -346,11 +346,30 @@ export class DocumentService {
     toggleChatterState() {
         this.chatterState.visible = !this.chatterState.visible;
 
-        if (this.env.isSmall && this.chatterState.visible) {
+        if (this.chatterState.visible) {
             this.observer = new MutationObserver(() => {
                 const chatterContainer = document.querySelector('.o-mail-Thread');
-                if (chatterContainer) {
-                    chatterContainer.scrollIntoView({ behavior: "smooth"});
+                if (chatterContainer && this.env.isSmall) {
+                    chatterContainer.scrollIntoView({ behavior: "smooth" });
+                    this.observer.disconnect();
+                    return;
+                }
+                const view = this.action.currentController.props.type;
+                if (
+                    chatterContainer &&
+                    ["kanban", "list"].includes(view)
+                ) {
+                    const selectedRecordClass =
+                        view === "kanban"
+                            ? ".o_kanban_record.o_record_selected"
+                            : ".o_data_row.o_data_row_selected";
+                    const selectedRecords = document.querySelectorAll(selectedRecordClass);
+                    if (selectedRecords?.length > 0) {
+                        selectedRecords[0].scrollIntoView({
+                            behavior: "instant",
+                            block: view === "kanban" ? "start" : "center",
+                        });
+                    }
                     this.observer.disconnect();
                 }
             });

documents/static/src/scss/documents_kanban_view.scss
@@ -107,7 +107,7 @@
             }
 
             .o_kanban_image_wrapper {
-                background-color: $gray-200;
+                background-color: $body-bg;
                 height: 140px;
                 width: 100%;
                 display: flex;
@@ -123,8 +123,8 @@
                 }
 
                 img {
-                    width: 100%;
-                    height: 100%;
+                    max-width: 100%;
+                    max-height: 100%;
                 }
 
                 .fa-star,.fa-star-o {

documents/static/src/scss/documents_views.scss
@@ -182,5 +182,14 @@
     border-left: 1px solid var(--border-color);
     .o-mail-Chatter-topbar {
         padding-top: 0.25rem;
+        @include media-breakpoint-up(lg) {
+            &:not(:hover) {
+                overflow-x: hidden !important;
+            }
+        }
+
+    }
+    &.o-mail-ChatterContainer.o-aside {
+        width: calc(#{$o-mail-Chatter-minWidth} + 20px + var(--Chatter-asideExtraWidth));
     }
 }

documents/static/src/views/documents_model_mixin.js
@@ -191,7 +191,7 @@ export const DocumentsModelMixin = (component) =>
          * Open/Close the chatter (the info will be stored in the local storage of the current user).
          */
         async onToggleChatter() {
-            await this.env.documentsView.bus.trigger("documents-toggle-chatter");
+            await this.documentService.toggleChatterState();
         }
 
         /**

documents/static/src/views/helper/documents_action_helper.js
@@ -1,5 +1,4 @@
 import { useService } from "@web/core/utils/hooks";
-import { Domain } from "@web/core/domain";
 import { Component, markup, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { escape } from "@web/core/utils/strings";
 import { _t } from "@web/core/l10n/translation";
@@ -12,7 +11,6 @@ export class DocumentsActionHelper extends Component {
 
     setup() {
         this.orm = useService("orm");
-        this.hasShareReadAccessRights = undefined;
         this.state = useState({
             mailTo: undefined,
         });
@@ -35,7 +33,7 @@ export class DocumentsActionHelper extends Component {
     get noContentHelp() {
         if (
             !this.selectedFolderId ||
-            ["RECENT", "SHARED", "TRASH", "MY"].includes(this.selectedFolderId)
+            ["RECENT", "SHARED", "TRASH", "MY", "COMPANY"].includes(this.selectedFolderId)
         ) {
             const helpMessage = (() => {
                 switch (this.selectedFolderId) {
@@ -58,26 +56,23 @@ export class DocumentsActionHelper extends Component {
 
     async updateShareInformation() {
         this.state.mailTo = undefined;
-        // Only load data if we are in a single folder.
-        let domain = this.env.searchModel.domain.filter(
+        // Only load data if we are in a single folder and selected folder has a real id
+        const filteredDomain = this.env.searchModel.domain.filter(
             (leaf) => Array.isArray(leaf) && leaf.includes("folder_id")
         );
-        if (domain.length !== 1) {
+        if (filteredDomain.length !== 1 || typeof this.selectedFolderId !== "number") {
             return;
         }
         // make sure we have a mail.alias configured
-        domain = Domain.and([domain, [["type", "=", "folder"]], [["alias_name", "!=", false]]]).toList();
-        if (this.hasShareReadAccessRights === undefined) {
-            this.hasShareReadAccessRights = false;
-        }
-        if (!this.hasShareReadAccessRights) {
+        const selectedFolder = this.env.searchModel.getFolderById(this.selectedFolderId);
+        if (
+            !selectedFolder ||
+            selectedFolder.user_permission === "none" ||
+            !selectedFolder.alias_name ||
+            !selectedFolder.alias_domain_id
+        ) {
             return;
         }
-        const folders = await this.orm.searchRead("documents.document", domain, ["id", "alias_id"], {
-            limit: 1,
-        });
-        if (folders.length) {
-            this.state.mailTo = folders[0].alias_id[1];
-        }
+        this.state.mailTo = `${selectedFolder.alias_name}@${selectedFolder.alias_domain_id[1]}`;
     }
 }

documents/static/src/views/helper/documents_action_helper.xml
@@ -4,7 +4,7 @@
         <div class="o_view_nocontent">
             <div class="o_nocontent_help">
                 <t t-out="noContentHelp"/>
-                <t t-if="!['MY', 'RECENT', 'SHARED', 'TRASH'].includes(selectedFolderId)">
+                <t t-if="selectedFolderId and !['MY', 'RECENT', 'SHARED', 'TRASH', 'COMPANY'].includes(selectedFolderId)">
                     <div t-if="state.mailTo">
                         <p class="fw-normal">Or send emails to
                             <a t-attf-href="mailto:{{state.mailTo}}">

documents/static/src/views/helper/documents_file_viewer.scss
@@ -4,7 +4,7 @@
 }
 
 .o-DocumentsFileViewer-main-with-chatter {
-    width: calc(100vw - (#{$o-mail-Chatter-minWidth})) !important;
+    width: calc(100vw - (#{$o-mail-Chatter-minWidth}  + 20px)) !important;
 }
 
 .o-DocumentsFileViewer-main-without-chatter {

documents/static/src/views/kanban/documents_kanban_renderer.xml
@@ -69,7 +69,7 @@
                 </div>
             </div>
             <t t-if="hasFolders() and !this.props.list.isGrouped">
-                <h3 class="o_documents_title m-2 border-bottom w-100">Folders</h3>
+                <h3 class="o_documents_title m-2 w-100 fs-4">Folders</h3>
                 <t t-foreach="getFolderRecords()" t-as="folderRecord" t-key="folderRecord.key">
                     <KanbanRecord
                         Compiler="props.Compiler"
@@ -83,7 +83,7 @@
                 </t>
                 <!-- kanban ghost cards are used to properly space last elements. -->
                 <div t-foreach="[,,,,,,]" t-as="i" t-key="i_index" class="o_kanban_record o_kanban_ghost flex-grow-1 flex-md-shrink-1 flex-shrink-0" />
-                <h3 t-if="hasFiles()" class="o_documents_title m-2 border-bottom w-100">Files</h3>
+                <h3 t-if="hasFiles()" class="o_documents_title m-2 w-100 fs-4">Files</h3>
             </t>
             <FileUploadProgressContainer fileUploads="this.documentUploads" Component="constructor.components.FileUploadProgressKanbanRecord" shouldDisplay="(upload) => !upload.data.get('document_id')"/>
         </t>

documents/static/src/views/search/documents_control_panel.scss
@@ -17,7 +17,7 @@
 
     @media (min-width: 992px) {
         .o_control_panel_breadcrumbs {
-            min-height: 5.6vh;
+            min-height: 2.4125rem;
         }
     }
 }

documents/static/src/views/search/documents_control_panel.xml
@@ -13,6 +13,7 @@
         <xpath expr="//*[hasclass('o_cp_switch_buttons')]" position="before">
             <button t-if="documentService.userIsInternal"
                 t-on-click="() => this.documentService.toggleChatterState()"
+                data-tooltip="Info &amp; tags"
                 class="btn btn-secondary o_switch_view o_kanban d-none d-md-inline"
                 t-att-class="{'active': this.chatterState.visible}">
                 <i class="fa fa-fw fa-info-circle mt-1"/>

documents/views/documents_document_views.xml
@@ -90,10 +90,10 @@
                                 <t t-set="youtubeVideoToken" t-value="youtubeUrlMatch ? youtubeUrlMatch.length > 1 ? youtubeUrlMatch[1] : false : false"/>
                                 <div name="document_preview" class="o_kanban_image_wrapper d-flex justify-content-center position-relative" t-att-title="fileRequest ? 'Upload file' : ''">
                                     <field name="is_favorited" nolabel="1" widget="boolean_favorite" class="position-absolute top-0 end-0 p-1 pt-2"/>
-                                    <img t-if="youtubeVideoToken" class="w-100 h-100" alt="Document"
+                                    <img t-if="youtubeVideoToken" alt="Document"
                                          t-attf-src="https://img.youtube.com/vi/{{youtubeVideoToken}}/0.jpg"/>
                                     <a t-elif="record.type.raw_value =='url'" target="_blank" t-att-href="record.url.raw_value" class="o_kanban_image_wrapper">
-                                        <img t-if="record.url_preview_image.raw_value" class="w-100 h-100"
+                                        <img t-if="record.url_preview_image.raw_value"
                                             t-att-src="record.url_preview_image.raw_value" t-att-alt="record.name.raw_value"/>
                                         <t t-else="">
                                             <span class="fa fa-link fa-3x text-muted"/>
@@ -101,7 +101,7 @@
                                     </a>
                                     <t t-elif="hasStoredThumbnail">
                                         <t t-set="unique" t-value="record.checksum.value ? record.checksum.value.slice(-8) : ''"/>
-                                        <img t-attf-src="/documents/thumbnail/#{record.access_token.raw_value}?unique=#{unique}" class="w-100 h-100" alt="Document preview"/>
+                                        <img t-attf-src="/documents/thumbnail/#{record.access_token.raw_value}?unique=#{unique}" alt="Document preview"/>
                                     </t>
                                     <div t-elif="fileRequest" class="fa fa-upload fa-3x text-muted">
                                         <input type="file" class="o_input_file o_hidden o_kanban_replace_document"/>
@@ -384,8 +384,9 @@
                 <field name="shortcut_document_id" column_invisible="1"/>
                 <field name="mimetype" column_invisible="1"/>
                 <field name="access_url" column_invisible="1"/>
-                <field name="alias_name" optional="hide"/>
-                <field name="alias_domain_id" optional="hide"/>
+                <field name="alias_email" optional="hide" widget="email"/>
+                <field name="alias_name" column_invisible="1"/>
+                <field name="alias_domain_id" column_invisible="1"/>
                 <field name="alias_tag_ids" widget="many2many_tags" options="{'color_field': 'color'}" column_invisible="1"/> <!-- fetch `color` too -->
                 <field name="create_activity_type_id" column_invisible="1"/>
                 <field name="create_activity_user_id" column_invisible="1"/>

documents/views/documents_menu_views.xml
@@ -22,12 +22,6 @@
         action="configuration_action"
         groups="base.group_system"
         sequence="10"/>
-    <menuitem name="Email Aliases"
-        id="documents.mail_alias_menu"
-        parent="documents.Config"
-        action="mail_alias_action"
-        groups="documents.group_documents_manager"
-        sequence="30"/>
 
     <menuitem name="Structure"
         id="documents.structure"

documents/views/mail_alias_views.xml
@@ -14,7 +14,7 @@
         </field>
     </record>
 
-    <!-- todo - transform share_action -->
+    <!-- todo - remove in master (with view(s)) -->
     <record id="mail_alias_action" model="ir.actions.act_window">
         <field name="name">Email links</field>
         <field name="res_model">mail.alias</field>