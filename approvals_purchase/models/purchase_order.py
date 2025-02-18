48f0e203ec19ed2fa49848f8d6de1e97b5aed6ac
akha <akha@odoo.com>
2025-02-18 11:51:53 +0100

    [PERF] approvals_purchase: improve performance
    To log the changes in the state of a purchase order to the chatter of the related approval requests,
    a search was done to find the product lines related to the approval requests.  This search was
    performed inside a loop.
    
    This commit performs the search outside of the loop to avoid slowing down the system when the
    state of multiple purchase orders has changed.
    
    Related-PR: https://github.com/odoo/enterprise/pull/71294
    
    task-3561514
    
    closes odoo/enterprise#79559
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- approvals_purchase/models/purchase_order.py
+++ approvals_purchase/models/purchase_order.py
@@ -9,20 +9,28 @@ class PurchaseOrder(models.Model):
 
     def write(self, vals):
         if 'state' in vals:
-            for purchase_order in self:
-                purchase_order._log_po_state_change_to_approval_request_chatter(purchase_order.state, vals['state'])
+            self = self.filtered(lambda purchase_order: purchase_order.state != vals['state'])
+            related_product_lines = self.sudo().env['approval.product.line'].search(
+                domain=[('purchase_order_line_id.order_id', 'in', self.ids)],
+            )
+            grouped_product_lines = dict()
+            for product_line in related_product_lines:
+                approval_request_id = product_line.approval_request_id
+                purchase_order_id = product_line.purchase_order_line_id.order_id
+                if not grouped_product_lines.get(approval_request_id):
+                    grouped_product_lines[approval_request_id] = dict()
+                if not grouped_product_lines[approval_request_id].get(purchase_order_id):
+                    grouped_product_lines[approval_request_id][purchase_order_id] = self.env['approval.product.line']
+                grouped_product_lines[approval_request_id][purchase_order_id] |= product_line
+
+            self._log_po_state_change_to_approval_request_chatter(vals['state'], grouped_product_lines)
         return super().write(vals)
 
-    def _log_po_state_change_to_approval_request_chatter(self, old_state, new_state):
-        self.ensure_one()
-        if old_state == new_state:
-            return
-        related_product_lines_by_approval_request = self.sudo().env['approval.product.line'].search([
-            ('purchase_order_line_id.order_id', '=', self.id)
-        ]).grouped('approval_request_id')
-        for approval_request, product_lines in related_product_lines_by_approval_request.items():
-            state_change_msg = self._create_state_change_msg(old_state, new_state, product_lines)
-            approval_request._message_log(body=state_change_msg)
+    def _log_po_state_change_to_approval_request_chatter(self, new_state, grouped_product_lines):
+        for approval_request, product_lines_by_purchase_order in grouped_product_lines.items():
+            for purchase_order, product_lines in product_lines_by_purchase_order.items():
+                state_change_msg = purchase_order._create_state_change_msg(purchase_order.state, new_state, product_lines)
+                approval_request._message_log(body=state_change_msg)
 
     def _create_state_change_msg(self, old_state, new_state, approval_request_products):
         state_label = dict(self._fields['state']._description_selection(self.env))
