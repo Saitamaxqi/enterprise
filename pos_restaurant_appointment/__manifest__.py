016718312776c38a80814bcf6ca3ff91ede55e1d
David Monnom <moda@odoo.com>
2025-02-19 14:17:44 +0000

    [IMP] pos_restaurant_appointment: Add minimal html editor in pos mode
    In the point of sale environnement we want to be able to edit html field
    in a minimal way. So only core plugins are added to the html_editor.
    
    As we have a special assets environnement for the point_of_sale we can
    directly patch the main `htmlField` definition without impacting main
    assets of Odoo.
    
    closes odoo/enterprise#81717
    
    Community: 198482
    X-original-commit: 19110e430dc2501a20e583f51a6f6b428f2b51c1
    Related: odoo/odoo#202392
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- pos_restaurant_appointment/__manifest__.py
+++ pos_restaurant_appointment/__manifest__.py
@@ -21,7 +21,31 @@
     'license': 'OEEL-1',
     'post_init_hook': '_pos_restaurant_appointment_after_init',
     'assets': {
+        # Having the editor add a lot of files to the PoS bundle.
+        # We can maybe find a way to have a "light editor" with smaller bundle
+        'pos_restaurant_appointment.html_editor': [
+            "html_editor/static/src/utils/*.js",
+            ("remove", "html_editor/static/src/utils/regex.js"),
+
+            'html_editor/static/src/html_migrations/**/*',
+            'html_editor/static/src/plugin.js',
+            'html_editor/static/src/plugin_sets.js',
+            'html_editor/static/src/wysiwyg.js',
+            'html_editor/static/src/wysiwyg.xml',
+            'html_editor/static/src/position_hook.js',
+            'html_editor/static/src/local_overlay_container.js',
+            'html_editor/static/src/local_overlay_container.xml',
+            'html_editor/static/src/core/**/*',
+            'html_editor/static/src/main/**/*',
+            'html_editor/static/src/fields/**/*',
+            'html_editor/static/src/others/**/*',
+            'html_editor/static/src/editor.js',
+
+            'web/static/lib/dompurify/DOMpurify.js',
+        ],
+
         'point_of_sale._assets_pos': [
+            ('include', 'pos_restaurant_appointment.html_editor'),
             'web_gantt/static/src/**/*',
             'pos_restaurant_appointment/static/src/**/*',
             'appointment/static/src/components/appointment_booking_action_helper/*',
