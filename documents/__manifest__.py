4b8ee92ea53aed3d392a1d19ee54394f2d157080
Florian Charlier <flch@odoo.com>
2025-02-19 18:01:21 +0100

    [REF] documents: move legacy test suite
    Moving qunit to a `legacy` directory simplifies greatly the
    manifest assets' management.
    
    Task-3861500
    
    X-original-commit: 97bf3015e58ec2a8f07bb173a2dcb5343a7690f6
    Part-of: odoo/enterprise#81976
    Signed-off-by: Patrick Hoste (pko) <pko@odoo.com>
    Signed-off-by: Florian Charlier (flch) <flch@odoo.com>

--- documents/__manifest__.py
+++ documents/__manifest__.py
@@ -94,28 +94,17 @@ App to upload and manage your documents.
             'web/static/src/start.js',
         ],
         'web.tests_assets': [
-            'documents/static/tests/helpers/**/*',
-            ('remove', 'documents/static/tests/helpers/data.js'),
+            'documents/static/tests/legacy/helpers/**/*',
         ],
         'web.assets_tests': [
             'documents/static/tests/tours/*',
         ],
         'web.assets_unit_tests': [
-            'documents/static/tests/helpers/**/*',
-            ('remove', 'documents/static/tests/helpers/mock_server/**/*'),
-            'documents/static/tests/error_dialog_patch.test.js',
-            "documents/static/tests/documents_systray_activity_menu.test.js",
-            "documents/static/tests/documents_test_helpers.js",
-            "documents/static/tests/mock_server/**/*",
+            'documents/static/tests/**/*',
+            ('remove', 'documents/static/tests/legacy/**/*'),
         ],
         'web.qunit_suite_tests': [
-            'documents/static/tests/**/*',
-            ('remove', 'documents/static/tests/helpers/**/*'),
-            ('remove', 'documents/static/tests/tours/*'),
-            ('remove', 'documents/static/tests/error_dialog_patch.test.js'),
-            ("remove", "documents/static/tests/documents_systray_activity_menu.test.js"),
-            ("remove", "documents/static/tests/documents_test_helpers.js"),
-            ("remove", "documents/static/tests/mock_server/**/*"),
+            'documents/static/tests/legacy/**/*',
         ],
     }
 }
