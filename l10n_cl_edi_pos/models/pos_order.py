2f1d01c77ca92d57880f5bc207ce14d1c92c1335
sjai-odoo <sjai@odoo.com>
2025-02-19 12:57:28 +0530

    [FIX] l10n_*_edi_pos: adapt global invoicing in l10n modules
    *: [cl, pe]
    
    Steps to reproduce:
    - Install pos_restaurant and any of above localization.
    - Create multiple orders.
    - TRy to create global invoice of those orders.
    
    Issue:
    - For global invoice feature we allowed multiple orders to be consolidated and
    can be generated a single invoice which was not handled in l10n modules.
    
    Cause:
    - We were using directly self.field_name to access some values which now
    can't be supported due to multiple orders in self.
    
    Fix:
    - We adapted the code to have consolidated invoices based on the mapped values.
    
    Runbot Error Task:
    https://runbot.odoo.com/odoo/action-573/134660
    https://runbot.odoo.com/odoo/action-573/134661
    
    closes odoo/enterprise#79648
    
    Related: odoo/odoo#198256
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- l10n_cl_edi_pos/models/pos_order.py
+++ l10n_cl_edi_pos/models/pos_order.py
@@ -10,9 +10,14 @@ class PosOrder(models.Model):
 
     def _prepare_invoice_vals(self):
         vals = super()._prepare_invoice_vals()
-        if self.amount_total >= 0:
-            if self.invoice_type == 'factura':
-                if self.amount_tax == 0:
+        invoice_types = self.mapped("invoice_type")
+        if len(set(invoice_types)) > 1:
+            raise ValueError("All orders must have the same invoice type")
+        amount_total = sum(self.mapped("amount_total"))
+        amount_tax = sum(self.mapped("amount_tax"))
+        if amount_total >= 0:
+            if invoice_types[0] == 'factura':
+                if amount_tax == 0:
                     vals.update({
                         'l10n_latam_document_type_id': self.env.ref('l10n_cl.dc_y_f_dte').id,
                     })
@@ -20,8 +25,8 @@ class PosOrder(models.Model):
                     vals.update({
                         'l10n_latam_document_type_id': self.env.ref('l10n_cl.dc_a_f_dte').id,
                     })
-            elif self.invoice_type == 'boleta':
-                if self.amount_tax == 0:
+            elif invoice_types[0] == 'boleta':
+                if amount_tax == 0:
                     vals.update({
                         'l10n_latam_document_type_id': self.env.ref('l10n_cl.dc_b_e_dte').id,
                     })
