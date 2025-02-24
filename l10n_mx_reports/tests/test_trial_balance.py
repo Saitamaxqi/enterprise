011fb0fec3ca83450f53d1e13966a673525a0702
Leonardo Medina <leml@odoo.com>
2025-02-24 17:49:56 +0000

    [IMP] l10n_mx: new default data for ISR Witheld tax
    This commit adds new tax ISR 1.25 witheld for mexican localization
    
    target: 17 -> master
    task-4486643
    
    closes odoo/enterprise#80829
    
    X-original-commit: cc161c7b1f6cbb49d46784fbeaf4127a35b24acb
    Related: odoo/odoo#200473
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>
    Signed-off-by: Leonardo Medina Lozano (leml) <leml@odoo.com>

--- l10n_mx_reports/tests/test_trial_balance.py
+++ l10n_mx_reports/tests/test_trial_balance.py
@@ -176,6 +176,8 @@ class TestL10nMXTrialBalanceReport(TestL10nMXTrialBalanceReportCommon):
             <catalogocuentas:Ctas CodAgrup="108.02" NumCta="108.02" Desc="Allowance for doubtful accounts foreign" Nivel="2" Natur="D"/>
             <catalogocuentas:Ctas CodAgrup="110" NumCta="110" Desc="Employment subsidy to be applied" Nivel="1" Natur="D"/>
             <catalogocuentas:Ctas CodAgrup="110.01" NumCta="110.01" Desc="Employment subsidy to be applied" Nivel="2" Natur="D"/>
+            <catalogocuentas:Ctas CodAgrup="113" NumCta="113" Desc="Tax credit" Nivel="1" Natur="D"/>
+            <catalogocuentas:Ctas CodAgrup="113.02" NumCta="113.02" Desc="Income tax to recover" Nivel="2" Natur="D"/>
             <catalogocuentas:Ctas CodAgrup="115" NumCta="115" Desc="Inventory" Nivel="1" Natur="D"/>
             <catalogocuentas:Ctas CodAgrup="115.01" NumCta="115.01" Desc="Inventory" Nivel="2" Natur="D"/>
             <catalogocuentas:Ctas CodAgrup="115.02" NumCta="115.02" Desc="Raw materials and materials" Nivel="2" Natur="D"/>
