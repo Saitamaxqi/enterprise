633b8688a969b873d630df4968b996cb600740fc
Xavier Bol (xbo) <xbo@odoo.com>
2025-02-27 08:45:23 +0000

    [FIX] project_enterprise: ensure test does not depend on demo data
    Before this commit, the `ProjectEnterpriseTestUi.test_01_ui` test fails
    without demo data because the user is not considered as overloaded. The
    reason is because the user has more than one task assigned to him when
    the demo data is installed and so when a new task is planned for him,
    the system will see the sum of the allocated_hours of all tasks assigned
    to him inside the period displayed in the gantt is higher than the hours
    he can do.
    
    This commit makes sure the test does not depend on the demo data and
    increase the allocated hours of the task creates inside the test.
    
    runbot-error-111415
    
    closes odoo/enterprise#82987
    
    X-original-commit: 36c3552baff426dde26ebee352a3d08655a2ee1b
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- project_enterprise/tests/test_ui.py
+++ project_enterprise/tests/test_ui.py
@@ -36,14 +36,17 @@ class ProjectEnterpriseTestUi(HttpCase):
         project = cls.env['project.project'].create({
             'name': 'Test Project',
         })
+        # Allocated hours is only computed without _origin
         first_day_in_current_month = fields.Datetime.now() + relativedelta(day=1, hour=0, minute=0, second=0)
-        cls.env['project.task'].create({
+        task = cls.env['project.task'].new({
             'project_id': project.id,
             'name': 'Test Task',
             'planned_date_begin': first_day_in_current_month - relativedelta(months=1),
             'date_deadline': first_day_in_current_month + relativedelta(months=2, days=-1),
             'user_ids': cls.env.ref('base.user_admin'),
         })
+        task._compute_allocated_hours()
+        task.create(task._convert_to_write(task._cache))
 
     def test_01_ui(self):
         self.start_tour("/", 'project_test_tour', login='admin')
