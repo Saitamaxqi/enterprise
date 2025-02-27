5f4ba4f68f93e6154d1fc3c31e9d97550efb82c0
Xavier Morel <xmo@odoo.com>
2025-02-27 12:53:34 +0100

    [FIX] account_bank_statement_extract: activate currency in order to validate document
    If EUR is not active, `check_all_status` fails (because
    `AccountMove._post` fails).
    
    Before #80180, EUR was implicitly enabled by the multi-company setup
    of MailCommon: it creates a company with the currency EUR, and
    `ResCompany.create` automatically activates the company
    currency. Since the dependency on `MailCommon` was removed from
    `TestBankStatementExtractProcess`.
    
    This is not visible on the runbot, because it always installs l10n_be
    which automatically enables EUR.
    
    closes odoo/enterprise#80361
    
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- account_bank_statement_extract/tests/test_account_bank_statement_extract.py
+++ account_bank_statement_extract/tests/test_account_bank_statement_extract.py
@@ -14,12 +14,14 @@ class TestBankStatementExtractProcess(AccountTestInvoicingCommon, TestExtractMix
 
         cls.env.user.group_ids |= cls.env.ref('base.group_system')
 
+        currency = cls.env.ref('base.EUR')
+        currency.active = True
         cls.bank_journal = cls.env['account.journal'].create({
             'name': 'Bank 123456',
             'code': 'BNK67',
             'type': 'bank',
             'bank_acc_number': '123456',
-            'currency_id': cls.env.ref('base.EUR').id,
+            'currency_id': currency.id,
         })
         cls.bank_statement = cls.env['account.bank.statement'].create({
             'journal_id': cls.bank_journal.id,
