6d32badebda8b627e035b21a7cd3fd6f921c59c8
Hubert Van De Walle <huvw@odoo.com>
2025-02-28 17:48:06 +0000

    [FIX] industry_fsm: editable records in calendar view
    Steps to reproduce
    ==================
    
    - Use a mobile viewport
    - Go to field service
    - Switch to the calendar view
    - Click on an empty slot to create a new record
    => The default dates are missing
    - Go back to the calendar view
    - Click on an existing record
    => We should be able to edit the selected record, but instead, we can
       create a new one
    
    opw-4596689
    
    closes odoo/enterprise#80625
    
    X-original-commit: 2235d7974d77f67d66ccc9e5454d9d5450d77057
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- industry_fsm/static/src/views/fsm_my_task_calendar/fsm_my_task_calendar_controller.js
+++ industry_fsm/static/src/views/fsm_my_task_calendar/fsm_my_task_calendar_controller.js
@@ -3,7 +3,13 @@ import { ProjectTaskCalendarController } from "@project/views/project_task_calen
 export class FsmMyTaskCalendarController extends ProjectTaskCalendarController {
     async editRecord(record, context = {}, shouldFetchFormViewId = true) {
         if (this.env.isSmall) {
-            return this.action.doAction("industry_fsm.project_task_fsm_mobile_server_action");
+            return this.action.doAction("industry_fsm.project_task_fsm_mobile_server_action", {
+                props: {
+                    resModel: this.model.resModel,
+                    resId: record.id || false,
+                },
+                additionalContext: context,
+            });
         }
         return super.editRecord(record, context, shouldFetchFormViewId);
     }
