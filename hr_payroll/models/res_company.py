e7c3fa3a6da9fbadd22619d7659ff7fce49eea77
Yash Vaishnav <yava@odoo.com>
2025-03-05 11:44:06 +0530

    [IMP] hr_payroll: improved payroll dashboard
    In this commit,
    - The link to "Payroll Tips & Tricks" has been removed from the Notes page
    and replaced with a warning message.
    - The Dashboard will display a helper message when no batches are available.
    - In the contract list view, the 'Index Contract' button will only be visible when
    at least one record is selected.
    
    Task-4199423
    
    closes odoo/enterprise#80763
    
    Related: odoo/odoo#200344
    Related: odoo/upgrade#7330
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_payroll/models/res_company.py
+++ hr_payroll/models/res_company.py
@@ -44,7 +44,12 @@ class ResCompany(models.Model):
                 ))
 
     def _create_dashboard_notes(self):
-        note = self.env['ir.qweb']._render('hr_payroll.hr_payroll_note_demo_content', {'date_today': fields.Date.today().strftime(self.env['res.lang']._get_data(code=self.env.user.lang).date_format)})
+        action = self.env.ref('hr_payroll.action_hr_payroll_dashboard_warning')
+        warning_url = f"{action.get_base_url()}/odoo/action-{action.id}"
+        note = self.env['ir.qweb']._render('hr_payroll.hr_payroll_note_demo_content', {
+            'date_today': fields.Date.today().strftime(self.env['res.lang']._get_data(code=self.env.user.lang).date_format),
+            'warning_url': warning_url,
+        })
         self.env['hr.payroll.note'].sudo().create([{
             'company_id': company.id,
             'name': self.env._('Note'),
