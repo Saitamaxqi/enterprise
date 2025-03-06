4e588dfbe8141c730dea146ef408ba3667b99d56
Basioni (basm) <basm@odoo.com>
2025-03-06 17:09:02 +0100

    [IMP] voip: add a shortcut button to the providers' list.
    This commit adds a button next to the providers' dropdown menu in the
    users' settings/preferences to lead to the providers' list view.
    
    Task-4416699
    
    closes odoo/enterprise#80448
    
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- voip/__manifest__.py
+++ voip/__manifest__.py
@@ -11,10 +11,10 @@
         "security/ir.model.access.csv",
         "security/voip_security.xml",
         "views/res_partner_views.xml",
+        "views/voip_provider_views.xml",   # load before res_config_settings_views.xml
         "views/res_users_views.xml",
         "views/res_users_settings_views.xml",
         "views/voip_call_views.xml",
-        "views/voip_provider_views.xml",   # load before res_config_settings_views.xml
         "views/res_config_settings_views.xml",
     ],
     "application": True,
