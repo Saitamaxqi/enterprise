f2050cb11ca87b822d30eac111bd922886be58a1
Jinane Maksoud <maji@odoo.com>
2025-03-11 15:40:10 +0000

    [FIX] hr_recruitment_integration_monster: change field label
    The field res.company.hr_recruitment_monster_password has
    the same label 'Password' as the field
    res.company.l10n_co_edi_password from module l10n_co_edi
    which generates warnings.
    
    closes odoo/enterprise#82237
    
    X-original-commit: a8af0e0464556db584a87a8873481ec637791109
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- hr_recruitment_integration_monster/models/res_company.py
+++ hr_recruitment_integration_monster/models/res_company.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    hr_recruitment_monster_username = fields.Char(string='Identifier', groups="base.group_system")
-    hr_recruitment_monster_password = fields.Char(string='Password', groups="base.group_system")
+    hr_recruitment_monster_username = fields.Char(string='Monster Identifier', groups="base.group_system")
+    hr_recruitment_monster_password = fields.Char(string='Monster Password', groups="base.group_system")
