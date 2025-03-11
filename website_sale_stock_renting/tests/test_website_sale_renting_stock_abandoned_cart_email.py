73483f1ff71baa4fe53e3a4d361e26af6cd0202d
nipl-odoo <nipl@odoo.com>
2025-03-11 16:01:00 +0530

    [IMP] website_sale: prevent Sending Abandoned Cart Emails to Old Carts
    Before this commit:
    - Enabling the abandoned cart email feature in settings triggered emails for
      both old and new abandoned carts by default.
    
    After this commit:
    - Abandoned cart emails will only be sent for carts abandoned after the feature
      is activated.
    
    Affected Version: Master
    
    closes odoo/enterprise#81150
    
    Task: 4512486
    Related: odoo/odoo#201179
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- website_sale_stock_renting/tests/test_website_sale_renting_stock_abandoned_cart_email.py
+++ website_sale_stock_renting/tests/test_website_sale_renting_stock_abandoned_cart_email.py
@@ -16,8 +16,17 @@ class TestWebsiteSaleStockRentingAbandonedCartEmail(TestWebsiteSaleCartAbandoned
     def test_website_sale_stock_renting_abandoned_cart_email(self):
         """Make sure the send_abandoned_cart_email method sends the correct emails."""
 
+        now = fields.Datetime.now()
         website = self.env['website'].get_current_website()
         website.send_abandoned_cart_email = True
+        website.write(
+            {
+                "send_abandoned_cart_email_activation_time": (
+                    now - relativedelta(hours=website.cart_abandoned_delay)
+                )
+                - relativedelta(minutes=10)
+            }
+        )
 
         renting_product_template = self.env['product.template'].create({
             'name': 'renting_product_template',
@@ -26,7 +35,6 @@ class TestWebsiteSaleStockRentingAbandonedCartEmail(TestWebsiteSaleCartAbandoned
             'allow_out_of_stock_order': False
         })
         renting_product_product = renting_product_template.product_variant_id
-        now = fields.Datetime.now()
         order_line = [[0, 0, {
             'product_id': renting_product_product.id,
             'product_uom_qty': 1,
