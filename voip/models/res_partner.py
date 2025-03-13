3b5d0089e37b329b435983847854922a7acecc61
Louis Wicket (wil) <wil@odoo.com>
2025-03-13 13:23:05 +0000

    [FIX] voip: remove extra OR from get_contacts domain
    PR #75203 removed the mobile field, and so updated search domains that
    used it in VoIP, but the corresponding ORs weren't correctly cleaned up,
    breaking the search.
    
    This commit gets rid of the obsolete ORs.
    
    closes odoo/enterprise#81458
    
    X-original-commit: 354568e22787e9b0c1153085086b9ca0bca69c9c
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- voip/models/res_partner.py
+++ voip/models/res_partner.py
@@ -10,12 +10,11 @@ class ResPartner(models.Model):
     def get_contacts(self, offset, limit, search_terms):
         domain = [("phone", "!=", False)]
         if search_terms:
-            search_domain = [
-                "|", "|", "|",
-                ("phone", "like", search_terms),
-                ("complete_name", "ilike", search_terms),
-                ("email", "ilike", search_terms),
-            ]
+            search_domain = expression.OR([
+                [("phone", "like", search_terms)],
+                [("complete_name", "ilike", search_terms)],
+                [("email", "ilike", search_terms)],
+            ])
             domain = expression.AND([domain, search_domain])
         return self.search(domain, offset=offset, limit=limit)._format_contacts()
 
