df78f8ce1aa21e51b5d1b0c1c9ea2f00f0cf8330
spe-odoo <spe@odoo.com>
2025-03-17 10:55:56 +0000

    [FIX] appointment: adapt short_code generation
    1. Replace "_" with "-" for name generation.
       eg. "appointment_with me" to "appointment-with-me"
    
    2. Generate a unique link.
       Case 1: generate "-" based link when no link exists for the appointment
               eg. "/schedule-a-demo".
       Case 2: if a link exists for the appointment:
               generate random code if appointment name based short code is
               already used in another configuration.
    
    Task-4610207
    
    closes odoo/enterprise#83155
    
    X-original-commit: c70349ced15a33fbe7bd06356dc89d6586016913
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Sahil Patel (spe) <spe@odoo.com>

--- appointment/static/src/components/appointment_booking_action_helper/appointment_booking_action_helper.js
+++ appointment/static/src/components/appointment_booking_action_helper/appointment_booking_action_helper.js
@@ -10,18 +10,26 @@ export class AppointmentBookingActionHelper extends Component {
     }
 
     async openShareDialog() {
-        const appointmentName = await this.orm.searchRead("appointment.type", [["id", "=", this.props.context.default_appointment_type_id]], ["name"]);
+        const context = {
+            'dialog_size': 'medium',
+        };
+        const appointmentId = this.props.context.default_appointment_type_id;
+        if (appointmentId) {
+            const appointment = await this.orm.searchRead("appointment.type", [["id", "=", appointmentId]], ["name"]);
+            const shortCode = appointment[0].name.trim().toLowerCase().replaceAll(" ", "-");
+            const isAlreadyUsed = await this.orm.searchCount("appointment.invite", [["short_code", "=", shortCode]]) !== 0;
+            context['default_appointment_type_ids'] = [appointmentId];
+            if (!isAlreadyUsed) {
+                context['default_short_code'] = shortCode;
+            }
+        }
         this.action.doAction({
             name: 'Create a Share Link',
             type: 'ir.actions.act_window',
             res_model: 'appointment.invite',
             views: [[false, 'form']],
             target: 'new',
-            context: {
-                'default_appointment_type_ids': [this.props.context.default_appointment_type_id],
-                'dialog_size': 'medium',
-                'default_short_code': appointmentName[0].name.toLowerCase().replace(" ", "_"),
-            },
+            context: context,
         });
     }
 };
