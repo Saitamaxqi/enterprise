83b755650577a9b0efca7ac298fdeda90de8bed9
Xavier Bol (xbo) <xbo@odoo.com>
2025-03-17 13:03:39 +0100

    [IMP] project_enterprise: display closed tasks as past event in calendar
    This commit adapts the code according to the changes made in community
    to display the closed tasks as past event in the calendar view of tasks.
    
    task-4647219
    
    closes odoo/enterprise#81579
    
    Related: odoo/odoo#202067
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- project_enterprise/static/src/views/project_task_calendar/project_task_calendar_view.js
+++ project_enterprise/static/src/views/project_task_calendar/project_task_calendar_view.js
@@ -1,13 +1,11 @@
 import { registry } from "@web/core/registry";
 import { projectTaskCalendarView } from "@project/views/project_task_calendar/project_task_calendar_view";
 import { ProjectEnterpriseTaskCalendarModel } from "./project_task_calendar_model";
-import { ProjectTaskCalendarRenderer } from "./project_task_calendar_renderer";
 import { ProjectTaskSearchModel } from "../project_task_search_model";
 
 export const projectEnterpriseTaskCalendarView = {
     ...projectTaskCalendarView,
     Model: ProjectEnterpriseTaskCalendarModel,
-    Renderer: ProjectTaskCalendarRenderer,
     SearchModel: ProjectTaskSearchModel,
 };
 
