4821187b87994c210dd22442a8bcce9678020264
jugj-odoo <jugj@odoo.com>
2025-03-19 15:37:54 +0000

    [PERF] l10n_be_hr_payroll: Fix benefits report query perf
    ROW_NUMBER() OVER() impacts performance on large recordsets following commit bc05eee92c5111141c069f08c0fb12c74956152a, we replace it
    in this context with a simple 1 as id.
    
    closes odoo/enterprise#81848
    
    X-original-commit: f4d4fbd36bb9279708dcc276836c3fc685154d93
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>

--- l10n_be_hr_payroll/report/l10n_be_work_entry_daily_benefit.py
+++ l10n_be_hr_payroll/report/l10n_be_work_entry_daily_benefit.py
@@ -27,7 +27,7 @@ class L10n_BeWorkEntryDailyBenefitReport(models.Model):
                     SELECT work_entry.employee_id,
                            GREATEST(day_serie.day_serie, timezone(calendar.tz::text, work_entry.date_start::timestamp with time zone))::date AS day,
                            advantage.benefit_name,
-                           ROW_NUMBER() OVER() AS id
+                           1 AS id
 
                       FROM hr_work_entry work_entry
                       JOIN hr_contract contract ON work_entry.contract_id = contract.id
