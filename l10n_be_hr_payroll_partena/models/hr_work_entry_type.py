97c8bae89d1d9be4351cb131bee15f8d79958a4a
flotchet <mafl@odoo.com>
2025-03-19 09:39:27 +0000

    [FIX] l10n_be_hr_payroll_partena: size of w.e.t.
    The partena code on work entry type should be 5 characters long
    and not 3.
    
    task-4658021
    
    closes odoo/enterprise#82616
    
    X-original-commit: 99c1ba371c3dc6ad71403734fe5d052e71e97482
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: Florent Maisse (mafl) <mafl@odoo.com>

--- l10n_be_hr_payroll_partena/models/hr_work_entry_type.py
+++ l10n_be_hr_payroll_partena/models/hr_work_entry_type.py
@@ -12,5 +12,5 @@ class HrWorkEntryType(models.Model):
 
     @api.constrains('partena_code')
     def _check_partena_code(self):
-        if any(we.partena_code and len(we.partena_code) != 3 for we in self):
-            raise ValidationError(_('The code should have 3 characters!'))
+        if any(we.partena_code and len(we.partena_code) != 5 for we in self):
+            raise ValidationError(_('The code should have 5 characters!'))
