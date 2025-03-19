22fb7cbb38319aee8d4e8435bffe2dbcbc600966
Andrew Gavgavian <andg@odoo.com>
2025-03-19 13:00:30 -0700

    [IMP] l10n_ar_edi: improve connection test popup window and style
    The previous connection test window showed up as an UserError with
    the title "Invalid Operation" which can be confusing to users when
    all services show as available.
    
    Replace the UserError with a RedirectWarning that has a better title
    message ("Odoo Warning"). As it needs to redirect to somewhere, we
    redirect back to the current accounting page in settings.
    
    task-4288536
    
    closes odoo/enterprise#80415
    
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- l10n_ar_edi/models/res_config_settings.py
+++ l10n_ar_edi/models/res_config_settings.py
@@ -1,7 +1,7 @@
 import re
 
 from odoo import _, fields, models
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, RedirectWarning
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
@@ -62,8 +62,12 @@ class ResConfigSettings(models.TransientModel):
                         error=repr(error),
                     ),
                 )
-
-        raise UserError("\n".join(results))
+        action = self.env.ref('account.action_account_config')
+        raise RedirectWarning(
+            "\n".join(results),
+            action=action.id,
+            button_text=_("Continue Configurations")
+        )
 
     def random_demo_cert(self):
         self.company_id.set_demo_random_cert()
