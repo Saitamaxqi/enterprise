1a8e8be5494383c31699a6a0c96db4967c145cb7
Guillaume Jacquet <guja@odoo.com>
2025-03-20 10:05:49 +0100

    [MOV] website_appointment: preparation for refactoring
    Part-of: odoo/enterprise#81314
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- website_appointment/__manifest__.py
+++ website_appointment/__manifest__.py
@@ -48,8 +48,6 @@ Allow clients to Schedule Appointments through your Website
         'web.assets_unit_tests_setup': [
             'website_appointment/static/src/snippets/**/*.js',
             ('remove', 'website_appointment/static/src/snippets/**/options.js'),
-            # TODO Remove when all 000 have been adapted
-            ('remove', 'website_appointment/static/src/snippets/**/000.js'),
         ],
         'web.assets_frontend': [
             'website_appointment/static/src/scss/website_appointment.scss',
@@ -58,7 +56,6 @@ Allow clients to Schedule Appointments through your Website
             'website_appointment/static/src/xml/appointment_no_slot.xml',
             'website_appointment/static/src/js/appointment_frontend/*.js',
             'website_appointment/static/src/snippets/**/*.js',
-            ('remove', 'website_appointment/static/src/snippets/**/000.js'),
             ('remove', 'website_appointment/static/src/snippets/**/options.js'),
         ],
         'website.assets_editor': [
