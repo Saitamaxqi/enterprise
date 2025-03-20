c49aafd8282646762916b34ebd68a15fa7f80dcc
SaddemAmine <sana@odoo.com>
2025-03-20 16:42:38 +0100

    [IMP] l10n_be_reports: remove redundant activity method
    Due to an oversight, we added an unnecessary activity scheduling method in
    d684d074af82fbd0fac6b3a1c56b4e01a17dd74a. This should be replaced
    by the already existing `activity_schedule` method in `account.move`.
    
    No task ID.
    
    closes odoo/enterprise#81855
    
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- l10n_be_reports/models/account_move.py
+++ l10n_be_reports/models/account_move.py
@@ -1,20 +1,10 @@
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields, models, _
+from odoo import models, _
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-
-    def _schedule_activity(self, activity_type_xmlid, summary, date_deadline):
-        self.ensure_one()
-        self.env['mail.activity'].create({
-            'activity_type_id': self.env.ref(activity_type_xmlid).id,
-            'summary': summary,
-            'res_id': self.id,
-            'res_model_id': self.env['ir.model']._get('account.move').id,
-            'date_deadline': date_deadline,
-        })
 
     def action_post(self):
         action = super().action_post()
@@ -42,17 +32,17 @@ class AccountMove(models.Model):
             grids_key = list(grids.keys())[0]
 
             if any(grid.get('value') for grid in grids[grids_key].values()):
-                record._schedule_activity(
-                    'l10n_be_reports.ec_sales_list_activity',
-                    _('Generate the EC Sales List: %s', ec_sales_list_deadline.strftime("%B %Y")),
-                    ec_sales_list_deadline
+                record.activity_schedule(
+                    act_type_xmlid='l10n_be_reports.ec_sales_list_activity',
+                    summary=_( 'Generate the EC Sales List: %s', ec_sales_list_deadline.strftime("&B &y")),
+                    date_deadline=ec_sales_list_deadline,
                 )
 
             if record.date.month in {10, 11, 12} and self.env.context.get('l10n_be_reports_generation_options', {}).get('client_nihil'):
-                record._schedule_activity(
-                    'l10n_be_reports.partner_vat_listing_report_activity',
-                    _('Generate the Partner VAT Listing: %s', partner_vat_listing_deadline.strftime("%Y")),
-                    partner_vat_listing_deadline
+                record.activity_schedule(
+                    act_type_xmlid='l10n_be_reports.partner_vat_listing_report_activity',
+                    summary=_( 'Generate the Partner VAT Listing: %s', partner_vat_listing_deadline.strftime("&y")),
+                    date_deadline=partner_vat_listing_deadline,
                 )
 
         return action
