f7d0dba9bed97c956e4657361dac159d43a0afac
Pedram (PEBR) <pebr@odoo.com>
2025-03-25 10:18:53 +0000

    [FIX] l10n_mx_edi_pos: prevent error when paying an order
    When a company partner is selected for an order, the commit https://github.com/odoo/odoo/commit/270779c8250272920d759fbe7b6cfc889e2f5969
    automatically sets the order to invoice. This causes the order to miss
    required localization fields, leading to an error during payment.
    
    opw-4670131
    
    closes odoo/enterprise#83432
    
    X-original-commit: 3258b2493635dba7b177f18cfa5a0751b252a80f
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- l10n_mx_edi_pos/static/src/app/screens/payment_screen/payment_screen.js
+++ l10n_mx_edi_pos/static/src/app/screens/payment_screen/payment_screen.js
@@ -1,6 +1,4 @@
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
-import { makeAwaitable } from "@point_of_sale/app/utils/make_awaitable_dialog";
-import { AddInfoPopup } from "@l10n_mx_edi_pos/app/components/popups/add_info_popup/add_info_popup";
 import { patch } from "@web/core/utils/patch";
 
 patch(PaymentScreen.prototype, {
@@ -11,15 +9,8 @@ patch(PaymentScreen.prototype, {
     //@override
     async toggleIsToInvoice() {
         if (this.pos.company.country_id?.code === "MX" && !this.currentOrder.isToInvoice()) {
-            const payload = await makeAwaitable(this.dialog, AddInfoPopup, {
-                order: this.currentOrder,
-            });
-            if (payload) {
-                this.currentOrder.l10n_mx_edi_cfdi_to_public =
-                    payload.l10n_mx_edi_cfdi_to_public === true ||
-                    payload.l10n_mx_edi_cfdi_to_public === "1";
-                this.currentOrder.l10n_mx_edi_usage = payload.l10n_mx_edi_usage;
-            } else {
+            const addedMxFields = await this.pos.addL10nMxEdiFields(this.currentOrder);
+            if (!addedMxFields) {
                 this.currentOrder.setToInvoice(!this.currentOrder.isToInvoice());
             }
         }
--- l10n_mx_edi_pos/static/src/app/services/pos_store.js
+++ l10n_mx_edi_pos/static/src/app/services/pos_store.js
@@ -1,6 +1,8 @@
 import { PosStore } from "@point_of_sale/app/services/pos_store";
 import { patch } from "@web/core/utils/patch";
 import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { AddInfoPopup } from "@l10n_mx_edi_pos/app/components/popups/add_info_popup/add_info_popup";
+import { makeAwaitable } from "@point_of_sale/app/utils/make_awaitable_dialog";
 import { _t } from "@web/core/l10n/translation";
 
 patch(PosStore.prototype, {
@@ -20,8 +22,25 @@ patch(PosStore.prototype, {
                 });
                 return;
             }
+            if (currentOrder.isToInvoice() && !currentOrder.l10n_mx_edi_usage) {
+                const addedMxFields = await this.addL10nMxEdiFields(currentOrder);
+                if (!addedMxFields) {
+                    currentOrder.setToInvoice(false);
+                }
+            }
         }
 
         return super.pay(...arguments);
+    },
+    async addL10nMxEdiFields(order) {
+        const payload = await makeAwaitable(this.dialog, AddInfoPopup, { order });
+        if (payload) {
+            order.l10n_mx_edi_cfdi_to_public =
+                payload.l10n_mx_edi_cfdi_to_public === true ||
+                payload.l10n_mx_edi_cfdi_to_public === "1";
+            order.l10n_mx_edi_usage = payload.l10n_mx_edi_usage;
+            return true;
+        }
+        return false;
     },
 });
