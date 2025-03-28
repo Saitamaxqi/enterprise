f9799758d36ba50084ad11eaa79e79952eddff3a
Mayurrajsinh Rathod <mrsr@odoo.com>
2025-03-28 18:46:50 +0530

    [IMP] account_sepa_direct_debit, *: improve contact ui
    This commit reuses `account_sepa_direct_debit_mandate_tree_act` action for the
    contact stat button to show the same helper text as the menu action in accounting.
    
    We update few demo datas to show correct type of partner
    
    Part of https://github.com/odoo/odoo/pull/190466
    
    Task-4377720
    
    Part-of: odoo/enterprise#75958
    Related: odoo/odoo#190466
    Related: odoo/upgrade#7477
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- account_sepa_direct_debit/models/res_partner.py
+++ account_sepa_direct_debit/models/res_partner.py
@@ -18,3 +18,13 @@ class ResPartner(models.Model):
         mapped_data = {partner.id: count for partner, count in sdd_data}
         for partner in self:
             partner.sdd_count = mapped_data.get(partner.id, 0)
+
+    def action_open_ssd_mandates(self):
+        self.ensure_one()
+        action = self.env['ir.actions.act_window']._for_xml_id('account_sepa_direct_debit.account_sepa_direct_debit_mandate_tree_act')
+        action['context'] = {
+            'default_partner_id': self.id,
+            'search_default_account_sdd_mandate_active_filter': 1,
+        }
+        action['domain'] = [('partner_id', '=', self.id)]
+        return action
