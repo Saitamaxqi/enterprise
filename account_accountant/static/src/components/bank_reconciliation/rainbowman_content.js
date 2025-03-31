e78b65b575b43c699c6234aa45a5ab3263553117
Denis Ledoux <dle@odoo.com>
2025-03-31 13:57:05 +0000

    [IMP] account_accountant: enable changing filters behind rainbowman
    This is more an UX problem rather than a bug.
    
    In the reconciliation tool, when you reconciled all the transactions
    of the current filter, rainbow mans appears.
    
    Then, all the background behind the rainbow overlay is disabled,
    you can no longer edit the filters in the search for instance,
    and the only buttons available become "All transactions" and "Back to
    dashboard". Both buttons will make you loose your search filters:
    - You loose the "Not Matched" filter which is set automatically
    when you hit the "x to reconcile" button in the accounting dashboard,
    and hence you see the already reconciled transactions,
    while you were actually interested only in the non-reconciled
    transactions
    - If you made a complicated filter, you loose them all and you
    are good to redo them all.
    
    This revision offers the possibility to have the background
    behind the rainbow overlay clickable, so you would be able
    to change the filters, for instance only the last filter,
    in order to continue your reconciliation where you were,
    instead of being completely reset.
    
    closes odoo/enterprise#82922
    
    X-original-commit: c6b32348741b2b8b8242204b438ceb9cb473c856
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- account_accountant/static/src/components/bank_reconciliation/rainbowman_content.js
+++ account_accountant/static/src/components/bank_reconciliation/rainbowman_content.js
@@ -1,5 +1,5 @@
 import { BankRecFinishButtons } from "./finish_buttons";
-import { Component, onWillUnmount } from "@odoo/owl";
+import { Component, onMounted, onWillUnmount } from "@odoo/owl";
 
 export class BankRecRainbowContent extends Component {
     static template = "account_accountant.BankRecRainbowContent";
@@ -9,6 +9,10 @@ export class BankRecRainbowContent extends Component {
     setup() {
         onWillUnmount(() => {
             this.env.methods.initReconCounter();
+        });
+        onMounted(() => {
+            document.querySelector(".o_reward").style.pointerEvents = "none";
+            document.querySelector(".o_reward_msg").style.pointerEvents = "auto";
         });
     }
 }
