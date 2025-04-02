6e728fdd9e630998d03a10553f3843e3c2dfad74
Julien Mougenot <jum@odoo.com>
2025-04-02 13:14:21 +0200

    [FIX] voip: fix tests taking too long
    This commit fixes a couple of unit tests in the VOIP addon which
    performed `advanceTime` calls with huge time leaps (e.g. 24h), which
    triggered a whole lot of `heartbeat` calls from the "multi_tab" service.
    
    This caused long needless CPU locks that may crash these tests.
    
    To fix this issue, this commit adds some `blockTimers: true` options
    to the `advanceTime` calls to prevent more timers to be added during the
    time leaps, and thus speeding up the tests to only take the time they
    actually need.
    
    Additionally, these tests were not properly awaiting changes made on the
    "res.users.settings" model when setting "Do not disturb" values,
    resulting in non-deterministic failures. This has been fixed as well, by
    properly awaiting changes on the model before proceeding.
    
    Part-of: odoo/enterprise#82734
    Related: odoo/odoo#204471
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- voip/static/tests/softphone/do_not_disturb_selector.test.js
+++ voip/static/tests/softphone/do_not_disturb_selector.test.js
@@ -1,8 +1,8 @@
 import { describe, test } from "@odoo/hoot";
 import { advanceTime, mockDate } from "@odoo/hoot-mock";
 
+import { waitNotifications } from "@bus/../tests/bus_test_helpers";
 import { click, contains, start, startServer } from "@mail/../tests/mail_test_helpers";
-
 import { setupVoipTests } from "@voip/../tests/voip_test_helpers";
 
 describe.current.tags("desktop");
@@ -30,11 +30,12 @@ test("Do not disturb selector change state correctly with limited time", async (
     await click(".o-voip-DoNotDisturbSelector-statusIcon[title='Available']");
     await click("p", { text: "Do Not Disturb" });
     await click("button", { text: "For 15 minutes" });
+    await waitNotifications(["res.users.settings"]);
     await click(".o-voip-DoNotDisturbSelector-statusIcon.text-danger");
     await contains("p", { text: "Until Jan 1, 2025, 1:15 AM" });
-    advanceTime(14 * 60 * 1000);
+    await advanceTime(14 * 60 * 1000, { blockTimers: true });
     await contains(".o-voip-DoNotDisturbSelector-statusIcon.text-danger");
-    advanceTime(1 * 60 * 1000);
+    await advanceTime(1 * 60 * 1000, { blockTimers: true });
     await contains(".o-voip-DoNotDisturbSelector-statusIcon.text-success");
 });
 
@@ -45,10 +46,12 @@ test("Do not disturb selector change state correctly with infinite time", async 
     await click(".o-voip-DoNotDisturbSelector-statusIcon[title='Available']");
     await click("p", { text: "Do Not Disturb" });
     await click("button", { text: "Until I turn it back on" });
+    await waitNotifications(["res.users.settings"]);
     await click(".o-voip-DoNotDisturbSelector-statusIcon.text-danger");
     await contains("p", { text: "Until I turn it back on" });
-    advanceTime(24 * 60 * 60 * 1000);
+    await advanceTime(24 * 60 * 60 * 1000, { blockTimers: true });
     await contains(".o-voip-DoNotDisturbSelector-statusIcon.text-danger");
     await click(".o-dropdown-item p", { text: "Available" });
+    await waitNotifications(["res.users.settings"]);
     await contains(".o-voip-DoNotDisturbSelector-statusIcon.text-success");
 });
