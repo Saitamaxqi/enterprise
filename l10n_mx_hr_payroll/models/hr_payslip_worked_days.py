d44c1339bfa6b1beaeee085a89f7ae22f63649bb
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-04-07 14:04:07 +0000

    [FIX] l10n_mx_hr_payroll_localisation: hourly wage
    Steps to reproduce:
    - Install payroll, payroll Mexican localisation
    - Switch to the Mexican company
    - Choose an employee from the Mexican company
    - Change the contract to be hourly waged instead of monthly
    - Set a value for the wage/hour
    - Create some work entries for that employee
    - Generate a payslip for the employee in the duration you are testing
    - Edit the work entries you have to have some unpaid leaves
    - Generate another payslip for the employee
    
    Current Behavior:
    Payslip is generated without taking into consideration the number of
    hours actually worked and the unpaid leaves taken by the employee. It
    also generates the payslip with the value of the wage/month regardless
    of the value of the wage/hour you added.
    
    Issue:
    In df8f53b0cad4738537f18890e628c286dedaa141, Mexican localisation was
    implemented but with computing the amount of the `payslip_worked_days`
    using the `contract.wage` which is the fixed amount of wage/month.
    
    https://github.com/odoo/enterprise/blob/25caeeab01abc0b47f6bd6b02658c3bbbe0aba96/l10n_mx_hr_payroll_localisation/models/hr_payslip_worked_days.py#L19-L20
    
    opw-4604675
    
    closes odoo/enterprise#84715
    
    X-original-commit: 92f48d4df3622aa4f420e599b1cf4cf1d96574d0
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>
    Signed-off-by: Hussein Yasser Hussein Ebrahim (hyhe) <hyhe@odoo.com>

--- l10n_mx_hr_payroll/models/hr_payslip_worked_days.py
+++ l10n_mx_hr_payroll/models/hr_payslip_worked_days.py
@@ -16,15 +16,23 @@ class HrPayslipWorkedDays(models.Model):
                 worked_days.amount = 0
                 continue
 
-            days_in_period = worked_days.payslip_id._rule_parameter('l10n_mx_schedule_table')[worked_days.payslip_id.contract_id.l10n_mx_schedule_pay]
-            period_wage = worked_days.payslip_id.contract_id.wage * days_in_period / 30
-
             start_date = max(worked_days.payslip_id.date_from, worked_days.contract_id.date_start)
             end_date = min(worked_days.payslip_id.date_to, worked_days.contract_id.date_end) if worked_days.contract_id.date_end else worked_days.payslip_id.date_to
             in_contract_days = (end_date - start_date).days + 1
             actual_period_days = (worked_days.payslip_id.date_to - worked_days.payslip_id.date_from).days + 1
             salary_factor = in_contract_days / actual_period_days
 
-            sum_worked_hours = sum(line.number_of_hours for line in worked_days.payslip_id.worked_days_line_ids if not line.is_credit_time and line.code != 'OUT')
-            worked_days.amount = period_wage * salary_factor * worked_days.number_of_hours / (sum_worked_hours or 1) if worked_days.is_paid else 0
+            period_wage = worked_days._get_period_wage()
+            worked_days.amount = period_wage * salary_factor
         return super(HrPayslipWorkedDays, self - mx_worked_days)._compute_amount()
+
+    def _get_period_wage(self):
+        self.ensure_one()
+        sum_worked_hours = sum(line.number_of_hours for line in
+    self.payslip_id.worked_days_line_ids if not line.is_credit_time and line.code != 'OUT')
+        if not self.is_paid:
+            return 0
+        if self.contract_id.wage_type == 'hourly':
+            return self.contract_id.hourly_wage * self.number_of_hours
+        else:
+            return self.contract_id.wage * self.number_of_hours / (sum_worked_hours or 1)
