ad5f50962c3bd6ac4c7137918796027b0f8f9a7d
Aurelien MARIE (auma) <auma@odoo.com>
2025-04-10 13:37:00 +0000

    [PERF] mrp_workorder: manual limit change on Shop Floor
    Description:
    
    By default, it is impossible to change the default limit of card
    displayed on shop floor. This PR aim to change that by adding a
    "limit" in the `context` field of the action.
    
    Reference:
    
    opw-4707551
    
    closes odoo/enterprise#85208
    
    X-original-commit: c194890887cacb2ceb02145409d3f367019d8917
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Aurélien Marie (auma) <auma@odoo.com>

--- mrp_workorder/static/src/mrp_display/mrp_display.js
+++ mrp_workorder/static/src/mrp_display/mrp_display.js
@@ -88,7 +88,7 @@ export class MrpDisplay extends Component {
             workcenters,
             canLoadSamples: false,
             offset: 0,
-            limit: 40,
+            limit: this.props.action?.context?.limit || 40,
             firstLoad: firstLoad,
         });
         this.recordCacheIds = [];
