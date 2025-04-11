e74453e9f08e1d1ba9334e45fe2c850e5e6c70c2
Valentin Chevalier <vcr@odoo.com>
2025-04-11 18:19:01 +0200

    [FIX] appointment_account_payment: allow public access to custom product placeholders
    Placeholders were introduced in odoo/odoo@c52453f for meta products such
    as discounts, gift cards, event tickets, and appointments.
    
    Previously, access rights prevented public users from viewing these
    placeholders. Even when no image was linked to a record, access was
    denied due to read restrictions, defaulting to the generic placeholder
    instead of the custom one provided by `_get_product_placeholder_filename`.
    
    This commit overrides `_can_return_content` in relevant modules to allow
    proper access for public users, ensuring the intended placeholder is
    displayed.
    
    closes odoo/enterprise#83326
    
    Related: odoo/odoo#205693
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- appointment_account_payment/models/__init__.py
+++ appointment_account_payment/models/__init__.py
@@ -5,5 +5,4 @@ from . import appointment_answer_input
 from . import appointment_type
 from . import calendar_booking_line
 from . import calendar_booking
-from . import product_product
 from . import templates
