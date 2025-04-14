65bcea82ed93e6c8b25ae27b9e5d4be831f51c45
Louis (loti) <loti@odoo.com>
2025-04-14 15:37:11 +0200

    [REF] l10n_co_edi: convert address page Public Widget to Interaction
    task-4653701
    
    closes odoo/enterprise#81476
    
    Related: odoo/odoo#201730
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- l10n_co_edi/__manifest__.py
+++ l10n_co_edi/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Electronic invoicing for Colombia with Carvajal',
@@ -37,7 +36,7 @@
     ],
     'assets': {
         'web.assets_frontend': [
-            'l10n_co_edi/static/src/js/portal_address.js',
+            'l10n_co_edi/static/src/interactions/**/*',
         ],
     },
     'installable': True,
