8635d440e4f2078e2c16ab3fc90db2834df3536b
mano-odoo <mano@odoo.com>
2025-04-17 11:27:42 +0000

    [FIX] mrp_workorder: shop floor remove locked operator
    This commit removes the styling associated with the locked state of the
    operators.
    
    The logic can probably be removed too, added some comment for dev.
    
    follow-up task-4346172
    task-4735784
    
    closes odoo/enterprise#85126
    
    X-original-commit: 36697dfc7f3206987380603ff9a000eae5df07cd
    Signed-off-by: Claeys Pieter (clpi) <clpi@odoo.com>
    Signed-off-by: Mathieu Notté (mano) <mano@odoo.com>

--- mrp_workorder/static/src/mrp_display/mrp_display.js
+++ mrp_workorder/static/src/mrp_display/mrp_display.js
@@ -395,6 +395,7 @@ export class MrpDisplay extends Component {
     }
 
     get adminWorkorderIds() {
+        // TODO remove lock,check if we can remove isPreviousAdmin
         const adminId =
             this.useEmployee.employees.admin.id ||
             this.useEmployee.employees.connected.find((e) => e.isPreviousAdmin)?.id;
