Saitamaxqi/enterprise

commit 42cdb3658688630c143797620a6b3c236defa08e

[IMP] account_followup: set activity on all salespersons
When a follow-up level is set to create an activity to Salespeople, it now creates an activity to all Salespersons from all Invoices part of the follow-up. Where before an activity would only be created for one SSalespersons.

closes odoo/enterprise#77029

Task: 4374391
Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

account_followup/models/res_partner.py
@@ -194,16 +194,25 @@ class ResPartner(models.Model):
             ('company_id', 'child_of', self.env.company.id),
         ]
 
-    def _get_followup_responsible(self):
+    def _get_followup_responsible(self, multiple_responsible=False):
         self.ensure_one()
 
         responsible_type = self.followup_line_id.activity_default_responsible_type
         if responsible_type == 'account_manager' and self.user_id:
             return self.user_id
 
-        most_delayed_aml = self._included_unreconciled_aml_max_followup().get('most_delayed_aml')
-        if responsible_type == 'salesperson' and most_delayed_aml and most_delayed_aml.move_id.invoice_user_id:
-            return most_delayed_aml.move_id.invoice_user_id
+        all_aml_responsible = self.env['res.users']
+        max_amount_aml = self.env['account.move.line']
+        for aml in self.unreconciled_aml_ids:
+            all_aml_responsible += aml.move_id.invoice_user_id
+            if max_amount_aml.amount_residual < aml.amount_residual:
+                max_amount_aml = aml
+
+        if responsible_type == 'salesperson' and max_amount_aml:
+            if multiple_responsible:
+                return all_aml_responsible
+            else:
+                return max_amount_aml.move_id.invoice_user_id
 
         if self.followup_responsible_id:
             return self.followup_responsible_id
@@ -211,8 +220,8 @@ class ResPartner(models.Model):
         if self.user_id:
             return self.user_id
 
-        if most_delayed_aml and most_delayed_aml.move_id.invoice_user_id:
-            return most_delayed_aml.move_id.invoice_user_id
+        if max_amount_aml and max_amount_aml.move_id.invoice_user_id:
+            return max_amount_aml.move_id.invoice_user_id
 
         return super()._get_followup_responsible()
 
@@ -227,45 +236,6 @@ class ResPartner(models.Model):
             followup_contacts = self.env['res.partner'].browse(self.address_get(['invoice'])['invoice'])
         return followup_contacts
 
-    def _included_unreconciled_aml_max_followup(self):
-        """ Computes the maximum delay in days and the highest level of followup (followup line with highest delay) of all the unreconciled amls included.
-        Also returns the delay for the next level (after the highest_followup_line), the most delayed aml and a boolean specifying if any invoice is overdue.
-        :return dict with key/values: most_delayed_aml, max_delay, highest_followup_line, next_followup_delay, has_overdue_invoices
-        """
-        self.ensure_one()
-        today = fields.Date.context_today(self)
-        highest_followup_line = None
-        most_delayed_aml = self.env['account.move.line']
-        first_followup_line = self._get_first_followup_level()
-        # Minimum value for delay, will always be smaller than any other delay
-        max_delay = first_followup_line.delay - 1
-        has_overdue_invoices = False
-        for aml in self.unreconciled_aml_ids:
-            aml_delay = (today - (aml.date_maturity or aml.date)).days
-
-            is_overdue = aml_delay > 0
-            if is_overdue:
-                has_overdue_invoices = True
-
-            if self.env.company in aml.company_id.parent_ids:
-                if aml.followup_line_id and aml.followup_line_id.delay >= (highest_followup_line or first_followup_line).delay:
-                    highest_followup_line = aml.followup_line_id
-                max_delay = max(max_delay, aml_delay)
-                if most_delayed_aml.amount_residual < aml.amount_residual:
-                    most_delayed_aml = aml
-        followup_lines_info = self._get_followup_lines_info()
-        next_followup_delay = None
-        if followup_lines_info:
-            key = highest_followup_line.id if highest_followup_line else None
-            current_followup_line_info = followup_lines_info.get(key)
-            next_followup_delay = current_followup_line_info.get('next_delay')
-        return {
-            'most_delayed_aml': most_delayed_aml,
-            'max_delay': max_delay,
-            'highest_followup_line': highest_followup_line,
-            'next_followup_delay': next_followup_delay,
-            'has_overdue_invoices': has_overdue_invoices,
-        }
 
     def _get_invoices_to_print(self, options):
         self.ensure_one()
@@ -482,12 +452,13 @@ class ResPartner(models.Model):
 
             if followup_line.create_activity:
                 # log a next activity for today
-                self.activity_schedule(
-                    activity_type_id=followup_line.activity_type_id and followup_line.activity_type_id.id or self._default_activity_type().id,
-                    note=followup_line.activity_note,
-                    summary=followup_line.activity_summary,
-                    user_id=(self._get_followup_responsible()).id
-                )
+                for user in self._get_followup_responsible(multiple_responsible=True):
+                    self.activity_schedule(
+                        activity_type_id=followup_line.activity_type_id and followup_line.activity_type_id.id or self._default_activity_type().id,
+                        note=followup_line.activity_note,
+                        summary=followup_line.activity_summary,
+                        user_id=user.id
+                    )
 
             self._update_next_followup_action_date(followup_line)
 

account_followup/tests/test_account_followup.py
@@ -93,10 +93,36 @@ class TestAccountFollowupReports(AccountTestInvoicingCommon):
         # 4- Modify the default responsible on followup level
         self.partner_a.followup_line_id.activity_default_responsible_type = 'salesperson'
         self.assertEqual(self.partner_a._get_followup_responsible(), user1)
+        self.assertEqual(self.partner_a._get_followup_responsible(multiple_responsible=True), (user1 + user2))
 
         self.partner_a.followup_line_id.activity_default_responsible_type = 'account_manager'
         self.partner_a.user_id = user2
         self.assertEqual(self.partner_a._get_followup_responsible(), self.partner_a.user_id)
+
+    def test_followup_activity(self):
+        first_followup_line = self.create_followup(delay=10)
+        first_followup_line.create_activity = True
+        first_followup_line.activity_default_responsible_type = 'salesperson'
+        user1 = self.env['res.users'].create({
+            'name': 'A User',
+            'login': 'a_user',
+            'email': 'a@user.com',
+            'groups_id': [Command.set([self.env.ref('account.group_account_user').id])]
+        })
+        user2 = self.env['res.users'].create({
+            'name': 'Another User',
+            'login': 'another_user',
+            'email': 'another@user.com',
+            'groups_id': [Command.set([self.env.ref('account.group_account_user').id])]
+        })
+        inv1 = self.create_invoice('2022-01-02')
+        inv1.invoice_user_id = user1
+        inv2 = self.create_invoice('2022-01-02')
+        inv2.invoice_user_id = user2
+
+        with freeze_time('2022-01-13'):
+            self.partner_a._execute_followup_partner(options={'snailmail': False})
+            self.assertEqual(self.partner_a.activity_ids.user_id, (user1 + user2))
 
     def test_followup_line_and_status(self):
         self.first_followup_line = self.create_followup(delay=-10)

commit 69119ca9db46e62ff066de5586880e3d233db3c8

[FIX] account_followup: no move_id.invoice_user_id on max_amount_aml
A followup report will crash if some partner don't have followup responsible. Bug introduced in commit https://github.com/odoo/enterprise/commit/42cdb3658688630c143797620a6b3c236defa08e

closes odoo/enterprise#83752

X-original-commit: cb6122584bbc1a3ac7a719e638b7de52de258ce1
Signed-off-by: William André (wan) <wan@odoo.com>

account_followup/models/res_partner.py
@@ -201,29 +201,21 @@ class ResPartner(models.Model):
         if responsible_type == 'account_manager' and self.user_id:
             return self.user_id
 
-        all_aml_responsible = self.env['res.users']
-        max_amount_aml = self.env['account.move.line']
-        for aml in self.unreconciled_aml_ids:
-            all_aml_responsible += aml.move_id.invoice_user_id
-            if max_amount_aml.amount_residual < aml.amount_residual:
-                max_amount_aml = aml
-
-        if responsible_type == 'salesperson' and max_amount_aml:
-            if multiple_responsible:
-                return all_aml_responsible
-            else:
-                return max_amount_aml.move_id.invoice_user_id
-
-        if self.followup_responsible_id:
-            return self.followup_responsible_id
-
-        if self.user_id:
-            return self.user_id
-
-        if max_amount_aml and max_amount_aml.move_id.invoice_user_id:
-            return max_amount_aml.move_id.invoice_user_id
-
-        return super()._get_followup_responsible()
+        all_aml_responsible = self.unreconciled_aml_ids.move_id.invoice_user_id
+        max_amount_aml = max(
+            self.unreconciled_aml_ids.filtered('move_id.invoice_user_id'),
+            default=self.env['account.move.line'],
+            key=lambda l: l.amount_residual,
+        )
+        return (
+            (responsible_type == 'salesperson' and (
+                all_aml_responsible if multiple_responsible else max_amount_aml.move_id.invoice_user_id
+            ))
+            or self.followup_responsible_id
+            or self.user_id
+            or max_amount_aml.move_id.invoice_user_id
+            or super()._get_followup_responsible()
+        )
 
     def _get_all_followup_contacts(self):
         """ Followup contacts are defined as billing address and defaults to

account_followup/tests/test_account_followup.py
@@ -113,6 +113,15 @@ class TestAccountFollowupReports(TestAccountFollowupCommon):
             self.partner_a._execute_followup_partner(options={'snailmail': False})
             self.assertEqual(self.partner_a.activity_ids.user_id, (user1 + user2))
 
+    def test_followup_no_invoice_user_id(self):
+        first_followup_line = self.create_followup(delay=10)
+        first_followup_line.create_activity = True
+        first_followup_line.activity_default_responsible_type = 'salesperson'
+        inv1 = self.create_invoice('2022-01-02')
+        inv1.invoice_user_id = None
+        with freeze_time('2022-01-13'):
+            self.assertEqual(self.partner_a._get_followup_responsible(), self.user)
+
     def test_followup_line_and_status(self):
         self.first_followup_line = self.create_followup(delay=-10)
         self.second_followup_line = self.create_followup(delay=10)