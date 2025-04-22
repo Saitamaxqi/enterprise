c9d4485ad559009a4ffe1788cacfbde67d8d60f8
Xavier Morel <xmo@odoo.com>
2025-04-22 11:09:38 +0000

    [FIX] l10n_cl_edi_stock: test ACLs
    Most tests of the module need to create a sale order, which requires
    the `group_sale_salesman` at least. Set this group.
    
    https://runbot.odoo.com/odoo/error/163649
    
    closes odoo/enterprise#83808
    
    X-original-commit: 153c660b689330ecd5ef7d3c4e8aca94e22dc074
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- l10n_cl_edi_stock/tests/common.py
+++ l10n_cl_edi_stock/tests/common.py
@@ -14,6 +14,7 @@ class TestL10nClEdiStockCommon(ValuationReconciliationTestCommon):
     def setUpClass(cls):
         super().setUpClass()
 
+        cls.env.user.group_ids |= cls.env.ref("sales_team.group_sale_salesman")
         cls.env.company.write({
             'name': 'Blanco Martin & Asociados EIRL',
             'street': 'Apoquindo 6410',
