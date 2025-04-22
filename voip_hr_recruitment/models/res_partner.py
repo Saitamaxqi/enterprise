0b98200d68d6f6a1bf474f96cbfd9babef0a1a0c
Louis Wicket (wil) <wil@odoo.com>
2025-04-22 17:53:51 +0200

    [FIX] voip_hr_recruitment: load contacts without hr.applicant access
    Before this commit, the code unconditionnally reads and sends
    hr.applicant records linked to a VoIP contact, leading to a crash in
    case the current user doesn't have access to hr.applicant records.
    
    After this commit, applicant data are only sent if the user has access
    to hr.applicant.
    
    closes odoo/enterprise#83844
    
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- voip_hr_recruitment/models/res_partner.py
+++ voip_hr_recruitment/models/res_partner.py
@@ -1,4 +1,5 @@
 from odoo import models
+from odoo.exceptions import AccessError
 
 from odoo.addons.mail.tools.discuss import Store
 
@@ -7,4 +8,15 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     def _voip_get_store_fields(self):
-        return [*super()._voip_get_store_fields(), Store.Many("applicant_ids", [Store.One("partner_id", []), "partner_name"])]
+        def can_read_applicant_ids(partner):
+            try:
+                partner.fetch(["applicant_ids"])
+            except AccessError:
+                return False
+            else:
+                return True
+
+        return [
+            *super()._voip_get_store_fields(),
+            Store.Many("applicant_ids", [Store.One("partner_id", []), "partner_name"], predicate=can_read_applicant_ids),
+        ]
