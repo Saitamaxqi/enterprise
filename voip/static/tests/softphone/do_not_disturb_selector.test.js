c8c83c158b08f63b34358d72da24da810410e1ec
Louis Wicket (wil) <wil@odoo.com>
2025-04-22 12:48:13 +0200

    [FIX] voip: block timers in test and wait for bus notifications
    Restore b9853be51a16d800b9b131a8c59c6c92a22c9552, inadvertently reverted
    during refactoring.
    
    closes odoo/enterprise#83801
    
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- voip/static/tests/softphone/do_not_disturb_selector.test.js
+++ voip/static/tests/softphone/do_not_disturb_selector.test.js
@@ -1,7 +1,10 @@
+import { waitNotifications } from "@bus/../tests/bus_test_helpers";
+
+import { click, contains, start, startServer } from "@mail/../tests/mail_test_helpers";
+
 import { describe, test } from "@odoo/hoot";
 import { advanceTime, mockDate } from "@odoo/hoot-mock";
 
-import { click, contains, start, startServer } from "@mail/../tests/mail_test_helpers";
 import { setupVoipTests } from "@voip/../tests/voip_test_helpers";
 
 describe.current.tags("desktop");
@@ -29,11 +32,12 @@ test("Do not disturb selector change state correctly with limited time", async (
     await click(".o-voip-DndSelector-badge[title='Available']");
     await click("p", { text: "Do Not Disturb" });
     await click("button", { text: "For 15 minutes" });
+    await waitNotifications(["res.users.settings"]);
     await click(".o-voip-DndSelector-badge i.text-danger");
     await contains("p", { text: "Until Jan 1, 2025, 1:15 AM" });
-    advanceTime(14 * 60 * 1000);
+    await advanceTime(14 * 60 * 1000, { blockTimers: true });
     await contains(".o-voip-DndSelector-badge i.text-danger");
-    advanceTime(1 * 60 * 1000);
+    await advanceTime(1 * 60 * 1000, { blockTimers: true });
     await contains(".o-voip-DndSelector-badge i.text-success");
 });
 
@@ -44,10 +48,12 @@ test("Do not disturb selector change state correctly with infinite time", async 
     await click(".o-voip-DndSelector-badge[title='Available']");
     await click("p", { text: "Do Not Disturb" });
     await click("button", { text: "Until I turn it back on" });
+    await waitNotifications(["res.users.settings"]);
     await click(".o-voip-DndSelector-badge i.text-danger");
     await contains("p", { text: "Until I turn it back on" });
-    advanceTime(24 * 60 * 60 * 1000);
+    await advanceTime(24 * 60 * 60 * 1000, { blockTimers: true });
     await click(".o-voip-DndSelector-badge i.text-danger");
     await click(".o-dropdown-item i.text-success");
+    await waitNotifications(["res.users.settings"]);
     await contains(".o-voip-DndSelector-badge i.text-success");
 });
