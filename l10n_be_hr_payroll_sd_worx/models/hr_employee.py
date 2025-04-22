11fba8d8ee6e763fd4fe7ee843e79d74e2fb9850
romc-odoo <romc@odoo.com>
2025-04-22 12:03:58 +0000

    [FIX] l10n_be_hr_payroll: multi record sdworx code check
    -- How to reproduce -- Duplicate two or more belgian employees.
    
    -- Reason -- The sdworx code check constraint was wrongly single record.
    
    -- Fix -- This commit fixes the issue by converting the check to multi records.
    
    task-4708324
    
    closes odoo/enterprise#83973
    
    X-original-commit: 851da33ee6955c6ac9448a1d59f97fd0586cd565
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- l10n_be_hr_payroll_sd_worx/models/hr_employee.py
+++ l10n_be_hr_payroll_sd_worx/models/hr_employee.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
@@ -12,5 +11,7 @@ class HrEmployee(models.Model):
 
     @api.constrains('sdworx_code')
     def _check_sdworx_code(self):
-        if self.sdworx_code and len(self.sdworx_code) != 7:
-            raise ValidationError(_('The SDWorx code should have 7 characters or should be left empty!'))
+        invalid_employees = self.filtered(lambda employee: employee.sdworx_code and len(employee.sdworx_code) != 7)
+        if invalid_employees:
+            error = _("The following employees should have a 7 characters SDWorx code or should be left empty:\n")
+            raise ValidationError(error + "\n".join(invalid_employees.mapped('name')))
--- l10n_be_hr_payroll_sd_worx/models/hr_work_entry_type.py
+++ l10n_be_hr_payroll_sd_worx/models/hr_work_entry_type.py
@@ -1,4 +1,3 @@
-#-*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
@@ -12,5 +11,7 @@ class HrWorkEntryType(models.Model):
 
     @api.constrains('sdworx_code')
     def _check_sdworx_code(self):
-        if self.sdworx_code and len(self.sdworx_code) != 4:
-            raise ValidationError(_('The code should have 4 characters!'))
+        invalid_work_entry_types = self.filtered(lambda we: we.sdworx_code and len(we.sdworx_code) != 4)
+        if invalid_work_entry_types:
+            error = _("The following work entry types should have a 4 characters SDWorx code or should be left empty:\n")
+            raise ValidationError(error + "\n".join(invalid_work_entry_types.mapped('name')))
