b439b9a41e7f12420699a42120da5787afe4788f
Xavier Morel <xmo@odoo.com>
2025-04-28 09:26:36 +0000

    [FIX] l10n_in_hr_payroll: dependency on demo data
    `hr.dep_rd` is demo data, it's not available anymore during tests in
    the default configuration. So replace it by creating a department on
    the fly. And remove the resolution of `employee_al` which is also demo
    data *and* seems unused.
    
    While at it, nuke a bunch of instance variables which don't seem
    used outside of `setUp`.
    
    https://runbot.odoo.com/odoo/error/161648
    
    closes odoo/enterprise#84350
    
    X-original-commit: ea35d11ea995ffe68f0e0dca13a37d1e121c404b
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- l10n_in_hr_payroll/tests/common.py
+++ l10n_in_hr_payroll/tests/common.py
@@ -10,67 +10,61 @@ from odoo.tests import tagged
 class TestPayrollCommon(TransactionCase):
 
     def setUp(self):
-        super(TestPayrollCommon, self).setUp()
+        super().setUp()
 
-        self.Bank = self.env['res.partner.bank']
-        self.Employee = self.env['hr.employee']
-        self.PayslipRun = self.env['hr.payslip.run']
-        self.PayslipEmployee = self.env['hr.payslip.employees']
-        self.Company = self.env['res.company']
-        self.partner = self.env.ref('base.partner_admin')
-        self.bank_1 = self.env.ref('base.res_bank_1')
-        self.in_country = self.env.ref('base.in')
-        self.rd_dept = self.env.ref('hr.dep_rd')
-        self.employee_fp = self.env.ref('hr.employee_admin')
-        self.employee_al = self.env.ref('hr.employee_al')
+        Bank = self.env['res.partner.bank']
+        Employee = self.env['hr.employee']
+        in_country = self.env.ref('base.in')
+        rd_dept = self.env['hr.department'].create({
+            'name': 'Research and Development',
+        })
+        employee_fp = self.env.ref('hr.employee_admin')
 
-        self.company_in = self.Company.create({
+        self.company_in = self.env['res.company'].create({
             'name': 'Company IN',
             'country_id': self.env.ref('base.in').id,
         })
 
         self.env = self.env(context=dict(self.env.context, allowed_company_ids=self.company_in.ids))
 
-        self.in_bank = self.env['res.bank'].create({
+        in_bank = self.env['res.bank'].create({
             'name': 'Bank IN',
             'bic': 'ABCD0123456'
         })
 
-        # I create a new employee “Rahul”
-        self.rahul_emp = self.Employee.create({
+        self.rahul_emp = Employee.create({
             'name': 'Rahul',
-            'country_id': self.in_country.id,
-            'department_id': self.rd_dept.id,
+            'country_id': in_country.id,
+            'department_id': rd_dept.id,
             'company_id': self.company_in.id,
             'l10n_in_esic_number': 93874944361284657,
         })
 
-        # I create a new employee “Rahul”
-        self.jethalal_emp = self.Employee.create({
+        self.jethalal_emp = Employee.create({
             'name': 'Jethalal',
-            'country_id': self.in_country.id,
-            'department_id': self.rd_dept.id,
+            'country_id': in_country.id,
+            'department_id': rd_dept.id,
             'company_id': self.company_in.id,
             'l10n_in_esic_number': 93487475100284657,
         })
 
-        self.res_bank = self.Bank.create({
+        res_bank = Bank.create({
             'acc_number': '3025632343043',
             'partner_id': self.rahul_emp.work_contact_id.id,
             'acc_type': 'bank',
-            'bank_id': self.in_bank.id,
+            'bank_id': in_bank.id,
             'allow_out_payment': True,
         })
-        self.rahul_emp.bank_account_id = self.res_bank
+        self.rahul_emp.bank_account_id = res_bank
 
-        self.res_bank_1 = self.Bank.create({
+        res_bank_1 = Bank.create({
             'acc_number': '3025632343044',
             'partner_id': self.jethalal_emp.work_contact_id.id,
             'acc_type': 'bank',
-            'bank_id': self.in_bank.id,
+            'bank_id': in_bank.id,
             'allow_out_payment': True,
         })
-        self.jethalal_emp.bank_account_id = self.res_bank_1
+        self.jethalal_emp.bank_account_id = res_bank_1
 
         self.contract_rahul = self.env['hr.contract'].create({
             'date_start': date(2023, 1, 1),
@@ -80,7 +74,7 @@ class TestPayrollCommon(TransactionCase):
             'l10n_in_esic_amount': 20.0,
             'employee_id': self.rahul_emp.id,
             'state': 'open',
-            'hr_responsible_id': self.employee_fp.id,
+            'hr_responsible_id': employee_fp.id,
         })
 
         self.contract_jethalal = self.env['hr.contract'].create({
@@ -91,5 +85,5 @@ class TestPayrollCommon(TransactionCase):
             'employee_id': self.jethalal_emp.id,
             'state': 'open',
             'l10n_in_esic_amount': 20.0,
-            'hr_responsible_id': self.employee_fp.id,
+            'hr_responsible_id': employee_fp.id,
         })
