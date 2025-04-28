d9a591f5109f5cca328569ea3636f08bc8d73b21
tonu-kaare <tonu.kaare@outlook.com>
2025-04-28 13:14:09 +0000

    [IMP] l10n_ee_reports: Update Standard VAT rate in Estonia from 22% to 24%
    Added compatibility with Estonia tax report version KMD6 that is valid from 1. July 2025.
    For technical info please see:
    https://www.emta.ee/en/business-client/e-services-training-courses/how-use-e-services/technical-information-services#value-added-tax-return
    
    closes odoo/enterprise#87793
    
    X-original-commit: ff6236eb4d95f0113960310a5bb5e400a23add34
    Related: odoo/odoo#214445
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>

--- l10n_ee_reports/models/account_report.py
+++ l10n_ee_reports/models/account_report.py
@@ -48,8 +48,10 @@ class L10n_EeTaxReportHandler(models.AbstractModel):
         """ Compute the version of the XML file to export.
         """
         date_to = fields.Date.from_string(options['date'].get('date_to'))
-        if date_to >= fields.Date.from_string("2025-01-01"):
+        if date_to >= fields.Date.from_string("2025-01-01") and date_to < fields.Date.from_string("2025-07-01"):
             return "KMD5"
+        elif date_to >= fields.Date.from_string("2025-07-01"):
+            return "KMD6"
         return "KMD4"
     
     def export_to_xml(self, options):
@@ -94,7 +96,7 @@ class L10n_EeTaxReportHandler(models.AbstractModel):
         # Tax report (KMD report)
         kmd_lines = report._get_lines(options)
         tax_line_prefix = 'l10n_ee.tax_report_line_'
-        tax_line_numbers = ('1', '1_1', '2', '2_1', '2_2', '3', '3_1', '3_1_1', '3_2', '3_2_1',
+        tax_line_numbers = ('1', '1_24', '1_1', '1_2', '2', '2_1', '2_2', '3', '3_1', '3_1_1', '3_2', '3_2_1',
                             '5', '5_1', '5_2', '5_3', '5_3_cars', '5_4', '5_4_cars',
                             '6', '6_1', '7', '7_1', '8', '9', '10', '11')
         tax_line_mapping = {
@@ -184,7 +186,9 @@ class L10n_EeKmdInfReportHandler(models.AbstractModel):
                         '20': 'VAT 20%',
                         '20erikord': 'VAT 20% special procedure §41/42',
                         '22': 'VAT 22%',
-                        '22erikord': 'VAT 22% special procedure §41/42'
+                        '22erikord': 'VAT 22% special procedure §41/42',
+                        '24': 'VAT 24%',
+                        '24erikord': 'VAT 24% special procedure §41/42'
                     }
                     line['name'] = row_names[line['columns'][5]['no_format']]  # column 'tax_rate'
                     comments = line['columns'][7]['no_format']  # column 'comments'
@@ -217,7 +221,7 @@ class L10n_EeKmdInfReportHandler(models.AbstractModel):
     def _get_warning_partners(self, report, options):
         """
         Returns a list of partner IDs that should potentially have been included in the report. Those are partners
-        with a move with taxable supply at 22%, 20%, 9% or 5% in the selected period and no VAT, if:
+        with a move with taxable supply at 24%, 22%, 20%, 13%, 9% or 5% in the selected period and no VAT, if:
         - For part A, if there is no country_id or if country_id is EE
         - For part B, if there is no country_id
         """
@@ -230,7 +234,7 @@ class L10n_EeKmdInfReportHandler(models.AbstractModel):
             move_type = "('in_invoice', 'in_refund')"
             add_where_clause = "AND res_partner.country_id IS NULL"
 
-        tax_group_xmlids = ['tax_group_vat_22', 'tax_group_vat_20', 'tax_group_vat_13', 'tax_group_vat_9', 'tax_group_vat_5']
+        tax_group_xmlids = ['tax_group_vat_24', 'tax_group_vat_22', 'tax_group_vat_20', 'tax_group_vat_13', 'tax_group_vat_9', 'tax_group_vat_5']
         tax_group_ids = [tax_group.id for xmlid in tax_group_xmlids
                          if (tax_group := self.env.ref(f"account.{self.env.company.id}_{xmlid}", raise_if_not_found=False))]
 
@@ -317,7 +321,7 @@ class L10n_EeKmdInfReportHandler(models.AbstractModel):
     def _report_custom_engine_kmd_inf_a(self, expressions, options, date_scope, current_groupby, next_groupby, offset=0, limit=None, warnings=None):
         """ Part A contains information about invoices issued.
 
-            Only invoices containing supply taxable at the rate of 22%, 20%, 13%, 9% or 5% are included. Different lines are
+            Only invoices containing supply taxable at the rate of 24%, 22%, 20%, 13%, 9% or 5% are included. Different lines are
             used for supply taxable under the general procedure, the special procedure provided for in § 41 and § 42 of
             the Value-Added Tax Act (special code 1) and the special procedure provided for in § 41^1 (special code 2).
             Furthermore, if the invoice contains the supply taxable at the rate of 0% or tax-exempt supply or if the
@@ -339,7 +343,7 @@ class L10n_EeKmdInfReportHandler(models.AbstractModel):
         """ Builds the query and dictionary necessary to display the report lines for the KMD INF.
 
         The query for part A and part B are built separately, as they have different columns and group by.
-        In both cases, only invoices containing a 22%, 13%, 9% or 5% tax (and 20% until end of 2025) are displayed.
+        In both cases, only invoices containing a 24%, 22%, 13%, 9% or 5% tax (and 20% until end of 2025) are displayed.
         Invoices from/to foreign companies not having a register code in Estonia are excluded. Both parts also
         include a column displaying the company registry number of the partner as well as their name, number of
         invoice, date of invoice, and the special code providing comments in the form of one or more code.
@@ -368,7 +372,7 @@ class L10n_EeKmdInfReportHandler(models.AbstractModel):
             # In part A, the invoice total without VAT and the taxable supply presented in the fields 1, 1^1,
             # 2 and 2^1 of Form KMD (VAT report) are displayed. We exclude partners whose Tax ID is / and exclude
             # foreigners who have a Tax ID not starting by EE.
-            xmlids = ['tax_report_line_1_tag', 'tax_report_line_1_1_tag', 'tax_report_line_2_tag', 'tax_report_line_2_1_tag', 'tax_report_line_2_2_tag']
+            xmlids = ['tax_report_line_1_tag', 'tax_report_line_1_24_tag', 'tax_report_line_1_1_tag', 'tax_report_line_1_2_tag', 'tax_report_line_2_tag', 'tax_report_line_2_1_tag', 'tax_report_line_2_2_tag']
 
             sql_query = SQL("""
                 WITH multiple_tax_moves AS (
--- l10n_ee_reports/tests/test_tax_report.py
+++ l10n_ee_reports/tests/test_tax_report.py
@@ -38,6 +38,8 @@ class EstonianTaxReportTest(AccountSalesReportCommon):
 
         cls.company_id = cls.env.company.id
         # Purchase Taxes
+        cls.vat_in_24_g = cls.env['account.chart.template'].ref('l10n_ee_vat_in_24_g')
+        cls.vat_in_24_s = cls.env['account.chart.template'].ref('l10n_ee_vat_in_24_s')
         cls.vat_in_22_g = cls.env['account.chart.template'].ref('l10n_ee_vat_in_22_g')
         cls.vat_in_22_s = cls.env['account.chart.template'].ref('l10n_ee_vat_in_22_s')
         cls.vat_in_9_g = cls.env['account.chart.template'].ref('l10n_ee_vat_in_9_g')
@@ -45,16 +47,25 @@ class EstonianTaxReportTest(AccountSalesReportCommon):
         cls.vat_in_0_g = cls.env['account.chart.template'].ref('l10n_ee_vat_in_0_g')
         cls.vat_in_0_eu_g = cls.env['account.chart.template'].ref('l10n_ee_vat_in_0_eu_g_22')
         cls.vat_in_0_eu_s = cls.env['account.chart.template'].ref('l10n_ee_vat_in_0_eu_s_22')
+        cls.vat_in_0_eu_g_24 = cls.env['account.chart.template'].ref('l10n_ee_vat_in_0_eu_g_24')
+        cls.vat_in_0_eu_s_24 = cls.env['account.chart.template'].ref('l10n_ee_vat_in_0_eu_s_24')
+        cls.vat_in_24_car = cls.env['account.chart.template'].ref('l10n_ee_vat_in_24_car')
+        cls.vat_in_24_car_part = cls.env['account.chart.template'].ref('l10n_ee_vat_in_24_car_part')
+        cls.vat_in_24_assets = cls.env['account.chart.template'].ref('l10n_ee_vat_in_24_assets')
         cls.vat_in_22_car = cls.env['account.chart.template'].ref('l10n_ee_vat_in_22_car')
         cls.vat_in_22_car_part = cls.env['account.chart.template'].ref('l10n_ee_vat_in_22_car_part')
         cls.vat_in_22_assets = cls.env['account.chart.template'].ref('l10n_ee_vat_in_22_assets')
         cls.vat_in_imp_cus = cls.env['account.chart.template'].ref('l10n_ee_vat_in_imp_cus')
         cls.vat_in_22_imp_kms_38 = cls.env['account.chart.template'].ref('l10n_ee_vat_in_22_imp_kms_38')
+        cls.vat_in_24_imp_kms_38 = cls.env['account.chart.template'].ref('l10n_ee_vat_in_24_imp_kms_38')
         cls.vat_in_0_kms_41_2 = cls.env['account.chart.template'].ref('l10n_ee_vat_in_0_kms_41_2')
+        cls.vat_in_0_kms_41_3 = cls.env['account.chart.template'].ref('l10n_ee_vat_in_0_kms_41_3')
         cls.vat_in_22_s.l10n_ee_kmd_inf_code = '11'  # added to test if the special comments column is filled
+        cls.vat_in_24_s.l10n_ee_kmd_inf_code = '11'  # added to test if the special comments column is filled
         cls.vat_in_13_s = cls.env['account.chart.template'].ref('l10n_ee_vat_in_13_s')
 
         # Sales Taxes
+        cls.vat_out_24_g = cls.env['account.chart.template'].ref('l10n_ee_vat_out_24_g')
         cls.vat_out_22_g = cls.env['account.chart.template'].ref('l10n_ee_vat_out_22_g')
         cls.vat_out_9_g = cls.env['account.chart.template'].ref('l10n_ee_vat_out_9_g')
         cls.vat_out_5_g = cls.env['account.chart.template'].ref('l10n_ee_vat_out_5_g')
@@ -66,8 +77,13 @@ class EstonianTaxReportTest(AccountSalesReportCommon):
         cls.vat_out_0_exp_s = cls.env['account.chart.template'].ref('l10n_ee_vat_out_0_exp_s')
         cls.vat_out_exempt = cls.env['account.chart.template'].ref('l10n_ee_vat_out_exempt')
         cls.vat_out_0_kms_41_2 = cls.env['account.chart.template'].ref('l10n_ee_vat_out_0_kms_41_2')
+        cls.vat_out_0_kms_41_3 = cls.env['account.chart.template'].ref('l10n_ee_vat_out_0_kms_41_3')
         cls.vat_out_22_erikord = cls.vat_out_22_g.copy({
             'name': '22% erikord',
+            'l10n_ee_kmd_inf_code': '1',
+        })
+        cls.vat_out_24_erikord = cls.vat_out_24_g.copy({
+            'name': '24% erikord',
             'l10n_ee_kmd_inf_code': '1',
         })
         cls.vat_out_13_s = cls.env['account.chart.template'].ref('l10n_ee_vat_out_13_s')
@@ -686,6 +702,240 @@ class EstonianTaxReportTest(AccountSalesReportCommon):
 
         options = self.kmd_report.get_options({})
         actual_xml = self.env[self.kmd_report.custom_handler_model_name].export_to_xml(options)['file_content']
+
+        self.assertXmlTreeEqual(
+            self.get_xml_tree_from_string(actual_xml),
+            self.get_xml_tree_from_string(expected_xml)
+        )
+
+    @freeze_time('2025-08-01')
+    def test_generate_xml_mixed_all_kmd6(self):
+        self.create_invoice(
+            move_type='in_invoice',
+            partner_id=self.partner_ee_1.id,
+            invoice_date='2025-07-11',
+            ref='INV001',
+            invoice_line_ids=[
+                {'name': 'PT1', 'price_unit': 500, 'tax_ids': self.vat_in_24_g.ids},
+                {'name': 'PT2', 'price_unit': 400, 'tax_ids': self.vat_in_0_kms_41_3.ids},
+            ]
+        )
+        self.create_invoice(
+            move_type='in_invoice',
+            partner_id=self.partner_ee_2.id,
+            invoice_date='2025-07-13',
+            ref='INV002',
+            invoice_line_ids=[
+                {'name': 'PT1', 'price_unit': 500, 'tax_ids': self.vat_in_24_s.ids},
+                {'name': 'PT2', 'price_unit': 300, 'tax_ids': self.vat_in_9_g.ids},
+                {'name': 'PT3', 'price_unit': 200, 'tax_ids': self.vat_in_5_g.ids},
+                {'name': 'PT4', 'price_unit': 150, 'tax_ids': self.vat_in_0_g.ids},
+            ],
+        )
+        self.create_invoice(
+            move_type='in_invoice',
+            partner_id=self.partner_a.id,
+            invoice_date='2025-07-20',
+            ref='INV003',
+            invoice_line_ids=[
+                {'name': 'PT1', 'price_unit': 800, 'tax_ids': self.vat_in_0_eu_g_24.ids},
+                {'name': 'PT2', 'price_unit': 700, 'tax_ids': self.vat_in_0_eu_s_24.ids},
+                {'name': 'PT3', 'price_unit': 600, 'tax_ids': self.vat_in_24_car.ids},
+                {'name': 'PT4', 'price_unit': 500, 'tax_ids': self.vat_in_24_car_part.ids},
+                {'name': 'PT5', 'price_unit': 400, 'tax_ids': self.vat_in_24_assets.ids},
+                {'name': 'PT6', 'price_unit': 300, 'tax_ids': self.vat_in_imp_cus.ids},
+                {'name': 'PT7', 'price_unit': 200, 'tax_ids': self.vat_in_24_imp_kms_38.ids},
+                {'name': 'PT8', 'price_unit': 100, 'tax_ids': self.vat_in_0_kms_41_3.ids},
+            ],
+        )
+        self.create_invoice(
+            move_type='out_invoice',
+            partner_id=self.partner_ee_1.id,
+            invoice_date='2025-07-11',
+            invoice_line_ids=[
+                {'name': 'PT1', 'price_unit': 500, 'tax_ids': self.vat_out_24_g.ids},
+                {'name': 'PT2', 'price_unit': 400, 'tax_ids': self.vat_out_0_kms_41_3.ids},
+                {'name': 'PT3', 'price_unit': 300, 'tax_ids': self.vat_out_24_erikord.ids},
+            ],
+        )
+        self.create_invoice(
+            move_type='out_invoice',
+            partner_id=self.partner_ee_2.id,
+            invoice_date='2025-07-13',
+            invoice_line_ids=[
+                {'name': 'PT1', 'price_unit': 500, 'tax_ids': self.vat_out_24_g.ids},
+                {'name': 'PT2', 'price_unit': 300, 'tax_ids': self.vat_out_9_g.ids},
+                {'name': 'PT3', 'price_unit': 200, 'tax_ids': self.vat_out_5_g.ids},
+                {'name': 'PT4', 'price_unit': 150, 'tax_ids': self.vat_out_0_g.ids},
+            ],
+        )
+        self.create_invoice(
+            move_type='out_invoice',
+            partner_id=self.partner_a.id,
+            invoice_date='2025-07-25',
+            invoice_line_ids=[
+                {'name': 'PT1', 'price_unit': 800, 'tax_ids': self.vat_out_0_eu_g.ids},
+                {'name': 'PT2', 'price_unit': 700, 'tax_ids': self.vat_out_0_eu_s.ids},
+                {'name': 'PT3', 'price_unit': 600, 'tax_ids': self.vat_out_0_exp_g.ids},
+                {'name': 'PT4', 'price_unit': 500, 'tax_ids': self.vat_out_0_pas.ids},
+                {'name': 'PT5', 'price_unit': 400, 'tax_ids': self.vat_out_0_exp_s.ids},
+                {'name': 'PT6', 'price_unit': 300, 'tax_ids': self.vat_out_exempt.ids},
+                {'name': 'PT7', 'price_unit': 200, 'tax_ids': self.vat_out_0_kms_41_3.ids},
+            ],
+        )
+
+        options_kmd = self._generate_options(self.kmd_report, '2025-07-01', '2025-07-31')
+        options_kmd_inf_a = self._generate_options(self.kmd_inf_a_report, '2025-07-01', '2025-07-31')
+        options_kmd_inf_b = self._generate_options(self.kmd_inf_b_report, '2025-07-01', '2025-07-31')
+
+        res = self.kmd_inf_a_report._get_lines(options_kmd_inf_a)
+
+        self.assertLinesValues(
+            res,
+            #    Name                                     Reg code       Buyer              Invoice number       Date             Invoice total    Tax rate        Taxable supply    Special code
+            [    0,                                       1,             2,                 3,                   4,               5,               6,              7,                8],
+            [
+                ('Part A - Invoices Issued',              '',            '',                '',                  '',              '',              '',             '',               ''),
+                ('INV/2025/00002',                        '',            '',                '',                  '',              '',              '',             '',               ''),
+                ('VAT 24%',                               '',            'Partner EE 2',    'INV/2025/00002',    '07/13/2025',    1150,            '24',           500,              '3'),
+                ('VAT 9%',                                '',            'Partner EE 2',    'INV/2025/00002',    '07/13/2025',    1150,            '9',            300,              '3'),
+                ('VAT 5%',                                '',            'Partner EE 2',    'INV/2025/00002',    '07/13/2025',    1150,            '5',            200,              '3'),
+                ('INV/2025/00001',                        '',            '',                '',                  '',              '',              '',             '',               ''),
+                ('VAT 24%',                               '98765432',    'Partner EE 1',    'INV/2025/00001',    '07/11/2025',    1200,            '24',           500,              ''),
+                ('VAT 24% special procedure §41^1',       '98765432',    'Partner EE 1',    'INV/2025/00001',    '07/11/2025',    1200,            '24',           '',               '2'),
+                ('VAT 24% special procedure §41/42',      '98765432',    'Partner EE 1',    'INV/2025/00001',    '07/11/2025',    1200,            '24erikord',    300,              '1'),
+            ],
+            options_kmd_inf_a,
+            currency_map={
+                5: {'currency': self.env.company.currency_id},
+                7: {'currency': self.env.company.currency_id},
+            },
+        )
+
+        self.assertLinesValues(
+            self.kmd_inf_b_report._get_lines(options_kmd_inf_b),
+            #    Name                             Reg code       Seller             Invoice number    Date             Invoice total    VAT     Special code
+            [    0,                               1,             2,                 3,                4,               5,               6,      7],
+            [
+                ('Part B - Invoices Received',    '',            '',                '',               '',              '',              '',     ''),
+                ('BILL/2025/07/0002 (INV002)',    '',            'Partner EE 2',    'INV002',         '07/13/2025',    1307,            157,    '11'),
+                ('BILL/2025/07/0001 (INV001)',    '98765432',    'Partner EE 1',    'INV001',         '07/11/2025',    1116,            216,    '12'),
+            ],
+            options_kmd_inf_b,
+        )
+
+        expected_xml = """
+            <vatDeclaration>
+                <taxPayerRegCode>12345678</taxPayerRegCode>
+                <year>2025</year>
+                <month>7</month>
+                <declarationType>1</declarationType>
+                <version>KMD6</version>
+                <declarationBody>
+                    <noSales>false</noSales>
+                    <noPurchases>false</noPurchases>
+                    <sumPerPartnerSales>false</sumPerPartnerSales>
+                    <sumPerPartnerPurchases>false</sumPerPartnerPurchases>
+                    <transactions24>3300.00</transactions24>
+                    <transactions9>300.00</transactions9>
+                    <transactions5>200.00</transactions5>
+                    <transactionsZeroVat>3150.00</transactionsZeroVat>
+                    <euSupplyInclGoodsAndServicesZeroVat>1500.00</euSupplyInclGoodsAndServicesZeroVat>
+                    <euSupplyGoodsZeroVat>800.00</euSupplyGoodsZeroVat>
+                    <exportZeroVat>1100.00</exportZeroVat>
+                    <salePassengersWithReturnVat>500.00</salePassengersWithReturnVat>
+                    <inputVatTotal>1405.00</inputVatTotal>
+                    <importVat>348.00</importVat>
+                    <fixedAssetsVat>96.00</fixedAssetsVat>
+                    <carsVat>144.00</carsVat>
+                    <carsPartialVat>60.00</carsPartialVat>
+                    <euAcquisitionsGoodsAndServicesTotal>1500.00</euAcquisitionsGoodsAndServicesTotal>
+                    <euAcquisitionsGoods>800.00</euAcquisitionsGoods>
+                    <acquisitionOtherGoodsAndServicesTotal>500.00</acquisitionOtherGoodsAndServicesTotal>
+                    <acquisitionImmovablesAndScrapMetalAndGold>500.00</acquisitionImmovablesAndScrapMetalAndGold>
+                    <supplyExemptFromTax>300.00</supplyExemptFromTax>
+                    <supplySpecialArrangements>600.00</supplySpecialArrangements>
+                </declarationBody>
+                <salesAnnex>
+                    <saleLine>
+                        <buyerName>Partner EE 2</buyerName>
+                        <invoiceNumber>INV/2025/00002</invoiceNumber>
+                        <invoiceDate>2025-07-13</invoiceDate>
+                        <invoiceSum>1150.00</invoiceSum>
+                        <taxRate>24</taxRate>
+                        <sumForRateInPeriod>500.00</sumForRateInPeriod>
+                        <comments>3</comments>
+                    </saleLine>
+                    <saleLine>
+                        <buyerName>Partner EE 2</buyerName>
+                        <invoiceNumber>INV/2025/00002</invoiceNumber>
+                        <invoiceDate>2025-07-13</invoiceDate>
+                        <invoiceSum>1150.00</invoiceSum>
+                        <taxRate>9</taxRate>
+                        <sumForRateInPeriod>300.00</sumForRateInPeriod>
+                        <comments>3</comments>
+                    </saleLine>
+                    <saleLine>
+                        <buyerName>Partner EE 2</buyerName>
+                        <invoiceNumber>INV/2025/00002</invoiceNumber>
+                        <invoiceDate>2025-07-13</invoiceDate>
+                        <invoiceSum>1150.00</invoiceSum>
+                        <taxRate>5</taxRate>
+                        <sumForRateInPeriod>200.00</sumForRateInPeriod>
+                        <comments>3</comments>
+                    </saleLine>
+                    <saleLine>
+                        <buyerRegCode>98765432</buyerRegCode>
+                        <buyerName>Partner EE 1</buyerName>
+                        <invoiceNumber>INV/2025/00001</invoiceNumber>
+                        <invoiceDate>2025-07-11</invoiceDate>
+                        <invoiceSum>1200.00</invoiceSum>
+                        <taxRate>24</taxRate>
+                        <sumForRateInPeriod>500.00</sumForRateInPeriod>
+                    </saleLine>
+                    <saleLine>
+                        <buyerRegCode>98765432</buyerRegCode>
+                        <buyerName>Partner EE 1</buyerName>
+                        <invoiceNumber>INV/2025/00001</invoiceNumber>
+                        <invoiceDate>2025-07-11</invoiceDate>
+                        <invoiceSum>1200.00</invoiceSum>
+                        <taxRate>24</taxRate>
+                        <comments>2</comments>
+                    </saleLine>
+                    <saleLine>
+                        <buyerRegCode>98765432</buyerRegCode>
+                        <buyerName>Partner EE 1</buyerName>
+                        <invoiceNumber>INV/2025/00001</invoiceNumber>
+                        <invoiceDate>2025-07-11</invoiceDate>
+                        <invoiceSum>1200.00</invoiceSum>
+                        <taxRate>24erikord</taxRate>
+                        <sumForRateInPeriod>300.00</sumForRateInPeriod>
+                        <comments>1</comments>
+                    </saleLine>
+                </salesAnnex>
+                <purchasesAnnex>
+                    <purchaseLine>
+                        <sellerName>Partner EE 2</sellerName>
+                        <invoiceNumber>INV002</invoiceNumber>
+                        <invoiceDate>2025-07-13</invoiceDate>
+                        <invoiceSumVat>1307.00</invoiceSumVat>
+                        <vatInPeriod>157.00</vatInPeriod>
+                        <comments>11</comments>
+                    </purchaseLine>
+                    <purchaseLine>
+                        <sellerRegCode>98765432</sellerRegCode>
+                        <sellerName>Partner EE 1</sellerName>
+                        <invoiceNumber>INV001</invoiceNumber>
+                        <invoiceDate>2025-07-11</invoiceDate>
+                        <invoiceSumVat>1116.00</invoiceSumVat>
+                        <vatInPeriod>216.00</vatInPeriod>
+                        <comments>12</comments>
+                    </purchaseLine>
+                </purchasesAnnex>
+            </vatDeclaration>
+        """
+
+        actual_xml = self.env[self.kmd_report.custom_handler_model_name].export_to_xml(options_kmd)['file_content']
 
         self.assertXmlTreeEqual(
             self.get_xml_tree_from_string(actual_xml),
