bce17c0966cf42e3575d877bd495f2d407b0fe3e
paan-odoo <paan@odoo.com>
2025-04-28 13:05:51 +0000

    [FIX] pos_self_order_preparation_display: load zero-amount paid orders
    Before this commit:
    
    - Orders with a total paid amount of zero were not loaded into the preparation
      display.
    
    After this commit:
    
    - Orders with a zero total paid amount are now correctly loaded and displayed in
      the preparation view.
    
    task-4757076
    
    closes odoo/enterprise#84576
    
    X-original-commit: ba3ae7e1819c01d0e9461adafa0a2a2282084798
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Anantkumar Parmar (paan) <paan@odoo.com>

--- pos_self_order_preparation_display/controllers/orders.py
+++ pos_self_order_preparation_display/controllers/orders.py
@@ -5,7 +5,19 @@ from odoo.addons.pos_self_order.controllers.orders import PosSelfOrderController
 
 class PosSelfOrderPreparationDisplayController(PosSelfOrderController):
     @http.route()
+    def process_order(self, order, access_token, table_identifier, device_type):
+        res = super().process_order(order, access_token, table_identifier, device_type)
+        self._send_to_preparation_display(res['pos.order'][0]['id'])
+        return res
+
+    @http.route()
     def change_printer_status(self, access_token, has_paper):
         super().change_printer_status(access_token, has_paper)
         pos_config = self._verify_pos_config(access_token)
         pos_config.env['pos.prep.display']._paper_status_change(pos_config)
+
+    def _send_to_preparation_display(self, pos_order_id):
+        """ Send paid orders to the preparation display. """
+        pos_order = http.request.env['pos.order'].browse(pos_order_id)
+        if pos_order.config_id.self_ordering_mode in ['kiosk', 'mobile'] and pos_order.state == 'paid':
+            pos_order.env['pos.prep.order'].sudo().process_order(pos_order.id)
