82de91568c6e4c9bed07d0d2ed9bb02d3b5f6be3
Thibault Delavallée <tde@odoo.com>
2025-05-08 15:17:40 +0000

    [IMP] marketing_automation: add performance tests
    Add long awaited performance tests for marketing automation. As it may
    be quite a time consuming module by generating participants, traces
    for millions of records, better start with some performance tests in
    order to prepare work for some optimizations.
    
    Task-4224145: [marketing_automation] Improve test coverage
    Prepares Task-4224152: [marketing_automation] Performance / Scalability
    
    X-original-commit: 99b12ccea036acdd4899346431fea5594373b2b1
    Part-of: odoo/enterprise#85247
    Related: odoo/odoo#209409
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- test_marketing_automation/models/test_models.py
+++ test_marketing_automation/models/test_models.py
@@ -15,6 +15,34 @@ class MarketingTest(models.Model):
     partner_id = fields.Many2one('res.partner', 'Partner')
 
 
+class MarketingTestPerformance(models.Model):
+    _name = 'marketing.test.performance'
+    _description = 'MarketAuto: Model for performance check'
+    _inherit = ['mail.thread.blacklist', 'mail.thread.phone', 'utm.mixin']
+    _primary_email = 'email_from'
+    _order = 'id ASC'
+
+    name = fields.Char()
+    company_id = fields.Many2one("res.company")
+    email_from = fields.Char(compute='_compute_from_customer', readonly=False, store=True)
+    phone = fields.Char(compute='_compute_from_customer', readonly=False, store=True)
+    description = fields.Text()
+    customer_id = fields.Many2one('res.partner', 'Partner')
+    # dummy selection fields to easily write server actions
+    selection_field = fields.Selection([("key1", "Key 1"), ("key2", "Key 2"), ("key3", "Key 3")])
+
+    @api.depends('customer_id')
+    def _compute_from_customer(self):
+        for record in self:
+            if not record.email_from and record.customer_id.email:
+                record.email_from = record.customer_id.email
+            if not record.phone and record.customer_id.phone:
+                record.phone = record.customer_id.phone
+
+    def _mail_get_partner_fields(self, introspect_fields=False):
+        return ['customer_id']
+
+
 class MarketingTestUtm(models.Model):
     _name = 'marketing.test.utm'
     _description = 'MarketAuto: simple thread-enabled model with UTMs'
@@ -48,5 +76,4 @@ class MarketingTestSms(models.Model):
                 record.phone = record.customer_id.phone
 
     def _mail_get_partner_fields(self, introspect_fields=False):
-        """ Override to return the recipient linked to SMS sending. """
         return ['customer_id']
