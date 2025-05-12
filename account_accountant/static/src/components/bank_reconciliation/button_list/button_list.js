780a9eaefe3db3094408ec2719959c43f9112ddc
Maximilien (malb) <malb@odoo.com>
2025-05-12 06:59:22 +0000

    [IMP] account_accountant: filter
    Since this commit: https://github.com/odoo/odoo/commit/d29af194d49dc35b9273712bc9158b05927a378e
    They changed the preferred internal group for a preferred account type.
    Also correcting a type in the name of a filter
    
    no task-id
    
    closes odoo/enterprise#85310
    
    X-original-commit: 16ca8ec7a61b1bdfba22c86dacaae1104b485f47
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- account_accountant/static/src/components/bank_reconciliation/button_list/button_list.js
+++ account_accountant/static/src/components/bank_reconciliation/button_list/button_list.js
@@ -88,11 +88,11 @@ export class BankRecButtonList extends Component {
             list_view_ref: "account_accountant.view_account_list_bank_rec_widget",
             search_view_ref: "account_accountant.view_account_search_bank_rec_widget",
             ...(this.statementLineData.amount > 0
-                ? { search_default_bankrec_income: 1, preferred_internal_group: "income" }
+                ? { search_default_bankrec_income: 1, preferred_account_type: "income" }
                 : {
                       search_default_bankrec_expense: 1,
                       search_default_bankrec_currentasset: 1,
-                      preferred_internal_group: "expense",
+                      preferred_account_type: "expense",
                   }),
         };
 
