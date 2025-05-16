4f595623bbb65706373083933db12a2e62127b60
Logan Staelens <lost@odoo.com>
2025-05-16 13:03:29 +0000

    [FIX] account_sepa_direct_debit: Anonymize account number in email
    Before this fix,
    The bank account was shown in clear inside the direct debit payment
    notification mail
    
    Now,
    we replace all chars by * except the 4 last ones.
    
    task-4592821
    
    closes odoo/enterprise#90470
    
    X-original-commit: 26a215515b7e861b5abcce9461f7013bb6924733
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>
    Signed-off-by: Logan Staelens (lost) <lost@odoo.com>

--- account_sepa_direct_debit/models/account_batch_payment.py
+++ account_sepa_direct_debit/models/account_batch_payment.py
@@ -2,6 +2,7 @@
 
 import ast
 import base64
+import re
 from datetime import datetime, timedelta
 
 from odoo import models, fields, api, _
@@ -283,12 +284,15 @@ class AccountBatchPayment(models.Model):
         sdd_codes = set(self.env['account.payment.method']._get_sdd_payment_method_code())
         for payment in self.payment_ids.filtered(lambda payment: payment.payment_method_code in sdd_codes and payment.sdd_mandate_id):
             mandate = payment.sdd_mandate_id
+            sanitized_acc_number = mandate.partner_bank_id.sanitized_acc_number
+            anonymized_bank_account_number = f"{re.sub(r'.', '*', sanitized_acc_number[:-4])}{sanitized_acc_number[-4:]}"
+
             ctx = {
-                'iban_last_4': mandate.partner_bank_id.sanitized_acc_number[-4:],
+                'iban_last_4': sanitized_acc_number[-4:],
                 'mandate_ref': mandate.name,
                 'collection_date': payment.batch_payment_id.sdd_required_collection_date,
                 'amount': payment.amount,
-                'creditor_iban': payment.journal_id.bank_acc_number,
+                'creditor_iban': anonymized_bank_account_number,
             }
             payment.with_context(ctx).message_post_with_source(source_ref=template, subtype_xmlid='mail.mt_note')
         return res
