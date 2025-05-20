55702d755a4ca4706489527c8ebcf1ff91766cec
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2025-05-20 13:44:27 +0000

    [FIX] l10n_de_reports: update EC Sales List CSV export to version 3.0
    After a recent change in German EDI, the data exported in CSV from the EC Sales List
    can no longer be uploaded because the expected structure has changed
    
    Steps to reproduce (with a DE Company):
    - Make an invoice to an EU Partner
    - Open EC Sales List
    - Export CSV
    
    When uploading the file on the Elster portal it will be rejected with an explicit error message,
    saying that we're using an old CSV format now deprecated, and prompts the user to refer to the new specifications
    
    Original message:
    Ihre CSV-Datei wurde für eine ältere Version der CSV-Schnittstelle erstellt.
    Bitte informieren Sie sich in der Versionstabelle der Importhilfe über die aktuellste Version der Schnittstelle sowie Änderungen.
    Sind die Versionsangaben korrekt, prüfen Sie bitte das Encoding Ihrer CSV-Datei. Das zulässige Encoding ist UTF-8.
    
    Sources:
    https://www.elster.de/eportal/helpGlobal?themaGlobal=zmdo_import_eop
    https://www.datev-community.de/t5/Betriebliches-Rechnungswesen/Import-CSV-Daten-nacvh-Elster-f%C3%BCr-ZM/td-p/480079
    
    opw-4751667
    opw-4674533
    
    closes odoo/enterprise#86558
    
    X-original-commit: a132204d92a0c6d881e7e4b4f2b293e17864324a
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>
    Signed-off-by: Andrea Grazioso (agr) <agr@odoo.com>

--- l10n_de_reports/models/account_sales_report.py
+++ l10n_de_reports/models/account_sales_report.py
@@ -59,9 +59,10 @@ class L10n_DeEcSalesReportHandler(models.AbstractModel):
         line_chunks = []
         chunks = [lines[i * 1000:(i + 1) * 1000] for i in range(math.ceil(len(lines) / 1000))]
         for chunk in chunks:
-            content = 'Länderkennzeichen,USt-IdNr.,Betrag (Euro),Art der Leistung\n'
-            for val in chunk:
-                content += ','.join([val[0], val[1], str(val[3]), val[2]]) + '\n'
+            content = 'Umsatzsteuer-Identifikationsnummer (USt-IdNr.),Betrag (Euro),Art der Leistung\n'
+            for vat_country_code, vat_number, service_type, amount, *vals in chunk:
+                full_vat_number = f'{vat_country_code}{vat_number}' if vat_country_code and vat_number else ''
+                content += f'{full_vat_number},{amount},{service_type}\n'
             line_chunks.append(content)
         return line_chunks
 
