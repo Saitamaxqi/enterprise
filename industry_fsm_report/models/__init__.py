b6a2069c4e72d6503653540b7fc0a4c8e5f92cfc
agbr-odoo <agbr@odoo.com>
2025-05-21 11:36:45 +0000

    [FIX] industry_fsm_report: fix error on assigning template to a task
    When user uninstalls and installs `industry_fsm` the `model_id` for the
    worksheet template turns `False` raising key error when accessing via model_id.
    
    Replication steps:
    * Install `industry_fsm` (No demo data required)
    * Field Service>configuration>settings>Enable Worksheet templates
    * Configuration>Templates>Create New Template
    * Uninstall and reinstall `Field Service`
    * Field Service>New>Set worksheet template to the one you created
    
    `KeyError: False`
    
    Solution:
    * Unlink user created worksheet template on uninstallation of the application.
    
    Sentry-6615576986
    
    closes odoo/enterprise#95691
    
    X-original-commit: 253736a92627adfebf70148a11d1c58128434e0e
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Anshik Gopalbhai Brahmbhatt (agbr) <agbr@odoo.com>

--- industry_fsm_report/models/__init__.py
+++ industry_fsm_report/models/__init__.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import ir_model
 from . import project_project
 from . import project_task
 from . import project_task_recurrence
