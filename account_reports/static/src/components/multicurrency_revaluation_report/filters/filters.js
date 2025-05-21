54f8c671045853145d26e67fc3408172fbf8de87
zorbmax <orma@odoo.com>
2025-05-21 12:16:31 +0000

    [FIX] account_reports: currency rate input
    Update on the filterExchangeRate function.
    
    The text input allowed users to enter non-numeric characters, which led to
    float conversion errors during processing.
    
    Now we parse the string into a float, catch errors and notify the user that the number was not valid.
    It also enables the user to use the thousands and decimal separators based on the language of the user.
    
    opw-4788246
    
    closes odoo/enterprise#86259
    
    X-original-commit: e6b5d0f5ae80720605fb9224da7f8145754fde5e
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>

--- account_reports/static/src/components/multicurrency_revaluation_report/filters/filters.js
+++ account_reports/static/src/components/multicurrency_revaluation_report/filters/filters.js
@@ -1,5 +1,7 @@
 import { AccountReport } from "@account_reports/components/account_report/account_report";
 import { AccountReportFilters } from "@account_reports/components/account_report/filters/filters";
+import { parseFloat } from "@web/views/fields/parsers";
+import { _t } from "@web/core/l10n/translation";
 
 export class MulticurrencyRevaluationReportFilters extends AccountReportFilters {
     static template = "account_reports.MulticurrencyRevaluationReportFilters";
@@ -8,7 +10,13 @@ export class MulticurrencyRevaluationReportFilters extends AccountReportFilters 
     // Custom filters
     //------------------------------------------------------------------------------------------------------------------
     async filterExchangeRate(ev, currencyId) {
-        this.controller.options.currency_rates[currencyId].rate = ev.currentTarget.value;
+        try {
+            this.controller.options.currency_rates[currencyId].rate = Math.abs(parseFloat(ev.currentTarget.value));
+        } catch {
+            this.notification.add(_t("Please enter a valid number."), {
+                type: "danger",
+            });
+        }
     }
 }
 
