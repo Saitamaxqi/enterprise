d219fd3923a10695987efd382b52f75b704ef61f
path-odoo <path@odoo.com>
2025-05-21 12:20:26 +0000

    [FIX] appointment_account_payment: prevent error on clicking Paid Seats
    Currently, an error occurs when a user tries to access `paid consultation` or `paid seats` if the booking fees are deleted by the user.
    
    **Steps to Reproduce:**
    - Install **appointment_account_payment** module without demo data.
    - Delete **Booking Fees** product`from `invoicing->customer->products`
    - Click on `Paid Seats` or `Paid Consultation` in the Appointment App.
    
    **Error:**
    `ValueError: No record found for unique ID appointment_account_payment.default_booking_product. It may have been deleted.`
    
    **Root Cause:**
    After deleting the **Booking Fees**, if user tries to open `Paid Seats` or `Paid Consultation`  the system will try to access id of
    `appointment_account_payment.default_booking_product` at [1] or [2] causing an error.
    
    [1]-
    https://github.com/odoo/enterprise/blob/a4f68967a9826fac981c1141ece1791a0baf647a/appointment_account_payment/models/templates/appointment_type.py#L43
    
    [2]-
    https://github.com/odoo/enterprise/blob/a4f68967a9826fac981c1141ece1791a0baf647a/appointment_account_payment/models/templates/appointment_type.py#L56
    
    **Solution:**
    This commit prevents error by restricting user from deleting the `Booking Product`.
    
    Sentry-6013528364
    
    closes odoo/enterprise#87567
    
    X-original-commit: cc816858a82d30b5e65a024247be54a9221395f5
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>
    Signed-off-by: Het Patel (path) <path@odoo.com>

--- appointment_account_payment/models/__init__.py
+++ appointment_account_payment/models/__init__.py
@@ -5,4 +5,6 @@ from . import appointment_answer_input
 from . import appointment_type
 from . import calendar_booking_line
 from . import calendar_booking
+from . import product_product
+from . import product_template
 from . import templates
