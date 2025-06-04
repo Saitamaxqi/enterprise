09c72282684a9c6fda50b8df8c5855545c60cd86
leclerc-leo <leol@odoo.com>
2025-06-04 15:30:28 +0000

    [FIX] account_reports: Create Tax Reports for fiscal position when creating taxes
    When we create a fiscal position, we are checking if the user has the
    taxes for that country create and display a button to have the user
    download them.
    
    Previously, we were trying to create the tax reports for that fiscal
    position when creating it, however since the l10n_report is loaded
    with the taxes, the creation of the tax return wouldn't be triggered.
    
    Now, we are creating the tax returns after the user has downloaded the
    taxes needed for that fiscal position.
    
    task-4840412
    
    closes odoo/enterprise#87903
    
    X-original-commit: 18b5adf2e3d0f9f83b7f98c3c4b5b98d33d8dfdf
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>
    Signed-off-by: Léo Leclerc (leol) <leol@odoo.com>

--- account_reports/models/account_fiscal_position.py
+++ account_reports/models/account_fiscal_position.py
@@ -4,7 +4,7 @@ from odoo import models
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
 
-    def _inverse_foreign_vat(self):
+    def action_create_foreign_taxes(self):
         # EXTENDS account
-        super()._inverse_foreign_vat()
+        super().action_create_foreign_taxes()
         self.env['account.return.type']._generate_or_refresh_all_returns(self.company_id.root_id)
