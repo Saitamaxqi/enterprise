2924a70eab83dc2579d52a51e8b0d74ca4daa0b7
Florian Charlier <flch@odoo.com>
2025-06-05 14:07:47 +0000

    [FIX] documents_account: clean tour style
    We might as well do it already as we'll come
    back to this soon.
    
    Task-4210376
    
    X-original-commit: f200193b97cfeb9626bce3e9c17b2ce3d06f05f6
    Part-of: odoo/enterprise#72957
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Florian Charlier (flch) <flch@odoo.com>

--- documents_account/static/src/js/tour.js
+++ documents_account/static/src/js/tour.js
@@ -6,8 +6,8 @@ registry.category("web_tour.tours").add("documents_account_tour", {
     url: "/odoo",
     steps: () => [
         {
-    trigger: '.o_app[data-menu-xmlid="documents.menu_root"]',
-    content: markup(_t("Want to become a <b>paperless company</b>? Let's discover Odoo Documents.")),
+            trigger: '.o_app[data-menu-xmlid="documents.menu_root"]',
+            content: markup(_t("Want to become a <b>paperless company</b>? Let's discover Odoo Documents.")),
             tooltipPosition: "bottom",
             run: "click",
         },
@@ -20,8 +20,8 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             trigger: "span.o_documents_in_folder",
         },
         {
-    trigger: 'body:not(:has(.o-FileViewer)) img[src="https://img.youtube.com/vi/Ayab6wZ_U1A/0.jpg"]',
-    content: markup(_t("Click on a thumbnail to <b>preview the document</b>.")),
+            trigger: 'body:not(:has(.o-FileViewer)) img[src="https://img.youtube.com/vi/Ayab6wZ_U1A/0.jpg"]',
+            content: markup(_t("Click on a thumbnail to <b>preview the document</b>.")),
             tooltipPosition: "bottom",
             run: "click",
         },
@@ -29,8 +29,8 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             trigger: ".o_documents_kanban",
         },
         {
-    trigger: "div[title='" + _t("Close (Esc)") + "']",
-    content: markup(_t("Click the cross to <b>exit preview</b>.")),
+            trigger: "div[title='" + _t("Close (Esc)") + "']",
+            content: markup(_t("Click the cross to <b>exit preview</b>.")),
             tooltipPosition: "left",
             run: "click",
         },
@@ -38,14 +38,18 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             trigger: "body:not(:has(.o-FileViewer)) .o_documents_kanban",
         },
         {
-    trigger: ".o_kanban_record:contains('Mails_inbox.pdf')",
-    content: markup(_t("Click on a card to <b>select the document</b>.")),
+            trigger: ".o_kanban_record:contains('Mails_inbox.pdf')",
+            content: markup(_t("Click on a card to <b>select the document</b>.")),
             tooltipPosition: "bottom",
             run: "click",
         },
         {
-    trigger: ".o_control_panel_actions button:contains('" + _t("Send To Finance") + "')",
-    content: markup(_t("Let's tag this mail as Finance<br/> <i>Tips: actions can be tailored to your process, according to the folder.</i>")),
+            trigger: ".o_control_panel_actions button:contains('" + _t("Send To Finance") + "')",
+            content: markup(
+                _t(
+                    "Let's tag this mail as Finance<br/> <i>Tips: actions can be tailored to your process, according to the folder.</i>"
+                )
+            ),
             tooltipPosition: "bottom",
             run: "click",
         },
@@ -54,7 +58,11 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         },
         {
             trigger: ".o_kanban_record:contains('" + _t("Finance") + "')",
-            content: markup(_t("Let's process documents in this folder.<br/><i>Tip: Use Tags to filter documents and structure your process.</i>")),
+            content: markup(
+                _t(
+                    "Let's process documents in this folder.<br/> <i>Tip: Use Tags to filter documents and structure your process.</i>"
+                )
+            ),
             tooltipPosition: "top",
             run: "click",
         },
@@ -62,52 +70,54 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             trigger: "body:not(:has(.o_documents_in_folder)) .o_documents_kanban",
         },
         {
-    trigger: ".o_kanban_record:contains('Mails_inbox.pdf')",
-    content: _t("Let's process this document now."),
+            trigger: ".o_kanban_record:contains('Mails_inbox.pdf')",
+            content: _t("Let's process this document now."),
             tooltipPosition: "bottom",
             run: "click",
         },
         {
-    trigger: ".o_documents_action_dropdown button:contains(" + _t("Action") + "')",
-    content: _t("Open the actions menu"),
+            trigger: ".o_documents_action_dropdown button:contains(" + _t("Action") + "')",
+            content: _t("Open the actions menu"),
             tooltipPosition: "bottom",
             run: "click",
         },
         {
-    trigger: ".o_documents_action_dropdown button.dropdown-item:contains('" +
-             _t("Split PDF") +
-             "')",
-    content: _t("As this PDF contains multiple documents, let's split and process in bulk."),
+            trigger: ".o_documents_action_dropdown button.dropdown-item:contains('" + _t("Split PDF") + "')",
+            content: _t("As this PDF contains multiple documents, let's split and process in bulk."),
             tooltipPosition: "left",
             run: "click",
         },
         {
-    trigger: '.o_page_splitter_wrapper:eq(3)',
-    content: markup(_t("Click on the <b>page separator</b>: we don't want to split these two pages as they belong to the same document.")),
+            trigger: '.o_page_splitter_wrapper:eq(3)',
+            content: markup(
+                _t(
+                    "Click on the <b>page separator</b>: we don't want to split these two pages as they belong to the same document."
+                )
+            ),
             tooltipPosition: "right",
             run: "click",
         },
         {
-    trigger: '.o_documents_pdf_page_selector:eq(5)',
-    content: markup(_t("<b>Deselect this page</b> as we plan to process all bills first.")),
+            trigger: '.o_documents_pdf_page_selector:eq(5)',
+            content: markup(_t("<b>Deselect this page</b> as we plan to process all bills first.")),
             tooltipPosition: "left",
             run: "click",
         },
         {
-    trigger: ".o_pdf_manager_button:contains('" + _t("Create Vendor Bill") + "')",
-    content: _t("Let's process these bills: turn them into vendor bills."),
+            trigger: ".o_pdf_manager_button:contains('" + _t("Create Vendor Bill") + "')",
+            content: _t("Let's process these bills: turn them into vendor bills."),
             tooltipPosition: "bottom",
             run: "click",
         },
         {
-    trigger: '.o_documents_pdf_page_selector',
-    content: markup(_t("<b>Select</b> this page to continue.")),
+            trigger: '.o_documents_pdf_page_selector',
+            content: markup(_t("<b>Select</b> this page to continue.")),
             tooltipPosition: "bottom",
             run: "click",
         },
         {
-    trigger: ".o_pdf_manager_button:contains('" + _t("Create Misc Entry") + "')",
-    content: _t("This should be processed as a misc entry."),
+            trigger: ".o_pdf_manager_button:contains('" + _t("Create Misc Entry") + "')",
+            content: _t("This should be processed as a misc entry."),
             tooltipPosition: "bottom",
             run: "click",
         },
