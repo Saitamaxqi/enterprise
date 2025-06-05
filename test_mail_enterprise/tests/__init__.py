c2d85b503289c42c4e82bed7c1c2898cf0641279
Thibault Delavallée <tde@odoo.com>
2025-06-05 09:41:56 +0200

    [IMP] test_mail_enterprise: add post performance test
    Add in "full options" enterprise test module a performance test for
    message_post. It notably tests ocn impact, in addition to standard
    mail and push.
    
    Add mock for ocn iap notifications, to avoid VERBOTEN external requests.
    
    Update counters accordingly.
    
    Prepares Task-4845982
    
    Part-of: odoo/enterprise#86998
    Related: odoo/odoo#212930
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- test_mail_enterprise/tests/__init__.py
+++ test_mail_enterprise/tests/__init__.py
@@ -3,6 +3,7 @@
 
 from . import test_activity
 from . import test_activity_performance
+from . import test_mail_performance
 from . import test_ocn_mobile
 from . import test_sms_performance
 from . import test_whatsapp_performance
