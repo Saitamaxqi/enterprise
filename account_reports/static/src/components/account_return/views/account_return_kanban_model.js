e5953cff25232696a65e84b4a02c98aa009c0dd5
Logan Staelens <lost@odoo.com>
2025-06-06 11:38:38 +0000

    [FIX] account_reports: moved scope of MAX_NUMBER_OPENED_GROUPS
    Before when we would open the return kanban, it would set for every other kanban the variable MAX_NUMBER_OPENED_GROUPS to max integer.
    
    This is not what we want, we only weant to restrict that to the Return Kanban Model only not every relational model.
    
    closes odoo/enterprise#87225
    
    X-original-commit: a837ae3616a3ef0d0ac7c707456d761554d29b27
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- account_reports/static/src/components/account_return/views/account_return_kanban_model.js
+++ account_reports/static/src/components/account_return/views/account_return_kanban_model.js
@@ -1,8 +1,9 @@
 import { RelationalModel } from "@web/model/relational_model/relational_model";
 
 export class AccountReturnKanbanModel extends RelationalModel {
+    static MAX_NUMBER_OPENED_GROUPS = Number.MAX_SAFE_INTEGER;
+
     setup() {
-        super.constructor.MAX_NUMBER_OPENED_GROUPS = Number.MAX_SAFE_INTEGER;
         super.setup(...arguments);
     }
 }
