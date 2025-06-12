e587472853a1c9ddcd79ebc3b6b8cbc294c99c23
paan-odoo <paan@odoo.com>
2025-06-12 08:19:10 +0000

    [FIX] pos_l10n_se: runbot error due to unknown key
    In this commit:
    
    - We are fixing the unknown key "test" runbot error.
    
    runbot error-226611
    
    closes odoo/enterprise#87693
    
    X-original-commit: 926339647c2fd7c5643244896a893b9a4850b5ef
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Anantkumar Parmar (paan) <paan@odoo.com>

--- l10n_se_pos/static/tests/tours/test_se_pos_tour.js
+++ l10n_se_pos/static/tests/tours/test_se_pos_tour.js
@@ -7,7 +7,6 @@ import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
 import { registry } from "@web/core/registry";
 
 registry.category("web_tour.tours").add("test_l10n_se_pos_01", {
-    test: true,
     steps: () =>
         [
             Chrome.startPoS(),
