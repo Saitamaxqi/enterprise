53d008e9ce11bbf870e5d2f248fd591fa679be0e
chha-odoo <chha@odoo.com>
2025-06-16 15:41:40 +0530

    [IMP] stock_barcode: display barcode preview on location form view
    A barcode preview is now displayed on the location form view when a
    barcode is set for the location.
    
    Task ID: [4728397](https://www.odoo.com/odoo/project/966/tasks/4728397)
    
    Part-of: odoo/enterprise#88319
    Related: odoo/odoo#215517
    Related: odoo/upgrade#7991
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- stock_barcode/models/stock_location.py
+++ stock_barcode/models/stock_location.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, api
+import base64
+
+from odoo import api, fields, models
 
 
 class StockLocation(models.Model):
     _inherit = 'stock.location'
     _barcode_field = 'barcode'
+
+    barcode_img = fields.Binary(compute="_compute_barcode_img")
+
+    @api.depends('barcode')
+    def _compute_barcode_img(self):
+        options = {
+            'width': 300,
+            'height': 100,
+            'humanreadable': True
+        }
+        for location in self:
+            if location.barcode:
+                barcode = self.env['ir.actions.report'].barcode(
+                    'Code128',
+                    location.barcode,
+                    **options
+                )
+                location.barcode_img = base64.b64encode(barcode).decode()
+            else:
+                location.barcode_img = False
 
     @api.model
     def _search(self, domain, *args, **kwargs):
