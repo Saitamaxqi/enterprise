b22bc5501d37cec13ef62d4d0243c1e7e1a32d90
Maximilien (malb) <malb@odoo.com>
2025-06-18 08:11:03 +0000

    [FIX] account_accountant: CSV import
    In the case of a csv import file, we don't trigger the cron_try_auto_reconcile
    but we open a wizard to match the column of the csv to a field. In that case, we
    add the context key to launch the try_loading at the creation of the statement
    lines.
    
    no task id
    
    closes odoo/enterprise#88085
    
    X-original-commit: 870bb01e3cee527fff7713623a29644c77eee469
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py
+++ account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py
@@ -3,7 +3,7 @@
 
 import psycopg2
 
-from odoo import _, api, fields, models, Command
+from odoo import _, api, fields, models, tools, Command
 from odoo.exceptions import UserError, ValidationError
 from odoo.addons.base_import.models.base_import import FIELDS_RECURSION_LIMIT
 
@@ -136,6 +136,12 @@ class Base_ImportImport(models.TransientModel):
                 'line_ids': [Command.set(res.get('ids', []))],
                 **options.get('statement_vals', {}),
             })
+            if not dryrun and statement.line_ids:
+                # 'limit_time_real_cron' defaults to -1.
+                # Manual fallback applied for non-POSIX systems where this key is disabled (set to None).
+                cron_limit_time = tools.config['limit_time_real_cron'] or -1
+                limit_time = cron_limit_time if 0 < cron_limit_time < 180 else 180
+                statement.line_ids._cron_try_auto_reconcile_statement_lines(limit_time=limit_time)
 
             try:
                 savepoint.close(rollback=dryrun)
