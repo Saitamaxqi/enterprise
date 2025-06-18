0f93042006e69e0084fc8f1c53b17906bbba4da4
Maximilien (malb) <malb@odoo.com>
2025-06-18 08:32:12 +0000

    [FIX] account_accountant: search default missing
    When clicking on "Fetch transactions" the search default was not set, and so we
    could see the transaction of all journals
    
    no task id
    
    X-original-commit: 728aca2323ab988d34d1aac4515ecb8944b2d71b
    Part-of: odoo/enterprise#88085
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- account_online_synchronization/models/account_online.py
+++ account_online_synchronization/models/account_online.py
@@ -878,7 +878,13 @@ class AccountOnlineLink(models.Model):
                     return self.env['account.bank.statement.line']._action_open_bank_reconciliation_widget(
                         extra_domain=domain,
                         name=_('Fetched Transactions'),
-                        default_context={**self.env.context, 'active_id': journal.id, 'default_journal_id': journal.id, 'duplicates_from_date': duplicates_from_date},
+                        default_context={
+                            **self.env.context,
+                            'active_id': journal.id,
+                            'default_journal_id': journal.id,
+                            'duplicates_from_date': duplicates_from_date,
+                            'search_default_journal_id': journal.id,
+                        },
                     )
                 else:
                     statement_lines = self.env['account.bank.statement.line']._online_sync_bank_statement(sorted_transactions, online_account)
