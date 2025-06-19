7da3123dc4487a7092deef8503a9791ceffddcfb
Xavier Morel <xmo@odoo.com>
2025-06-19 15:41:21 +0200

    [IMP] account_reports: perf of account_codes engine
    The `account_codes` engine generates a giant `UNION ALL` to map
    prefixes to accounts. This fully loads postgres for up to minutes at a
    time, which in the default configuration loads 3 full cores. This also
    leads to significant *variance* on runbot: runbots are configured with
    as many builds as they have cores[^1], so the test can take 3 cores
    when the machine is idle but during the day when machines tend to be
    fully loaded it contends a ton and slows down dramatically.
    
    `l10n_ro_reports.account_financial_report_ro_bs_large` for instance
    has nearly 400 different clauses, something postgres does not cope
    with well https://explain.dalibo.com/plan/ea5d23aefd3b861f#grid
    
    This is not helped by those searches being filtered by account codes,
    which have not been indexed since 18.0 (because it's now
    `company_dependent`).
    
    Since the accounts table is pretty small (800 records on runbot, 8000
    on odoo.com) we can just load the entire thing in memory and index
    it[^2].
    
    Local runtime of `test_balance_sheet_balanced`
    
    - before: 164.80s user 1.71s system 12% cpu 23:03.28 total
    - after: 124.43s user 1.80s system 23% cpu 8:46.15 total
    
    PERF NOTES
    
    - Currently the code (prefix) search uses a binary search so
      complexity is O(m log(n)). This could be improved by sorting the
      non-tag formulas, as we could then use synchronised linear search,
      working in O(m + n).
    - Assuming a given report has a constant `company_domain`, the
      accounts list & index(es) could be computed higher up the stack iff
      we have at least one use of the `account_codes` engine.
    
    [^1]: while they are hyperthreaded that's only an extra 10-ish%
          performance opportunity at best for CPU-bound workloads,
          sometimes less
    [^2]: this adds about 10MB to the maximum RSS of
        `test_generate_all_export_files`
    
    closes odoo/enterprise#88161
    
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -2,9 +2,11 @@
 
 import ast
 import base64
+import bisect
 import contextlib
 import datetime
 import io
+import itertools
 import json
 import logging
 import re
@@ -3945,74 +3947,56 @@ class AccountReport(models.Model):
         Example 2: '123D\C' will return the balance of accounts starting with '123D' if it's negative, 0 otherwise.
         """
         self._check_groupby_fields((next_groupby.split(',') if next_groupby else []) + ([current_groupby] if current_groupby else []))
+        prefilter = self.env['account.account']._check_company_domain(self.get_report_company_ids(options))
 
+        accounts = self.env['account.account'].search_read([*prefilter, ('code', '!=', False)], ['code', 'tag_ids'])
+        accounts.sort(key=lambda acc: acc['code'])
+        tags_map = defaultdict(list)
+        for acc in accounts:
+            # If an account has no tags, map it to the pseudo-tag `False` such
+            # that a ref which does not exist matches it, otherwise DK balance
+            # fails in `test_generate_all_export_files`
+            for tag in (acc['tag_ids'] or [False]):
+                tags_map[tag].append(acc)
+
+        accounts_prefix_map = defaultdict(set)
         # Gather the account code prefixes to compute the total from
         prefix_details_by_formula = {}  # in the form {formula: [(1, prefix1), (-1, prefix2)]}
-        prefixes_to_compute = set()
         for formula in formulas_dict:
             prefix_details_by_formula[formula] = []
-            for token in ACCOUNT_CODES_ENGINE_SPLIT_REGEX.split(formula.replace(' ', '')):
-                if token:
-                    token_match = ACCOUNT_CODES_ENGINE_TERM_REGEX.match(token)
+            for token in filter(None, ACCOUNT_CODES_ENGINE_SPLIT_REGEX.split(formula.replace(' ', ''))):
+                token_match = ACCOUNT_CODES_ENGINE_TERM_REGEX.match(token)
 
-                    if not token_match:
-                        raise UserError(_("Invalid token '%(token)s' in account_codes formula '%(formula)s'", token=token, formula=formula))
+                if not token_match:
+                    raise UserError(_("Invalid token '%(token)s' in account_codes formula '%(formula)s'", token=token, formula=formula))
 
-                    parsed_token = token_match.groupdict()
+                multiplicator = -1 if token_match['sign'] == '-' else 1
+                excluded_prefixes_match = token_match['excluded_prefixes']
+                excluded_prefixes = tuple(excluded_prefixes_match.split(',')) if excluded_prefixes_match else ()
+                prefix = token_match['prefix']
 
-                    if not parsed_token:
-                        raise UserError(_("Could not parse account_code formula from token '%s'", token))
+                # We group using both prefix and excluded_prefixes as keys, for the case where two expressions would
+                # include the same prefix, but exlcude different prefixes (example 104\(1041) and 104\(1042))
+                prefix_key = (prefix, *excluded_prefixes)
+                prefix_details_by_formula[formula].append((multiplicator, prefix_key, token_match['balance_character']))
 
-                    multiplicator = -1 if parsed_token['sign'] == '-' else 1
-                    excluded_prefixes_match = token_match['excluded_prefixes']
-                    excluded_prefixes = excluded_prefixes_match.split(',') if excluded_prefixes_match else []
-                    prefix = token_match['prefix']
-
-                    # We group using both prefix and excluded_prefixes as keys, for the case where two expressions would
-                    # include the same prefix, but exlcude different prefixes (example 104\(1041) and 104\(1042))
-                    prefix_key = (prefix, *excluded_prefixes)
-                    prefix_details_by_formula[formula].append((multiplicator, prefix_key, token_match['balance_character']))
-                    prefixes_to_compute.add((prefix, tuple(excluded_prefixes)))
-
-        # Create the subquery for the WITH linking our prefixes with account.account entries
-        all_prefixes_queries: list[SQL] = []
-        prefilter = self.env['account.account']._check_company_domain(self.get_report_company_ids(options))
-        for prefix, excluded_prefixes in prefixes_to_compute:
-            account_domain = [
-                *prefilter,
-            ]
-
-            tag_match = ACCOUNT_CODES_ENGINE_TAG_ID_PREFIX_REGEX.match(prefix)
-
-            if tag_match:
-                if tag_match['ref']:
-                    tag_id = self.env['ir.model.data']._xmlid_to_res_id(tag_match['ref'])
+                if tag := ACCOUNT_CODES_ENGINE_TAG_ID_PREFIX_REGEX.match(prefix):
+                    if tag['ref']:
+                        tag_id = self.env['ir.model.data']._xmlid_to_res_id(tag['ref'])
+                    else:
+                        tag_id = int(tag['id'])
+                    accs = tags_map[tag_id]
                 else:
-                    tag_id = int(tag_match['id'])
+                    idx = bisect.bisect_left(accounts, prefix, key=lambda acc: acc['code'])
+                    accs = itertools.takewhile(
+                        lambda acc: acc['code'].startswith(prefix),
+                        itertools.islice(accounts, idx, None),
+                    )
 
-                account_domain.append(('tag_ids', 'in', [tag_id]))
-            else:
-                account_domain.append(('code', '=like', f'{prefix}%'))
-
-            excluded_prefixes_domains = []
-
-            for excluded_prefix in excluded_prefixes:
-                excluded_prefixes_domains.append([('code', '=like', f'{excluded_prefix}%')])
-
-            if excluded_prefixes_domains:
-                account_domain.append('!')
-                account_domain += osv.expression.OR(excluded_prefixes_domains)
-
-            prefix_query = self.env['account.account']._where_calc(account_domain)
-            all_prefixes_queries.append(prefix_query.select(
-                SQL("%s AS prefix", [prefix, *excluded_prefixes]),
-                SQL("account_account.id AS account_id"),
-            ))
-
-        # Build a map to associate each account with the prefixes it matches
-        accounts_prefix_map = defaultdict(list)
-        for prefix, account_id in self.env.execute_query(SQL(' UNION ALL ').join(all_prefixes_queries)):
-            accounts_prefix_map[account_id].append(tuple(prefix))
+                for account in accs:
+                    if excluded_prefixes and account['code'].startswith(excluded_prefixes):
+                        continue
+                    accounts_prefix_map[account['id']].add(prefix_key)
 
         # Run main query
         query = self._get_report_query(options, date_scope)
