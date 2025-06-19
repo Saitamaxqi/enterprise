00e184b35a926c11031f46ff737467caa626f67c
Kadam Darji <kmdi@odoo.com>
2025-06-19 06:50:23 +0000

    [FIX] knowledge: remove redundant paper plane from comment composer
    Steps to reproduce
    ===============
    1. Go to knowledge.
    2. Open any article with write access.
    2. Open the comment composer for any text of article.
    --> Two buttons are shown to post a comment. (Paper-plane and `Log` button)
    
    From [Commit], the condition to show the paper plane was modified, which will be
    always evaluated to `true` for the knowledge comment composer.
    
    After this commit, we will explicitly make that condition false for knowledge
    comment composer.
    
    Task-4866473
    
    [Commit]: https://github.com/odoo/odoo/commit/9ba04f2a28f4d2b1adeb04628383d4730b5e72b6
    
    closes odoo/enterprise#89457
    
    X-original-commit: 1397fefd0c3c813fa53fe33838177055b1acac4b
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- knowledge/static/tests/tours/knowledge_article_comments_tour.js
+++ knowledge/static/tests/tours/knowledge_article_comments_tour.js
@@ -32,6 +32,9 @@ registry.category('web_tour.tours').add('knowledge_article_comments', {
             run: "click",
         }, {
             trigger: '.o-mail-Thread :contains("Marc, can you check this?")',
+        }, {
+            content: "Check that composer doesn't have paper plane button but has a send button",
+            trigger: ".o-mail-Composer:has(button.o-mail-Composer-send):not(:has(i.fa-paper-plane-o))",
         },
         ...addAnswerComment("Sure thing boss, all done!"),
         {
