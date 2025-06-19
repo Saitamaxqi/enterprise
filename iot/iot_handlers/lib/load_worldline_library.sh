667b77a9284894a10107931648f15c7a8c9204f4
Louis Travaux <lotr@odoo.com>
2025-06-19 11:09:08 +0000

    [FIX] iot: libeasyctep.so download
    The call to the script downloading and extracting `libeasyctep.so` was returning a
    traceback if it failed to remount the system readonly.
    We moved the rw/ro mounting logic from the sh script to the python call to avoid this.
    
    closes odoo/enterprise#88215
    
    Task: 4852374
    X-original-commit: 122d60885f79c6f1c7a8eea2342ad68f44a17936
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- iot/iot_handlers/lib/load_worldline_library.sh
+++ iot/iot_handlers/lib/load_worldline_library.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-sudo mount -o remount,rw /
-sudo mount -o remount,rw /root_bypass_ramdisks
-
 PATH_ZIP_LIB=/home/pi/odoo/addons/hw_drivers/iot_handlers/lib/
 PATH_LIB=${PATH_ZIP_LIB}ctep/lib/
 
@@ -22,6 +19,3 @@ fi
 if id odoo > /dev/null 2>&1; then
 	sudo chown -R odoo:odoo "${PATH_ZIP_LIB}"
 fi
-
-sudo mount -o remount,ro /
-sudo mount -o remount,ro /root_bypass_ramdisks
