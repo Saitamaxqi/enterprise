c6ead605f61e9d9d7f5360542de2e9f4c9df12e4
Levi Siuzdak (sile) <sile@odoo.com>
2025-06-23 12:22:26 +0000

    [FIX] website_sale_renting: add date-based rental duration tour
    Adds a tour that ensure date-based rental durations are set correctly,
    i.e. renting from 2025-01-01 to 2025-01-02 should result in a rental
    period of 2 days.
    
    This should prevent commits like 574e111 (reverted by 5982e11) from
    introducing faulty logic.
    
    closes odoo/enterprise#89064
    
    X-original-commit: 474303cd34f586533ce3464d6fcbdc341b17bed8
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- website_sale_renting/static/tests/tours/website_sale_change_rental_duration.js
+++ website_sale_renting/static/tests/tours/website_sale_change_rental_duration.js
@@ -3,6 +3,11 @@
 import { registry } from "@web/core/registry";
 import * as tourUtils from '@website_sale/js/tours/tour_utils';
 
+function getFutureDate(days) {
+    days = (days ?? 0) + 7;
+    return luxon.DateTime.now().set({ weekday: 1 }).plus({ days }).toFormat('MM/dd/yyyy');
+}
+
 registry.category("web_tour.tours").add("rental_cart_update_duration", {
     url: "/shop",
     steps: () => [
@@ -65,5 +70,32 @@ registry.category("web_tour.tours").add("rental_cart_update_duration", {
             content: "Verify order line rental period return time",
             trigger: 'div.text-muted.small span:contains("12:00")',
         },
+    ],
+});
+
+registry.category('web_tour.tours').add('date_based_rental_duration', {
+    steps: () => [
+        ...tourUtils.searchProduct("Computer", { select: true }),
+        {
+            content: "Select the return date",
+            trigger: 'input[name=renting_end_date]',
+            run: `edit ${getFutureDate(2)}`,
+        },
+        {
+            content: "Rent for 2 days",
+            trigger: 'input[name=renting_start_date]',
+            run: `edit ${getFutureDate(1)}`,
+        },
+        {
+            content: "Add to cart",
+            trigger: '#product_detail form #add_to_cart',
+            run: 'click',
+        },
+        {
+            content: "Rental duration should display 2 days",
+            trigger: 'span.o_renting_details:contains(2 Days)',
+        },
+        tourUtils.goToCart(),
+        ...tourUtils.assertCartAmounts({ untaxed: "40.00" }), // $ 20.00 per day
     ],
 });
--- website_sale_renting/tests/test_ui.py
+++ website_sale_renting/tests/test_ui.py
@@ -2,6 +2,7 @@
 
 from freezegun import freeze_time
 
+from odoo import Command
 from odoo.tests import HttpCase, tagged
 
 from odoo.addons.website_sale_renting.tests.common import TestWebsiteSaleRentingCommon
@@ -61,6 +62,23 @@ class TestUi(HttpCase, TestWebsiteSaleRentingCommon):
     def test_website_sale_update_rental_duration(self):
         self.computer.website_published = True
         self.start_tour('/shop', 'rental_cart_update_duration')
+
+    def test_website_sale_update_rental_duration_days(self):
+        self.computer.update({
+            'website_published': True,
+            'product_pricing_ids': [
+                Command.clear(),
+                Command.create({
+                    'recurrence_id': self.recurrence_day.id,
+                    'price': 20.0,
+                }),
+            ],
+        })
+        self.env.company.update({
+            'renting_minimal_time_duration': 1,
+            'renting_minimal_time_unit': 'day',
+        })
+        self.start_tour('/shop', 'date_based_rental_duration')
 
     def test_website_sale_renting_select_wrong_period(self):
         self.computer.website_published = True
