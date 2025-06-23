f4e907e9a045bf6064423a7aec29f4346b914577
Antoine Dupuis <andu@odoo.com>
2025-06-23 14:21:02 +0200

    [FIX] l10n_be_disallowed_expenses: Only write on existing accounts
    In the post-init script of `l10n_be_disallowed_expenses` we try to write
    the `disallowed_expenses_category_id` field on all the accounts with
    xmlids in the data/template/account.account-be.csv file.
    
    This will fail if the account doesn't exist.
    
    Fix: Only write on the accounts that exist. The user can reload the CoA
    to create any accounts that don't exist yet if needed.
    
    task-none
    
    closes odoo/enterprise#87598
    
    Related: odoo/odoo#214151
    Related: odoo/upgrade#7891
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- l10n_be_disallowed_expenses/__init__.py
+++ l10n_be_disallowed_expenses/__init__.py
@@ -5,6 +5,12 @@ from . import models
 def _l10n_be_disallowed_expenses_post_init(env):
     for company in env['res.company'].search([('chart_template', 'in', ('be_comp', 'be_asso')), ('parent_id', '=', False)]):
         Template = env['account.chart.template'].with_company(company)
-        Template._load_data({
-            'account.account': Template._get_be_disallowed_expenses_accounts(),
-        })
+        disallowed_expenses_accounts = {
+            xmlid: vals
+            for xmlid, vals in Template._get_be_disallowed_expenses_accounts().items()
+            if Template.ref(xmlid, raise_if_not_found=False)
+        }
+        if disallowed_expenses_accounts:
+            Template._load_data({
+                'account.account': disallowed_expenses_accounts,
+            })
