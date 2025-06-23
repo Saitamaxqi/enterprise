70db8b115894b40cb5fffa478c8ae9f57e3ce36d
Florian(flg) <flg@odoo.com>
2025-06-23 12:05:37 +0000

    [FIX] l10n_se_sie4_import: Allow tabulations in import file
    The aim of this commit is replacing all the tabulations in the
    file while reading it for spaces as the csv reader is using
    spaces as delimiter.
    This commit also introduces a new user error which indicates the
    user that the #ADRESS line is malformed.
    
    opw-4868415
    
    closes odoo/enterprise#88909
    
    X-original-commit: 499b1d9f207032ce5a713d602309b2f648dec893
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- l10n_se_sie4_import/wizard/import_wizard.py
+++ l10n_se_sie4_import/wizard/import_wizard.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 from odoo import _, api, Command, fields, models, modules, tools
+from odoo.exceptions import UserError
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 DATEFORMAT_SIE4 = '%Y%m%d'
@@ -236,7 +237,7 @@ class SIE4ImportWizard(models.TransientModel):
         :param str sie4_line: a string of a line in the SIE 4 file
         :rtype: list[str]
         """
-        sie4_line = sie4_line.replace('{', '"').replace('}', '"').strip()
+        sie4_line = sie4_line.replace('{', '"').replace('}', '"').replace('\t', ' ').strip()
         sie4_line = re.sub(r' +', ' ', sie4_line)  # filter double spaces between words
         reader = csv.reader([sie4_line], delimiter=' ')
         return next(reader)
@@ -253,6 +254,13 @@ class SIE4ImportWizard(models.TransientModel):
             case '#ORGNR':
                 return {'org_number': args[0]}
             case '#ADRESS':
+                if len(args) < 4:
+                    raise UserError(_(
+                        'Missing element in #ADRESS line.\n'
+                        'Expected format: "contact" "distribution address" "postal address" "telephone".\n'
+                        'Received data: %(data)s.',
+                        data=" ".join(args),
+                    ))
                 return {'addr_detail': args[1], 'addr_main': args[2], 'telephone': args[3]}
             case '#FNAMN':
                 return {'company_name': args[0]}
