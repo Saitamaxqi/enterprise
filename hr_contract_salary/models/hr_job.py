b68569868c00765bca910b5630a72db75347c03d
Omar Khair <omkh@odoo.com>
2025-06-24 11:31:58 +0200

    [FIX] {l10n_be_,}hr_contract_salary: multi-company inconsistency contract template
    Enforce the constraint of the job and the linked contract template to have the same company.
    Fix the demo data to align with this contraint.
    
    task-4885755
    
    closes odoo/enterprise#88369
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_contract_salary/models/hr_job.py
+++ hr_contract_salary/models/hr_job.py
@@ -2,7 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class HrJob(models.Model):
@@ -10,3 +11,16 @@ class HrJob(models.Model):
 
     contract_template_id = fields.Many2one('hr.version', domain="[('company_id', '=', company_id), ('employee_id', '=', False)]", string="Contract Template",
         help="Default contract used to generate an offer. If empty, benefits will be taken from current contract of the employee/nothing for an applicant.")
+    company_id = fields.Many2one(compute='_compute_company_id', readonly=False, store=True, precompute=True)
+
+    @api.depends('contract_template_id.company_id')
+    def _compute_company_id(self):
+        for job in self:
+            if job.contract_template_id:
+                job.company_id = job.contract_template_id.company_id
+
+    @api.constrains('contract_template_id', 'company_id')
+    def _check_contract_template_company(self):
+        for job in self:
+            if job.contract_template_id and job.contract_template_id.company_id != job.company_id:
+                raise ValidationError(self.env._("The contract template's company must match the job's company."))
