50fb961eff8f4249b6e37315216bc886f5e7466e
Xavier Bol (xbo) <xbo@odoo.com>
2025-06-24 08:22:22 +0000

    [FIX] hr_payroll: make payrunId prop optional in PayslipActionHelper
    Before this commit, the payrunId prop definition said it is a required
    prop and an integer. However, that property is not really required.
    
    This commit changes the property definition to make that property
    optional.
    
    runbot-error-226806
    
    closes odoo/enterprise#88426
    
    X-original-commit: 949a46f0fba30fef463bb68e365219d9be387e8d
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- hr_payroll/static/src/components/payslip_action_helper/payslip_action_helper.js
+++ hr_payroll/static/src/components/payslip_action_helper/payslip_action_helper.js
@@ -4,7 +4,7 @@ import { Component } from "@odoo/owl";
 export class PayslipActionHelper extends Component {
     static template = "hr_payroll.PayslipActionHelper";
     static props = {
-        payrunId: { type: Number },
+        payrunId: { type: Number, optional: true },
         onClickCreate: { type: Function },
         onClickGenerate: { type: Function },
     };
