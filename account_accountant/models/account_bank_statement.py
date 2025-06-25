b58a3154477041ae247d1304749e2273893dd7be
igbe <igbe@odoo.com>
2025-06-25 15:37:05 +0000

    [FIX] account_accountant: increase length for full match
    Before, aml.ref had to be at least 5char length to be a full match candidate.
    
    But it seems 5 is not enough and leads to unwanted matches, so let's increase the limit to 7.
    
    no-task
    
    closes odoo/enterprise#88840
    
    X-original-commit: bb0c1c368144de27ef87c490db4fa3c7b8cf05d9
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>
    Signed-off-by: Igor Bertrand (igbe) <igbe@odoo.com>

--- account_accountant/models/account_bank_statement.py
+++ account_accountant/models/account_bank_statement.py
@@ -265,16 +265,16 @@ class AccountBankStatementLine(models.Model):
                             FROM regexp_matches(st_line.payment_ref, '\\w{2,5}/?\\d{4}/\\d+(?:/\\d+)?(?:-\\d+)?', 'g') AS match
                           ), '|') || ')\\M')
                           -- Second Rule, check for full match between aml ref and st_line label
-                          OR (LENGTH(aml.ref) >= 5 AND POSITION(aml.ref in st_line.payment_ref) > 0)
+                          OR (LENGTH(aml.ref) >= 7 AND POSITION(aml.ref in st_line.payment_ref) > 0)
                           -- Third Rule, check if there is a word longer than 16 characters matching both fields
                           OR aml.ref ~ ('\\m(' || array_to_string((
                             SELECT array_agg(regexp_replace(match[1], '([\\.+*?\\[\\]^$(){}=!<>|:])', '\\\1', 'g'))
                             FROM regexp_matches(st_line.payment_ref, '\\S{16,}', 'g') AS match
                           ), '|') || ')\\M')
                           -- Fourth Rule, full match with move_name
-                          OR (LENGTH(aml.move_name) >= 5 AND POSITION(aml.move_name in st_line.payment_ref) > 0)
+                          OR (LENGTH(aml.move_name) >= 7 AND POSITION(aml.move_name in st_line.payment_ref) > 0)
                           -- Fifth Rule, full match with payment_ref on move
-                          OR (LENGTH(move.payment_reference) >= 5 AND POSITION(move.payment_reference in st_line.payment_ref) > 0))
+                          OR (LENGTH(move.payment_reference) >= 7 AND POSITION(move.payment_reference in st_line.payment_ref) > 0))
                    ) AS ref_aml_ids
               FROM account_bank_statement_line st_line, account_move_line aml
          LEFT JOIN account_move move ON aml.move_id = move.id
@@ -293,13 +293,13 @@ class AccountBankStatementLine(models.Model):
                       SELECT array_agg(regexp_replace(match[1], '([\\.+*?\\[\\]^$(){}=!<>|:])', '\\\1', 'g'))
                       FROM regexp_matches(st_line.payment_ref, '\\w{2,5}/?\\d{4}/\\d+(?:/\\d+)?(?:-\\d+)?', 'g') AS match
                     ), '|') || ')\\M')
-                    OR (LENGTH(aml.ref) >= 5 AND POSITION(aml.ref in st_line.payment_ref) > 0)
+                    OR (LENGTH(aml.ref) >= 7 AND POSITION(aml.ref in st_line.payment_ref) > 0)
                     OR aml.ref ~ ('\\m(' || array_to_string((
                       SELECT array_agg(regexp_replace(match[1], '([\\.+*?\\[\\]^$(){}=!<>|:])', '\\\1', 'g'))
                       FROM regexp_matches(st_line.payment_ref, '\\S{16,}', 'g') AS match
                     ), '|') || ')\\M')
-                    OR (LENGTH(aml.move_name) >= 5 AND POSITION(aml.move_name in st_line.payment_ref) > 0)
-                    OR (LENGTH(move.payment_reference) >= 5 AND POSITION(move.payment_reference in st_line.payment_ref) > 0))
+                    OR (LENGTH(aml.move_name) >= 7 AND POSITION(aml.move_name in st_line.payment_ref) > 0)
+                    OR (LENGTH(move.payment_reference) >= 7 AND POSITION(move.payment_reference in st_line.payment_ref) > 0))
               )
                END
                AND st_line.move_id != aml.move_id
