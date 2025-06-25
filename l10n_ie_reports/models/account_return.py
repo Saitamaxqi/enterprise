03b6073e9a7f7e0fe41d229da1e55e90aeaf5c25
jond-odoo <jond@odoo.com>
2025-06-25 11:25:24 +0200

    [IMP] l10n_*: Add tax return for IE and UK
    Add the tax return for the irish and british EC Sales List
    and Intrastat reports.
    
    closes odoo/enterprise#88545
    
    Task-id: 4784433
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- l10n_ie_reports/models/account_return.py
+++ l10n_ie_reports/models/account_return.py
@@ -10,15 +10,14 @@ class AccountReturnType(models.Model):
     def _generate_all_returns(self, country_code, main_company, tax_unit=None):
         rslt = super()._generate_all_returns(country_code, main_company, tax_unit=tax_unit)
 
-        if main_company.is_northern_irish():
-            ec_sales_return_type = self.env.ref('l10n_uk_reports.uk_ec_sales_list_return_type')
+        if country_code == 'IE':
+            ec_sales_return_type = self.env.ref('l10n_ie_reports.ie_ec_sales_list_return_type')
             offset = ec_sales_return_type._get_periodicity_months_delay(main_company)
             date_in_previous_period = fields.Date.context_today(self) - relativedelta(months=offset)
             date_from, date_to = ec_sales_return_type._get_period_boundaries(main_company, date_in_previous_period)
-            intracom_taxes = self.env.ref(f'account.{main_company.id}_account_fiscal_position_ni_to_eu_b2b').tax_ids
-
+            intra_community_taxes = self.env.ref(f'account.{main_company.id}_ie_fp_eu').tax_ids
             domain = [
-                ('tax_ids', 'in', intracom_taxes),
+                ('tax_ids', 'in', intra_community_taxes),
                 ('balance', '!=', 0),
                 ('date', '>=', date_from),
                 ('date', '<=', date_to),
@@ -28,16 +27,3 @@ class AccountReturnType(models.Model):
                 ec_sales_return_type._try_create_return_for_period(date_from, main_company, tax_unit)
 
         return rslt
-
-
-class AccountReturn(models.Model):
-    _inherit = 'account.return'
-
-    @api.model
-    def _evaluate_deadline(self, company, return_type, return_type_external_id, date_from, date_to):
-        if return_type_external_id == 'l10n_uk_reports.uk_tax_return_type' and not return_type.deadline_days_delay:
-            return date_to + relativedelta(days=7) + relativedelta(months=1)
-        elif return_type_external_id == 'l10n_uk_reports.uk_ec_sales_list_return_type':
-            return date_to + relativedelta(days=21)
-
-        return super()._evaluate_deadline(company, return_type, return_type_external_id, date_from, date_to)
--- l10n_uk_reports/models/__init__.py
+++ l10n_uk_reports/models/__init__.py
@@ -1,4 +1,5 @@
 from . import account_return
+from . import res_company
 from . import res_users
 from . import account_financial_report
 from . import hmrc_vat_obligation
