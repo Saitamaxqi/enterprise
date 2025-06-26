291219b3d1ab25850c0ae1eb8c56dc0eb4ed4acd
Levi Siuzdak (sile) <sile@odoo.com>
2025-06-26 12:03:37 +0000

    [FIX] sale_renting: prevent product configurator traceback
    Versions
    --------
    - 18.0+
    
    Steps
    -----
    1. Create a rental product with multiple variants;
    2. create a subscription order via the Rental app;
    3. add the rental product;
    4. confirm & invoice the subscription;
    5. renew the subscription;
    6. add the rental product to the renewal.
    
    Issue
    -----
    > `Uncaught Promise > value.setZone is not a function`
    
    Cause
    -----
    Commit 76526bdb3835 implemented the `_getAdditionalDialogProps` method for using OWL in the product configurator. In our flow, it simply checks whether the order is a rental order, and if so, it attempts to serialize its rental dates.
    
    Issue is that rental dates are not a required field as long as the order hasn't been confirmed yet, causing the traceback.
    
    Solution
    --------
    Instead of checking whether it is a rental order, verify that the order has rental dates before attempting to serialize them.
    
    opw-4898437
    
    closes odoo/enterprise#89152
    
    X-original-commit: b5b48325e54a032e879b63f6b22e96f8c91bdf2c
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- sale_renting/static/src/js/sale_product_field.js
+++ sale_renting/static/src/js/sale_product_field.js
@@ -5,20 +5,20 @@ import { patch } from '@web/core/utils/patch';
 patch(SaleOrderLineProductField.prototype, {
     _getAdditionalRpcParams() {
         const params = super._getAdditionalRpcParams();
-        const saleOrder = this.props.record.model.root;
-        if (saleOrder.data.is_rental_order) {
-            params.start_date = serializeDateTime(saleOrder.data.rental_start_date);
-            params.end_date = serializeDateTime(saleOrder.data.rental_return_date);
+        const { rental_start_date, rental_return_date } = this.props.record.model.root.data;
+        if (rental_start_date && rental_return_date) {
+            params.start_date = serializeDateTime(rental_start_date);
+            params.end_date = serializeDateTime(rental_return_date);
         }
         return params;
     },
 
     _getAdditionalDialogProps() {
         const props = super._getAdditionalDialogProps();
-        const saleOrder = this.props.record.model.root;
-        if (saleOrder.data.is_rental_order) {
-            props.start_date = serializeDateTime(saleOrder.data.rental_start_date);
-            props.end_date = serializeDateTime(saleOrder.data.rental_return_date);
+        const { rental_start_date, rental_return_date } = this.props.record.model.root.data;
+        if (rental_start_date && rental_return_date) {
+            props.start_date = serializeDateTime(rental_start_date);
+            props.end_date = serializeDateTime(rental_return_date);
         }
         return props;
     },
