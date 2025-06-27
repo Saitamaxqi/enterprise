2c5aa69ce95a10db00d4d0d3ac860eff378540f3
Louis Travaux <lotr@odoo.com>
2025-06-27 15:05:39 +0000

    [IMP] pos_iot: display associated PoS on device records
    In order to ease the configuration and use of IoT devices in PoS, we now display
    the PoS where devices are configured directly on device records.
    
    closes odoo/enterprise#90523
    
    Task: 4901312
    X-original-commit: bdc6aa07365554e8a6ac90f01b0ff345ee67379d
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- pos_iot/models/iot_device.py
+++ pos_iot/models/iot_device.py
@@ -1,11 +1,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, api
+from odoo import api, fields, models
+from odoo.fields import Domain
 
 
 class IotDevice(models.Model):
     _name = 'iot.device'
     _inherit = ['iot.device', 'pos.load.mixin']
+
+    associated_pos_config_ids = fields.Many2many(
+        'pos.config', string="Associated PoS", compute='_compute_associated_pos_config_ids'
+    )
 
     @api.model
     def _load_pos_data_domain(self, data, config):
@@ -14,3 +19,26 @@ class IotDevice(models.Model):
     @api.model
     def _load_pos_data_fields(self, config):
         return ['iot_ip', 'iot_id', 'identifier', 'type', 'manual_measurement']
+
+    @api.depends('type')
+    def _compute_associated_pos_config_ids(self):
+        """Compute the associated PoS config ids for the Current device"""
+        pos_config = self.env['pos.config']
+        field_map = {
+            'scanner': ('iface_scanner_ids', 'in'),
+            'printer': ('iface_printer_id', '='),
+            'display': ('iface_display_id', '='),
+            'scale': ('iface_scale_id', '='),
+            'fiscal_data_module':
+                ('iface_fiscal_data_module', '=') if 'iface_fiscal_data_module' in pos_config._fields else (None, None),
+        }
+
+        for device in self:
+            field, operator = field_map.get(device.type, (None, None))
+
+            if not field:
+                device.associated_pos_config_ids = pos_config
+                continue
+
+            domain = Domain(['&', ('is_posbox', '=', True), (field, operator, device.id)])
+            device.associated_pos_config_ids = pos_config.search(domain)
