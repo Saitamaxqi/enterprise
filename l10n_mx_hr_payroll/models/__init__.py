ef09dec4c859b51495832c7dccd34e98a57cd913
Yannick Tivisse <yti@odoo.com>
2025-07-01 07:31:08 +0000

    [REV] l10n_mx_hr_payroll: added error msg for caev rule
    This reverts commit 3e83eed71139e0a166d35ce291a9020e578a448f.
    
    Improper override of `_compute_rule` and misleading test coverage
    The override of `_compute_rule` was applied globally, affecting all
    salary rules, including irrelevant ones (e.g., for Belgian companies),
    even though the logic was only needed for a specific rule. This
    introduced unintended side effects across the system.
    
    Additionally, the `_check_int_day_wage_requirements` method raises a
    `UserError`, but the test does not correctly catch this specific error
    but another one, giving a false sense of reliability.
    
    Moreover, instead of using:
    
    ```python
    payslip.line_ids.filtered(lambda l: l.code == 'BASIC').mapped('total')
    ```
    
    we already have a more efficient method, `_get_payslip_line_values`,
    that performs the same logic using a single SQL query for multiple codes.
    
    Due to these issues, the change is being reverted in favor of previous
    commit.
    
    closes odoo/enterprise#89335
    
    X-original-commit: fc9e67f5f9b81b3c8a8754e52ee442203157dedf
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: Abdelrahman Mahmoud (amah) <amah@odoo.com>

--- l10n_mx_hr_payroll/models/__init__.py
+++ l10n_mx_hr_payroll/models/__init__.py
@@ -7,4 +7,3 @@ from . import hr_payroll_structure
 from . import hr_payroll_structure_type
 from . import l10n_mx_hr_infonavit
 from . import l10n_mx_hr_fonacot
-from . import hr_salary_rule
--- l10n_mx_hr_payroll/tests/__init__.py
+++ l10n_mx_hr_payroll/tests/__init__.py
@@ -1,2 +1,1 @@
 from . import test_payslip_worked_days
-from . import test_payslip_computation
