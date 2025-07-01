aa9869a0552a86abc08a60032acbd2741de63240
3amo-magdy <ammg@odoo.com>
2025-07-01 07:36:48 +0000

    [FIX] hr_contract_salary: version smartbutton for offer
    - Fixed an issue where clicking the "Signed Contract" smartbutton after full signature opened an incorrect employee version.
    
    Task-4873800
    
    closes odoo/enterprise#89821
    
    X-original-commit: 6220b4254023ecb076c15abd6038129f34afa8c3
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_contract_salary/models/hr_contract_salary_offer.py
+++ hr_contract_salary/models/hr_contract_salary_offer.py
@@ -257,14 +257,16 @@ class HrContractSalaryOffer(models.Model):
 
     def action_view_contract(self):
         self.ensure_one()
-        version_id = self.employee_version_id.id or self.env['hr.version'].search([("applicant_id", "=", self.applicant_id.id)], limit=1).id
+        version = self.env['hr.version'].search([('originated_offer_id', '=', self.id)], limit=1) or \
+                  self.employee_version_id or \
+                  self.env['hr.version'].search([("applicant_id", "=", self.applicant_id.id)], limit=1)
         return {
             'type': 'ir.actions.act_window',
             'view_mode': 'form',
             'res_model': 'hr.employee',
             'res_id': self.employee_id.id,
             'target': 'current',
-            'context': {'version_id': version_id}
+            'context': {'version_id': version.id}
         }
 
     def _mail_get_partners(self, introspect_fields=False):
