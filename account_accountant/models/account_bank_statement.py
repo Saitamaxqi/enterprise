101858aaf7cceaea775aaa5c181bbdcbed585c8c
Maximilien (malb) <malb@odoo.com>
2025-07-02 08:53:35 +0000

    [FIX] account_accountant: cron company
    Before this commit when the cron was triggered in a multi currency environment,
    the self.env.company could be a different one from the one of the statement line
    that would cause problem when computing the suspense amount since the
    currency_rate would be different.
    
    In this example:
    - 2 companies one in EUR, one in USD
    - On the USD company do a transaction
    - the cron is trigger with the self.env.company as the EUR one
    - It will do a partial reconcile with an invoice in EUR
    - The amount currency of the suspense will be computed by doing the balance *
    the currency rate. Since the company is the EUR one, the rate is not one and the
     open currency amount is wrongly computed
    - This leads to an error of unbalanced move
    
    no task id
    
    closes odoo/enterprise#90107
    
    X-original-commit: 7d496d1ff2af868fcab2a5911ab3c0dec585acb9
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- account_accountant/models/account_bank_statement.py
+++ account_accountant/models/account_bank_statement.py
@@ -320,7 +320,7 @@ class AccountBankStatementLine(models.Model):
         for st_line_id, all_aml_ids, total_residual, ref_aml_ids in self.env.cr.fetchall():
             st_line = self.browse(st_line_id).with_prefetch(self._prefetch_ids)  # guarantees batch prefetching if needed
             if total_residual == st_line.amount:
-                st_line.set_line_bank_statement_line(all_aml_ids)
+                st_line.with_company(st_line.company_id).set_line_bank_statement_line(all_aml_ids)
             elif ref_aml_ids:
                 ref_amls = self.env['account.move.line'].browse(ref_aml_ids).with_prefetch(self._prefetch_ids)
 
@@ -353,7 +353,7 @@ class AccountBankStatementLine(models.Model):
                         ref_amls -= aml
                     else:
                         break
-                st_line.set_line_bank_statement_line(ref_amls.ids)
+                st_line.with_company(st_line.company_id).set_line_bank_statement_line(ref_amls.ids)
             else:
                 # no valid candidates yet
                 continue
@@ -467,7 +467,7 @@ class AccountBankStatementLine(models.Model):
         # process then remove matched statement lines
         for st_line_id, aml_id in self.env.cr.fetchall():
             st_line = self.browse(st_line_id).with_prefetch(self._prefetch_ids)  # guarantees batch prefetching if needed
-            st_line.set_line_bank_statement_line(aml_id)
+            st_line.with_company(st_line.company_id).set_line_bank_statement_line(aml_id)
             if st_line.currency_id.is_zero(st_line.amount_residual):
                 processed_st_line_ids.add(st_line_id)
         remaining_st_line_ids = list(set(remaining_st_line_ids) - processed_st_line_ids)
