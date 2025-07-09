df8c90b4f198e225cb648270e8ecb7d3d203a7a7
Ajit Singh <aksi@odoo.com>
2025-07-09 12:07:22 +0000

    [FIX] hr_contract_salary: avoid access error
    After this commit odoo/enterprise@40d3314d1b06c8110b476690d2264e86b3492b3e we are not able to cancel a sign request,
    if user do not have read/write access on `hr.contract.salary.offer` model
    
    step to reproduce:
    - install `hr_contract_salary`
    - create a new user with following right:
      Sign        -> Administrator
      Contracts   -> Employee Manager
      Recruitment -> Interviewer
    - login with test user
    - open sign -> All Documents (with demo data 2, there should be 2 documents)
    - try to cancel one of them
    
    Observation:
    - even with Admin rights of Sign, we are not able to cancel a sign request
    and we receive a Access Error
    `You are not allowed to access 'Salary Package Offer' (hr.contract.salary.offer)
    records`
    
    Fix:
    we use `sudo()` to allow everyone to cancel their sign request.
    
    opw-4859774
    
    closes odoo/enterprise#91843
    
    X-original-commit: 2bede75c01cbb805514922e92a6a2c5521b500ec
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>
    Signed-off-by: Ajit Kumar  Krishnanand Singh (aksi) <aksi@odoo.com>

--- hr_contract_salary/models/sign_request.py
+++ hr_contract_salary/models/sign_request.py
@@ -6,8 +6,10 @@ class SignRequest(models.Model):
 
     def cancel(self):
         super().cancel()
-        offers = self.env['hr.contract.salary.offer'].search([('sign_request_ids', 'in', self.ids)])
-        offers.applicant_id.unlink_archived_versions()
-        offers.write({'state': 'cancelled'})
-        for offer in offers:
+        # sudo(), as not everyone has access to 'hr.contract.salary.offer'
+        # but they should be able to cancel their sign request
+        offers_sudo = self.env['hr.contract.salary.offer'].sudo().search([('sign_request_ids', 'in', self.ids)])
+        offers_sudo.applicant_id.unlink_archived_versions()
+        offers_sudo.write({'state': 'cancelled'})
+        for offer in offers_sudo:
             offer.message_post(body=_("The offer has been cancelled due to the signature request cancellation."))
--- hr_contract_salary/tests/test_sign_request_cancel.py
+++ hr_contract_salary/tests/test_sign_request_cancel.py
@@ -1,6 +1,6 @@
 from odoo import Command
 from odoo.tools import file_open
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase, new_test_user
 
 
 class TestSignRequestCancel(TransactionCase):
@@ -35,7 +35,7 @@ class TestSignRequestCancel(TransactionCase):
             'job_id': cls.job.id,
         })
 
-        cls.env['sign.item'].create([{
+        cls.sign_item = cls.env['sign.item'].create([{
             'type_id': cls.env.ref('sign.sign_item_type_text').id,
             'required': True,
             'responsible_id': cls.env.ref('sign.sign_item_role_customer').id,
@@ -47,21 +47,35 @@ class TestSignRequestCancel(TransactionCase):
             'height': 0.015,
         }])
 
-        partner = cls.env['res.partner'].create({'name': 'Employee', 'email': 'employee@example.com'})
-        cls.sign_request = cls.env['sign.request'].create({
-            'template_id': cls.template_id.id,
-            'reference': 'Test Offer',
-            'request_item_ids': [Command.create({
-                'partner_id': partner.id,
-                'role_id': cls.env.ref('sign.sign_item_role_customer').id,
-            })],
-        })
+        cls.partner = cls.env['res.partner'].create({'name': 'Employee', 'email': 'employee@example.com'})
 
         cls.salary_offer = cls.env['hr.contract.salary.offer'].create({
             'contract_template_id': cls.contract_template.id,
-            'sign_request_ids': cls.sign_request,
         })
 
     def test_cancel_sign_request(self):
-        self.sign_request.cancel()
+        new_test_user(self.env, login='user1', groups='sign.group_sign_user')
+        with self.with_user("user1"):
+            # sudo() to avoid recreating everything again and instead use already present record
+            template_id = self.env['sign.template'].sudo().create({
+                'name': 'Employee Contract Template(1)',
+            })
+            self.env['sign.document'].sudo().create({
+                'template_id': template_id.id,
+                'attachment_id': self.attachment.id,
+                'sign_item_ids': self.sign_item
+            })
+            sign_request = self.env['sign.request'].create({
+                'template_id': template_id.id,
+                'reference': 'Test Offer',
+                'request_item_ids': [Command.create({
+                    'partner_id': self.partner.id,
+                    'role_id': self.env.ref('sign.sign_item_role_customer').id,
+                })],
+            })
+        self.salary_offer.sign_request_ids = sign_request
+
+        with self.with_user("user1"):
+            sign_request.cancel()
+            self.assertEqual(sign_request.state, 'canceled', "User should be able to cancel his Sign Request.")
         self.assertEqual(self.salary_offer.state, 'cancelled', "The offer should be cancelled when the sign request is cancelled.")
