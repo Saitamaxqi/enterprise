d6b12e49ade3d9e60b12cdf472bf39bb8919d757
Maximilien (malb) <malb@odoo.com>
2025-07-16 06:51:53 +0000

    [FIX] account_accountant_batch_payment, sale_account_accountant: dead code
    In this commit: https://github.com/odoo/enterprise/pull/80787/commits/04a304d58a56c7d2eeb70429340eb048fa9f2be6
    We refactored the reconciliation model to be easier to use for users.
    However, by doing the refactor we forgot to remove some code.
    
    no task id
    
    closes odoo/enterprise#91284
    
    X-original-commit: fe0e9b29682f0d1dae08830e220fc427d0daf0c1
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- account_accountant_batch_payment/models/__init__.py
+++ account_accountant_batch_payment/models/__init__.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from . import account_batch_payment
-from . import account_reconcile_model
 from . import account_bank_statement_line
 from . import account_payment
