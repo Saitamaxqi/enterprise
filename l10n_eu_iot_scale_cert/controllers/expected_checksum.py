d59ff1ffc8cd410d4d4f4ce820865dbc8bea62da
yaso <yaso@odoo.com>
2025-07-16 13:07:40 +0000

    [IMP] pos_iot: adapt scale checksum to scale error
    This PR adapts the scale checksum to the PR adding the error checks to Toledo scales:
    odoo/odoo#217674
    
    closes odoo/enterprise#91191
    
    Forward-port-of: #89602
    X-original-commit: d024dd48471257a55c39884462a50353984838ac
    Related: odoo/odoo#220930
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- l10n_eu_iot_scale_cert/controllers/expected_checksum.py
+++ l10n_eu_iot_scale_cert/controllers/expected_checksum.py
@@ -2,4 +2,4 @@
 # in this module (see controllers/checksum.py for the full list).
 # Any change to these files will require re-certification with LNE.
 # DO NOT CHANGE THEM OR THIS CHECKSUM WITHOUT CONTACTING THE POS TEAM FIRST!
-EXPECTED_CHECKSUM = "6f8f78707dab7b1828ed4ef4cd474b5eb6f7133a46eb6f64bfd6bdd8986df014"
+EXPECTED_CHECKSUM = "5fa905f9662192a650679a54b2f7b2c59bcfa5647c38089dab19859b4ca06dc8"
