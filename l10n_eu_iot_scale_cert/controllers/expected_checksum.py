db238a936709f82692feaca422340ee3c66aeaf8
Max Whale <mawh@odoo.com>
2025-07-18 11:36:04 +0200

    [ADD] pos_iot_adam_scale: move Adam driver to separate module
    Before this commit, the Adam Equipment serial scale driver was included
    by default on the IoT box. However, this causes issues as there is no
    way to detect the presence of one of these scales automatically. This
    caused any unidentified serial device to be registered as an Adam scale,
    leading to much confusion with customers, especially regarding setting
    up an FDM.
    
    After this commit, the Adam driver is moved into its own module, and a
    checkbox added to the PoS settings to enable it. This way, only
    customers who actually need the driver will get it, and everyone else
    can benefit from not having it wrongly detect devices.
    
    task-4954021
    
    closes odoo/enterprise#91410
    
    Related: odoo/odoo#221326
    Related: odoo/upgrade#8142
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- l10n_eu_iot_scale_cert/controllers/expected_checksum.py
+++ l10n_eu_iot_scale_cert/controllers/expected_checksum.py
@@ -2,4 +2,4 @@
 # in this module (see controllers/checksum.py for the full list).
 # Any change to these files will require re-certification with LNE.
 # DO NOT CHANGE THEM OR THIS CHECKSUM WITHOUT CONTACTING THE POS TEAM FIRST!
-EXPECTED_CHECKSUM = "5fa905f9662192a650679a54b2f7b2c59bcfa5647c38089dab19859b4ca06dc8"
+EXPECTED_CHECKSUM = "1dda1cfb44054b67404639a710555fd00752ec9f3325b5b31ce1ba241998ef40"
