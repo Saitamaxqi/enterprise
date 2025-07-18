19cb3fbf0ddee427276aef06eea6d94f65976fc0
Patrick Hoste <pko@odoo.com>
2025-07-18 12:32:58 +0000

    [FIX] documents: fix multiple mobile kanban issues
    This commit fix several issue in kanban mobile view:
    
      - When a folder has folders AND documents, there is a huge gap between the
      two because of the kanban ghost records.
      - In the 'Recent' folder, folders and documents doesn't take all width.
      - In the 'Recent' folder, we can't scroll to see all the documents.
    
    Task-4963198
    
    closes odoo/enterprise#100325
    
    X-original-commit: 983464d4f2afc8ca2e1d4e7cdc931fd0869c43da
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>
    Signed-off-by: Patrick Hoste (pko) <pko@odoo.com>

--- documents/static/src/views/kanban/documents_kanban_renderer.js
+++ documents/static/src/views/kanban/documents_kanban_renderer.js
@@ -13,7 +13,7 @@ import { DocumentsDropZone } from "@documents/views/helper/documents_drop_zone";
 import { DocumentsFileViewer } from "@documents/views/helper/documents_file_viewer";
 import { DocumentsKanbanRecord } from "@documents/views/kanban/documents_kanban_record";
 
-import { useExternalListener, useRef } from "@odoo/owl";
+import { onMounted, useExternalListener, useRef } from "@odoo/owl";
 
 export class DocumentsKanbanRenderer extends DocumentsRendererMixin(KanbanRenderer) {
     static props = [...KanbanRenderer.props, "previewStore"];
@@ -90,6 +90,11 @@ export class DocumentsKanbanRenderer extends DocumentsRendererMixin(KanbanRender
                 this.props.list.selection[0].data.id == detail.recordId
             ) {
                 this.render(true); // Re-render this Component and its children on activity add/edit/unlink
+            }
+        });
+        onMounted(() => {
+            if (this.isMobile && this.isRecentFolder) {
+                this.root.el.classList.add('o_documents_recent');
             }
         });
     }
