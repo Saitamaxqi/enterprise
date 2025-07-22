ca3e017979196e432df6b3962ba7ce9c49fac2f4
Joren Van Onder <jov@odoo.com>
2025-07-22 10:15:39 -0700

    [IMP] l10n_br_avatax: correct the NCM code data model
    This model was readonly without any views when it was originally
    created [1]. We forgot to add the correct constraints, and we duplicated
    the code inside the name field to avoid overwriting display_name and
    having to add a _rec_name_search.
    
    This commit fixes those issues. First of all, we require both the code
    and name field. Second of all, we remove the code from the name field
    and modify display_name and _rec_name_search accordingly. To not have
    identical display names in a dropdown, we also add a unique constraint
    on the combination of name and code. It's possible to have multiple
    identical NCM codes with different EX values, but in those cases the
    user should give them unique names.
    
    We also take the opportunity to put the NCM form view in a sheet,
    looks a bit more modern that way.
    
    [1] https://github.com/odoo/enterprise/pull/43797
    
    task-4998498
    
    closes odoo/enterprise#91750
    
    Related: odoo/upgrade#8178
    Signed-off-by: Antoine Boonen (aboo) <aboo@odoo.com>

--- l10n_br_avatax/models/l10n_br_ncm_code.py
+++ l10n_br_avatax/models/l10n_br_ncm_code.py
@@ -1,16 +1,27 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class L10n_BrNcmCode(models.Model):
     _name = 'l10n_br.ncm.code'
     _description = "NCM Code"
+    _rec_names_search = ["code", "name"]
 
-    code = fields.Char("Code")
-    name = fields.Char("Name")
+    code = fields.Char("Code", required=True)
+    name = fields.Char("Name", required=True)
     ex = fields.Char(
         string="EX",
         help="Brazil: Use this field to indicate an 'EX Citation' which identifies exceptions to Avalara’s standard fiscal rules.\n"
             "EX Citations help define specific tax treatments (e.g., CST, ST, rate reductions, special benefits) for products "
             "with tax behavior different from Avalara’s default settings."
     )
+
+    _name_uniq = models.Constraint(
+        'UNIQUE(name, code)',
+        'This combination of name and code already exists.',
+    )
+
+    @api.depends("code", "name")
+    def _compute_display_name(self):
+        for ncm in self:
+            ncm.display_name = f"[{ncm.code}] {ncm.name}"
