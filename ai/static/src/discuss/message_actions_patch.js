9de03aa3e9d07aa6c400f0d1fb9ee1b2b7d75b5f
Didier (did) <did@odoo.com>
2025-07-24 09:30:55 +0200

    [IMP] ai: format message_actions_patch.js with web tool
    Part-of: odoo/enterprise#90637
    Related: odoo/odoo#219890
    Signed-off-by: Didier Debondt (did) <did@odoo.com>

--- ai/static/src/discuss/message_actions_patch.js
+++ ai/static/src/discuss/message_actions_patch.js
@@ -1,6 +1,6 @@
 import { patch } from "@web/core/utils/patch";
 
-import { user } from '@web/core/user';
+import { user } from "@web/core/user";
 import { _t } from "@web/core/l10n/translation";
 import { messageActionsRegistry, messageActionsInternal } from "@mail/core/common/message_actions";
 import { unwrapContents } from "@html_editor/utils/dom";
@@ -8,22 +8,20 @@ import { setElementContent } from "@web/core/utils/html";
 
 messageActionsRegistry
     .add("insertToComposer", {
-        condition: (component) => (
+        condition: (component) =>
             !!component.props.thread.aiSpecialActions?.insert &&
-            component.store.aiInsertButtonTarget && (  // after a reload both parts of the below conditions are undefined and but we don't want to button to appear
-                component.store.aiInsertButtonTarget === component.props.thread.aiChatSource ||
-                component.env.isSmall
-            ) &&
-            component.message.author.userId !== user.userId
-        ),
+            component.store.aiInsertButtonTarget && // after a reload both parts of the below conditions are undefined and but we don't want to button to appear
+            (component.store.aiInsertButtonTarget === component.props.thread.aiChatSource ||
+                component.env.isSmall) &&
+            component.message.author.userId !== user.userId,
         title: () => _t("Use this"),
         onClick: (component) => {
             const fragment = document.createDocumentFragment();
-            const content_root = document.createElement('span');
-            content_root.setAttribute('InsertorId', 'AIInsertion');
+            const content_root = document.createElement("span");
+            content_root.setAttribute("InsertorId", "AIInsertion");
             setElementContent(content_root, component.props.message.body);
             // check if the content is enclosed in a <p> element, if so, unwrap it
-            const paragraphElements = content_root.querySelectorAll('p');
+            const paragraphElements = content_root.querySelectorAll("p");
             if (paragraphElements.length === 1) {
                 unwrapContents(paragraphElements[0]);
             }
@@ -36,14 +34,12 @@ messageActionsRegistry
         sequence: 10,
     })
     .add("send-message-direct", {
-        condition: (component) => (
+        condition: (component) =>
             !!component.props.thread.aiSpecialActions?.sendMessage &&
-            component.message.author.userId !== user.userId  && // don't show the buttons for the user's messages
-            component.store.aiInsertButtonTarget && (  // after a reload both parts of the below conditions are undefined and but we don't want to button to appear
-                component.store.aiInsertButtonTarget === component.props.thread.aiChatSource ||
-                component.env.isSmall
-            )
-        ),
+            component.message.author.userId !== user.userId && // don't show the buttons for the user's messages
+            component.store.aiInsertButtonTarget && // after a reload both parts of the below conditions are undefined and but we don't want to button to appear
+            (component.store.aiInsertButtonTarget === component.props.thread.aiChatSource ||
+                component.env.isSmall),
         title: _t("Send as Message"),
         onClick: (component) => {
             component.props.thread.aiSpecialActions.sendMessage(component.props.message.body);
@@ -51,32 +47,39 @@ messageActionsRegistry
         sequence: 20,
     })
     .add("log-note-direct", {
-        condition: (component) => (
+        condition: (component) =>
             !!component.props.thread.aiSpecialActions?.logNote &&
-            component.message.author.userId !== user.userId  && // don't show the buttons for the user's messages
-            component.store.aiInsertButtonTarget && (  // after a reload both parts of the below conditions are undefined and but we don't want to button to appear
-                component.store.aiInsertButtonTarget === component.props.thread.aiChatSource ||
-                component.env.isSmall
-            )
-        ),
+            component.message.author.userId !== user.userId && // don't show the buttons for the user's messages
+            component.store.aiInsertButtonTarget && // after a reload both parts of the below conditions are undefined and but we don't want to button to appear
+            (component.store.aiInsertButtonTarget === component.props.thread.aiChatSource ||
+                component.env.isSmall),
         title: _t("Log as Note"),
-        onClick: (component) => component.props.thread.aiSpecialActions.logNote(component.props.message.body),
+        onClick: (component) =>
+            component.props.thread.aiSpecialActions.logNote(component.props.message.body),
         sequence: 30,
     });
 
 patch(messageActionsInternal, {
     condition(component, id, action) {
-        const requiredActions = ["insertToComposer", "copy-message", "send-message-direct", "log-note-direct"];
+        const requiredActions = [
+            "insertToComposer",
+            "copy-message",
+            "send-message-direct",
+            "log-note-direct",
+        ];
         if (
             component.props.thread?.channel_type === "ai_composer" &&
             !requiredActions.includes(id)
         ) {
-            return false
+            return false;
         } else if (component.message?.author?.im_status === "agent") {
-            return false
+            return false;
         }
         if (id === "copy-message") {
-            return super.condition(component, id, action) || component.props.thread?.channel_type === "ai_composer";
+            return (
+                super.condition(component, id, action) ||
+                component.props.thread?.channel_type === "ai_composer"
+            );
         }
         return super.condition(component, id, action);
     },
@@ -85,5 +88,5 @@ patch(messageActionsInternal, {
             return 50;
         }
         return super.sequence(component, id, action);
-    }
+    },
 });
