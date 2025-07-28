Saitamaxqi/enterprise

commit 926c8f041b51a04f0153be27cfcc8658a07be5b0

[FIX] sale_subscription: use daterange widget instead
before this commit, commission plan form date field selector
seems broken in mobiles devices because we did not directly used widget
available.

after this commit, use daterange widget.

closes odoo/enterprise#70700

Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

sale_commission/views/sale_commission_plan_view.xml
@@ -43,9 +43,8 @@
                         </group>
                         <group>
                             <label for="date_from" string="Effective Period"/>
-                            <div>
-                                <field name="date_from" class="oe_inline" readonly="state != 'draft'"/>
-                                -> <field name="date_to" class="oe_inline" readonly="state != 'draft'"/>
+                            <div class="d-inline-flex">
+                                <field name="date_from" string="Date" widget="daterange" options="{'end_date_field': 'date_to'}" readonly="state != 'draft'"/>
                             </div>
                             <field name="periodicity" string="Payment Frequency" readonly="state != 'draft'"/>
                         </group>

commit 5c89b87cabba6e3c1c9304110d80720f40d5d645

[FIX] sale_commission: prevent error when clearing date_to in commission plan
Currently, an error occurs when clear the date_to field from an already created
record.

Steps to Reproduce:
 - Install the sale_commission module.
 - Go to Commission Plans and create a record.
 - Now, clear the date_from field and save.

TypeError: '>' not supported between instances of 'bool' and 'datetime.date'

This error occurs when we remove the date_to field from an already created
record. The error started occurring after
https://github.com/odoo/enterprise/commit/926c8f041b51a04f0153be27cfcc8658a07be5b0
this commit that introduced the daterange widget for the date_from field.
And when we clear the date_to field and try to save, it triggers the constraint
[1] and raises the error.

[1] https://github.com/odoo/enterprise/blob/377321fd7e7b1b2026734b75c79c74c8947e2e6c/sale_commission/model/commission_plan.py#L64

This commit ensures that the record is saved only if the date_to field is also
present in the record.

sentry-6710699656

closes odoo/enterprise#91059

X-original-commit: 099ce35a6e3e8eaddc4c1444fb50ea4160dcb6c5
Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>
Signed-off-by: Ashutosh Sharma (assh) <assh@odoo.com>

sale_commission/model/commission_plan.py
@@ -61,7 +61,7 @@ CREATE INDEX IF NOT EXISTS account_move_invoice_user_id_date_idx ON account_move
     @api.constrains('date_from', 'date_to')
     def _date_constraint(self):
         for plan in self:
-            if not plan.date_to > plan.date_from:
+            if plan.date_to and plan.date_from and not plan.date_to > plan.date_from:
                 raise ValidationError(_("The start date must be before the end date."))
 
     @api.constrains('team_id', 'user_type')

sale_commission/views/sale_commission_plan_view.xml
@@ -75,7 +75,7 @@
                         <group>
                             <label for="date_from" string="Effective Period"/>
                             <div class="d-inline-flex">
-                                <field name="date_from" string="Date" widget="daterange" options="{'end_date_field': 'date_to'}" readonly="state != 'draft'" force_save="1"/>
+                                <field name="date_from" string="Date" widget="daterange" options="{'end_date_field': 'date_to'}" required="1" readonly="state != 'draft'" force_save="1"/>
                             </div>
                             <field name="periodicity" string="Target Frequency" readonly="state != 'draft'" force_save="1"/>
                         </group>