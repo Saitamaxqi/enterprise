ec44e818c26c2f4e410606d2f972e26ca1e6a6ae
John Laterre (jol) <jol@odoo.com>
2025-07-31 13:57:00 +0200

    [REF] l10n_it_edi_reports: Merge `l10n_it_edi_withholding` module
    This commit integrates the functionality from the `l10n_it_edi_withholding` module
    into its parent, `l10n_it_edi`.
    
    The separate module was originally created to backport this feature
    to a stable version (16.0), making it obsolete now.
    
    Original commit: https://github.com/odoo/odoo/commit/90f9f9f23478bd54625c152c7f5ab2d0c18e3780
    
    task-4988044
    
    closes odoo/enterprise#91429
    
    Related: odoo/odoo#221365
    Related: odoo/upgrade#8147
    Signed-off-by: William André (wan) <wan@odoo.com>

--- l10n_it_edi_withholding_reports/__manifest__.py
+++ l10n_it_edi_withholding_reports/__manifest__.py
@@ -7,11 +7,11 @@ Accounting reports for Italy
 
     """,
     'category': 'Accounting/Accounting',
-    'depends': ['l10n_it_edi_withholding', 'account_reports'],
+    'depends': ['l10n_it_edi', 'account_reports'],
     'data': [
         'data/account_return_data.xml',
     ],
-    'auto_install': ['l10n_it_edi_withholding', 'account_reports'],
+    'auto_install': ['l10n_it_edi', 'account_reports'],
     'installable': True,
     'author': 'Odoo S.A.',
     'license': 'OEEL-1',
