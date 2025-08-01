0a2c9c030397b6beb9dddba8478926968d145844
Renaud Thiry <reth@odoo.com>
2025-08-01 13:07:08 +0000

    [FIX] test_mail_enterprise: add query for action done
    In [1] we now send the differential in activity count for relevant users
    when late activities are added or removed for them.
    
    This leads to an additional query to read the original count.
    
    [1]: odoo/odoo@bc0e1275be40faa919f1b0f70a14e5190c18bd6b
    
    closes odoo/enterprise#91665
    
    X-original-commit: 46e144fb00367b9522e404bdc900ba07447678ef
    Related: odoo/odoo#221822
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>

--- test_mail_enterprise/tests/test_activity_performance.py
+++ test_mail_enterprise/tests/test_activity_performance.py
@@ -190,7 +190,7 @@ class TestActivityPerformance(BaseMailPerformance):
         ])
         self.env.flush_all()
 
-        with self.assertQueryCount(employee=64):
+        with self.assertQueryCount(employee=65):
             activities.action_feedback(
                 feedback='Intense feedback',
                 attachment_ids=attachments.ids,
