3ca538e0657e53e130fe23974c9221e30c0ea69d
Habib (ayh) <ayh@odoo.com>
2025-08-04 11:51:31 +0000

    [FIX] l10n_lu_reports,l10n_ro_saft: Auto install accountant
    When choosing one app free in these countries, Accountant is automatically installed.
    This should only be the case when Accountant is the one app free.
    
    Fix:
    Change the auto-install dependencies to ensure that these modules are only installed when Accountant is already installed.
    
    closes odoo/enterprise#91670
    
    X-original-commit: e48ec2b72a156cd9fb502c3d0110ddf72affb124
    Signed-off-by: William André (wan) <wan@odoo.com>

--- l10n_lu_reports/__manifest__.py
+++ l10n_lu_reports/__manifest__.py
@@ -43,7 +43,7 @@ Necessary master data is also included.
     ],
     'demo': ['demo/demo_company.xml'],
     'post_init_hook': '_l10n_lu_reports_post_init',
-    'auto_install': ['l10n_lu', 'account_reports'],
+    'auto_install': ['l10n_lu', 'account_asset'],
     'author': 'Odoo S.A.',
     'license': 'OEEL-1',
     'assets': {
--- l10n_ro_saft/__manifest__.py
+++ l10n_ro_saft/__manifest__.py
@@ -30,6 +30,6 @@ must submit monthly or quarterly, depending on their tax reporting period.
     ],
     'author': 'Odoo S.A.',
     'license': 'OEEL-1',
-    'auto_install': ['l10n_ro', 'account_saft'],
+    'auto_install': True,
     'post_init_hook': '_update_saft_fields_on_taxes',
 }
