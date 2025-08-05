61050f2f8cf1769f5be91182191b0b41c9f7407b
alan-odoo <alan@odoo.com>
2025-08-05 12:21:42 +0200

    [IMP] *: update the permissions of the preferences form
    This commit removes the setting allowing the users to modify some fields of
    the preferences / profile form. From now on, those fields must be editable
    by users.
    
    The human resource users are notified when changes are done on the hr_employee
    table from the preferences form by one of the users they are responsible of as
    now they can change their own data. It must be done in the hr module and not
    in hr_payroll
    
    Community PR: https://github.com/odoo/odoo/pull/215789
    Upgrade PR: https://github.com/odoo/upgrade/pull/8207
    
    Task-4775990
    
    Part-of: odoo/enterprise#88518
    Related: odoo/odoo#215789
    Related: odoo/upgrade#8207
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- hr_payroll/models/__init__.py
+++ hr_payroll/models/__init__.py
@@ -15,7 +15,6 @@ from . import hr_payslip_input
 from . import hr_payslip_input_type
 from . import hr_payslip_run
 from . import res_company
-from . import res_users
 from . import hr_rule_parameter
 from . import hr_work_entry_type
 from . import hr_work_entry
