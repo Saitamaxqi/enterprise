45a64db596db96e25561225e026c5bc0705c01be
Andrew Gavgavian <andg@odoo.com>
2025-08-07 09:36:44 -0700

    [IMP] l10n_co_dian: automate certification process
    New customers starting out with odoo in Colombia have to go through a
    registration process where they send specific records to the DIAN via a
    specific endpoint. Currently, the process is:
    
    1) Activate Certification process in settings.
    2) Create 3 journals (SETP, NC, ND) for invoice, debit notes, and credit
       notes.
    3) Create a specific product with a tax enabled.
    4) Create the specific amount of invoices/debit notes/credit notes as
       required from within the DIAN portal.
    
    This process is slow and for most of the users coming to Odoo, they will
    not have experience with accounting software or understand where to
    setup such records. As such, a one click setting has been added for
    certification where they input the number of records necessary, and it
    will automatically do everything they need.
    
    Notes on implementation:
    - This will perform changes that could be considered irreversible on
      customers databases. As such there is a warning that it should only
      occur on a staging/duplicated database.
    - The values that are hard coded, Technical Key, Min/Max Range Values,
      and the Authorization Number all come from DIAN and are shared across
      all users, as such hard coding them to these values are not releasing
      any secrets.
    
      task-4366022
    
    closes odoo/enterprise#91755
    
    Signed-off-by: Joren Van Onder (jov) <jov@odoo.com>

--- l10n_co_dian/tests/test_dian_flows.py
+++ l10n_co_dian/tests/test_dian_flows.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from unittest.mock import patch
 
-from odoo.tests import tagged, freeze_time
+from odoo import fields
 from odoo.exceptions import UserError
+from odoo.tests import freeze_time, tagged
+
 from .common import TestCoDianCommon
 
 
@@ -258,3 +260,70 @@ class TestDianFlows(TestCoDianCommon):
             'name': 'Real Company Name',
             'email': 'company@mail.com',
         }])
+
+
+@freeze_time('2024-01-30')
+@tagged('post_install_l10n', 'post_install', '-at_install')
+class TestDianFlowsSettings(TestCoDianCommon):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.settings = cls.env['res.config.settings'].create({})
+        cls.settings.l10n_co_dian_test_environment = True
+        cls.settings.l10n_co_dian_certification_process = True
+        cls.settings.l10n_co_dian_cert_invoice_count = 2
+        cls.settings.l10n_co_dian_cert_credit_count = 2
+        cls.settings.l10n_co_dian_cert_debit_count = 2
+
+    def _mocked_build_and_send_request(self, record, payload, service, company):
+        """Build_and_send_request is a module level function in xml_util that contains a parameter
+        self. To be able to mock it from within the test class we need an additional parameter to
+        make sure it can be called."""
+        if service == 'SendTestSetAsync':
+            return self._mocked_response('SendTestSetAsync.xml', 200)
+        if service == 'GetStatusZip':
+            return self._mocked_response('GetStatusZip_habilitation.xml', 200)
+        return self._mocked_response('', 200)
+
+    def test_certification_process(self):
+        """ Test the certification process with the DIAN. """
+        with (
+            patch(f'{self.utils_path}._build_and_send_request', autospec=True, side_effect=self._mocked_build_and_send_request),
+            self._disable_get_acquirer_call(),
+        ):
+            self.settings.action_l10n_co_certify_with_dian()
+        # Check that the journals were created or updated.
+        journals = self.env['account.journal'].search([('code', 'in', ['SETP', 'NC', 'ND'])])
+        self.assertEqual(len(journals), 3)
+
+        # Unique journal values
+        self.assertRecordValues(journals, [
+            {'code': 'SETP', 'l10n_co_edi_min_range_number': 990000000, 'l10n_co_edi_max_range_number': 995000000, 'l10n_co_edi_debit_note': False},
+            {'code': 'NC', 'l10n_co_edi_min_range_number': 1, 'l10n_co_edi_max_range_number': 1000, 'l10n_co_edi_debit_note': False},
+            {'code': 'ND', 'l10n_co_edi_min_range_number': 1, 'l10n_co_edi_max_range_number': 1000, 'l10n_co_edi_debit_note': True},
+        ])
+
+        # Shared journal values
+        self.assertRecordValues(journals, [{
+            'l10n_co_dian_technical_key': 'fc8eac422eba16e22ffd8c6f94b3f40a6e38162c',
+            'l10n_co_edi_dian_authorization_date': fields.Date.from_string('2019-01-19'),
+            'l10n_co_edi_dian_authorization_end_date': fields.Date.from_string('2030-01-19'),
+            'l10n_co_edi_dian_authorization_number': '18760000001',
+        }] * 3)
+
+        # Check that the moves were created
+        moves = self.env['account.move'].search([
+            ('move_type', 'in', ['out_invoice', 'out_refund']),
+            ('state', '=', 'posted'),
+            ('journal_id', 'in', journals.ids),
+        ])
+        self.assertEqual(len(moves), 6)
+
+        # The test file we use is one already accepted so it should all be marked as already accepted.
+        for move in moves:
+            self.assertRecordValues(move.l10n_co_dian_document_ids, [{
+                'zip_key': '42424242',
+                'state': 'invoice_rejected',
+                'message': "<p>Set de prueba con identificador 123456 se encuentra Aceptado.</p>",
+            }])
