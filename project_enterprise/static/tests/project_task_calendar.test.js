ff688bc08892e1765231e09f873f652450858b99
Adrien Dieudonne <adr@odoo.com>
2025-08-18 20:13:52 +0200

    [IMP] project_enterprise: daterange_picker: date range icon in picker
    Before this commit, we had to click on “Add start/end date” next
    to the input field.
    
    This has now been replaced with an icon in the date picker,
    which you can toggle easily without closing the picker.
    It also means that the user no longer needs to know whether it
    is a start or an end date.
    
    These tests have been removed because the button is no longer there,
    and the icon is always present. This part is already tested in web.
    
    task-4613140
    
    closes odoo/enterprise#93286
    
    Related: odoo/odoo#224439
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- project_enterprise/static/tests/project_task_calendar.test.js
+++ project_enterprise/static/tests/project_task_calendar.test.js
@@ -2,7 +2,6 @@ import { beforeEach, describe, expect, test } from "@odoo/hoot";
 import { queryFirst } from "@odoo/hoot-dom";
 import { mockDate } from "@odoo/hoot-mock";
 import { contains, mountView, onRpc } from "@web/../tests/web_test_helpers";
-import { clickDate, selectDateRange } from "@web/../tests/views/calendar/calendar_test_helpers";
 import { defineProjectModels } from "@project/../tests/project_models";
 import { ProjectTask } from "@project_enterprise/../tests/task_gant_model";
 import { serializeDateTime } from "@web/core/l10n/dates";
@@ -39,25 +38,6 @@ const calendarMountParams = {
     `,
     config: { views: [[false, "form"]] },
 };
-
-test("Planned Date Begin Should Not Be Set When Selecting A Single Day", async () => {
-    await mountView(calendarMountParams);
-    expect(".o_calendar_view").toHaveCount(1);
-    await clickDate("2016-11-13");
-    expect(".modal-title").toHaveText("New Task");
-    expect("button.o_add_start_date").toHaveCount(1, {
-        message: "the planned date begin should not be set when selecting a single day",
-    });
-});
-
-test("Planned Date Begin Should Be Set When Selecting Multiple Days", async () => {
-    await mountView(calendarMountParams);
-    await selectDateRange("2016-11-13", "2016-11-16");
-    expect(".modal-title").toHaveText("New Task");
-    expect("button.o_add_start_date").toHaveCount(0, {
-        message: "the planned date begin should not be set when selecting a single day",
-    });
-});
 
 test("Drag and drop task to schedule in month scale", async () => {
     let expectedDate = null;
