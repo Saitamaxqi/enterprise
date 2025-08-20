b31de6317d497398476dc44efb9c777078bc00ff
Louis Wicket (wil) <wil@odoo.com>
2025-08-20 11:00:11 +0200

    [IMP] voip: lint code
    Part-of: odoo/enterprise#93690
    Signed-off-by: Mohammed Basioni (basm) <basm@odoo.com>

--- voip/models/voip_provider.py
+++ voip/models/voip_provider.py
@@ -2,17 +2,22 @@ from odoo import fields, models
 
 
 class VoipProvider(models.Model):
-    _name = 'voip.provider'
+    _name = "voip.provider"
     _description = "VoIP Provider"
 
     name = fields.Char(required=True)
     company_id = fields.Many2one("res.company", string="Company")
-
     ws_server = fields.Char(
-        "WebSocket", help="The URL of your WebSocket", default="ws://localhost", groups="base.group_system",
+        "WebSocket",
+        help="The URL of your WebSocket",
+        default="ws://localhost",
+        groups="base.group_system",
     )
     pbx_ip = fields.Char(
-        "PBX Server IP", help="The IP address of your PBX Server", default="localhost", groups="base.group_system",
+        "PBX Server IP",
+        help="The IP address of your PBX Server",
+        default="localhost",
+        groups="base.group_system",
     )
     mode = fields.Selection(
         [
