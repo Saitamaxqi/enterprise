cf024b64ea35cabf471def03f5a1f03eeb84c353
Xavier Bol (xbo) <xbo@odoo.com>
2025-08-20 17:59:29 +0200

    [IMP] timesheet_grid: don't cache list and kanban of timesheet with timer
    Before this commit, since now the list view rpcs are cached in
    timesheets, when the user stops the timer in another view and go back to
    list view, the timer is still showed as running because of the values in
    cache.
    
    This commit disables the cache feature on list view of timesheet when
    using the timer to avoid having desynchronisation.
    
    task-4284216
    
    Part-of: odoo/enterprise#81686
    Related: odoo/odoo#202313
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- timesheet_grid/static/src/views/timesheet_kanban/timesheet_timer_kanban_view.js
+++ timesheet_grid/static/src/views/timesheet_kanban/timesheet_timer_kanban_view.js
@@ -3,9 +3,14 @@ import { kanbanView } from "@web/views/kanban/kanban_view";
 import { TimesheetTimerKanbanController } from "./timesheet_timer_kanban_controller";
 import { TimesheetTimerKanbanRenderer } from "./timesheet_timer_kanban_renderer";
 
+class TimesheetTimerKanbanModel extends kanbanView.Model {
+    static withCache = false;
+}
+
 export const timesheetTimerKanbanView = {
     ...kanbanView,
     Controller: TimesheetTimerKanbanController,
+    Model: TimesheetTimerKanbanModel,
     Renderer: TimesheetTimerKanbanRenderer,
 };
 
--- timesheet_grid/static/src/views/timesheet_list/timesheet_timer_list_view.js
+++ timesheet_grid/static/src/views/timesheet_list/timesheet_timer_list_view.js
@@ -3,9 +3,14 @@ import { TimesheetTimerListController } from "./timesheet_timer_list_controller"
 import { TimesheetTimerListRenderer } from "./timesheet_timer_list_renderer";
 import { listView } from "@web/views/list/list_view";
 
+class TimesheetTimerListModel extends listView.Model {
+    static withCache = false;
+}
+
 export const timesheetTimerListView = {
     ...listView,
     Controller: TimesheetTimerListController,
+    Model: TimesheetTimerListModel,
     Renderer: TimesheetTimerListRenderer,
 };
 
