41bc10b6252baa91214f49218d506a93b3f1c122
Lancelot Semal <lase@odoo.com>
2025-08-21 08:07:54 +0000

    [FIX] *: update quality check picking_id when related move is reassigned
    *{quality_control,stock}_picking_batch
    
    Steps to reproduce:
    
    - Got to Quality > Quality control > Control Point
    - Create a quality control point:
      - Operation: receipt
      - Control per quantity or product
    - Create a and confirm a receipt transfer with 2 products
    - Go to the receipt list view > select your receipt > Wheel action >
      Add to wave > Add to a new wave > Add only one of the move line to the
      wave
    > A new picking is created and the move line reassigned to it but the
    > related quality check picking_id is not updated accordingly.
    > In particular, there is no "quality check" button on the new picking
    > and the "quality check" button of the first picking allows you to
    > process a QC related to the wave transfer.
    
    Cause of the issue:
    
    While the move lines or move are can be moved to a new picking during
    the `_add_to_wave` call:
    https://github.com/odoo/odoo/blob/605e47a85561614c17fe2e6f59618610f87c69bb/addons/stock_picking_batch/models/stock_move_line.py#L69-L90
    Nothing is done with respect to the quality check which pciking_id field
    is not computed:
    https://github.com/odoo/enterprise/blob/d73f7ef6fe61ccddbe1fe4e32c1670611ba3c5d2/quality/models/quality.py#L185
    
    Fix:
    
    While the quality check measured on move_line are linked to a move line,
    the quality checks measured on products and operation are not. For the
    first kind, we rely on an override of the write method of stock move
    lines to reassign the check to the apporpiate picking. For the other
    kinds, we add a post batch hook to unlink the obsolete checks and
    recreate the appropiate one. Note that since operation and product types
    are created during the action confirm of moves and since certain moves
    will be created and auto confirm during the new picking creation here:
    https://github.com/odoo/odoo/blob/73fd3af560c967f41339bfc4c71a51dc5baba4a8/addons/stock_picking_batch/models/stock_move_line.py#L90
    https://github.com/odoo/odoo/blob/73fd3af560c967f41339bfc4c71a51dc5baba4a8/addons/stock/models/stock_picking.py#L857
    https://github.com/odoo/odoo/blob/73fd3af560c967f41339bfc4c71a51dc5baba4a8/addons/stock/models/stock_picking.py#L1263-L1267
    https://github.com/odoo/enterprise/blob/b99d7073a34b24d4d3b863278e68f292fdd3c0b0/quality_control/models/stock_move.py#L12-L15
    we rely on the `extra_move_mode` to avoid quality check creation during
    this step (as they will be created in the hook).
    
    opw-5009635
    
    closes odoo/enterprise#94400
    
    X-original-commit: 8b33445a77025f7d794c7af9194951288838fd5a
    Related: odoo/odoo#226492
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

--- quality_control_picking_batch/models/__init__.py
+++ quality_control_picking_batch/models/__init__.py
@@ -2,5 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import quality_check
+from . import stock_picking
 from . import stock_picking_batch
 from . import stock_move_line
--- quality_control_picking_batch/models/stock_move_line.py
+++ quality_control_picking_batch/models/stock_move_line.py
@@ -7,6 +7,12 @@ from odoo import models
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
+    def write(self, vals):
+        res = super().write(vals)
+        if 'picking_id' in vals:
+            self.check_ids.filtered(lambda qc: qc.point_id.measure_on == 'move_line').picking_id = vals['picking_id']
+        return res
+
     def _get_check_values(self, quality_point):
         vals = super()._get_check_values(quality_point)
         vals.update(batch_id=self.picking_id.batch_id.id)
