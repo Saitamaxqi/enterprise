01d095a8760384a8bb494c253cc15fcf676d0ae0
Louis Baudoux <lba@odoo.com>
2025-08-22 15:36:24 +0000

    [PERF] iap_extract: remove savepoint when fetch OCR results
    Very similar to commit 7f7ff8d, see full explanation in that commit
    message.
    
    This savepoint was also initiated in a loop which could cause
    performances issues when too many of them are active at the same time.
    
    This one is much less critical and can be removed without much trouble.
    It was only allowing to recover from a serialization failure, in the
    case where a user was concurrently editing a document while it was being
    updated by the OCR.
    
    closes odoo/enterprise#93114
    
    X-original-commit: eaa0fbed68c3d7971b997d01d8c00e29ecd15b02
    Signed-off-by: Thibault Francois <tfr@odoo.com>
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>

--- iap_extract/models/extract_mixin.py
+++ iap_extract/models/extract_mixin.py
@@ -423,8 +423,7 @@ class ExtractMixin(models.AbstractModel):
     def _try_to_check_ocr_status(self):
         self.ensure_one()
         try:
-            with self.env.cr.savepoint():
-                self._check_ocr_status()
+            self._check_ocr_status()
             self.env.cr.commit()
         except Exception as e:
             _logger.warning("Couldn't check OCR status of %s with id %d: %s", self._name, self.id, str(e))
