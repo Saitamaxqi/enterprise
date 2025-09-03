95a2e47625e533f8801834eb717be63247e80bf6
Bastien PIERRE <ipb@odoo.com>
2025-09-03 10:20:32 +0000

    [FIX] planning: fix planning_test_tour
    Now the tour use edit with the date of today and not
    a function
    
    runbot build error: 164213
    
    X-original-commit: 630aa15abaaf9822ebabc6781a44bb5ad3537543
    
    x
    
    closes odoo/enterprise#94014
    
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>
    Signed-off-by: Bastien Pierre (ipb) <ipb@odoo.com>

--- planning/static/tests/tours/planning_tests_tours.js
+++ planning/static/tests/tours/planning_tests_tours.js
@@ -1,5 +1,6 @@
 import { markup } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+const today = luxon.DateTime.now();
 
 registry.category("web_tour.tours").add('planning_test_tour', {
     url: '/odoo',
@@ -36,14 +37,7 @@ registry.category("web_tour.tours").add('planning_test_tour', {
 }, {
     trigger: ".o_field_widget[name='start_datetime'] input",
     content: "Set start datetime",
-    run: function (actions) {
-        const input = this.anchor;
-        input.value = input.value.replace(/\d{2}:\d{2}/g, '08:00');
-        input.dispatchEvent(new InputEvent('input', {
-            bubbles: true,
-        }));
-        input.dispatchEvent(new Event("change", { bubbles: true, cancelable: false }));
-    }
+    run: `edit ${today.toFormat("MM/dd/yyyy")} 09:00`,
 }, {
     trigger: "button[data-field=end_datetime]",
     content: "Open the date picker",
@@ -51,14 +45,7 @@ registry.category("web_tour.tours").add('planning_test_tour', {
 }, {
     trigger: "input[data-field=end_datetime]",
     content: "Set end datetime",
-    run: function (actions) {
-        const input = this.anchor;
-        input.value = input.value.replace(/\d{2}:\d{2}/g, '11:59');
-        input.dispatchEvent(new InputEvent('input', {
-            bubbles: true,
-        }));
-        input.dispatchEvent(new Event("change", { bubbles: true, cancelable: false }));
-    }
+    run: `edit ${today.toFormat("MM/dd/yyyy")} 11:59`,
 }, {
     trigger: "button[name='action_save_template']",
     content: "Save this shift as a template",
@@ -72,7 +59,7 @@ registry.category("web_tour.tours").add('planning_test_tour', {
     trigger: ".o_gantt_pill :contains('11:59')",
     content: markup("<b>Drag & drop</b> your shift to reschedule it. <i>Tip: hit CTRL (or Cmd) to duplicate it instead.</i> <b>Adjust the size</b> of the shift to modify its period."),
     run: function () {
-        const expected = "8:00 AM - 11:59 PM";
+        const expected = "9:00 AM - 11:59 AM";
         // Without the replace below, this step could break since luxon
         // (via Intl) uses sometimes U+202f instead of a simple space.
         // Note: U+202f is a narrow non-break space.
