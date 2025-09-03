b9ff87d76e5a9bbb220336b563fca0bada980900
Adrien Schoffeniels <adsc@odoo.com>
2025-09-03 15:40:12 +0000

    [IMP] ai: add models access rights tests
    Purpose:
    --------
    This commit adds a few basic tests to check model permissions.
    (the fix from the original commit about the `ai.tool` model is not
    forwardported as the model has been removed in 19.0 (replaced by server
    actions).
    
    Task-5064332
    
    closes odoo/enterprise#94483
    
    X-original-commit: d6b2127dbdab8c10c11358864e38ea24405175ba
    Signed-off-by: Panagiotis Kyriakou (paky) <paky@odoo.com>
    Signed-off-by: Adrien Schoffeniels (adsc) <adsc@odoo.com>

--- ai/tests/__init__.py
+++ ai/tests/__init__.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import common
+from . import test_ai_access
 from . import test_ai_agent
 from . import test_ai_logging
 from . import test_ai_methods
