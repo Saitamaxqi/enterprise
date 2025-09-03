428b0cbc31c800bedce8f14926fda03c2a0c829a
Lucas Perais (lpe) <lpe@odoo.com>
2025-09-03 13:09:14 +0000

    [FIX] web_studio: test_rename fix undeterminism
    The def test_rename function (tour web_studio_main_and_rename) crashed undeterministically
    because the tour engine is now faster, inputs soimetime do not have the time to update.
    
    This commit fixes one instance of this.
    
    runbot-error-163471
    
    closes odoo/enterprise#93964
    
    X-original-commit: 5574c52df8794b3f23d385a670cac8fb5355e229
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>

--- web_studio/static/tests/tours/web_studio_main_tours.js
+++ web_studio/static/tests/tours/web_studio_main_tours.js
@@ -282,10 +282,7 @@ registry.category("web_tour.tours").add("web_studio_main_and_rename", {
         },
         {
             // verify that the field name has changed (post-fixed by _1)
-            trigger: '.o_web_studio_sidebar input[name="technical_name"]',
-            run(helper) {
-                assertEqual(this.anchor.value, "coucou_1");
-            },
+            trigger: '.o_web_studio_sidebar input[name="technical_name"]:value(coucou_1)',
             // the rename operation (/web_studio/rename_field + /web_studio/edit_view)
             // takes a while and sometimes reaches the default 10s timeout
             timeout: 20000,
