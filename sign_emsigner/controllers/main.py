b52cdba758a1e6af3819275b0347994b98b197b0
Nikhil Joshi <nikj@odoo.com>
2025-09-10 11:50:54 +0530

    [FIX] sign_emsigner: adjust page coordinates and wizard download button
    Version:
    - 19.0
    
    Issue:
    - The wizard shows a download button even when using emsigner, but the file
      should only be downloaded after signing.
    - When users sign with Aadhaar and download the document, the text appears
      too small.
    
    Fixes:
    -  Hide the download button in the wizard for emsigner requests.
    - Adjust page coordinates so the font size displays correctly.
    
    Impact:
    - Makes the text clear and readable in documents signed with Aadhaar.
    - Hides the download button until the user signs. After signing with eSigner,
      they can download the document with the certificate.
    
    task-5075596
    
    closes odoo/enterprise#94279
    
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- sign_emsigner/controllers/main.py
+++ sign_emsigner/controllers/main.py
@@ -133,7 +133,7 @@ class SignEmsigner(SignController):
         signature_position = self._get_signature_fields_position(document_id, request_item_sudo)
         page_numbers = ",".join(str(item['page']) for item in signature_position)
         page_coordinates_with_page = ";".join([
-            f"{item['page']},{item['left'] + 8},{item['top'] - (item['top'] - item['height'])},{item['width']},{item['height'] - 10}"
+            f"{item['page']},{item['left'] + 8},{item['top'] - (item['top'] - item['height'])},{item['width']},{item['height'] - 20}"
             for item in signature_position
         ])
 
@@ -143,12 +143,18 @@ class SignEmsigner(SignController):
             to_sign_data = compress_pdf_base64(to_sign_data)
 
         # Prepare the dynamic content for the emsigner
-        dyamic_content = [{
-            "Key": "Aadhaar eSign by",
-            "Value": request_item_sudo.partner_id.email
-        }]
-        dyamic_content_byes = json.dumps(dyamic_content).encode('utf-8')
-        dyamic_content_data = base64.b64encode(dyamic_content_byes)
+        dyamic_content = [
+            {
+                "Key": "eSigned using Aadhaar by",
+                "Value": request_item_sudo.partner_id.email
+            },
+            {
+                "Key": "Date",
+                "Value": "DateTime"
+            }
+        ]
+        dyamic_content_bytes = json.dumps(dyamic_content).encode('utf-8')
+        dyamic_content_data = base64.b64encode(dyamic_content_bytes)
 
         return {
             "Name": request_item_sudo.partner_id.name,
