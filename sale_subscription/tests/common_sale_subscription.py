6dbdb2128c709834cdd20f5dc829d45db0a817b5
Nisarg (nipl) <nipl@odoo.com>
2025-09-10 11:44:20 +0530

    [CLN] sale_subscription: Clean optional products tests' method
    closes odoo/enterprise#94276
    
    Related: odoo/odoo#226052
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- sale_subscription/tests/common_sale_subscription.py
+++ sale_subscription/tests/common_sale_subscription.py
@@ -341,14 +341,7 @@ class TestSubscriptionCommon(TestSaleCommon):
     def _get_optional_product_lines(order):
         """Returns the order lines that are optional products. """
         return order.order_line.filtered(
-            lambda line: not line.display_type
-            and (
-                line.parent_id.is_optional
-                or (
-                    line.parent_id.display_type == 'line_subsection'
-                    and line.parent_id.parent_id.is_optional
-                )
-            ),
+            lambda line: not line.display_type and line._is_line_optional(),
         )
 
     @classmethod
