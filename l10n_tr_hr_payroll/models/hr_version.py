98a3710d321d0e48c7d3e447cd8056340b5bc49e
kawkb <kdeb@odoo.com>
2025-09-15 13:10:05 +0200

    [FIX] l10n_tr_hr_payroll: restrict field to hr group
    The test test_employee_fields_groups crashed with:
    
    AccessError: The fields “l10n_tr_is_net_to_gross”, which you are trying to read, are not available for employee public profiles.
    
    This field exist without HR group restriction as a result, a non-HR internal user calling .read([]) triggered the AccessError.
    
    This commit adds `groups="hr_payroll.group_hr_payroll_user"` to this field so it is only accessible to HR users, resolving the error.
    
    RB-231736
    
    closes odoo/enterprise#94796
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- l10n_tr_hr_payroll/models/hr_version.py
+++ l10n_tr_hr_payroll/models/hr_version.py
@@ -6,4 +6,6 @@ class HrVersion(models.Model):
 
     l10n_tr_is_net_to_gross = fields.Boolean(
         string='Net to Gross',
-        help='If checked, the gross salary will be calculated based on the net salary.')
+        help='If checked, the gross salary will be calculated based on the net salary.',
+        groups="hr_payroll.group_hr_payroll_user"
+    )
