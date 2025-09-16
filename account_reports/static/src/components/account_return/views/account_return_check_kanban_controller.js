129f0ef7b7c18d8a1ab45e758a56f096ff19db75
leclerc-leo <leol@odoo.com>
2025-09-16 14:29:43 +0200

    [IMP] account_reports: Keep the scroll position when coming back to the Audit checks
    Part-of: odoo/enterprise#94806
    Related: odoo/odoo#227281
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- account_reports/static/src/components/account_return/views/account_return_check_kanban_controller.js
+++ account_reports/static/src/components/account_return/views/account_return_check_kanban_controller.js
@@ -1,4 +1,5 @@
-import { useSubEnv } from "@odoo/owl";
+import { useSubEnv, useEffect } from "@odoo/owl";
+import { useSetupAction } from "@web/search/action_hook";
 import { KanbanController } from "@web/views/kanban/kanban_controller";
 
 export class AccountReturnCheckKanbanController extends KanbanController {
@@ -8,5 +9,28 @@ export class AccountReturnCheckKanbanController extends KanbanController {
         useSubEnv({
             reload: () => this.model.load(),
         })
+
+        useSetupAction({
+            rootRef: this.rootRef,
+            getLocalState: () => {
+                const renderer = this.rootRef.el.querySelector(".kanban_return_and_checks_cards");
+                return {
+                    rendererScrollPositions: {
+                        top: renderer?.scrollTop || 0,
+                    },
+                };
+            },
+        });
+
+        let { rendererScrollPositions } = this.props.state || {};
+        useEffect(() => {
+            if (rendererScrollPositions) {
+                const renderer = this.rootRef.el.querySelector(".kanban_return_and_checks_cards");
+                if (renderer) {
+                    renderer.scrollTop = rendererScrollPositions.top;
+                    rendererScrollPositions = null;
+                }
+            }
+        });
     }
 }
