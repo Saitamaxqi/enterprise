8a441aadab0fe71c844249893de4e6239eb1fb16
aamo-odoo <aamo@odoo.com>
2025-09-19 17:30:08 +0530

    [FIX] ai: clean up unused methods and routes
    Methods like '_ai_submit_to_model', '_ai_add_message_to_context', and
    '_ai_create_response' have been removed, along with the
    route '/ai/generate_w_composer', from this [commit].
    
    [commit]: https://github.com/odoo/enterprise/commit/f8b9d475c0f19e5746ec47fe535c2030d117b534#diff-292213f329cde8a43c2882d1a0a972016a2fccb2f467fc79b196ebc2ae00f681L133-L191
    
    In this commit, we are removing the methods where the route and the referenced
    methods are called.
    
    sentry-6685514254
    
    closes odoo/enterprise#95110
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- ai/controllers/main.py
+++ ai/controllers/main.py
@@ -1,25 +1,11 @@
 from werkzeug.exceptions import NotFound
 
 from odoo import http
-from odoo.http import request
-from odoo.tools.mail import html_sanitize
 from odoo.addons.mail.tools.discuss import add_guest_to_context
 from odoo.addons.mail.controllers.thread import ThreadController
 
 
 class AIController(ThreadController):
-
-    @http.route(["/ai/generate_w_composer"], type="jsonrpc", auth="user")
-    def generate_text(self, prompt, channel_id):
-        composer_channel = request.env['discuss.channel'].search([('id', '=', channel_id)], limit=1)
-        # remove HTML tags from the prompt (LLMs get confused and format their replies using HTML)
-        prompt = html_sanitize(prompt).striptags()
-        # generate response by sending prompt to the chatgpt api
-        response = composer_channel._ai_submit_to_model(prompt, composer_channel.ai_context)
-        # add original prompt to the conversation history (context)
-        composer_channel._ai_add_message_to_context(prompt, 'user')
-        # post response as odoobot
-        composer_channel._ai_create_response(response)
 
     # auth=public to allow visitors to interact with ai agents through livechat
     @http.route(["/ai/generate_response"], type="jsonrpc", auth="public")
