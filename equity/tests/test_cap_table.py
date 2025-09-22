4a96ea3b3793679f60316a1e3984b23e46ae963e
kawkb <kdeb@odoo.com>
2025-09-22 15:27:45 +0200

    [FIX] equity: stabilize test by asserting records individually
    The test `test_option_into_share` was failing because `assertRecordValues`
    compares lists in order, and the records returned by the cap.table search
    were not in the same sequence as the expected values in the test.
    
    This commit makes the test deterministic by replacing list-based `assertRecordValues`
    calls with individual assertions per record.
    
    RB-232585
    
    closes odoo/enterprise#95187
    
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- equity/tests/test_cap_table.py
+++ equity/tests/test_cap_table.py
@@ -28,10 +28,9 @@ class TestEquity(TestEquityCommon):
                 'security_class_id': self.share_class_ord.id,
             },
         ])
-        self.assertRecordValues(self.env['equity.cap.table'].search([('partner_id', '=', self.company.id)]), [
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_1.id,
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_1.id)]),
+            [{
                 'securities': 75,
                 'votes': 75,
                 'ownership': 0.75,
@@ -39,10 +38,11 @@ class TestEquity(TestEquityCommon):
                 'dividend_payout': 0.75,
                 'dilution': 0.75,
                 'valuation': 750,
-            },
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_2.id,
+            }],
+        )
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_2.id)]),
+            [{
                 'securities': 25,
                 'votes': 25,
                 'ownership': 0.25,
@@ -50,8 +50,8 @@ class TestEquity(TestEquityCommon):
                 'dividend_payout': 0.25,
                 'dilution': 0.25,
                 'valuation': 250,
-            },
-        ])
+            }],
+        )
 
     def test_share_more_vote(self):
         self.env['equity.transaction'].create([
@@ -74,24 +74,24 @@ class TestEquity(TestEquityCommon):
                 'security_class_id': self.share_class_b.id,
             },
         ])
-        self.assertRecordValues(self.env['equity.cap.table'].search([('partner_id', '=', self.company.id)]), [
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_1.id,
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_1.id)]),
+            [{
                 'securities': 100,
                 'votes': 200,
                 'ownership': 0.5,
                 'voting_rights': 0.66666666666666666666666,
-            },
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_2.id,
+            }],
+        )
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_2.id)]),
+            [{
                 'securities': 100,
                 'votes': 100,
                 'ownership': 0.5,
                 'voting_rights': 0.33333333333333333333333,
-            },
-        ])
+            }],
+        )
 
     def test_sell_shares(self):
         self.env['equity.transaction'].create([
@@ -115,28 +115,28 @@ class TestEquity(TestEquityCommon):
                 'security_class_id': self.share_class_ord.id,
             },
         ])
-        self.assertRecordValues(self.env['equity.cap.table'].search([('partner_id', '=', self.company.id)]), [
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_1.id,
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_1.id)]),
+            [{
                 'securities': 70,
                 'votes': 70,
                 'ownership': 0.7,
                 'voting_rights': 0.7,
                 'dilution': 0.7,
                 'valuation': 700,
-            },
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_2.id,
+            }],
+        )
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_2.id)]),
+            [{
                 'securities': 30,
                 'votes': 30,
                 'ownership': 0.3,
                 'voting_rights': 0.3,
                 'dilution': 0.3,
                 'valuation': 300,
-            },
-        ])
+            }],
+        )
 
     def test_option_into_share_exceed(self):
         self.env['equity.transaction'].create([{
@@ -182,17 +182,20 @@ class TestEquity(TestEquityCommon):
             'security_price': 10,
             'security_class_id': self.option_class_2.id,
         }])
-        self.assertRecordValues(self.env['equity.cap.table'].search([('partner_id', '=', self.company.id)]), [{
-            'partner_id': self.company.id,
-            'holder_id': self.contact_1.id,
-            'security_class_id': self.option_class_2.id,
-            'securities': 100,
-            'votes': 0,
-            'ownership': 0,
-            'voting_rights': 0,
-            'dilution': 1,
-            'valuation': 1000,
-        }])
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id)]),
+            [{
+                'partner_id': self.company.id,
+                'holder_id': self.contact_1.id,
+                'security_class_id': self.option_class_2.id,
+                'securities': 100,
+                'votes': 0,
+                'ownership': 0,
+                'voting_rights': 0,
+                'dilution': 1,
+                'valuation': 1000,
+            }],
+        )
 
         self.env['equity.transaction'].create([{
             'partner_id': self.company.id,
@@ -204,30 +207,28 @@ class TestEquity(TestEquityCommon):
             'security_class_id': self.option_class_2.id,
             'destination_class_id': self.share_class_a.id,
         }])
-        self.assertRecordValues(self.env['equity.cap.table'].search([('partner_id', '=', self.company.id)]), [
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_1.id,
-                'security_class_id': self.share_class_a.id,
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_1.id), ('security_class_id', '=', self.share_class_a.id)]),
+            [{
                 'securities': 20,
                 'votes': 40,
                 'ownership': 1,
                 'voting_rights': 1,
                 'dilution': 0.2,
                 'valuation': 200,
-            },
-            {
-                'partner_id': self.company.id,
-                'holder_id': self.contact_1.id,
-                'security_class_id': self.option_class_2.id,
+            }],
+        )
+        self.assertRecordValues(
+            self.env['equity.cap.table'].search([('partner_id', '=', self.company.id), ('holder_id', '=', self.contact_1.id), ('security_class_id', '=', self.option_class_2.id)]),
+            [{
                 'securities': 80,
                 'votes': 0,
                 'ownership': 0,
                 'voting_rights': 0,
                 'dilution': 0.8,
                 'valuation': 800,
-            },
-        ])
+            }],
+        )
 
         # Looking in the past, it should still display the old value
         self.assertRecordValues(
