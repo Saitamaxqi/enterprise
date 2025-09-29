c77a03b401f965aca06bc29ca99892f56b41946f
Nikhil Joshi <nikj@odoo.com>
2025-09-29 12:11:37 +0000

    [FIX] sign: fix readonly + mandatory field issue
    Version:
    - saas-18.4
    
    Steps to reproduce:
    - Install the Sign app
    - Upload a PDF and add a strikethrough or multiline field
    - Mark the field as readonly and mandatory
    - Try to sign the document
    
    Issue:
    - The sign request cannot be validated if the strikethrough or multiline field
      is both readonly and mandatory.
    
    Solution:
    - For strikethrough fields: use "striked" instead of the item name "strikethrough"
      to set the correct value when the strikethorugh field is constant.
    
    - For multiline fields: when the field is constant, the value doesn’t come through
      element.value; instead, it’s in element.textContent, so we need to set the value
      there.
    
    Impact:
    - Users can now mark these fields as readonly and mandatory without blocking the
      validation of the sign request.
    
    task-5098750
    
    closes odoo/enterprise#100461
    
    X-original-commit: 3869e057cead0742ab1a0a8cba8aa8e91b13cc17
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>
    Signed-off-by: Nikhil Joshi (nikj) <nikj@odoo.com>

--- sign/static/src/components/sign_request/signable_PDF_iframe.js
+++ sign/static/src/components/sign_request/signable_PDF_iframe.js
@@ -148,6 +148,9 @@ export class SignablePDFIframe extends PDFIframe {
                         signItemElement.value = "striked";
                     }
                 });
+            } else {
+                signItemElement.firstChild.classList.add("o_sign_strikethrough_line_striked");
+                signItemElement.value = "striked";
             }
         }
 
@@ -532,7 +535,7 @@ export class SignablePDFIframe extends PDFIframe {
         // Removing wrap in order to have scrollWidth > width
         element.setAttribute("wrap", "off");
 
-        const strRawValue = element.value;
+        const strRawValue = element.value || element.textContent;
         element.value = "";
         if (!strRawValue) {
            return element.value;
