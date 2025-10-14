da2b19866c7aa5abd448b51d94ebea62782754e2
Akash Pandey <apan@odoo.com>
2025-10-14 18:17:08 +0530

    [FIX] l10n_au_hr_payroll_api: typo in table name
    There is a `s` in the table name of `ir_config_parameter`
    ```py
    2025-10-14 12:30:43,043 278 ERROR ? odoo.sql_db: bad query: b"DELETE FROM ir_config_parameters WHERE key = 'l10n_au_payroll_iap.endpoint';\nUPDATE res_company SET l10n_au_payroll_mode = 'test';"
    ERROR: relation "ir_config_parameters" does not exist
    LINE 1: DELETE FROM ir_config_parameters WHERE key = 'l10n_au_payrol...
                        ^
    
    2025-10-14 12:30:43,043 278 CRITICAL ? odoo.cli.neutralize: An error occurred during the neutralization. THE DATABASE IS NOT NEUTRALIZED!
    ```
    
    closes odoo/enterprise#97129
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- l10n_au_hr_payroll_api/data/neutralize.sql
+++ l10n_au_hr_payroll_api/data/neutralize.sql
@@ -1,2 +1,2 @@
-DELETE FROM ir_config_parameters WHERE key = 'l10n_au_payroll_iap.endpoint';
+DELETE FROM ir_config_parameter WHERE key = 'l10n_au_payroll_iap.endpoint';
 UPDATE res_company SET l10n_au_payroll_mode = 'test';
