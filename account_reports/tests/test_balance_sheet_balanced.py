9850d7a7085a514ae7afc76cd18c30b5f8381ec7
Woojang (WOPY) <wopy@odoo.com>
2025-10-16 09:43:19 +0800

    [IMP] l10n_tw_reports: update profit & loss and balance sheet reports
    This commit adds new improved accounting reports (balance sheet & profit & loss), providing users with improved reports that aligns better with the common Taiwanese business practices. The old balance sheet and profit & loss reports are depreciated and will be fully removed in later versions.
    
    Task-4915057
    
    closes odoo/enterprise#94443
    
    Related: odoo/odoo#226540
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- account_reports/tests/test_balance_sheet_balanced.py
+++ account_reports/tests/test_balance_sheet_balanced.py
@@ -308,6 +308,10 @@ REPORT_CONFIG = {
         'asset_line_ref': 'l10n_tw_reports.account_financial_report_total_assets0_l10n_tw',
         'liability_line_ref': 'l10n_tw_reports.account_financial_report_libailities_and_equity',
     },
+    'l10n_tw_reports.l10n_tw_balance_sheet': {
+        'asset_line_ref': 'l10n_tw_reports.l10n_tw_balance_sheet_tw_a',
+        'liability_line_ref': 'l10n_tw_reports.l10n_tw_balance_sheet_tw_lbeq',
+    },
     'l10n_tz_reports.l10n_tz_balance_sheet': {
         'asset_line_ref': 'l10n_tz_reports.account_financial_report_tz_active',
         'liability_line_ref': 'l10n_tz_reports.account_financial_report_tz_passive',
