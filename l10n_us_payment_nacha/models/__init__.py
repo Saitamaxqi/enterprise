Eddy-C127/enterprise18

commit a061a2b82a967ca2949a50979189832870f650e2

[IMP] l10n_us_payment_nacha: pass check_style_ruff
"".format() is no longer allowed. This is enforced by the
check_style_ruff step in runbot. Every bugfix touching these lines
requires an exception from the runbot team. This converts every string
formatting operation in l10n_us_payment_nacha to an f-string. Every
bugfix will now conflict, but at least we won't have to bother the
runbot team.

This was done automatically with flynt [1], which as far as I can tell
did a perfect job.

Fixing that makes other errors pop up (e.g. FURB113 to replace
multiple append() calls with a single extend()). This commit fixes
those as well. The remaining ones in the module are easy and were done
as well (removing # coding and super() arguments).

[1] https://pypi.org/project/flynt/

closes odoo/enterprise#66618

Signed-off-by: William André (wan) <wan@odoo.com>

l10n_us_payment_nacha/models/__init__.py
@@ -1,5 +1,4 @@
-# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import account_batch_payment
 from . import account_journal
-from . import account_payment
 from . import account_payment_method

l10n_us_payment_nacha/__init__.py
@@ -1,2 +1,2 @@
-# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import models

l10n_us_payment_nacha/tests/__init__.py
@@ -1,2 +1,2 @@
-# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import test_l10n_us_payment_nacha

l10n_us_payment_nacha/__manifest__.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     "name": "NACHA Payments",
     "summary": """Export payments as NACHA files""",

l10n_us_payment_nacha/models/account_batch_payment.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 import base64
 import math
 
@@ -12,7 +12,13 @@ class AccountBatchPayment(models.Model):
     def _validate_bank_for_nacha(self, payment):
         bank = payment.partner_bank_id
         if not bank.aba_routing:
-            raise ValidationError(_("Please set an ABA routing number on the %(account)s bank account for %(partner)s.", account=bank.display_name, partner=payment.partner_id.display_name))
+            raise ValidationError(
+                _(
+                    "Please set an ABA routing number on the %(account)s bank account for %(partner)s.",
+                    account=bank.display_name,
+                    partner=payment.partner_id.display_name,
+                )
+            )
 
     def _validate_journal_for_nacha(self):
         journal = self.journal_id
@@ -44,72 +50,61 @@ class AccountBatchPayment(models.Model):
         return sum(round(payment.amount * 100) for payment in payments)
 
     def _generate_nacha_header(self):
-        header = []
-        header.append("1")  # Record Type Code
-        header.append("01")  # Priority Code
-        header.append("{:>10.10}".format(self.journal_id.nacha_immediate_destination))  # Immediate Destination
-        header.append("{:>10.10}".format(self.journal_id.nacha_immediate_origin))  # Immediate Origin
-
         now_in_client_tz = fields.Datetime.context_timestamp(self, fields.Datetime.now())
-        header.append("{:6.6}".format(now_in_client_tz.strftime("%y%m%d")))  # File Creation Date
-        header.append("{:4.4}".format(now_in_client_tz.strftime("%H%M")))  # File Creation Time
-
         nr = self.search_count([("id", "!=", self.id), ("date", "=", self.date)])
-        header.append("{:1.1}".format(chr(min(90, ord("A") + nr))))  # File ID Modifier
 
-        header.append("094")  # Record Size
-        header.append("{:02d}".format(self._get_blocking_factor()))  # Blocking Factor
-        header.append("1")  # Format Code
-        header.append("{:23.23}".format(self.journal_id.nacha_destination))  # Destination
-        header.append("{:23.23}".format(self.journal_id.company_id.name))  # Origin or Company Name
-
-        # ideally this would be the display_name but it will be too long
-        header.append("{:8d}".format(self.id))  # Reference Code
-
-        return "".join(header)
+        return "".join((
+            "1",  # Record Type Code
+            "01",  # Priority Code
+            f"{self.journal_id.nacha_immediate_destination:>10.10}",  # Immediate Destination
+            f"{self.journal_id.nacha_immediate_origin:>10.10}",  # Immediate Origin
+            f"{now_in_client_tz.strftime('%y%m%d'):6.6}",  # File Creation Date
+            f"{now_in_client_tz.strftime('%H%M'):4.4}",  # File Creation Time
+            f"{chr(min(90, ord('A') + nr)):1.1}",  # File ID Modifier
+            "094",  # Record Size
+            f"{self._get_blocking_factor():02d}",  # Blocking Factor
+            "1",  # Format Code
+            f"{self.journal_id.nacha_destination:23.23}",  # Destination
+            f"{self.journal_id.company_id.name:23.23}",  # Origin or Company Name
+            f"{self.id:8d}",  # Reference Code, ideally this would be the display_name but it will be too long
+        ))
 
     def _generate_nacha_batch_header_record(self, date, batch_nr):
         description = f"BATCH {batch_nr}"
-        batch = []
-        batch.append("5")  # Record Type Code
-        batch.append("200" if self.journal_id.nacha_is_balanced else "220")  # Service Class Code
-        batch.append("{:16.16}".format(self.journal_id.company_id.name))  # Company Name
-
-        # Truncate beginning of name if needed because that remains more recognizable:
-        # "MYBATCH/OUT/2023/00005" becomes "BATCH/OUT/2023/00005" instead of "MYBATCH/OUT/2023/000"
-        batch.append(f"{self.journal_id.nacha_discretionary_data or self.name[-20:]:20.20}")  # Company Discretionary Data (optional)
-
-        batch.append(f"{self.journal_id.nacha_company_identification:10.10}")  # Company Identification
-        batch.append(self.journal_id.nacha_entry_class_code)  # Standard Entry Class Code
-        batch.append("{:10.10}".format(description))  # Company Entry Description
-        batch.append("{:6.6}".format(date.strftime("%y%m%d")))  # Company Descriptive Date
-        batch.append("{:6.6}".format(date.strftime("%y%m%d")))  # Effective Entry Date
-        batch.append("{:3.3}".format(""))  # Settlement Date (Julian)
-        batch.append("1")  # Originator Status Code
-        batch.append("{:8.8}".format(self.journal_id.nacha_origination_dfi_identification))  # Originating DFI Identification
-        batch.append("{:07d}".format(batch_nr))  # Batch Number
-
-        return "".join(batch)
+        return "".join((
+            "5",  # Record Type Code
+            "200" if self.journal_id.nacha_is_balanced else "220",  # Service Class Code
+            f"{self.journal_id.company_id.name:16.16}",  # Company Name
+            # Truncate beginning of name if needed because that remains more recognizable:
+            # "MYBATCH/OUT/2023/00005" becomes "BATCH/OUT/2023/00005" instead of "MYBATCH/OUT/2023/000"
+            f"{self.journal_id.nacha_discretionary_data or self.name[-20:]:20.20}",  # Company Discretionary Data (optional)
+            f"{self.journal_id.nacha_company_identification:10.10}",  # Company Identification
+            self.journal_id.nacha_entry_class_code,  # Standard Entry Class Code
+            f"{description:10.10}",  # Company Entry Description
+            f"{date.strftime('%y%m%d'):6.6}",  # Company Descriptive Date
+            f"{date.strftime('%y%m%d'):6.6}",  # Effective Entry Date
+            f"{'':3.3}",  # Settlement Date (Julian)
+            "1",  # Originator Status Code
+            f"{self.journal_id.nacha_origination_dfi_identification:8.8}",  # Originating DFI Identification
+            f"{batch_nr:07d}",  # Batch Number
+        ))
 
     def _generate_nacha_entry_detail(self, payment_nr, payment, is_offset):
         bank = payment.partner_bank_id
-        entry = []
-        entry.append("6")  # Record Type Code
-        entry.append("27" if is_offset else "22")  # Transaction Code
-        entry.append("{:8.8}".format(bank.aba_routing[:-1]))  # RDFI Routing Transit Number
-        entry.append("{:1.1}".format(bank.aba_routing[-1]))  # Check Digit
-        entry.append("{:17.17}".format(bank.acc_number))  # DFI Account Number
-        entry.append("{:010d}".format(self._get_total_cents(payment)))  # Amount
-        entry.append("{:15.15}".format(payment.partner_id.vat or ""))  # Individual Identification Number (optional)
-        entry.append("{:22.22}".format("OFFSET" if is_offset else payment.partner_id.name))  # Individual Name
-        entry.append("  ")  # Discretionary Data Field
-        entry.append("0")  # Addenda Record Indicator
-
-        # trace number
-        entry.append("{:8.8}".format(self.journal_id.nacha_origination_dfi_identification))  # Trace Number (80-87)
-        entry.append("{:07d}".format(payment_nr))  # Trace Number (88-94)
-
-        return "".join(entry)
+        return "".join((
+            "6",  # Record Type Code
+            "27" if is_offset else "22",  # Transaction Code
+            f"{bank.aba_routing[:-1]:8.8}",  # RDFI Routing Transit Number
+            f"{bank.aba_routing[-1]:1.1}",  # Check Digit
+            f"{bank.acc_number:17.17}",  # DFI Account Number
+            f"{self._get_total_cents(payment):010d}",  # Amount
+            f"{payment.partner_id.vat or '':15.15}",  # Individual Identification Number (optional)
+            f"{'OFFSET' if is_offset else payment.partner_id.name:22.22}",  # Individual Name
+            "  ",  # Discretionary Data Field
+            "0",  # Addenda Record Indicator
+            f"{self.journal_id.nacha_origination_dfi_identification:8.8}",  # Trace Number (80-87)
+            f"{payment_nr:07d}",  # Trace Number (88-94)
+        ))
 
     def _calculate_aba_hash(self, aba_routing):
         # [:-1]: remove check digit
@@ -121,20 +116,19 @@ class AccountBatchPayment(models.Model):
         return str(hashes)[-10:]  # take the rightmost 10 characters
 
     def _generate_nacha_batch_control_record(self, payments, offset_payment, batch_nr):
-        control = []
-        control.append("8")  # Record Type Code
-        control.append("200" if self.journal_id.nacha_is_balanced else "220")  # Service Class Code
-        control.append("{:06d}".format(len(payments | offset_payment)))  # Entry/Addenda Count
-        control.append("{:0>10}".format(self._calculate_aba_hash_for_payments(payments | offset_payment)))  # Entry Hash
-        control.append("{:012d}".format(self._get_total_cents(offset_payment)))  # Total Debit Entry Dollar Amount in Batch
-        control.append("{:012d}".format(self._get_total_cents(payments)))  # Total Credit Entry Dollar Amount in Batch
-        control.append("{:0>10.10}".format(self.journal_id.nacha_company_identification))  # Company Identification
-        control.append("{:19.19}".format(""))  # Message Authentication Code (leave blank)
-        control.append("{:6.6}".format(""))  # Reserved (leave blank)
-        control.append("{:8.8}".format(self.journal_id.nacha_origination_dfi_identification))  # Originating DFI Identification
-        control.append("{:07d}".format(batch_nr))  # Batch Number
-
-        return "".join(control)
+        return "".join((
+            "8",  # Record Type Code
+            "200" if self.journal_id.nacha_is_balanced else "220",  # Service Class Code
+            f"{len(payments | offset_payment):06d}",  # Entry/Addenda Count
+            f"{self._calculate_aba_hash_for_payments(payments | offset_payment):0>10}",  # Entry Hash
+            f"{self._get_total_cents(offset_payment):012d}",  # Total Debit Entry Dollar Amount in Batch
+            f"{self._get_total_cents(payments):012d}",  # Total Credit Entry Dollar Amount in Batch
+            f"{self.journal_id.nacha_company_identification:0>10.10}",  # Company Identification
+            f"{'':19.19}",  # Message Authentication Code (leave blank)
+            f"{'':6.6}",  # Reserved (leave blank)
+            f"{self.journal_id.nacha_origination_dfi_identification:8.8}",  # Originating DFI Identification
+            f"{batch_nr:07d}",  # Batch Number
+        ))
 
     def _get_nr_of_records(self, nr_of_batches, nr_of_payments):
         # File header
@@ -147,24 +141,20 @@ class AccountBatchPayment(models.Model):
 
     def _generate_nacha_file_control_record(self, nr_of_batches, payments, offset_payments):
         nr_of_payments = len(payments | offset_payments)
-
-        control = []
-        control.append("9")  # Record Type Code
-        control.append("{:06d}".format(nr_of_batches))  # Batch Count
-
         # Records / Blocking Factor (always 10).
         # We ceil because we'll pad the file with 999's until a multiple of 10.
         block_count = math.ceil(self._get_nr_of_records(nr_of_batches, nr_of_payments) / self._get_blocking_factor())
-        control.append("{:06d}".format(block_count))
 
-        control.append("{:08d}".format(nr_of_payments))  # Entry/ Addenda Count
-        control.append("{:0>10}".format(self._calculate_aba_hash_for_payments(payments | offset_payments)))  # Entry Hash
-
-        control.append("{:012d}".format(self._get_total_cents(offset_payments)))  # Total Debit Entry Dollar Amount in File
-        control.append("{:012d}".format(self._get_total_cents(payments)))  # Total Credit Entry Dollar Amount in File
-        control.append("{:39.39}".format(""))  # Blank
-
-        return "".join(control)
+        return "".join((
+            "9",  # Record Type Code
+            f"{nr_of_batches:06d}",  # Batch Count
+            f"{block_count:06d}",
+            f"{nr_of_payments:08d}",  # Entry/ Addenda Count
+            f"{self._calculate_aba_hash_for_payments(payments | offset_payments):0>10}",  # Entry Hash
+            f"{self._get_total_cents(offset_payments):012d}",  # Total Debit Entry Dollar Amount in File
+            f"{self._get_total_cents(payments):012d}",  # Total Credit Entry Dollar Amount in File
+            f"{'':39.39}",  # Blank
+        ))
 
     def _generate_padding(self, nr_of_batches, nr_of_payments):
         padding = []
@@ -214,7 +204,7 @@ class AccountBatchPayment(models.Model):
         return "\r\n".join([header] + entries)
 
     def _get_methods_generating_files(self):
-        res = super(AccountBatchPayment, self)._get_methods_generating_files()
+        res = super()._get_methods_generating_files()
         res.append("nacha")
         return res
 
@@ -225,7 +215,7 @@ class AccountBatchPayment(models.Model):
             date = fields.Datetime.today().strftime("%m-%d-%Y")  # US date format
             return {
                 "file": base64.encodebytes(data.encode()),
-                "filename": "NACHA-%s-%s.txt" % (self.journal_id.code, date),
+                "filename": f"NACHA-{self.journal_id.code}-{date}.txt",
             }
         else:
-            return super(AccountBatchPayment, self)._generate_export_file()
+            return super()._generate_export_file()

l10n_us_payment_nacha/models/account_journal.py
@@ -1,5 +1,5 @@
-# coding: utf-8
-from odoo import api, fields, models
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models
 
 
 class AccountJournal(models.Model):

l10n_us_payment_nacha/models/account_payment.py
@@ -1,18 +1,18 @@
-# coding: utf-8
-from odoo import api, models, _
-from odoo.exceptions import ValidationError
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, models
+
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     @api.model
     def _get_method_codes_using_bank_account(self):
-        res = super(AccountPayment, self)._get_method_codes_using_bank_account()
+        res = super()._get_method_codes_using_bank_account()
         res.append('nacha')
         return res
 
     @api.model
     def _get_method_codes_needing_bank_account(self):
-        res = super(AccountPayment, self)._get_method_codes_needing_bank_account()
+        res = super()._get_method_codes_needing_bank_account()
         res.append('nacha')
         return res

l10n_us_payment_nacha/models/account_payment_method.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, models
 
 

Saitamaxqi/enterprise

commit 7fce5ae934398d433c4d2fefb008670d11419d7a

[FIX] l10n_us_payment_nacha: load account_payment
Accidentally removed in a061a2b82a967ca2 which caused the bank account
selection field to not show when doing a NACHA payment.

closes odoo/enterprise#97564

X-original-commit: 99c51624b445446cb02f6ddb2293063ea4c5e52d
Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

l10n_us_payment_nacha/models/__init__.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import account_batch_payment
 from . import account_journal
+from . import account_payment
 from . import account_payment_method