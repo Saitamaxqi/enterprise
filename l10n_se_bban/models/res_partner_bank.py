cb1cb078c4cdb3c6d88786799dc442affb47f5a5
Mathieu Coutant <mcou@odoo.com>
2025-10-24 09:17:56 +0000

    [FIX] l10n_se_bban: recognise specific Swedish account numbers
    Issue:
    Some valid Swedish account numbers are not recognized as Swedish.
    
    Steps to reproduce:
    - Install 'l10n_se_bban' and switch to Swedish company
    - Create a new partner, under the 'Accounting' page create a new bank account
    	1. 62074-0
    	2. 678653833066
    	3. 99603406872188
    - In the partner list view select this new partner
    - Click Actions > Export, select "Banks" and "Bank Type"
    - Check the file
    	1. 62074-0 not recognized as Plusgiro
    	2. 678653833066 not recognized as BBAN
    	3. 99603406872188 not recognized as BBAN
    
    Cause:
    These numbers are not recognized by the checks of Odoo but are valid numbers:
    1. Plusgiro account numbers can be 2 to 8 digits long, Odoo accepted only 7 to 8 digits account numbers
    2. Old Handelsbanken numbers (6000-6999) can have 8 digits instead of 9, Odoo only accepts 9 digits numbers
    3. Only clearing numbers starting with 8 are 5 digits long, Odoo also included ranges 9500-9549 and 9960-9969
    
    Sources:
    1 and 3: https://www.amcbanking.com/kb/swedish-payments-how-to-configure-sender-and-vendor-bank-accounts-in-fo/
    1 and 3: https://github.com/Tobbe/kontonummer.js/blob/04959502d7d2d52938aabda80b8a3464efddfdd1/kontonummer.js
    2: https://github.com/barsoom/banktools-se/commit/b964806d5cad0491ea121419520fd5b5d4478c15
    
    opw-5099867
    
    closes odoo/enterprise#98736
    
    X-original-commit: 007f519bb55ff54ffd92b82f961e8c16784808d3
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- l10n_se_bban/models/res_partner_bank.py
+++ l10n_se_bban/models/res_partner_bank.py
@@ -5,7 +5,7 @@ from stdnum import luhn
 
 from odoo import api, models, _
 
-PLUSGIRO_ACCOUNT_NUMBER_RE = re.compile(r'^\d{6,7}-\d$')
+PLUSGIRO_ACCOUNT_NUMBER_RE = re.compile(r'^\d{1,7}-\d$')
 BANKGIRO_ACCOUNT_NUMBER_RE = re.compile(r'^\d{3,5}-\d{2,5}$')
 NOT_DIGIT_RE = re.compile(r'\D')
 
@@ -30,7 +30,7 @@ class ResPartnerBank(models.Model):
         return super().retrieve_acc_type(acc_number)
 
     def _se_validate_plusgiro(self, acc_number):
-        """Validate PlusGiro number: format XXXXXXX-C (6-8 digits total), dash at end, Luhn checksum."""
+        """Validate PlusGiro number: format XXXXXXX-C (2-8 digits total), dash at end, Luhn checksum."""
         return PLUSGIRO_ACCOUNT_NUMBER_RE.match(acc_number.replace(' ', '')) and luhn.is_valid(NOT_DIGIT_RE.sub('', acc_number))
 
     def _se_validate_bankgiro(self, acc_number):
@@ -62,7 +62,7 @@ class ResPartnerBank(models.Model):
         if not clearing_range:
             return False, False, False
 
-        bank_code_length = 5 if clearing_range.checksum == 'mod10_max_10_digits' else 4
+        bank_code_length = 5 if clearing_range.checksum == 'mod10_max_10_digits_5' else 4
         bank_code = cleaned_acc_number[:bank_code_length]
         account_number = cleaned_acc_number[bank_code_length:]
 
@@ -79,10 +79,12 @@ class ResPartnerBank(models.Model):
         elif checksum == 'mod11_11_digits':  # Validate account number with 11 digits using Mod11 checksum
             return len(account_number) == 7 and mod11_is_valid(bank_code + account_number)
         # Type 2 accounts
-        elif checksum == 'mod10_max_10_digits':  # Validate account number with up to 10 digits using Mod10 checksum
+        elif checksum in {'mod10_max_10_digits', 'mod10_max_10_digits_5'}:  # Validate account number with up to 10 digits using Mod10 checksum
             return len(account_number) <= 10 and luhn.is_valid(account_number)
         elif checksum == 'mod10_10_digits':  # Validate account number with 10 digits using Mod10 checksum
             return len(account_number) == 10 and luhn.is_valid(account_number)
         elif checksum == 'mod11_9_digits':   # Validate account number with 9 digits using Mod11 checksum
             return len(account_number) == 9 and mod11_is_valid(account_number)
+        elif checksum == 'mod11_8_9_digits':   # Validate account number with 8 or 9 digits using Mod11 checksum
+            return len(account_number) in {8, 9} and mod11_is_valid(account_number)
         return False
