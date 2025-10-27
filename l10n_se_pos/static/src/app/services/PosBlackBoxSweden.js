d577962626e1dbaae120647b58e22249e1b186b7
Max Whale <mawh@odoo.com>
2025-10-27 07:35:52 +0000

    [FIX] pos_l10n_se: fix blackbox order validation
    Before this commit, the Swedish blackbox was completely broken in 18.0
    because the `push_single_order` function is no longer used.
    
    To fix this, the same approach as `pos_blackbox_be` was used, where
    the `preSyncAllOrders` function was overridden instead.
    In addition, the old receipt reprinting logic was no longer working, and
    so it now uses the `nb_print` field to keep track of the number of
    reprints.
    
    Finally, we also forward port a fix from 17.0 (ddbc1fc) to correct the
    format of the organisation number, which for some reason was not forward
    ported originally.
    
    task-5077448
    
    closes odoo/enterprise#98437
    
    X-original-commit: f936f1bfd042403da162e9b8b00ec4feb65cd6ad
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- l10n_se_pos/static/src/app/services/PosBlackBoxSweden.js
+++ l10n_se_pos/static/src/app/services/PosBlackBoxSweden.js
@@ -1,7 +1,7 @@
 import { PosStore } from "@point_of_sale/app/services/pos_store";
-import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
+import { SwedenBlackboxError } from "@l10n_se_pos/app/errors/error_handlers";
 const { DateTime } = luxon;
 
 patch(PosStore.prototype, {
@@ -18,6 +18,14 @@ patch(PosStore.prototype, {
         const result = super.disallowLineQuantityChange(...arguments);
         return this.useBlackBoxSweden() || result;
     },
+    async preSyncAllOrders(orders) {
+        if (this.useBlackBoxSweden() && orders.length > 0) {
+            for (const order of orders) {
+                await this.pushSingleOrder(order);
+            }
+        }
+        return super.preSyncAllOrders(orders);
+    },
     async pushSingleOrder(order) {
         if (this.useBlackBoxSweden() && order) {
             if (!order.receipt_type) {
@@ -25,32 +33,28 @@ patch(PosStore.prototype, {
                 order.sequence_number = await this.getOrderSequenceNumber();
             }
             try {
-                order.blackbox_tax_category_a = order.getSpecificTax(25);
-                order.blackbox_tax_category_b = order.getSpecificTax(12);
-                order.blackbox_tax_category_c = order.getSpecificTax(6);
-                order.blackbox_tax_category_d = order.getSpecificTax(0);
+                order.blackbox_tax_category_a = order.getSpecificTax("A");
+                order.blackbox_tax_category_b = order.getSpecificTax("B");
+                order.blackbox_tax_category_c = order.getSpecificTax("C");
+                order.blackbox_tax_category_d = order.getSpecificTax("D");
                 const data = await this.pushOrderToSwedenBlackbox(order);
                 if (data.value.error && data.value.error.errorCode != "000000") {
                     throw data.value.error;
                 }
                 this.setDataForPushOrderFromSwedenBlackBox(order, data);
             } catch (err) {
-                this.env.services.dialog.add(AlertDialog, {
-                    title: _t("Blackbox error"),
-                    body: _t(err.status.message_title ? err.status.message_title : err.status),
-                });
-                return;
+                order.state = "draft";
+                throw new SwedenBlackboxError(err?.status?.message_title ?? err?.status ?? err);
             }
         }
-        return super.pushSingleOrder(...arguments);
     },
     async pushOrderToSwedenBlackbox(order) {
         const fdm = this.hardwareProxy.deviceControllers.fiscal_data_module;
         const data = {
             date: new DateTime(order.date_order).toFormat("yyyyMMddHHmm"),
             receipt_id: order.sequence_number.toString(),
-            pos_id: order.pos.config.id.toString(),
-            organisation_number: this.company.company_registry,
+            pos_id: this.config.id.toString(),
+            organisation_number: this.company.company_registry.replace(/\D/g, ""),
             receipt_total: order.displayPrice.toFixed(2).toString().replace(".", ","),
             negative_total:
                 order.totalDue < 0
@@ -76,11 +80,13 @@ patch(PosStore.prototype, {
             fdm.action({
                 action: "registerReceipt",
                 high_level_message: data,
-            }).then((response) => {
-                if (!response.result) {
-                    reject(_t("Blackbox is disconnected"));
-                }
-            });
+            })
+                .then((response) => {
+                    if (!response.result) {
+                        reject(_t("Blackbox is disconnected"));
+                    }
+                })
+                .catch(reject);
         });
     },
     setDataForPushOrderFromSwedenBlackBox(order, data) {
@@ -88,7 +94,7 @@ patch(PosStore.prototype, {
         order.blackbox_unit_id = data.unit_id;
     },
     async getOrderSequenceNumber() {
-        return await this.data.call("pos.config", "getOrderSequenceNumber", [this.config.id]);
+        return await this.data.call("pos.config", "get_order_sequence_number", [this.config.id]);
     },
     async getProfoOrderSequenceNumber() {
         return await this.data.call("pos.config", "get_profo_order_sequence_number", [
