245ae1421b11600d0d9d129766633e9b3470b6e2
MaximeNoirhomme <noma@odoo.com>
2025-11-04 15:46:49 +0000

    [FIX] mrp_workorder: prevent traceback when clicking add button on shopfloor
    **Issue**
    In shopfloor, a traceback occurs when clicking on the add button if the associated operation has a `quality_check` of type `register_consumed_materials`.
    
    **Steps to reproduce**
    1. Create two products (product and component)
    2. Create a BOM for this product that consumes that component
    3. Create an operation linked to that BOM (Manufacturing > Configuration > Operations)
    4. Add a quality check of type “Register Consumable Material” for this operation
    5. Create a MO from that BOM and confirm it
    6. Click on the shopfloor smart button
       → If debug mode is activated, a traceback occurs
       → Otherwise
    7. Click on the 'add' button
       → A traceback occurs
    
    **Cause**
    In the method [`subRecordProps`](https://github.com/odoo/enterprise/blob/d180235b7946c7219385df263f13f780e7faea50/mrp_workorder/static/src/mrp_display/mrp_display_record.js#L190C9-L194C14), when a quality check of type `register_consumed_materials` is done,
    the variable `production` is not propagated into the props.
    And [this](https://github.com/odoo/enterprise/blob/d180235b7946c7219385df263f13f780e7faea50/mrp_workorder/static/src/mrp_display/mrp_record_line/stock_move.js#L184) tries to access the production in the props,
    which is [called](https://github.com/odoo/enterprise/blob/d180235b7946c7219385df263f13f780e7faea50/mrp_workorder/static/src/mrp_display/mrp_record_line/stock_move.js#L163) when the user clicks the 'add' button.
    
    **Solution**
    Add the `production` variable to the props.
    
    opw-5165259
    
    closes odoo/enterprise#100816
    
    X-original-commit: 5de301f066b73081a943ce40d6788e93bccc621a
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>
    Signed-off-by: Maxime Noirhomme (noma) <noma@odoo.com>

--- mrp_workorder/static/src/mrp_display/mrp_display_record.js
+++ mrp_workorder/static/src/mrp_display/mrp_display_record.js
@@ -230,6 +230,7 @@ export class MrpDisplayRecord extends Component {
                     : this.props.production.data.move_byproduct_ids;
             subRecord = moves.records.find((m) => m.data.check_id.resIds.includes(subRecord.resId));
             props.displayUOM = this.displayUOM;
+            props.production = this.props.production;
             props.startWorking = this.startWorking.bind(this);
             props.production = this.props.production;
         } else if (subRecord.resModel === "stock.move") {
