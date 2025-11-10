a9de9963004a3fcd9a890bc34d451fa6dc600dd8
Xavier Bol (xbo) <xbo@odoo.com>
2025-11-10 16:19:37 +0100

    [FIX] l10n_be_hr_payroll_group_s: move group S unicity constraint per employee
    Before this commit, the group S unicity constraint were per versions
    since before it was per contract. Now, since we no longer any contract
    model and more than version could represent the same contract, that
    constraint is a bit too restrictive. Moreover, the group S is more
    stored on the employee, so it would surely make more sense to trigger
    an error if 2 different employees have the same group S code.
    
    This commit moves the group S unicity constraint in hr.employee model to
    make sure we check per employee instead of per version.
    
    Part-of: odoo/enterprise#99086
    Related: odoo/odoo#234932
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- l10n_be_hr_payroll_group_s/models/hr_version.py
+++ l10n_be_hr_payroll_group_s/models/hr_version.py
@@ -19,7 +19,7 @@ class HrVersion(models.Model):
                 ('group_s_code', 'in', self.mapped('group_s_code')),
             ],
             groupby=['group_s_code'],
-            aggregates=['id:recordset'],
+            aggregates=['employee_id:recordset'],
         ))
         if any(
             similar_group_s_codes.get(version.group_s_code)
@@ -28,4 +28,4 @@ class HrVersion(models.Model):
             and version.group_s_code
             for version in self
         ):
-            raise ValidationError(_('The Groups S code should be unique!'))
+            raise ValidationError(_('The Groups S code should be unique by employee!'))
