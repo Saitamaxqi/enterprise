7c63f728d741082f97cce7df8f9e27150eb650d9
David Monnom <moda@odoo.com>
2025-11-12 09:56:18 +0100

    [FIX] pos_blackbox_be: handle offline in syncAllOrders
    When trying to sync orders while being offline, a `ConnectionLostError`
    is raised. This error was not handled in the pos_blackbox_be module.
    Now, if an order was not signed correctly by the blackbox, we put its
    state back to "draft", allowing the cashier to retry later
    (when the connection to bbox is re-established).
    
    closes odoo/enterprise#99202
    
    X-original-commit: c298970
    Related: odoo/odoo#235147
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- pos_blackbox_be/static/src/pos/app/utils/order_payment_validation.js
+++ pos_blackbox_be/static/src/pos/app/utils/order_payment_validation.js
@@ -3,6 +3,7 @@ import { patch } from "@web/core/utils/patch";
 import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { _t } from "@web/core/l10n/translation";
 import { BlackboxError } from "@pos_blackbox_be/pos/app/utils/blackbox_error";
+import { ConnectionLostError } from "@web/core/network/rpc";
 const EMPTY_SIGNATURE = "                                        ";
 
 patch(OrderPaymentValidation.prototype, {
@@ -22,7 +23,14 @@ patch(OrderPaymentValidation.prototype, {
     },
     async afterOrderValidation() {
         if (!this.order.blackbox_signature || this.order.blackbox_signature == EMPTY_SIGNATURE) {
-            await this.pos.syncAllOrders({ orders: [this.order], throw: true });
+            try {
+                await this.pos.syncAllOrders({ orders: [this.currentOrder], throw: true });
+            } catch (error) {
+                if (error instanceof BlackboxError || error instanceof ConnectionLostError) {
+                    this.currentOrder.state = "draft";
+                    throw error;
+                }
+            }
         }
         return super.afterOrderValidation();
     },
