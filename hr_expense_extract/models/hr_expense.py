e7e5372892e3b805c5854b0f6a75c7c032f397a0
Xavier Morel <xmo@odoo.com>
2025-11-18 15:10:52 +0000

    [FIX] *: remove duplicate methods
    found by pylint 4
    
    closes odoo/enterprise#100078
    
    X-original-commit: 84ee887174ef8885eead364ff4de9698cabc136b
    Related: odoo/odoo#236766
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- hr_expense_extract/models/hr_expense.py
+++ hr_expense_extract/models/hr_expense.py
@@ -38,12 +38,6 @@ class HrExpense(models.Model):
         for expense in self:
             expense.extract_state_processed = expense.extract_state == 'waiting_extraction' and expense.state == 'draft'
 
-    @api.depends('extract_state', 'state')
-    def _compute_extract_state_processed(self):
-        # Overrides 'iap_extract'
-        for expense in self:
-            expense.extract_state_processed = expense.extract_state == 'waiting_extraction' and expense.state == 'draft'
-
     @api.model
     def _contact_iap_extract(self, pathinfo, params):
         params['version'] = OCR_VERSION
