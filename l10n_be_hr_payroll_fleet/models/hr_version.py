43923d5fcfbd8d5ec2cb633347db8c2b85103f21
Dylan Kiss (dyki) <dyki@odoo.com>
2025-11-27 14:21:38 +0000

    [I18N] l10n_be_hr_payroll_fleet: fix translatable selection field
    The selection field `fuel_type` in the `hr.contract` model was not
    properly translatable, because it used a function yielding static
    strings for the options. This commit changes the field to use the
    variable directly, allowing them to be translated.
    
    closes odoo/enterprise#101046
    
    X-original-commit: 41ebddd6b0d04077441c5b532b653e2a337534a3
    Signed-off-by: Chong Wang (cwg) <cwg@odoo.com>
    Signed-off-by: Dylan Kiss (dyki) <dyki@odoo.com>

--- l10n_be_hr_payroll_fleet/models/hr_version.py
+++ l10n_be_hr_payroll_fleet/models/hr_version.py
@@ -1,6 +1,5 @@
 from odoo import api, fields, models
 from odoo.fields import Domain
-from odoo.addons.fleet.models.fleet_vehicle_model import FUEL_TYPES
 
 
 class HrVersion(models.Model):
@@ -94,7 +93,7 @@ class HrVersion(models.Model):
     max_unused_cars = fields.Integer(compute='_compute_max_unused_cars', groups='hr.group_hr_user')
     acquisition_date = fields.Date(related='car_id.acquisition_date', readonly=False, groups="fleet.fleet_group_manager")
     car_value = fields.Float(related="car_id.car_value", readonly=False, groups="fleet.fleet_group_manager")
-    fuel_type = fields.Selection(selection=lambda self: FUEL_TYPES, compute="_compute_fuel_type", readonly=False, groups="fleet.fleet_group_manager")
+    fuel_type = fields.Selection(selection=lambda self: self.env['fleet.vehicle']._fields['fuel_type']._description_selection(self.env), compute="_compute_fuel_type", readonly=False, groups="fleet.fleet_group_manager")
     co2 = fields.Float(related="car_id.co2", readonly=False, groups="fleet.fleet_group_manager")
     driver_id = fields.Many2one('res.partner', related="car_id.driver_id", readonly=False, groups="fleet.fleet_group_manager")
     car_open_contracts_count = fields.Integer(compute='_compute_car_open_contracts_count', groups="fleet.fleet_group_manager")
