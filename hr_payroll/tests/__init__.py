b8522a69ad60d57a6c02c2fe1f13da627652a90a
amga-odoo <amga@odoo.com>
2025-11-27 16:24:32 +0530

    [FIX] hr_work_entry: test reset work entry
    Steps to reproduce:
    - Install the hr_payroll module.
    - Open the form view of an employee with a running contract and click on the
    Payslip smart button.
    - Create or open an existing payslip, then open the Work Entries using the
    smart button.
    - Select any work entry and click Reset Selected Work Entries.
    
    Issue:
    Clicking Reset Selected Work Entries removes the work entry for that day
    and leaves it empty.
    
    Cause:
    The issue occurs because an incorrect active_id is passed in the context.
    It works correctly when coming from the employee form (where active_id is
    the employee ID), but fails when accessed from the payslip (where active_id is
    the payslip ID).
    
    ---
    Fix:
    PR https://github.com/odoo/odoo/pull/235211 updates the context to ensure the
    correct employee ID is passed.
    
    - This commit contains test for the reset work entry problem
    
    Task-5095098
    
    closes odoo/enterprise#100719
    
    Related: odoo/odoo#235211
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_payroll/tests/__init__.py
+++ hr_payroll/tests/__init__.py
@@ -21,3 +21,4 @@ from . import test_rule_category
 from . import test_domain_condition_rules
 from . import test_payroll_properties
 from . import test_payslip_allocations
+from . import test_work_entry_reset
