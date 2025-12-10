Saitamaxqi/enterprise

commit d0c1e7845feeee1c2e85a21b5d40570d051458d3

[IMP] stock: remove name field on stock.move
Remove this field as it is not really relevant as we already have the
reference field.
We make sure to adapt the `compute_reference` to catch all cases where a
custom name used to be set on the move

task-4783553

Part-of: odoo/enterprise#86304
Related: odoo/odoo#211488
Related: odoo/upgrade#7736
Signed-off-by: Claeys Pieter (clpi) <clpi@odoo.com>

sale_stock_renting/tests/test_rental.py
@@ -409,7 +409,6 @@ class TestRentalWizard(TestRentalCommon):
             'move_ids': [Command.create({
                 'location_id': self.warehouse_id.lot_stock_id.id,
                 'location_dest_id':  self.ref('stock.stock_location_customers'),
-                'name': 'Lovely product move',
                 'product_id': product.id,
                 'product_uom_qty': 20,
             })]
@@ -556,7 +555,6 @@ class TestRentalWizard(TestRentalCommon):
             'move_ids': [Command.create({
                 'location_id': self.warehouse_id.lot_stock_id.id,
                 'location_dest_id':  self.ref('stock.stock_location_customers'),
-                'name': 'Lovely product move',
                 'product_id': product.id,
                 'product_uom_qty': 20,
             })]
@@ -701,7 +699,6 @@ class TestRentalWizard(TestRentalCommon):
             'move_ids': [Command.create({
                 'location_id': self.warehouse_id.lot_stock_id.id,
                 'location_dest_id':  self.ref('stock.stock_location_customers'),
-                'name': 'Lovely product move',
                 'product_id': product.id,
                 'product_uom_qty': 20,
             })]

sale_amazon/models/stock_move.py
@@ -1,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models, _
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.depends('sale_line_id.order_id.amazon_channel')
+    def _compute_reference(self):
+        super()._compute_reference()
+        for record in self:
+            if record.sale_line_id and record.sale_line_id.order_id.amazon_channel == 'fba':
+                record.reference = _('Amazon move: %s', record.reference)

sale_shopee/models/stock_move.py
@@ -1,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models, _
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.depends('sale_line_id.order_id.shopee_fulfillment_type')
+    def _compute_reference(self):
+        super()._compute_reference()
+        for record in self:
+            if record.sale_line_id and record.sale_line_id.order_id.shopee_fulfillment_type == 'fbs':
+                record.reference = _('Shopee move: %s', record.reference)

delivery_bpost/tests/test_delivery_bpost.py
@@ -223,7 +223,6 @@ class TestDeliveryBpost(TransactionCase):
 
         order1_vals = {
                     'product_id': self.iPadMini.id,
-                    'name': "[A1232] iPad Mini",
                     'product_uom_qty': 1.0,
                     'location_id': self.stock_location.id,
                     'location_dest_id': self.customer_location.id}

delivery_dhl/tests/test_delivery_dhl.py
@@ -251,7 +251,6 @@ class TestDeliveryDHL(TransactionCase):
 
         order1_vals = {
                     'product_id': self.iPadMini.id,
-                    'name': "[A1232] iPad Mini",
                     'product_uom': self.uom_unit.id,
                     'product_uom_qty': 1.0,
                     'location_id': self.stock_location.id,

delivery_dhl_rest/tests/test_delivery_dhl.py
@@ -288,7 +288,6 @@ class TestDeliveryDHL(TransactionCase):
 
         order1_vals = {
             'product_id': self.iPadMini.id,
-            'name': "[A1232] iPad Mini",
             'product_uom': self.uom_unit.id,
             'product_uom_qty': 1.0,
             'location_id': self.stock_location.id,

delivery_fedex/tests/test_delivery_fedex.py
@@ -233,7 +233,6 @@ class TestDeliveryFedex(TransactionCase):
 
         order1_vals = {
                     'product_id': self.iPadMini.id,
-                    'name': "[A1232] iPad Mini",
                     'product_uom': self.uom_unit.id,
                     'product_uom_qty': 1.0,
                     'location_id': self.stock_location.id,

delivery_fedex_rest/tests/test_delivery_fedex.py
@@ -260,7 +260,6 @@ class TestDeliveryFedex(TransactionCase):
 
         order1_vals = {
                     'product_id': self.iPadMini.id,
-                    'name': "[A1232] iPad Mini",
                     'product_uom_qty': 1.0,
                     'location_id': self.stock_location.id,
                     'location_dest_id': self.customer_location.id}

commit ef1a6962687575851afb3fd09bb79dee8a30802f

[FIX] sale_stock_renting: add reference to manual rental transfer
Steps to reproduce:

- Create a rentable, storable product
- Create and confirm a rental order for 1 unit of this product
- Click on pickup
- Inventory > Reporting > Moves History
> Your pickup move appears without any reference

Cause of the issue:

The issue has been introduced in d0c1e7845feeee1c2e85a21b5d40570d051458d3
which purpose was to remove the `name` field of the `stock.move` model.
However, the `_compute_reference` compute method use to rely on this
`move.name` to propagate the info that the move was related to a rental
order (since there is no picking). Indeed prior to saas-18.4, the
compute method was:
https://github.com/odoo/odoo/blob/404cb10283cbc706eae67dd793ced363273f3602/addons/stock/models/stock_move.py#L325-L328
And the reference to the rental order was set on the move at pickup:
https://github.com/odoo/enterprise/blob/1466a0139ee64ecd059738bc414f6e7e5a9f4354/sale_stock_renting/models/sale_order_line.py#L303-L313
https://github.com/odoo/enterprise/blob/1466a0139ee64ecd059738bc414f6e7e5a9f4354/sale_stock_renting/models/sale_order_line.py#L248-L257

Fix:

Since the reference field is a computed and stored fields and since some
of its dependencies are set at creation the rental move we can not set
the `reference` directly in the creation of the record as we used to do
for its name since the compute method will then override and erase
or reference.

opw-5385004

closes odoo/enterprise#101810

X-original-commit: faa4dbd8a8b066afc1d4c999d1301daef2bef0fa
Signed-off-by: William Henrotin (whe) <whe@odoo.com>
Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

sale_stock_renting/models/stock_move.py
@@ -108,6 +108,15 @@ class StockMove(models.Model):
         if moves_to_super:
             super(StockMove, moves_to_super)._compute_location_dest_id()
 
+    @api.depends('sale_line_id.order_id.name')
+    def _compute_reference(self):
+        moves_with_reference = set()
+        for move in self:
+            if move.sale_line_id.is_rental and not move.picking_id:
+                move.reference = self.env._("Rental move: %(order)s", order=move.sale_line_id.order_id.name)
+                moves_with_reference.add(move.id)
+        super(StockMove, self - self.env['stock.move'].browse(moves_with_reference))._compute_reference()
+
     def _set_rental_sm_qty(self):
         self.ensure_one()
         return self

sale_stock_renting/tests/test_rental.py
@@ -1470,3 +1470,45 @@ class TestRentalPicking(TestRentalCommon):
             'product_uom_qty': rental_order.order_line.product_uom_qty,
             'move_dest_ids': rental_order.picking_ids.move_ids[0].ids
         }])
+
+    @freeze_time('2025-01-01 00:00:00')
+    def test_rental_pickup_reference(self):
+        """
+        Check that move and move lines created with respect to pickup and returns
+        of a rental order have a set reference.
+        """
+        # Disable "rental transfers" and rely on the qty_in_rent fot the forecast
+        self.env['res.config.settings'].create({'group_rental_stock_picking': False}).execute()
+        self.assertFalse(self.env.user.has_group('sale_stock_renting.group_rental_stock_picking'))
+        rental_order = self.env['sale.order'].with_context(in_rental_app=True).create({
+            'partner_id': self.cust1.id,
+            'rental_start_date': Datetime.today(),
+            'rental_return_date': Datetime.today() + timedelta(days=2),
+            'order_line': [
+                Command.create({
+                    'product_id': self.product_id.id,
+                    'product_uom_qty': 1.0,
+                }),
+                Command.create({
+                    'product_id': self.tracked_product_id.id,
+                    'product_uom_qty': 1.0,
+                    'reserved_lot_ids': [Command.set(self.lot_id1.ids)],
+                }),
+            ],
+        })
+        rental_order.action_confirm()
+        pickup_action = Form.from_action(self.env, rental_order.action_open_pickup()).save()
+        pickup_action.apply()
+        self.assertRecordValues(rental_order.order_line.move_ids.sorted(lambda m: m.product_id.id), [
+            {'product_id': self.product_id.id, 'reference': f"Rental move: {rental_order.name}"},
+            {'product_id': self.tracked_product_id.id, 'reference': f"Rental move: {rental_order.name}"},
+        ])
+        return_action = Form.from_action(self.env, rental_order.action_open_return()).save()
+        return_action.apply()
+        stock, rental = self.warehouse_id.lot_stock_id.id, self.env.company.rental_loc_id.id
+        self.assertRecordValues(rental_order.order_line.move_ids.sorted(lambda m: (m.product_id.id, m.id)), [
+            {'product_id': self.product_id.id, 'reference': f"Rental move: {rental_order.name}", 'location_id': stock, 'location_dest_id': rental},
+            {'product_id': self.product_id.id, 'reference': f"Rental move: {rental_order.name}", 'location_id': rental, 'location_dest_id': stock},
+            {'product_id': self.tracked_product_id.id, 'reference': f"Rental move: {rental_order.name}", 'location_id': stock, 'location_dest_id': rental},
+            {'product_id': self.tracked_product_id.id, 'reference': f"Rental move: {rental_order.name}", 'location_id': rental, 'location_dest_id': stock},
+        ])