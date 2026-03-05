b0110ed4968c628471afb8f0945054b929a4f0b0
Saitamaxqi <Ahmedx39311@gmail.com>
2026-03-05 17:02:43 +0300

    base structure

---
+++ admin_asset_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Administration Asset Management',
+    'version': '1.0',
+    'summary': 'Administrative Asset Tracking',
+    'category': 'Administration',
+    'author': 'Open-Inside',
+    'depends': ['account_asset', 'hr'],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/menus.xml',
+        'views/asset_views.xml',
+        'views/asset_location_views.xml',
+        'views/asset_move_history_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
