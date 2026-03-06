c4cb12dbb75d06b7f23fd3ee064a6d32e678ce38
Saitamaxqi <Ahmedx39311@gmail.com>
2026-03-06 07:50:55 +0300

    finalizing the approval workflow

--- admin_asset_management/__manifest__.py
+++ admin_asset_management/__manifest__.py
@@ -6,8 +6,8 @@
     'author': 'Open-Inside',
     'depends': ['account_asset', 'hr'],
     'data': [
+        'security/asset_move_history_security.xml',
         'security/ir.model.access.csv',
-
         'views/asset_views.xml',
         'views/asset_location_views.xml',
         'views/asset_move_history_views.xml',
--- admin_asset_management/models/asset_move_history.py
+++ admin_asset_management/models/asset_move_history.py
@@ -1,19 +1,20 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class AssetMoveHistory(models.Model):
     _name = 'asset.move.history'
     _description = 'Asset Move History'
 
-    asset_id = fields.Many2one('account.asset', required=True)
+    asset_id = fields.Many2one('account.asset', required=True,readonly=True)
 
-    previous_employee_id = fields.Many2one('hr.employee')
-    current_employee_id = fields.Many2one('hr.employee')
+    from_employee_id = fields.Many2one('hr.employee',readonly=True)
+    to_employee_id = fields.Many2one('hr.employee',required=True)
 
-    previous_location_id = fields.Many2one('asset.location')
-    current_location_id = fields.Many2one('asset.location')
+    from_location_id = fields.Many2one('asset.location',readonly=True)
+    to_location_id = fields.Many2one('asset.location',required=True)
 
-    moved_by = fields.Many2one('res.users', default=lambda self: self.env.user)
+    moved_by = fields.Many2one('res.users', default=lambda self: self.env.user,readonly=True)
     move_date = fields.Datetime(
         string="Move Date",
         default=fields.Datetime.now)
@@ -23,14 +24,32 @@ class AssetMoveHistory(models.Model):
         ('assigned', 'Assigned'),
         ('declined', 'Declined')
     ], default='draft')
+    is_current_user = fields.Boolean(
+    compute="_compute_is_current_user"
+)
+
+    @api.depends('to_employee_id')
+    def _compute_is_current_user(self):
+        for rec in self:
+            rec.is_current_user = rec.to_employee_id.user_id == self.env.user
 
     def set_assigned(self):
         for rec in self:
+            if rec.to_employee_id.user_id != self.env.user:
+                raise UserError("You are not allowed to approve this move.")
             rec.state = 'assigned'
-
             if rec.asset_id:
-                rec.asset_id.employee_id = rec.current_employee_id.id
+                rec.asset_id.employee_id = rec.to_employee_id
+                rec.asset_id.location_id = rec.to_location_id
     
     def set_declined(self):
         for rec in self:
+            if rec.to_employee_id.user_id != self.env.user:
+                raise UserError("You are not allowed to decline this move.")
             rec.state = 'declined'
+
+    @api.onchange('asset_id')
+    def _onchange_asset(self):
+        if self.asset_id:
+            self.from_employee_id = self.asset_id.employee_id
+            self.from_location_id = self.asset_id.location_id
