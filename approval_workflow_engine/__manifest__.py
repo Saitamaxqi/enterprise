ea50a890b95b2774bfc76b83abd42f654fa1407d
Saitamaxqi <Ahmedx39311@gmail.com>
2026-03-30 15:08:38 +0300

    base odoo module structure initilization

---
+++ approval_workflow_engine/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Approval Workflow Engine',
+    'version': '1.0',
+    'summary': 'Approval Workflow Engine',
+    'category': 'Administration',
+    'author': 'Open-Inside',
+    'depends': ['base', 'mail'],
+    'data': [
+        
+        'security/ir.model.access.csv',
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
