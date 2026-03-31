bac0b60b5500ef41207a0fec113b9b990daaef0a
Saitamaxqi <Ahmedx39311@gmail.com>
2026-03-31 11:01:12 +0300

    added to workflow the model we are targting related fields

--- approval_workflow_engine/models/approval_workflow.py
+++ approval_workflow_engine/models/approval_workflow.py
@@ -5,6 +5,17 @@ class ApprovalWorkflow(models.Model):
     _description = 'Approval Workflow'
 
     name = fields.Char(string='Name', required=True)
+    model_id = fields.One2many(
+        'ir.model',
+        'workflow_id',
+    )
+
+    model_name = fields.Char(
+        string='Model Technical Name',
+        related='model_id.model',
+        store=True,
+        readonly=True
+    )
     number_of_stages = fields.Integer(string='Number of Stages', required=True, default=1)
     stages_ids = fields.One2many('approval.stage', 'workflow_id', string='Stages')
     active = fields.Boolean(string='Active', default=True)
