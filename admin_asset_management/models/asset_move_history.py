8d30be75f0bfcbd0a73bee92a761ab1067aa2060
Nimbbus <mohammad-karim@hotmail.com>
2026-04-19 15:12:54 +0300

    made it so adding the details for moved assets is specified by the user

--- admin_asset_management/models/asset_move_history.py
+++ admin_asset_management/models/asset_move_history.py
@@ -1,37 +1,35 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 
-
 class AssetMoveHistory(models.Model):
     _name = 'asset.move.history'
     _description = 'Asset Move History'
 
-    asset_id = fields.Many2one('account.asset', required=True,readonly=True)
+    asset_id = fields.Many2one('account.asset', required=True)  
 
-    from_employee_id = fields.Many2one('hr.employee',readonly=True)
-    to_employee_id = fields.Many2one('hr.employee',required=True)
+    from_employee_id = fields.Many2one('hr.employee', readonly=False)
+    to_employee_id = fields.Many2one('hr.employee', required=True)
 
-    from_location_id = fields.Many2one('asset.location',readonly=True)
-    to_location_id = fields.Many2one('asset.location',required=True)
+    from_location_id = fields.Many2one('asset.location', readonly=False)
+    to_location_id = fields.Many2one('asset.location', required=True)
 
-    moved_by = fields.Many2one('res.users', default=lambda self: self.env.user,readonly=True)
-    move_date = fields.Datetime(
-        string="Move Date",
-        default=fields.Datetime.now)
+    moved_by = fields.Many2one('res.users', default=lambda self: self.env.user, readonly=True)
+    move_date = fields.Datetime(string="Move Date", default=fields.Datetime.now)
 
     state = fields.Selection([
         ('draft', 'Draft'),
         ('assigned', 'Assigned'),
         ('declined', 'Declined')
     ], default='draft')
-    is_current_user = fields.Boolean(
-    compute="_compute_is_current_user"
-)
+
+    is_current_user = fields.Boolean(compute="_compute_is_current_user")
 
     @api.depends('to_employee_id')
     def _compute_is_current_user(self):
         for rec in self:
-            rec.is_current_user = rec.to_employee_id.user_id == self.env.user
+            rec.is_current_user = (
+                rec.to_employee_id.user_id == rec.env.user if rec.to_employee_id else False
+            )
 
     def set_assigned(self):
         for rec in self:
@@ -43,6 +41,7 @@ class AssetMoveHistory(models.Model):
                     'employee_id': rec.to_employee_id.id,
                     'location_id': rec.to_location_id.id,
                 })
+
     def set_declined(self):
         for rec in self:
             if rec.to_employee_id.user_id != self.env.user:
@@ -58,5 +57,8 @@ class AssetMoveHistory(models.Model):
     @api.constrains('to_employee_id', 'asset_id')
     def _check_to_employee_not_current(self):
         for rec in self:
-            if rec.asset_id and rec.to_employee_id and rec.asset_id.employee_id and rec.to_employee_id == rec.asset_id.employee_id:
+            if (
+                rec.asset_id and rec.asset_id.employee_id and rec.to_employee_id
+                and rec.to_employee_id == rec.asset_id.employee_id
+            ):
                 raise UserError("The selected employee is already assigned to this asset.")
